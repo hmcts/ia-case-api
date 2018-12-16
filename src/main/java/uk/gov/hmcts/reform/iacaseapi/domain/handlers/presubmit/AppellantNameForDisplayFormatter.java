4261fca0f58f20becf12a37bf6ef5b9c5bb4a8c1
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-12-16 00:44:27 +0000

    Appellant name formatter for display

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AppellantNameForDisplayFormatter.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AppellantNameForDisplayFormatter.java
@@ -4,15 +4,13 @@ import static java.util.Objects.requireNonNull;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 
 @Component
-public class CaseBuildingReadyForSubmissionUpdater implements PreSubmitCallbackHandler<AsylumCase> {
+public class AppellantNameForDisplayFormatter implements PreSubmitCallbackHandler<AsylumCase> {
 
     public boolean canHandle(
         PreSubmitCallbackStage callbackStage,
@@ -32,27 +30,26 @@ public class CaseBuildingReadyForSubmissionUpdater implements PreSubmitCallbackH
             throw new IllegalStateException("Cannot handle callback");
         }
 
-        final State caseState =
-            callback
-                .getCaseDetails()
-                .getState();
-
         final AsylumCase asylumCase =
             callback
                 .getCaseDetails()
                 .getCaseData();
 
-        if (caseState == State.CASE_BUILDING) {
+        final String appellantGivenNames =
+            asylumCase
+                .getAppellantGivenNames()
+                .orElseThrow(() -> new IllegalStateException("appellantGivenNames is not present"));
 
-            if (asylumCase.getCaseArgumentDocument().isPresent()) {
-                asylumCase.setCaseBuildingReadyForSubmission(YesOrNo.YES);
-            } else {
-                asylumCase.setCaseBuildingReadyForSubmission(YesOrNo.NO);
-            }
+        final String appellantLastName =
+            asylumCase
+                .getAppellantLastName()
+                .orElseThrow(() -> new IllegalStateException("appellantLastName is not present"));
 
-        } else {
-            asylumCase.clearCaseBuildingReadyForSubmission();
-        }
+        String appellantNameForDisplay = appellantGivenNames + " " + appellantLastName;
+
+        asylumCase.setAppellantNameForDisplay(
+            appellantNameForDisplay.replaceAll("\\s+", " ").trim()
+        );
 
         return new PreSubmitCallbackResponse<>(asylumCase);
     }
