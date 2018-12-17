1dca9c8f88fe5e60497b46f83b7d388aa53741fa
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-12-17 11:26:55 +0000

    AAT infrastructure configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,14 +1,16 @@
-provider "azurerm" {}
+# Temporary fix for template API version error on deployment
+provider "azurerm" {
+  version = "1.19.0"
+}
 
 locals {
+  preview_app_service_plan     = "${var.product}-${var.component}-${var.env}"
+  non_preview_app_service_plan = "${var.product}-${var.env}"
+  app_service_plan             = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
 
-  preview_app_service_plan        = "${var.product}-${var.component}-${var.env}"
-  non_preview_app_service_plan    = "${var.product}-${var.env}"
-  app_service_plan                = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
-
-  preview_vault_name              = "${var.raw_product}-aat"
-  non_preview_vault_name          = "${var.raw_product}-${var.env}"
-  key_vault_name                  = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
+  preview_vault_name     = "${var.raw_product}-aat"
+  non_preview_vault_name = "${var.raw_product}-${var.env}"
+  key_vault_name         = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -22,8 +24,83 @@ data "azurerm_key_vault" "ia_key_vault" {
   resource_group_name = "${local.key_vault_name}"
 }
 
-data "azurerm_key_vault_secret" "ia_case_api_url" {
-  name      = "ia-case-api-url"
+data "azurerm_key_vault_secret" "test_caseofficer_username" {
+  name      = "test-caseofficer-username"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "test_caseofficer_password" {
+  name      = "test-caseofficer-password"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "test_law_firm_a_username" {
+  name      = "test-law-firm-a-username"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "test_law_firm_a_password" {
+  name      = "test-law-firm-a-password"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "case_notifications_api_url" {
+  name      = "case-notifications-api-url"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "system_username" {
+  name      = "system-username"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "system_password" {
+  name      = "system-password"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_client_id" {
+  name      = "idam-client-id"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_secret" {
+  name      = "idam-secret"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_redirect_uri" {
+  name      = "idam-redirect-uri"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  name      = "s2s-secret"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "s2s_microservice" {
+  name      = "s2s-microservice"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "ccd_url" {
+  name      = "ccd-url"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "dm_url" {
+  name      = "dm-url"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_url" {
+  name      = "idam-url"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "s2s_url" {
+  name      = "s2s-url"
   vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
 }
 
@@ -44,6 +121,19 @@ module "ia_case_api" {
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = false
     LOGBACK_REQUIRE_ERROR_CODE  = false
-    IA_CASE_API_URL             = "${data.azurerm_key_vault_secret.ia_case_api_url.value}"
+
+    IA_CASE_NOTIFICATIONS_API_URL = "${data.azurerm_key_vault_secret.case_notifications_api_url.value}"
+    IA_SYSTEM_USERNAME            = "${data.azurerm_key_vault_secret.system_username.value}"
+    IA_SYSTEM_PASSWORD            = "${data.azurerm_key_vault_secret.system_password.value}"
+    IA_IDAM_CLIENT_ID             = "${data.azurerm_key_vault_secret.idam_client_id.value}"
+    IA_IDAM_SECRET                = "${data.azurerm_key_vault_secret.idam_secret.value}"
+    IA_IDAM_REDIRECT_URI          = "${data.azurerm_key_vault_secret.idam_redirect_uri.value}"
+    IA_S2S_SECRET                 = "${data.azurerm_key_vault_secret.s2s_secret.value}"
+    IA_S2S_MICROSERVICE           = "${data.azurerm_key_vault_secret.s2s_microservice.value}"
+
+    CCD_URL  = "${data.azurerm_key_vault_secret.ccd_url.value}"
+    DM_URL   = "${data.azurerm_key_vault_secret.dm_url.value}"
+    IDAM_URL = "${data.azurerm_key_vault_secret.idam_url.value}"
+    S2S_URL  = "${data.azurerm_key_vault_secret.s2s_url.value}"
   }
 }
