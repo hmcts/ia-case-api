d7679a5ab9c8eacae72bb328fb6432565cc6928a
Daniele Polencic <daniele.polencic@gmail.com>
2018-12-17 13:10:27 +0000

    updated naming conventions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,14 +1,13 @@
 provider "azurerm" {}
 
 locals {
+  preview_app_service_plan     = "${var.product}-${var.component}-${var.env}"
+  non_preview_app_service_plan = "${var.product}-${var.env}"
+  app_service_plan             = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
 
-  preview_app_service_plan        = "${var.product}-${var.component}-${var.env}"
-  non_preview_app_service_plan    = "${var.product}-${var.env}"
-  app_service_plan                = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
-
-  preview_vault_name              = "${var.raw_product}-aat"
-  non_preview_vault_name          = "${var.raw_product}-${var.env}"
-  key_vault_name                  = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
+  preview_vault_name     = "${var.raw_product}-aat"
+  non_preview_vault_name = "${var.raw_product}-${var.env}"
+  key_vault_name         = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -27,8 +26,8 @@ data "azurerm_key_vault_secret" "ia_case_notifications_api_url" {
   vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
 }
 
-data "azurerm_key_vault_secret" "ia_idam_id" {
-  name      = "ia-idam-id"
+data "azurerm_key_vault_secret" "ia_idam_client_id" {
+  name      = "ia-idam-client-id"
   vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
 }
 
@@ -87,19 +86,19 @@ module "ia_case_api" {
   asp_rg              = "${local.app_service_plan}"
 
   app_settings = {
-    LOGBACK_REQUIRE_ALERT_LEVEL   = false
-    LOGBACK_REQUIRE_ERROR_CODE    = false
+    LOGBACK_REQUIRE_ALERT_LEVEL = false
+    LOGBACK_REQUIRE_ERROR_CODE  = false
 
     IA_CASE_NOTIFICATIONS_API_URL = "${data.azurerm_key_vault_secret.ia_case_notifications_api_url.value}"
-    IA_IDAM_ID                    = "${data.azurerm_key_vault_secret.ia_idam_id.value}"
+    IA_IDAM_CLIENT_ID             = "${data.azurerm_key_vault_secret.ia_idam_client_id.value}"
     IA_IDAM_SECRET                = "${data.azurerm_key_vault_secret.ia_idam_secret.value}"
     IA_IDAM_REDIRECT_URI          = "${data.azurerm_key_vault_secret.ia_idam_redirect_uri.value}"
     IA_S2S_SECRET                 = "${data.azurerm_key_vault_secret.ia_s2s_secret.value}"
     IA_S2S_MICROSERVICE           = "${data.azurerm_key_vault_secret.ia_s2s_microservice.value}"
 
-    CCD_URL                       = "${data.azurerm_key_vault_secret.ccd_url.value}"
-    DM_URL                        = "${data.azurerm_key_vault_secret.dm_url.value}"
-    IDAM_URL                      = "${data.azurerm_key_vault_secret.idam_url.value}"
-    S2S_URL                       = "${data.azurerm_key_vault_secret.s2s_url.value}"
+    CCD_URL  = "${data.azurerm_key_vault_secret.ccd_url.value}"
+    DM_URL   = "${data.azurerm_key_vault_secret.dm_url.value}"
+    IDAM_URL = "${data.azurerm_key_vault_secret.idam_url.value}"
+    S2S_URL  = "${data.azurerm_key_vault_secret.s2s_url.value}"
   }
 }
