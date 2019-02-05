d3c3fa887f0892aa82e3523e798c8043f07fd51c
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2019-02-05 15:52:53 +0000

    Welcome endpoint re-enabled with JSON and no cache headers (for Zap) (#68)
    * Re-enabled welcome endpoint (to mute the logs when Azure pings the root endpoint) and added no cache headers (for Zap)
    
    * Fixing EndpointSecurityTest to hit all endpoints
    
    * Logging welcome message with instance id to see which apps are bing checked
    
    * Corrected @ApiOperation message

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/WelcomeTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/WelcomeTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.iacaseapi.util.AuthorizationHeadersProvider;
@@ -29,15 +30,16 @@ public class WelcomeTest {
         RestAssured.baseURI = targetInstance;
         RestAssured.useRelaxedHTTPSValidation();
 
-        String response = SerenityRest
-            .given()
-            .headers(authorizationHeadersProvider.getCaseOfficerAuthorization())
-            .when()
-            .get("/")
-            .then()
-            .statusCode(HttpStatus.OK.value())
-            .and()
-            .extract().body().asString();
+        String response =
+            SerenityRest
+                .given()
+                .when()
+                .get("/")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .and()
+                .extract().body().asString();
 
         assertThat(response)
             .contains("Welcome to Immigration & Asylum case API");
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/WelcomeController.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/WelcomeController.java
@@ -1,24 +1,31 @@
 package uk.gov.hmcts.reform.iacaseapi.infrastructure.controllers;
 
-import static org.springframework.http.ResponseEntity.ok;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(
     value = "/",
-    consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
     produces = MediaType.APPLICATION_JSON_UTF8_VALUE
 )
 
 @RestController
 public class WelcomeController {
+
+    private static final Logger LOG = getLogger(WelcomeController.class);
+    private static final String INSTANCE_ID = UUID.randomUUID().toString();
+    private static final String MESSAGE = "Welcome to Immigration & Asylum case API";
 
     /**
      * Root GET endpoint.
@@ -29,17 +36,26 @@ public class WelcomeController {
      *
      * @return Welcome message from the service.
      */
-    @ApiOperation("Welcome page for the Immigration & Asylum case API")
+    @ApiOperation("Welcome message for the Immigration & Asylum case API")
     @ApiResponses({
         @ApiResponse(
             code = 200,
-            message = "Welcome Page",
+            message = "Welcome message",
             response = String.class
         )
     })
-    @GetMapping(path = "/")
+    @GetMapping(
+        path = "/",
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    @ResponseBody
     public ResponseEntity<String> welcome() {
-        return ok("Welcome to Immigration & Asylum case API");
-    }
 
+        LOG.info("Welcome message '{}' from running instance: {}", MESSAGE, INSTANCE_ID);
+
+        return ResponseEntity
+            .ok()
+            .cacheControl(CacheControl.noCache())
+            .body("{\"message\": \"" + MESSAGE + "\"}");
+    }
 }
