03a9e82e2aaaf089f6f8f76cc51d1f96551d1452
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2019-04-23 17:15:30 +0100

    AddAppealResponseHandler must execute before RespondentReviewAppealResponseAddedUpdater (new hash)

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AddAppealResponseHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AddAppealResponseHandlerTest.java
@@ -22,6 +22,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentWithMetadata;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
@@ -60,6 +61,11 @@ public class AddAppealResponseHandlerTest {
                 documentReceiver,
                 documentsAppender
             );
+    }
+
+    @Test
+    public void should_be_handled_early() {
+        assertEquals(DispatchPriority.EARLY, addAppealResponseHandler.getDispatchPriority());
     }
 
     @Test
