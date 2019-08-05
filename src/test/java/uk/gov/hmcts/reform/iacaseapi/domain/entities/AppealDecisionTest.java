7257d3ceb161faf9c234a6fe125dc0a4851bea8f
makowalski <marcin.kowalski.it.consulting@gmail.com>
2019-08-05 16:44:04 +0100

    RIA-1622 overview pages in decided state

---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AppealDecisionTest.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.entities;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.Test;
+
+public class AppealDecisionTest {
+
+    @Test
+    public void has_correct_values() {
+        assertEquals(AppealDecision.DISMISSED.toString(), AppealDecision.DISMISSED.getValue());
+        assertEquals(AppealDecision.ALLOWED.toString(), AppealDecision.ALLOWED.getValue());
+
+        assertEquals("dismissed", AppealDecision.DISMISSED.toString());
+        assertEquals("allowed", AppealDecision.ALLOWED.toString());
+
+        assertEquals(AppealDecision.DISMISSED, AppealDecision.from("dismissed"));
+        assertEquals(AppealDecision.ALLOWED, AppealDecision.from("allowed"));
+    }
+
+    @Test
+    public void should_throw_exception_when_name_unrecognised() {
+
+        assertThatThrownBy(() -> AppealDecision.from("unknown"))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessage("unknown not an AppealDecision")
+            .hasNoCause();
+    }
+
+    @Test
+    public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
+        assertEquals(2, AppealDecision.values().length);
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/HoursAndMinutesTest.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class HoursAndMinutesTest {
+
+    @Test
+    public void should_create_hours_and_minutes() {
+
+        HoursAndMinutes hoursAndMinutes = new HoursAndMinutes();
+
+        assertNull(hoursAndMinutes.getHours());
+        assertNull(hoursAndMinutes.getMinutes());
+
+        hoursAndMinutes = new HoursAndMinutes("5", "30");
+
+        assertEquals("5", hoursAndMinutes.getHours());
+        assertEquals("30", hoursAndMinutes.getMinutes());
+    }
+}
