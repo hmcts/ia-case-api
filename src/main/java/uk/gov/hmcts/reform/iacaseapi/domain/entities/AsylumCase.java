9433604c3a9b4844c862192166fdf610d4c6ef83
makowalski <marcin.kowalski.it.consulting@gmail.com>
2019-09-20 13:35:08 +0100

    RIA-1654 admin edit listing (#138)

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCase.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCase.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.reform.iacaseapi.domain.entities;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
 import java.util.HashMap;
 import java.util.Optional;
+
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseData;
 
 public class AsylumCase extends HashMap<String, Object> implements CaseData {
@@ -18,6 +20,13 @@ public class AsylumCase extends HashMap<String, Object> implements CaseData {
         return this.read(extractor);
     }
 
+    /*
+    Security vulnerabilities (CVE-2019-14540, CVE-2019-16335) forced us to update jackson-core library to version: 2.10.0.pr3
+    That version has change in ObjectMapper API where <T> generic should be passed to "convertValue" method instead of taking any type <?>
+    Returning Object and casting to T ref is safe because we defined all types in AsylumCaseDefinition enum.
+    We cannot parametrized enum with T ref and we want to keep AsylumCaseDefinition as it is, that is why we need below workaround.
+    */
+    @SuppressWarnings("unchecked")
     public <T> Optional<T> read(AsylumCaseFieldDefinition extractor) {
 
         Object o = this.get(extractor.value());
@@ -26,9 +35,9 @@ public class AsylumCase extends HashMap<String, Object> implements CaseData {
             return Optional.empty();
         }
 
-        T value = objectMapper.convertValue(o, extractor.getTypeReference());
+        Object value = objectMapper.convertValue(o, extractor.getTypeReference());
 
-        return Optional.of(value);
+        return Optional.of((T) value);
     }
 
     public <T> void write(AsylumCaseFieldDefinition extractor, T value) {
