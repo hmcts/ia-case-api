1a8020321f0756d328802eb3d2cc55af5443bebc
makowalski <marcin.kowalski.it.consulting@gmail.com>
2019-09-25 12:19:58 +0100

    RIA-1278 additional evidence ho + unit tests + functional tests

---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/UploadAdditionalEvidenceHomeOfficeHandlerTest.java
@@ -0,0 +1,199 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentTag;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentWithDescription;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentWithMetadata;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.DocumentReceiver;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.DocumentsAppender;
+
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings("unchecked")
+public class UploadAdditionalEvidenceHomeOfficeHandlerTest {
+
+    @Mock private DocumentReceiver documentReceiver;
+    @Mock private DocumentsAppender documentsAppender;
+    @Mock private Callback<AsylumCase> callback;
+    @Mock private CaseDetails<AsylumCase> caseDetails;
+    @Mock private AsylumCase asylumCase;
+    @Mock private DocumentWithDescription additionalEvidenceDoc;
+    @Mock private DocumentWithMetadata additionalEvidenceDocWithMetadata;
+    @Mock private List<IdValue<DocumentWithMetadata>> respondentDocuments;
+    @Mock private List<IdValue<DocumentWithMetadata>> allRespondentDocuments;
+
+    @Captor private ArgumentCaptor<List<IdValue<DocumentWithMetadata>>> existingAdditionalEvidenceDocumentsCaptor;
+
+    private UploadAdditionalEvidenceHomeOfficeHandler uploadAdditionalEvidenceHomeOfficeHandler;
+
+    private List<IdValue<DocumentWithDescription>> additionalEvidence;
+    private List<DocumentWithMetadata> additionalEvidenceWithMetadata;
+
+    @Before
+    public void setUp() {
+
+        additionalEvidence = Arrays.asList(new IdValue<>("1", additionalEvidenceDoc));
+        additionalEvidenceWithMetadata = Arrays.asList(additionalEvidenceDocWithMetadata);
+
+        uploadAdditionalEvidenceHomeOfficeHandler =
+            new UploadAdditionalEvidenceHomeOfficeHandler(
+                documentReceiver,
+                documentsAppender
+            );
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(callback.getEvent()).thenReturn(Event.UPLOAD_ADDITIONAL_EVIDENCE_HOME_OFFICE);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+    }
+
+    @Test
+    public void should_append_new_evidence_to_existing_additional_evidence_documents_for_the_case() {
+
+        when(asylumCase.read(RESPONDENT_DOCUMENTS)).thenReturn(Optional.of(respondentDocuments));
+        when(asylumCase.read(ADDITIONAL_EVIDENCE_HOME_OFFICE)).thenReturn(Optional.of(additionalEvidence));
+
+        when(documentReceiver.tryReceive(additionalEvidenceDoc, DocumentTag.ADDITIONAL_EVIDENCE))
+            .thenReturn(Optional.of(additionalEvidenceDocWithMetadata));
+
+        when(documentsAppender.append(respondentDocuments, additionalEvidenceWithMetadata))
+            .thenReturn(allRespondentDocuments);
+
+        PreSubmitCallbackResponse<AsylumCase> callbackResponse =
+            uploadAdditionalEvidenceHomeOfficeHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertNotNull(callbackResponse);
+        assertEquals(asylumCase, callbackResponse.getData());
+
+        verify(asylumCase, times(1)).read(ADDITIONAL_EVIDENCE_HOME_OFFICE);
+
+        verify(documentReceiver, times(1)).tryReceive(additionalEvidenceDoc, DocumentTag.ADDITIONAL_EVIDENCE);
+
+        verify(documentsAppender, times(1)).append(respondentDocuments, additionalEvidenceWithMetadata);
+
+        verify(asylumCase, times(1)).write(RESPONDENT_DOCUMENTS, allRespondentDocuments);
+        verify(asylumCase, times(1)).clear(ADDITIONAL_EVIDENCE_HOME_OFFICE);
+    }
+
+    @Test
+    public void should_add_new_evidence_to_the_case_when_no_additional_evidence_documents_exist() {
+
+        when(asylumCase.read(RESPONDENT_DOCUMENTS)).thenReturn(Optional.empty());
+        when(asylumCase.read(ADDITIONAL_EVIDENCE_HOME_OFFICE)).thenReturn(Optional.of(additionalEvidence));
+
+        when(documentReceiver.tryReceive(additionalEvidenceDoc, DocumentTag.ADDITIONAL_EVIDENCE))
+            .thenReturn(Optional.of(additionalEvidenceDocWithMetadata));
+
+        when(documentsAppender.append(any(List.class), eq(additionalEvidenceWithMetadata)))
+            .thenReturn(allRespondentDocuments);
+
+        PreSubmitCallbackResponse<AsylumCase> callbackResponse =
+            uploadAdditionalEvidenceHomeOfficeHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertNotNull(callbackResponse);
+        assertEquals(asylumCase, callbackResponse.getData());
+
+        verify(asylumCase, times(1)).read(ADDITIONAL_EVIDENCE_HOME_OFFICE);
+
+        verify(documentReceiver, times(1)).tryReceive(additionalEvidenceDoc, DocumentTag.ADDITIONAL_EVIDENCE);
+
+        verify(documentsAppender, times(1)).append(existingAdditionalEvidenceDocumentsCaptor.capture(), eq(additionalEvidenceWithMetadata));
+
+        List<IdValue<DocumentWithMetadata>> actualExistingAdditionalDocuments =
+            existingAdditionalEvidenceDocumentsCaptor
+                .getAllValues()
+                .get(0);
+
+        assertEquals(0, actualExistingAdditionalDocuments.size());
+
+        verify(asylumCase, times(1)).write(RESPONDENT_DOCUMENTS, allRespondentDocuments);
+        verify(asylumCase, times(1)).clear(ADDITIONAL_EVIDENCE_HOME_OFFICE);
+    }
+
+    @Test
+    public void should_throw_when_new_evidence_is_not_present() {
+
+        when(asylumCase.read(ADDITIONAL_EVIDENCE_HOME_OFFICE)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> uploadAdditionalEvidenceHomeOfficeHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback))
+            .hasMessage("additionalEvidenceHomeOffice is not present")
+            .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void handling_should_throw_if_cannot_actually_handle() {
+
+        assertThatThrownBy(() -> uploadAdditionalEvidenceHomeOfficeHandler.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback))
+            .hasMessage("Cannot handle callback")
+            .isExactlyInstanceOf(IllegalStateException.class);
+
+        when(callback.getEvent()).thenReturn(Event.END_APPEAL);
+        assertThatThrownBy(() -> uploadAdditionalEvidenceHomeOfficeHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback))
+            .hasMessage("Cannot handle callback")
+            .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void it_can_handle_callback() {
+
+        for (Event event : Event.values()) {
+
+            when(callback.getEvent()).thenReturn(event);
+
+            for (PreSubmitCallbackStage callbackStage : PreSubmitCallbackStage.values()) {
+
+                boolean canHandle = uploadAdditionalEvidenceHomeOfficeHandler.canHandle(callbackStage, callback);
+
+                if (event == Event.UPLOAD_ADDITIONAL_EVIDENCE_HOME_OFFICE
+                    && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT) {
+
+                    assertTrue(canHandle);
+                } else {
+                    assertFalse(canHandle);
+                }
+            }
+
+            reset(callback);
+        }
+    }
+
+    @Test
+    public void should_not_allow_null_arguments() {
+
+        assertThatThrownBy(() -> uploadAdditionalEvidenceHomeOfficeHandler.canHandle(null, callback))
+            .hasMessage("callbackStage must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> uploadAdditionalEvidenceHomeOfficeHandler.canHandle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
+            .hasMessage("callback must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> uploadAdditionalEvidenceHomeOfficeHandler.handle(null, callback))
+            .hasMessage("callbackStage must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> uploadAdditionalEvidenceHomeOfficeHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
+            .hasMessage("callback must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+    }
+}
