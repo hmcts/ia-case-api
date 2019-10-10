09bdd8929447be781e673d1f5a60bfafe4658734
makowalski <marcin.kowalski.it.consulting@gmail.com>
2019-10-10 12:47:49 +0100

    add missing DocumentTag values (#166)

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTag.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTag.java
@@ -14,6 +14,12 @@ public enum DocumentTag {
     CASE_SUMMARY("caseSummary"),
     HEARING_BUNDLE("hearingBundle"),
     ADDENDUM_EVIDENCE("addendumEvidence"),
+    DECISION_AND_REASONS_DRAFT("decisionAndReasons"),
+    DECISION_AND_REASONS_COVER_LETTER("decisionAndReasonsCoverLetter"),
+    FINAL_DECISION_AND_REASONS_PDF("finalDecisionAndReasonsPdf"),
+    APPEAL_SKELETON_BUNDLE("submitCaseBundle"),
+    END_APPEAL("endAppeal"),
+
     @JsonEnumDefaultValue
     NONE("");
 
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
@@ -17,11 +17,16 @@ public class DocumentTagTest {
         assertEquals("caseSummary", DocumentTag.CASE_SUMMARY.toString());
         assertEquals("hearingBundle", DocumentTag.HEARING_BUNDLE.toString());
         assertEquals("addendumEvidence", DocumentTag.ADDENDUM_EVIDENCE.toString());
+        assertEquals("decisionAndReasons", DocumentTag.DECISION_AND_REASONS_DRAFT.toString());
+        assertEquals("decisionAndReasonsCoverLetter", DocumentTag.DECISION_AND_REASONS_COVER_LETTER.toString());
+        assertEquals("finalDecisionAndReasonsPdf", DocumentTag.FINAL_DECISION_AND_REASONS_PDF.toString());
+        assertEquals("submitCaseBundle", DocumentTag.APPEAL_SKELETON_BUNDLE.toString());
+        assertEquals("endAppeal", DocumentTag.END_APPEAL.toString());
         assertEquals("", DocumentTag.NONE.toString());
     }
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(10, DocumentTag.values().length);
+        assertEquals(15, DocumentTag.values().length);
     }
 }
