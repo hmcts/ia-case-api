592f34b2c2f537e7b0051c541e82d9cc1113b202
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-01-15 15:29:19 +0000

    RIA-1891 mark application completed after ending appeal

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/EndAppealHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/EndAppealHandler.java
@@ -1,22 +1,27 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
+import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.ADD_CASE_NOTE_ACTION_DISABLED;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.END_APPEAL_DATE;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.RECORD_APPLICATION_ACTION_DISABLED;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.DateProvider;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.Application;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ApplicationType;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
-
 
 @Component
 public class EndAppealHandler implements PreSubmitCallbackHandler<AsylumCase> {
@@ -63,6 +68,36 @@ public class EndAppealHandler implements PreSubmitCallbackHandler<AsylumCase> {
         asylumCase.clear(APPLICATION_WITHDRAW_EXISTS);
         asylumCase.clear(DISABLE_OVERVIEW_PAGE);
 
+        changeWithdrawApplicationsToCompleted(asylumCase);
+
         return new PreSubmitCallbackResponse<>(asylumCase);
     }
+
+    private void changeWithdrawApplicationsToCompleted(AsylumCase asylumCase) {
+        asylumCase.write(APPLICATIONS, asylumCase.<List<IdValue<Application>>>read(APPLICATIONS)
+            .orElse(emptyList())
+            .stream()
+            .map(application -> {
+                String applicationType = application.getValue().getApplicationType();
+                if (ApplicationType.WITHDRAW.toString().equals(applicationType)) {
+
+                    return new IdValue<>(application.getId(), new Application(
+                        application.getValue().getApplicationDocuments(),
+                        application.getValue().getApplicationSupplier(),
+                        applicationType,
+                        application.getValue().getApplicationReason(),
+                        application.getValue().getApplicationDate(),
+                        application.getValue().getApplicationDecision(),
+                        application.getValue().getApplicationDecisionReason(),
+                        application.getValue().getApplicationDateOfDecision(),
+                        "Completed"
+                    ));
+                }
+
+                return application;
+            })
+            .collect(Collectors.toList())
+        );
+    }
+
 }
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/EndAppealHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/EndAppealHandlerTest.java
@@ -1,20 +1,27 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.ADD_CASE_NOTE_ACTION_DISABLED;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.END_APPEAL_DATE;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.RECORD_APPLICATION_ACTION_DISABLED;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.END_APPEAL;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.iacaseapi.domain.DateProvider;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.Application;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ApplicationType;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
@@ -22,6 +29,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -32,6 +40,16 @@ public class EndAppealHandlerTest {
     @Mock private CaseDetails<AsylumCase> caseDetails;
     @Mock private AsylumCase asylumCase;
     @Mock private DateProvider dateProvider;
+
+    @Captor private ArgumentCaptor<List<IdValue<Application>>> applicationsCaptor;
+
+    private String applicationSupplier = "Legal representative";
+    private String applicationReason = "applicationReason";
+    private String applicationDate = "30/01/2019";
+    private String applicationDecision = "Granted";
+    private String applicationDecisionReason = "Granted";
+    private String applicationDateOfDecision = "31/01/2019";
+    private String applicationStatus = "In progress";
 
     private EndAppealHandler endAppealHandler;
 
@@ -56,7 +74,6 @@ public class EndAppealHandlerTest {
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
         when(callback.getEvent()).thenReturn(Event.END_APPEAL);
-
         PreSubmitCallbackResponse<AsylumCase> callbackResponse =
             endAppealHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
 
@@ -117,5 +134,37 @@ public class EndAppealHandlerTest {
         assertThatThrownBy(() -> endAppealHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
             .hasMessage("callback must not be null")
             .isExactlyInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void should_handle_withdraw_and_set_application_status_to_completed() {
+
+        List<IdValue<Application>> expectedApplications = newArrayList(new IdValue<>("1", new Application(
+            Collections.emptyList(),
+            applicationSupplier,
+            ApplicationType.WITHDRAW.toString(),
+            applicationReason,
+            applicationDate,
+            applicationDecision,
+            applicationDecisionReason,
+            applicationDateOfDecision,
+            applicationStatus
+        )));
+
+        when(callback.getEvent()).thenReturn(END_APPEAL);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.read(APPLICATIONS)).thenReturn(Optional.of(expectedApplications));
+
+        PreSubmitCallbackResponse<AsylumCase> callbackResponse =
+            endAppealHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertNotNull(callbackResponse);
+        assertEquals(asylumCase, callbackResponse.getData());
+
+        verify(asylumCase).clear(APPLICATION_WITHDRAW_EXISTS);
+        verify(asylumCase).clear(DISABLE_OVERVIEW_PAGE);
+        verify(asylumCase).write(eq(APPLICATIONS), applicationsCaptor.capture());
+        assertEquals("Completed", applicationsCaptor.getValue().get(0).getValue().getApplicationStatus());
     }
 }
