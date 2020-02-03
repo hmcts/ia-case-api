0b9f1babcd4282cc684c4a74ed3a3cdbbb68dc21
tarun-palisetty <tarun.mca@gmail.com>
2020-02-03 13:42:53 +0000

    RIA-2526 downstream services health check

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/health/ServiceHealthIndicator.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.reform.iacaseapi.infrastructure.health;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+public class ServiceHealthIndicator implements HealthIndicator {
+
+    private String uri;
+    private String matcher;
+    private RestTemplate restTemplate;
+
+    public ServiceHealthIndicator(String uri, String matcher, RestTemplate restTemplate) {
+        this.uri = uri;
+        this.matcher = matcher;
+        this.restTemplate = restTemplate;
+    }
+
+
+    @Override
+    public Health health() {
+
+        try {
+            ResponseEntity<String> response = restTemplate
+                .getForEntity(uri, String.class);
+
+            String responseBody = response.getBody().replaceAll("\\s", "");
+
+            if (response.getStatusCode() == HttpStatus.OK
+                && responseBody.contains(matcher)
+                && !responseBody.contains("DOWN")) {
+
+                return new Health
+                    .Builder(Status.UP)
+                    .build();
+            } else {
+
+                return new Health
+                    .Builder(Status.DOWN)
+                    .build();
+            }
+        } catch (RestClientException ex) {
+
+            log.error("Downstream service exception {}", Health.down(ex).build().getDetails());
+            return new Health
+                .Builder()
+                .down(ex)
+                .build();
+        }
+    }
+}
