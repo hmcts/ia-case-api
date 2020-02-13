cdb051388feeb8ded1eb09ec97c74a3f72ee3441
chrisgrimble <chris.grimble@hmcts.net>
2020-02-13 16:47:43 +0000

    RIA-2632 Prevent send direction for AIP journey
    Prevent the send direction event being fired for an aip journey as we
    have not implemented this yet.
    Prevent caseworker selecting Appellant for send direction for a repped
    case.

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/eventvalidation/AsylumCaseSendDirectionEventValidForJourneyTypeChecker.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.iacaseapi.infrastructure.eventvalidation;
+
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.SEND_DIRECTION_PARTIES;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType.REP;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.Parties;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType;
+
+@Slf4j
+@Component
+public class AsylumCaseSendDirectionEventValidForJourneyTypeChecker implements EventValidChecker<AsylumCase> {
+    @Override
+    public EventValid check(Callback<AsylumCase> callback) {
+        Event event = callback.getEvent();
+        if (event == Event.SEND_DIRECTION) {
+            final AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+            final JourneyType journeyType = asylumCase.<JourneyType>read(AsylumCaseFieldDefinition.JOURNEY_TYPE).orElse(REP);
+
+            Parties directionTo = asylumCase.read(SEND_DIRECTION_PARTIES, Parties.class)
+                    .orElseThrow(() -> new IllegalStateException("sendDirectionParties is not present"));
+
+            if (journeyType == JourneyType.AIP) {
+                log.info("Cannot send a direction for an AIP case");
+                return new EventValid("You cannot use this function to send a direction to an appellant in person.");
+            } else if (journeyType == REP && directionTo == Parties.APPELLANT) {
+                log.info("Cannot send an appellant a direction for a repped case");
+                return new EventValid("This is a legally represented case. You cannot select appellant as the recipient.");
+            }
+        }
+
+        return new EventValid();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/eventvalidation/AsylumCaseSendDirectionEventValidForJourneyTypeCheckerTest.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.reform.iacaseapi.infrastructure.eventvalidation;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.Parties;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AsylumCaseSendDirectionEventValidForJourneyTypeCheckerTest {
+    @Mock
+    private Callback<AsylumCase> callback;
+    @Mock
+    private CaseDetails<AsylumCase> caseDetails;
+    @Mock
+    private AsylumCase asylumCase;
+
+    @Test
+    public void cannotSendDirectionForAipCase() {
+        setupCallback(Event.SEND_DIRECTION, JourneyType.AIP, Parties.APPELLANT);
+        EventValid eventValid = new AsylumCaseSendDirectionEventValidForJourneyTypeChecker().check(callback);
+
+        assertThat(eventValid, is(new EventValid("You cannot use this function to send a direction to an appellant in person.")));
+    }
+
+    @Test
+    public void cannotSendDirectionToAppellantForReppedCase() {
+        setupCallback(Event.SEND_DIRECTION, JourneyType.REP, Parties.APPELLANT);
+        EventValid eventValid = new AsylumCaseSendDirectionEventValidForJourneyTypeChecker().check(callback);
+
+        assertThat(eventValid, is(new EventValid("This is a legally represented case. You cannot select appellant as the recipient.")));
+    }
+
+    @Test
+    public void canSendDirectionToLegalRepForReppedCase() {
+        setupCallback(Event.SEND_DIRECTION, JourneyType.REP, Parties.LEGAL_REPRESENTATIVE);
+        EventValid eventValid = new AsylumCaseSendDirectionEventValidForJourneyTypeChecker().check(callback);
+
+        assertThat(eventValid, is(EventValid.VALID_EVENT));
+    }
+
+    private void setupCallback(Event event, JourneyType journeyType, Parties party) {
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(callback.getEvent()).thenReturn(event);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.read(AsylumCaseFieldDefinition.JOURNEY_TYPE)).thenReturn(Optional.of(journeyType));
+        when(asylumCase.read(AsylumCaseFieldDefinition.SEND_DIRECTION_PARTIES, Parties.class)).thenReturn(Optional.of(party));
+    }
+}
