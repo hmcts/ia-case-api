071f348d48866b2a028093e97655ec394923de4d
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-02-20 14:20:57 +0000

    HTML zap testing report

--- security.sh
+++ security.sh
@@ -8,7 +8,9 @@ echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001
 cat zap.out
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o api-report.html -f html
 
 cp report.json functional-output/
+cp api-report.html functional-output/
 
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
