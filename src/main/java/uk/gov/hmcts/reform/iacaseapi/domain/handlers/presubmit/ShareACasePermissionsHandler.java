98eab796b0a638e0b2596b6c7176d7df04107f41
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-03-03 13:22:35 +0000

    RIA-2468 share case validation (#284)

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ShareACasePermissionsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ShareACasePermissionsHandler.java
@@ -6,21 +6,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.DynamicList;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.CcdUpdater;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.ProfessionalUsersRetriever;
 
 @Slf4j
 @Service
 public class ShareACasePermissionsHandler implements PreSubmitCallbackHandler<AsylumCase> {
 
-    private CcdUpdater ccdUpdater;
+    private final CcdUpdater ccdUpdater;
+    private final ProfessionalUsersRetriever professionalUsersRetriever;
 
-    public ShareACasePermissionsHandler(CcdUpdater ccdUpdater) {
+    public ShareACasePermissionsHandler(CcdUpdater ccdUpdater, ProfessionalUsersRetriever professionalUsersRetriever) {
         this.ccdUpdater = ccdUpdater;
+        this.professionalUsersRetriever = professionalUsersRetriever;
     }
 
     @Override
@@ -43,11 +47,37 @@ public class ShareACasePermissionsHandler implements PreSubmitCallbackHandler<As
 
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
 
+        if (!hasValidUserId(asylumCase)) {
+
+            PreSubmitCallbackResponse<AsylumCase> response = new PreSubmitCallbackResponse<>(callback.getCaseDetails().getCaseData());
+            response.addError("You can share a case only with Active Users in your Organization.");
+
+            asylumCase.clear(AsylumCaseFieldDefinition.ORG_LIST_OF_USERS);
+
+            return response;
+        }
+
         ccdUpdater.updatePermissions(callback);
 
         asylumCase.clear(AsylumCaseFieldDefinition.ORG_LIST_OF_USERS);
 
         return new PreSubmitCallbackResponse<>(callback.getCaseDetails().getCaseData());
 
+    }
+
+    private boolean hasValidUserId(AsylumCase asylumCase) {
+
+        String userId = asylumCase.read(AsylumCaseFieldDefinition.ORG_LIST_OF_USERS, DynamicList.class)
+            .orElseThrow(() -> new IllegalStateException(
+                AsylumCaseFieldDefinition.ORG_LIST_OF_USERS + " is empty in case data when required.")
+            )
+            .getValue()
+            .getCode();
+
+        return professionalUsersRetriever
+            .retrieve()
+            .getProfessionalUsers()
+            .stream()
+            .anyMatch(user -> userId.equalsIgnoreCase(user.getUserIdentifier()) && user.getIdamStatus().equalsIgnoreCase("ACTIVE"));
     }
 }
