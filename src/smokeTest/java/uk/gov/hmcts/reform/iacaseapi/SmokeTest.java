2cdafa8e0ec3ab2202a0dbbdb7161640f76777d8
tarun-palisetty <tarun.mca@gmail.com>
2020-03-18 13:03:16 +0000

    RIA-1906 smoke test improvements (#289)

--- src/smokeTest/java/uk/gov/hmcts/reform/iacaseapi/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/iacaseapi/SmokeTest.java
@@ -1,13 +1,24 @@
 package uk.gov.hmcts.reform.iacaseapi;
 
+import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.restassured.RestAssured;
-import net.serenitybdd.rest.SerenityRest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 
+/**
+ * This smoke test checks for all the downstream services health.
+ * And returns the overall health of the ia-case-api.
+ *
+ * <p>If any of the downstream services is down then the overall
+ * health of the service will show as down.
+ */
 public class SmokeTest {
 
     private final String targetInstance =
@@ -16,21 +27,32 @@ public class SmokeTest {
             "http://localhost:8090"
         );
 
+
     @Test
-    public void should_prove_app_is_running_and_healthy() {
+    public void should_check_downstream_services_and_return_overall_health() {
 
-        RestAssured.baseURI = targetInstance;
-        RestAssured.useRelaxedHTTPSValidation();
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri(targetInstance)
+            .setRelaxedHTTPSValidation()
+            .build();
 
-        String response = SerenityRest
+        Response response = given(requestSpecification)
             .when()
             .get("/health")
             .then()
-            .statusCode(HttpStatus.OK.value())
-            .and()
-            .extract().body().asString();
+            .extract().response();
 
-        assertThat(response)
-            .contains("UP");
+        switch (response.getStatusCode()) {
+            case 200:
+                assertThat(response.getBody().asString()).contains("UP");
+                break;
+            case 503:
+                throw new RestClientException(
+                    response.getBody().asString(),
+                    new HttpClientErrorException(HttpStatus.valueOf(response.getStatusCode()), "One or more downstream services are unavailable."));
+            default:
+                throw new IllegalStateException("Issue with downstream services");
+        }
+
     }
 }
