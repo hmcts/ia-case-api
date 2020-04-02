ff05daca1aa06a14065437acf0094791b4c5c7d3
david.crespo <david.crespo@hmcts.net>
2020-04-02 14:18:56 +0200

    fix unit test after rebase

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -65,6 +65,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(55, Event.values().length);
+        assertEquals(56, Event.values().length);
     }
 }
