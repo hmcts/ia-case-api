90ed562653b7db413a67edab6adecf8efb1a9848
david.crespo <david.crespo@hmcts.net>
2020-04-27 03:01:17 +0200

    add unit tests for the handle method add production code to pass tests

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/MinorTagHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/MinorTagHandler.java
@@ -1,15 +1,24 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPELLANT_DATE_OF_BIRTH;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
 
+import java.time.LocalDate;
+import java.time.Period;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 
+@Component
+@Slf4j
 public class MinorTagHandler implements PreSubmitCallbackHandler<AsylumCase> {
     @Override
     public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
@@ -21,6 +30,29 @@ public class MinorTagHandler implements PreSubmitCallbackHandler<AsylumCase> {
     @Override
     public PreSubmitCallbackResponse<AsylumCase> handle(PreSubmitCallbackStage callbackStage,
                                                         Callback<AsylumCase> callback) {
-        return null;
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+        asylumCase.write(AsylumCaseFieldDefinition.IS_APPELLANT_MINOR, isAppellantMinor(getAppellantDob(asylumCase)));
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+
+    private LocalDate getAppellantDob(AsylumCase asylumCase) {
+        String adultAppellantFallback = "1979-02-15";
+        String appellantDobAsString = asylumCase.read(APPELLANT_DATE_OF_BIRTH, String.class)
+            .orElse(adultAppellantFallback);
+        try {
+            return LocalDate.parse(appellantDobAsString);
+        } catch (Exception e) {
+            log.warn("Error when parsing Appellant dob: ", e);
+            return LocalDate.parse(adultAppellantFallback);
+        }
+    }
+
+    private YesOrNo isAppellantMinor(LocalDate appellantDob) {
+        return Period.between(appellantDob, LocalDate.now()).getYears() < 18 ? YesOrNo.YES : YesOrNo.NO;
     }
 }
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/MinorTagHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/MinorTagHandlerTest.java
@@ -3,7 +3,11 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPELLANT_DATE_OF_BIRTH;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.IS_APPELLANT_MINOR;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.MID_EVENT;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -19,9 +23,12 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
 @RunWith(JUnitParamsRunner.class)
 public class MinorTagHandlerTest {
@@ -31,6 +38,8 @@ public class MinorTagHandlerTest {
 
     @Mock
     private Callback<AsylumCase> callback;
+    @Mock
+    private CaseDetails<AsylumCase> caseDetails;
 
     private final MinorTagHandler minorTagHandler = new MinorTagHandler();
     public static final String APPELLANT_ADULT = LocalDate.of(1979, 2, 1).toString();
@@ -71,7 +80,53 @@ public class MinorTagHandlerTest {
             }
             return scenarios;
         }
+
     }
+
+    @Test
+    @Parameters(method = "generateAppellantDobScenarios")
+    public void given_appellant_is_minor_should_tag_case_as_minor(AppellantDobScenario scenario) {
+        when(callback.getEvent()).thenReturn(Event.SUBMIT_APPEAL);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+
+        AsylumCase asylumCase = new AsylumCase();
+        asylumCase.write(APPELLANT_DATE_OF_BIRTH, scenario.appellantDob);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+
+        PreSubmitCallbackResponse<AsylumCase> actual = minorTagHandler.handle(ABOUT_TO_SUBMIT, callback);
+
+        YesOrNo isAppellantMinor = actual.getData().read(IS_APPELLANT_MINOR, YesOrNo.class)
+            .orElse(YesOrNo.NO);
+
+        assertThat(isAppellantMinor).isEqualTo(scenario.isAppellantMinorExpected);
+    }
+
+    private List<AppellantDobScenario> generateAppellantDobScenarios() {
+        return AppellantDobScenario.builder();
+    }
+
+    @Value
+    private static class AppellantDobScenario {
+        String appellantDob;
+        YesOrNo isAppellantMinorExpected;
+
+        private static List<AppellantDobScenario> builder() {
+            List<AppellantDobScenario> scenarios = new ArrayList<>();
+
+            LocalDate appellantAdult = LocalDate.of(1979, 2, 1);
+            scenarios.add(new AppellantDobScenario(appellantAdult.toString(), YesOrNo.NO));
+
+            LocalDate appellantMinor = LocalDate.now();
+            scenarios.add(new AppellantDobScenario(appellantMinor.toString(), YesOrNo.YES));
+
+            scenarios.add(new AppellantDobScenario(null, YesOrNo.NO));
+            scenarios.add(new AppellantDobScenario("", YesOrNo.NO));
+
+            return scenarios;
+        }
+
+    }
+
 
     @Test
     public void should_not_allow_null_arguments() {
@@ -82,9 +137,21 @@ public class MinorTagHandlerTest {
         assertThatThrownBy(() -> minorTagHandler.canHandle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
             .hasMessage("callback must not be null")
             .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> minorTagHandler.handle(null, callback))
+            .hasMessage("callbackStage must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> minorTagHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
+            .hasMessage("callback must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
     }
 
     @Test
-    public void handle() {
+    public void given_canHandled_is_false_should_throw_exception() {
+        assertThatThrownBy(() -> minorTagHandler.handle(ABOUT_TO_START, callback))
+            .hasMessage("Cannot handle callback")
+            .isExactlyInstanceOf(IllegalStateException.class);
     }
+
 }
