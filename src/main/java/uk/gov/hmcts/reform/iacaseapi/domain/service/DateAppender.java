c4b60091b29ad119e5cc43e12d8aed77c1d5ced0
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-04-30 10:53:55 +0100

    Codestyle fixes

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/DateAppender.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/DateAppender.java
@@ -1,23 +1,22 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.service;
 
-import org.springframework.util.CollectionUtils;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.PreviousDates;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+import static com.google.common.collect.Lists.newArrayList;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static com.google.common.collect.Lists.newArrayList;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.PreviousDates;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 
 public class DateAppender {
 
-    private DateAppender() {}
+    private DateAppender() {
+    }
 
     public static List<IdValue<PreviousDates>> appendPreviousDates(List<IdValue<PreviousDates>> previousDates, String dateDue, String dateSent) {
-        if (CollectionUtils.isEmpty(previousDates)) {
+        if (previousDates.isEmpty()) {
 
             return newArrayList(new IdValue<>("1", new PreviousDates(dateDue, dateSent)));
         } else {
