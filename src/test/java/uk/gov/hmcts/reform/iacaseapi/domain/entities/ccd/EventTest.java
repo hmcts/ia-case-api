8bf018fe738d19d865e8f23e2905a91c503f36fb
Lewis Williams <lewis.williams@transformuk.com>
2020-05-01 15:21:38 +0100

    test fix

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -71,6 +71,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(61, Event.values().length);
+        assertEquals(62, Event.values().length);
     }
 }
