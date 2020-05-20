0a75eee61a6f308e8fd248b6de24c5bb19c60114
chrisdavecm <christopher.davidson@btinternet.com>
2020-05-20 10:39:19 +0100

    fix tests

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -76,6 +76,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(65, Event.values().length);
+        assertEquals(67, Event.values().length);
     }
 }
