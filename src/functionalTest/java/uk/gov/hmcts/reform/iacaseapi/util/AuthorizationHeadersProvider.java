5ba17e1d2cc12bceb0f7da64b522b9b430f8fc14
chrisgrimble <chris.grimble@hmcts.net>
2020-05-29 12:09:52 +0100

    RIA-3144 Appellant documents on Documents tab
    Create an appellantDocuments list to be displayed on the documents
    tab. This will include any evidence the appellant has submitted about
    their case. For the moment this includes the reasons for appeal evidence
    and any clarify questions evidence.

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/AuthorizationHeadersProvider.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/AuthorizationHeadersProvider.java
@@ -142,4 +142,16 @@ public class AuthorizationHeadersProvider {
         );
     }
 
+    public Headers getCitizenAuthorization() {
+        String serviceToken = serviceAuthTokenGenerator.generate();
+        String accessToken = idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_CITIZEN_USERNAME"),
+                System.getenv("TEST_CITIZEN_PASSWORD")
+        );
+
+        return new Headers(
+                new Header("ServiceAuthorization", serviceToken),
+                new Header("Authorization", accessToken)
+        );
+    }
 }
