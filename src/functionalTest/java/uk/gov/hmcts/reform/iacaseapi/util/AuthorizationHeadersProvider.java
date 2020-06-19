ed66bb24e0bad5cb03fc952ac8176c496271e020
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-06-19 16:09:03 +0100

    improve functional tests stability

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/AuthorizationHeadersProvider.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/AuthorizationHeadersProvider.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.iacaseapi.util;
 
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
@@ -16,12 +18,17 @@ public class AuthorizationHeadersProvider {
     @Autowired
     private IdamAuthorizor idamAuthorizor;
 
+    private final Map<String, String> tokens = new ConcurrentHashMap<>();
+
     public Headers getLegalRepresentativeAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_LAW_FIRM_A_USERNAME"),
-            System.getenv("TEST_LAW_FIRM_A_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "LegalRepresentative",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_LAW_FIRM_A_USERNAME"),
+                System.getenv("TEST_LAW_FIRM_A_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -32,10 +39,13 @@ public class AuthorizationHeadersProvider {
 
     public Headers getCaseOfficerAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_CASEOFFICER_USERNAME"),
-            System.getenv("TEST_CASEOFFICER_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "CaseOfficer",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_CASEOFFICER_USERNAME"),
+                System.getenv("TEST_CASEOFFICER_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -46,10 +56,13 @@ public class AuthorizationHeadersProvider {
 
     public Headers getAdminOfficerAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_ADMINOFFICER_USERNAME"),
-            System.getenv("TEST_ADMINOFFICER_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "AdminOfficer",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_ADMINOFFICER_USERNAME"),
+                System.getenv("TEST_ADMINOFFICER_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -60,10 +73,13 @@ public class AuthorizationHeadersProvider {
 
     public Headers getHomeOfficeApcAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_HOMEOFFICE_APC_USERNAME"),
-            System.getenv("TEST_HOMEOFFICE_APC_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "HomeOfficeApc",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_HOMEOFFICE_APC_USERNAME"),
+                System.getenv("TEST_HOMEOFFICE_APC_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -74,10 +90,13 @@ public class AuthorizationHeadersProvider {
 
     public Headers getHomeOfficeLartAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_HOMEOFFICE_LART_USERNAME"),
-            System.getenv("TEST_HOMEOFFICE_LART_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "HomeOfficeLart",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_HOMEOFFICE_LART_USERNAME"),
+                System.getenv("TEST_HOMEOFFICE_LART_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -88,10 +107,13 @@ public class AuthorizationHeadersProvider {
 
     public Headers getHomeOfficePouAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_HOMEOFFICE_POU_USERNAME"),
-            System.getenv("TEST_HOMEOFFICE_POU_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "HomeOfficePou",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_HOMEOFFICE_POU_USERNAME"),
+                System.getenv("TEST_HOMEOFFICE_POU_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -102,10 +124,13 @@ public class AuthorizationHeadersProvider {
 
     public Headers getHomeOfficeGenericAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_HOMEOFFICE_GENERIC_USERNAME"),
-            System.getenv("TEST_HOMEOFFICE_GENERIC_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "HomeOfficeGeneric",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_HOMEOFFICE_GENERIC_USERNAME"),
+                System.getenv("TEST_HOMEOFFICE_GENERIC_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -116,10 +141,13 @@ public class AuthorizationHeadersProvider {
 
     public Headers getLegalRepresentativeOrgAAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_LAW_FIRM_SHARE_CASE_A_USERNAME"),
-            System.getenv("TEST_LAW_FIRM_SHARE_CASE_A_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "LegalRepresentativeOrgA",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_LAW_FIRM_SHARE_CASE_A_USERNAME"),
+                System.getenv("TEST_LAW_FIRM_SHARE_CASE_A_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -130,10 +158,13 @@ public class AuthorizationHeadersProvider {
 
     public Headers getJudgeAuthorization() {
 
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
-            System.getenv("TEST_JUDGE_X_USERNAME"),
-            System.getenv("TEST_JUDGE_X_PASSWORD")
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "Judge",
+            user -> idamAuthorizor.exchangeForAccessToken(
+                System.getenv("TEST_JUDGE_X_USERNAME"),
+                System.getenv("TEST_JUDGE_X_PASSWORD")
+            )
         );
 
         return new Headers(
@@ -143,15 +174,19 @@ public class AuthorizationHeadersProvider {
     }
 
     public Headers getCitizenAuthorization() {
-        String serviceToken = serviceAuthTokenGenerator.generate();
-        String accessToken = idamAuthorizor.exchangeForAccessToken(
+
+        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
+        String accessToken = tokens.computeIfAbsent(
+            "Citizen",
+            user -> idamAuthorizor.exchangeForAccessToken(
                 System.getenv("TEST_CITIZEN_USERNAME"),
                 System.getenv("TEST_CITIZEN_PASSWORD")
+            )
         );
 
         return new Headers(
-                new Header("ServiceAuthorization", serviceToken),
-                new Header("Authorization", accessToken)
+            new Header("ServiceAuthorization", serviceToken),
+            new Header("Authorization", accessToken)
         );
     }
 }
