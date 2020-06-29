29c4999d09fec1f749dab322423e2443fe288775
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-06-29 15:44:05 +0100

    Added submitCmaRequirements event to the document generator list of allowed events.

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/GenerateDocumentHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/GenerateDocumentHandler.java
@@ -67,7 +67,8 @@ public class GenerateDocumentHandler implements PreSubmitCallbackHandler<AsylumC
             Event.SEND_DECISION_AND_REASONS,
             Event.EDIT_CASE_LISTING,
             Event.ADJOURN_HEARING_WITHOUT_DATE,
-            Event.END_APPEAL);
+            Event.END_APPEAL,
+            Event.SUBMIT_CMA_REQUIREMENTS);
         if (isEmStitchingEnabled) {
             allowedEvents.add(Event.GENERATE_HEARING_BUNDLE);
             allowedEvents.add(Event.SUBMIT_CASE);
