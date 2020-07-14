d141b712f7ef7e977731e738f50981ae3ad44f8a
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2020-07-14 10:50:03 +0100

    RIA-3091-Update-Details-After-Appointment (#441)

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/UpdateAppointmentAfterCmaConfirmation.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit;
+
+import static java.util.Objects.requireNonNull;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PostSubmitCallbackHandler;
+
+@Component
+public class UpdateAppointmentAfterCmaConfirmation implements PostSubmitCallbackHandler<AsylumCase> {
+
+    public boolean canHandle(
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callback, "callback must not be null");
+        return callback.getEvent() == Event.UPDATE_DETAILS_AFTER_CMA;
+    }
+
+    public PostSubmitCallbackResponse handle(
+        Callback<AsylumCase> callback
+    ) {
+        if (!canHandle(callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        PostSubmitCallbackResponse postSubmitResponse =
+            new PostSubmitCallbackResponse();
+
+        postSubmitResponse.setConfirmationHeader("# You have updated the details of the appointment");
+        postSubmitResponse.setConfirmationBody(
+            "#### What happens next\n\n"
+            + "The updated details are available to view in the hearing and appointment tab. All parties will be notified that the appointments details have been updated."
+        );
+
+        return postSubmitResponse;
+    }
+}
