7a06cb56a6ca94684d3cf12bd4fc914e3b59b71d
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-07-14 12:34:53 +0100

    Added new hearing centres: - Glasgow tribunal centre - Coventry - Newcastle

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/HearingCentre.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/HearingCentre.java
@@ -7,14 +7,17 @@ import java.util.Optional;
 
 public enum HearingCentre {
 
+    BIRMINGHAM("birmingham"),
     BRADFORD("bradford"),
+    COVENTRY("coventry"),
+    GLASGOW("glasgow"),
+    GLASGOW_TRIBUNAL_CENTRE("glasgowTribunalsCentre"),
+    HATTON_CROSS("hattonCross"),
     MANCHESTER("manchester"),
     NEWPORT("newport"),
-    TAYLOR_HOUSE("taylorHouse"),
     NORTH_SHIELDS("northShields"),
-    BIRMINGHAM("birmingham"),
-    HATTON_CROSS("hattonCross"),
-    GLASGOW("glasgow");
+    NOTTINGHAM("nottingham"),
+    TAYLOR_HOUSE("taylorHouse");
 
     @JsonValue
     private final String value;
