8589d45a61cf2ed3cd3f3eb5f88f923d0ba4cbfb
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-07-24 11:20:29 +0100

    Added read only field for dates to avoid while reviewing the cma requirements

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ReviewCmaRequirementsPreparer.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ReviewCmaRequirementsPreparer.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.INTERPRETER_LANGUAGE;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.INTERPRETER_LANGUAGE_READONLY;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.DatesToAvoid;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.InterpreterLanguage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
@@ -45,6 +45,7 @@ public class ReviewCmaRequirementsPreparer implements PreSubmitCallbackHandler<A
                 .getCaseData();
 
         decorateInterpreterDetails(asylumCase);
+        decorateDatesToAvoid(asylumCase);
 
         return new PreSubmitCallbackResponse<>(asylumCase);
     }
@@ -56,6 +57,16 @@ public class ReviewCmaRequirementsPreparer implements PreSubmitCallbackHandler<A
             .stream()
             .map(i ->
                 "Language\t\t" + i.getValue().getLanguage() + "\nDialect\t\t\t" + i.getValue().getLanguageDialect() + "\n")
+            .collect(Collectors.joining("\n"))));
+    }
+
+    private static void decorateDatesToAvoid(AsylumCase asylumCase) {
+        final Optional<List<IdValue<DatesToAvoid>>> datesToAvoid = asylumCase.read(DATES_TO_AVOID);
+
+        datesToAvoid.ifPresent(idValues -> asylumCase.write(DATES_TO_AVOID_READONLY, idValues
+            .stream()
+            .map(i ->
+                "Date\t\t\t" + i.getValue().getDateToAvoid() + "\nReason\t\t\t" + i.getValue().getDateToAvoidReason() + "\n")
             .collect(Collectors.joining("\n"))));
     }
 }
