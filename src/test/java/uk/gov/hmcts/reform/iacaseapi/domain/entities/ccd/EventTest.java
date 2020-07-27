3656dbb794488c4167843500f52c784fedc0aa85
Moussa Kayad <mkayad@gmail.com>
2020-07-27 13:36:22 +0100

    make the tests pass

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -89,6 +89,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(80, Event.values().length);
+        assertEquals(81, Event.values().length);
     }
 }
