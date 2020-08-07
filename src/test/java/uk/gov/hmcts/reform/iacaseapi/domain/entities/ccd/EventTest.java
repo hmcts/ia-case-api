8a4e1968e4d487bb156cbbea3a0b77036621ffb2
Moussa Kayad <mkayad@gmail.com>
2020-08-07 11:13:16 +0100

    fix tests

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -91,6 +91,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(82, Event.values().length);
+        assertEquals(83, Event.values().length);
     }
 }
