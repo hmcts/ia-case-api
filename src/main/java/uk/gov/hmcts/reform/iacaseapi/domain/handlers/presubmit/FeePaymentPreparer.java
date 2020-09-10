35edc6d6044ec171de247fe1bff28d956870d9c3
tarun-palisetty <tarun.mca@gmail.com>
2020-09-10 17:02:32 +0100

    RIA-3574 Duplicate payment error handling

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/FeePaymentPreparer.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/FeePaymentPreparer.java
@@ -1,16 +1,19 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
 
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AppealType;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.PaymentStatus;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacaseapi.domain.service.FeePayment;
@@ -55,16 +58,79 @@ public class FeePaymentPreparer implements PreSubmitCallbackHandler<AsylumCase> 
             throw new IllegalStateException("Cannot handle callback");
         }
 
-        AsylumCase asylumCase =
+        final AsylumCase asylumCase =
             callback
                 .getCaseDetails()
                 .getCaseData();
 
-        asylumCase.write(AsylumCaseFieldDefinition.IS_FEE_PAYMENT_ENABLED, isfeePaymentEnabled ? YesOrNo.YES : YesOrNo.NO);
+        final State currentState = callback.getCaseDetails().getState();
 
-        asylumCase = feePayment.aboutToStart(callback);
+        final PreSubmitCallbackResponse<AsylumCase> asylumCasePreSubmitCallbackResponse
+            = new PreSubmitCallbackResponse<>(asylumCase);
 
-        return new PreSubmitCallbackResponse<>(asylumCase);
+        final PaymentStatus paymentStatus = asylumCase.read(PAYMENT_STATUS, PaymentStatus.class)
+            .orElse(PaymentStatus.PAYMENT_DUE);
+
+        asylumCase.read(APPEAL_TYPE, AppealType.class)
+            .map(type -> {
+                switch (type) {
+                    case EA:
+                    case HU:
+                        asylumCase.read(EA_HU_APPEAL_TYPE_PAYMENT_OPTION, String.class)
+                            .filter(option ->  option.equals("payOffline")
+                                                || paymentStatus == PaymentStatus.PAID)
+                            .map(s -> {
+                                if (callback.getEvent() == Event.PAY_AND_SUBMIT_APPEAL) {
+
+                                    asylumCasePreSubmitCallbackResponse
+                                        .addError("The Pay and submit your appeal option is not available. "
+                                                  + "Select Submit your appeal if you want to submit the appeal now.");
+                                }
+                                if (callback.getEvent() == Event.PAYMENT_APPEAL) {
+
+                                    asylumCasePreSubmitCallbackResponse.addError("The Make a payment option is not available.");
+                                }
+
+                                return asylumCasePreSubmitCallbackResponse;
+                            });
+                        break;
+
+                    case PA:
+                        asylumCase.read(PA_APPEAL_TYPE_PAYMENT_OPTION, String.class)
+                            .filter(option -> option.equals("payOffline")
+                                                || paymentStatus == PaymentStatus.PAID
+                                                || (option.equals("payLater") && currentState == State.APPEAL_STARTED))
+                            .map(s -> {
+                                if (callback.getEvent() == Event.PAY_AND_SUBMIT_APPEAL) {
+
+                                    asylumCasePreSubmitCallbackResponse
+                                        .addError("The Pay and submit your appeal option is not available. "
+                                                  + "Select Submit your appeal if you want to submit the appeal now.");
+                                }
+                                if (callback.getEvent() == Event.PAYMENT_APPEAL) {
+
+                                    asylumCasePreSubmitCallbackResponse.addError("The Make a payment option is not available.");
+                                }
+                                return asylumCasePreSubmitCallbackResponse;
+                            });
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+                return asylumCasePreSubmitCallbackResponse;
+            });
+
+
+        asylumCase.write(IS_FEE_PAYMENT_ENABLED, isfeePaymentEnabled ? YesOrNo.YES : YesOrNo.NO);
+
+        if (asylumCasePreSubmitCallbackResponse.getErrors().isEmpty()) {
+            asylumCasePreSubmitCallbackResponse.setData(feePayment.aboutToStart(callback));
+        }
+
+        return asylumCasePreSubmitCallbackResponse;
     }
 
 }
