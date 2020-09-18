3b4136676c7c66cfe42e39fa1a6bdfe5d24ac7c6
david.crespo <david.crespo@hmcts.net>
2020-09-19 01:44:46 +0200

    add allocateCaseConfirmation handler

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/AllocateCaseConfirmation.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit;
+
+import static java.util.Objects.requireNonNull;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PostSubmitCallbackHandler;
+
+@Component
+public class AllocateCaseConfirmation implements PostSubmitCallbackHandler<AsylumCase> {
+
+    public boolean canHandle(
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callback, "callback must not be null");
+
+        return callback.getEvent() == Event.ALLOCATE_CASE;
+    }
+
+    public PostSubmitCallbackResponse handle(
+        Callback<AsylumCase> callback
+    ) {
+        if (!canHandle(callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        PostSubmitCallbackResponse postSubmitResponse =
+            new PostSubmitCallbackResponse();
+
+        postSubmitResponse.setConfirmationHeader("# You have allocated the case");
+        String confirmationBodyContent = String.format("#### What happens next%n%n"
+            + "###### The tasks for this case will now appear in your task list."
+            + "%nShould you wish, you can write [a case note]"
+            + "(/case/IA/Asylum/%s/trigger/addCaseNote).", callback.getCaseDetails().getId());
+        postSubmitResponse.setConfirmationBody(confirmationBodyContent);
+
+        return postSubmitResponse;
+    }
+}
