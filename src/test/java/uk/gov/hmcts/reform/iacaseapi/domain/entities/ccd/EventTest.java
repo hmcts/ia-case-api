80c58e933a7235fb282b61d14504037f9bc67839
Brad Sorour <brad.sorour@gmail.com>
2020-09-21 13:45:15 +0100

    RIA-3059 merge

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -94,6 +94,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(86, Event.values().length);
+        assertEquals(87, Event.values().length);
     }
 }
