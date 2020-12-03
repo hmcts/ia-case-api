ee7b32fa8d473721d0f7ab6befeab7d7cf041919
tarun-palisetty <tarun.mca@gmail.com>
2020-12-03 13:42:39 +0000

    RIA-3859 Remission details appender service added

---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/RemissionDetailsTest.java
@@ -0,0 +1,101 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+
+class RemissionDetailsTest {
+
+    private String feeRemissionType = "A remission type";
+    private String asylumSupportReference = "Asylum support";
+    private Document asylumSupportDocument = mock(Document.class);
+    private String legalAidAccountNumber = "legalAidAccountNumber";
+    private Document section17Document = mock(Document.class);
+    private Document section20Document = mock(Document.class);
+    private Document homeOfficeWaiverDocument = mock(Document.class);
+    private String helpWithFeesReferenceNumber = "helpWithFeesReferenceNumber";
+    private String exceptionalCircumstances = "exceptionalCircumstances";
+    private List<IdValue<Document>> remissionEcEvidenceDocuments = Collections.emptyList();
+    private String remissionDecision = "remissionDecision";
+    private String feeAmount = "14000";
+    private String amountRemitted = "4000";
+    private String amountLeftToPay = "4000";
+    private String remissionDecisionReason = "remissionDecisionReason";
+
+    @Test
+    void should_hold_asylum_support_remission_details() {
+
+        RemissionDetails remissionDetails =
+            new RemissionDetails(feeRemissionType, asylumSupportReference, asylumSupportDocument);
+
+        assertEquals(feeRemissionType, remissionDetails.getFeeRemissionType());
+        assertEquals(asylumSupportReference, remissionDetails.getAsylumSupportReference());
+        assertEquals(asylumSupportDocument, remissionDetails.getAsylumSupportDocument());
+    }
+
+    @Test
+    void should_hold_legal_aid_remission_details() {
+
+        RemissionDetails remissionDetails =
+            new RemissionDetails(feeRemissionType, legalAidAccountNumber, "");
+
+        assertEquals(feeRemissionType, remissionDetails.getFeeRemissionType());
+        assertEquals(legalAidAccountNumber, remissionDetails.getLegalAidAccountNumber());
+    }
+
+    @Test
+    void should_hold_help_with_fees_remission_details() {
+
+        RemissionDetails remissionDetails =
+            new RemissionDetails(feeRemissionType, null, helpWithFeesReferenceNumber);
+
+        assertEquals(feeRemissionType, remissionDetails.getFeeRemissionType());
+        assertEquals(helpWithFeesReferenceNumber, remissionDetails.getHelpWithFeesReferenceNumber());
+    }
+
+    @Test
+    void should_hold_section_17_remission_details() {
+
+        RemissionDetails remissionDetails =
+            new RemissionDetails(feeRemissionType, section17Document, null, null);
+
+        assertEquals(feeRemissionType, remissionDetails.getFeeRemissionType());
+        assertEquals(section17Document, remissionDetails.getSection17Document());
+    }
+
+    @Test
+    void should_hold_section_20_remission_details() {
+
+        RemissionDetails remissionDetails =
+            new RemissionDetails(feeRemissionType, null, section20Document, null);
+
+        assertEquals(feeRemissionType, remissionDetails.getFeeRemissionType());
+        assertEquals(section20Document, remissionDetails.getSection20Document());
+    }
+
+    @Test
+    void should_hold_home_office_waiver_remission_details() {
+
+        RemissionDetails remissionDetails =
+            new RemissionDetails(feeRemissionType, null, null, homeOfficeWaiverDocument);
+
+        assertEquals(feeRemissionType, remissionDetails.getFeeRemissionType());
+        assertEquals(homeOfficeWaiverDocument, remissionDetails.getHomeOfficeWaiverDocument());
+    }
+
+    @Test
+    void should_hold_exceptional_circumstances_remission_details() {
+
+        RemissionDetails remissionDetails =
+            new RemissionDetails(feeRemissionType, exceptionalCircumstances, remissionEcEvidenceDocuments);
+
+        assertEquals(feeRemissionType, remissionDetails.getFeeRemissionType());
+        assertEquals(exceptionalCircumstances, remissionDetails.getExceptionalCircumstances());
+        assertEquals(remissionEcEvidenceDocuments, remissionDetails.getRemissionEcEvidenceDocuments());
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/service/RemissionDetailsAppenderTest.java
@@ -0,0 +1,154 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.RemissionDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+
+@ExtendWith(MockitoExtension.class)
+@SuppressWarnings("unchecked")
+class RemissionDetailsAppenderTest {
+
+    @Mock private IdValue<RemissionDetails> remissionDetailsById1;
+    private RemissionDetailsAppender remissionDetailsAppender;
+
+    private String feeRemissionType = "feeRemissionType";
+
+    @BeforeEach
+    void setUp() {
+
+        remissionDetailsAppender = new RemissionDetailsAppender();
+    }
+
+    @Test
+    void append_asylum_support_remission_details() {
+
+        String asylumSupportReference = "asylumSupportReference";
+        Document asylumSupportDocument = mock(Document.class);
+
+        RemissionDetails remissionDetails1 = mock(RemissionDetails.class);
+        when(remissionDetailsById1.getValue()).thenReturn(remissionDetails1);
+
+        List<IdValue<RemissionDetails>> existingRemissionDetails = Arrays.asList(remissionDetailsById1);
+
+        List<IdValue<RemissionDetails>> remissionDetails =
+            remissionDetailsAppender
+                .appendAsylumSupportRemissionDetails(existingRemissionDetails, feeRemissionType, asylumSupportReference, asylumSupportDocument);
+
+        assertNotNull(remissionDetails);
+        assertEquals(2, remissionDetails.size());
+    }
+
+    void append_legal_aid_remission_details() {
+
+        String legalAidAccountNumber = "legalAidAccountNumber";
+
+        RemissionDetails remissionDetails1 = mock(RemissionDetails.class);
+        when(remissionDetailsById1.getValue()).thenReturn(remissionDetails1);
+
+        List<IdValue<RemissionDetails>> existingRemissionDetails = Arrays.asList(remissionDetailsById1);
+
+        List<IdValue<RemissionDetails>> remissionDetails =
+            remissionDetailsAppender.appendLegalAidRemissionDetails(existingRemissionDetails, feeRemissionType, legalAidAccountNumber);
+
+        assertNotNull(remissionDetails);
+        assertEquals(2, remissionDetails.size());
+    }
+
+    @Test
+    void append_help_with_fees_remission_details() {
+
+        String helpWithFeeReference = "helpWithFeeReference";
+
+        RemissionDetails remissionDetails1 = mock(RemissionDetails.class);
+        when(remissionDetailsById1.getValue()).thenReturn(remissionDetails1);
+
+        List<IdValue<RemissionDetails>> existingRemissionDetails = Arrays.asList(remissionDetailsById1);
+
+        List<IdValue<RemissionDetails>> remissionDetails =
+            remissionDetailsAppender.appendHelpWithFeeReferenceRemissionDetails(existingRemissionDetails, feeRemissionType, helpWithFeeReference);
+
+        assertNotNull(remissionDetails);
+        assertEquals(2, remissionDetails.size());
+    }
+
+    @Test
+    void append_section17_remission_details() {
+
+        Document section17Document = mock(Document.class);
+
+        RemissionDetails remissionDetails1 = mock(RemissionDetails.class);
+        when(remissionDetailsById1.getValue()).thenReturn(remissionDetails1);
+
+        List<IdValue<RemissionDetails>> existingRemissionDetails = Arrays.asList(remissionDetailsById1);
+
+        List<IdValue<RemissionDetails>> remissionDetails =
+            remissionDetailsAppender.appendSection17RemissionDetails(existingRemissionDetails, feeRemissionType, section17Document);
+
+        assertNotNull(remissionDetails);
+        assertEquals(2, remissionDetails.size());
+    }
+
+    @Test
+    void append_section20_remission_details() {
+
+        Document section20Document = mock(Document.class);
+
+        RemissionDetails remissionDetails1 = mock(RemissionDetails.class);
+        when(remissionDetailsById1.getValue()).thenReturn(remissionDetails1);
+
+        List<IdValue<RemissionDetails>> existingRemissionDetails = Arrays.asList(remissionDetailsById1);
+
+        List<IdValue<RemissionDetails>> remissionDetails =
+            remissionDetailsAppender.appendSection17RemissionDetails(existingRemissionDetails, feeRemissionType, section20Document);
+
+        assertNotNull(remissionDetails);
+        assertEquals(2, remissionDetails.size());
+    }
+
+    @Test
+    void append_home_office_waiver_remission_details() {
+
+        Document homeOfficeWaiver = mock(Document.class);
+
+        RemissionDetails remissionDetails1 = mock(RemissionDetails.class);
+        when(remissionDetailsById1.getValue()).thenReturn(remissionDetails1);
+
+        List<IdValue<RemissionDetails>> existingRemissionDetails = Arrays.asList(remissionDetailsById1);
+
+        List<IdValue<RemissionDetails>> remissionDetails =
+            remissionDetailsAppender.appendSection17RemissionDetails(existingRemissionDetails, feeRemissionType, homeOfficeWaiver);
+
+        assertNotNull(remissionDetails);
+        assertEquals(2, remissionDetails.size());
+    }
+
+    @Test
+    void append_exceptional_circumstances_remission_details() {
+
+        String exceptionalCircumstances = "exceptionalCircumstances";
+        List<IdValue<Document>> ecDocuments = Collections.emptyList();
+
+        RemissionDetails remissionDetails1 = mock(RemissionDetails.class);
+        when(remissionDetailsById1.getValue()).thenReturn(remissionDetails1);
+
+        List<IdValue<RemissionDetails>> existingRemissionDetails = Arrays.asList(remissionDetailsById1);
+
+        List<IdValue<RemissionDetails>> remissionDetails =
+            remissionDetailsAppender
+                .appendExceptionalCircumstancesRemissionDetails(existingRemissionDetails, feeRemissionType, exceptionalCircumstances, ecDocuments);
+
+        assertNotNull(remissionDetails);
+        assertEquals(2, remissionDetails.size());
+    }
+}
