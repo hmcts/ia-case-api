f3443da327785c935b95178ba1ced7dc7f6a5546
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-12-09 16:16:40 +0000

    RIA-3849 cleanup after migration to Postgresql 11

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,24 +24,6 @@ data "azurerm_key_vault" "ia_key_vault" {
   resource_group_name = "${local.key_vault_name}"
 }
 
-module "ia_case_api_database" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = "${var.product}-${var.component}-postgres-db"
-  location           = "${var.location}"
-  env                = "${var.env}"
-  database_name      = "${var.postgresql_database_name}"
-  postgresql_user    = "${var.postgresql_user}"
-  postgresql_version = "10"
-  common_tags        = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
-  subscription       = "${var.subscription}"
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name         = "${var.component}-POSTGRES-PASS"
-  value        = module.ia_case_api_database.postgresql_password
-  key_vault_id = data.azurerm_key_vault.ia_key_vault.id
-}
-
 module "ia_case_api_database_11" {
   source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
   product            = "${var.product}-${var.component}-postgres-11-db"
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/PreSubmitCallbackControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/PreSubmitCallbackControllerTest.java
@@ -2,18 +2,14 @@ package uk.gov.hmcts.reform.iacaseapi.infrastructure.controllers;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
@@ -21,11 +17,9 @@ import org.mockito.quality.Strictness;
 import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
-import uk.gov.hmcts.reform.iacaseapi.domain.service.FeatureToggler;
 import uk.gov.hmcts.reform.iacaseapi.infrastructure.PreSubmitCallbackDispatcher;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,21 +34,15 @@ class PreSubmitCallbackControllerTest {
     private Callback<AsylumCase> callback;
     @Mock
     private CaseDetails<AsylumCase> caseDetails;
-    @Mock
-    private FeatureToggler featureToggler;
-    @Mock
-    private AsylumCase asylumCase;
 
     private PreSubmitCallbackController preSubmitCallbackController;
 
     @BeforeEach
     public void setUp() {
-        when(featureToggler.getValue("suspend-submit-case", false)).thenReturn(false);
 
         preSubmitCallbackController =
             new PreSubmitCallbackController(
-                callbackDispatcher,
-                featureToggler
+                callbackDispatcher
             );
     }
 
@@ -118,53 +106,10 @@ class PreSubmitCallbackControllerTest {
         );
     }
 
-    @ParameterizedTest
-    @EnumSource(value = Event.class, names = {"SUBMIT_APPEAL", "PAY_AND_SUBMIT_APPEAL"})
-    public void should_not_dispatch_callback_when_suspend_flag_is_true_and_event_is_submit_appeal(Event event) {
-
-        when(callback.getCaseDetails()).thenReturn(caseDetails);
-        when(callback.getCaseDetails().getCaseData()).thenReturn(asylumCase);
-        when(featureToggler.getValue("suspend-submit-case", false)).thenReturn(true);
-        when(callback.getCaseDetails()).thenReturn(caseDetails);
-        when(callback.getEvent()).thenReturn(event);
-
-        ResponseEntity<PreSubmitCallbackResponse<AsylumCase>> actualResponse =
-            preSubmitCallbackController.ccdAboutToSubmit(callback);
-
-        assertNotNull(actualResponse);
-        assertNotNull(actualResponse.getBody());
-        assertTrue(actualResponse.getBody().getErrors().contains(
-            "There is planned maintenance work in progress. Your appeal has been saved and you will be able to submit it later. Please try again in few hours."));
-        verifyNoInteractions(callbackDispatcher);
-    }
-
-    @Test
-    void should_dispatch_callback_when_suspend_flag_is_true_and_event_is_not_submit_appeal() {
-
-        when(callback.getCaseDetails()).thenReturn(caseDetails);
-        when(callback.getEvent()).thenReturn(Event.START_APPEAL);
-
-        doReturn(callbackResponse)
-            .when(callbackDispatcher)
-            .handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
-
-        ResponseEntity<PreSubmitCallbackResponse<AsylumCase>> actualResponse =
-            preSubmitCallbackController.ccdAboutToSubmit(callback);
-
-        assertNotNull(actualResponse);
-        assertNotNull(actualResponse.getBody());
-        assertTrue(actualResponse.getBody().getErrors().isEmpty());
-        verifyNoInteractions(featureToggler);
-        verify(callbackDispatcher, times(1)).handle(
-            PreSubmitCallbackStage.ABOUT_TO_SUBMIT,
-            callback
-        );
-    }
-
     @Test
     void should_not_allow_null_constructor_arguments() {
 
-        assertThatThrownBy(() -> new PreSubmitCallbackController(null, featureToggler))
+        assertThatThrownBy(() -> new PreSubmitCallbackController(null))
             .hasMessage("callbackDispatcher must not be null")
             .isExactlyInstanceOf(NullPointerException.class);
     }
