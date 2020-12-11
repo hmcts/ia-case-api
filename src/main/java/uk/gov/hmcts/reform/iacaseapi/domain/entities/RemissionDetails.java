1b7687a100d645aa52c2fa372d07d0f23f8a6f67
tarun-palisetty <tarun.mca@gmail.com>
2020-12-11 14:07:45 +0000

    RIA-3859 Remission details as collection (#695)

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/RemissionDetails.java
@@ -0,0 +1,82 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.entities;
+
+import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+
+@Getter
+@EqualsAndHashCode
+@ToString
+public class RemissionDetails {
+
+    private String feeRemissionType;
+    private String asylumSupportReference;
+    private Document asylumSupportDocument;
+    private String legalAidAccountNumber;
+    private Document section17Document;
+    private Document section20Document;
+    private Document homeOfficeWaiverDocument;
+    private String helpWithFeesReferenceNumber;
+    private String exceptionalCircumstances;
+    private List<IdValue<Document>> remissionEcEvidenceDocuments;
+    private String remissionDecision;
+    private String feeAmount;
+    private String amountRemitted;
+    private String amountLeftToPay;
+    private String remissionDecisionReason;
+
+    private RemissionDetails() {
+
+    }
+
+    public RemissionDetails(String feeRemissionType, String asylumSupportReference, Document asylumSupportDocument) {
+        this.feeRemissionType = feeRemissionType;
+        this.asylumSupportReference = asylumSupportReference;
+        this.asylumSupportDocument = asylumSupportDocument;
+    }
+
+    public RemissionDetails(String feeRemissionType, String legalAidAccountNumber, String helpWithFeesReferenceNumber) {
+        this.feeRemissionType = feeRemissionType;
+        this.legalAidAccountNumber = legalAidAccountNumber;
+        this.helpWithFeesReferenceNumber = helpWithFeesReferenceNumber;
+    }
+
+    public RemissionDetails(
+        String feeRemissionType, Document section17Document,
+        Document section20Document, Document homeOfficeWaiverDocument) {
+        this.feeRemissionType = feeRemissionType;
+        this.section17Document = section17Document;
+        this.section20Document = section20Document;
+        this.homeOfficeWaiverDocument = homeOfficeWaiverDocument;
+    }
+
+    public RemissionDetails(String feeRemissionType,
+                            String exceptionalCircumstances, List<IdValue<Document>> remissionEcEvidenceDocuments) {
+        this.feeRemissionType = feeRemissionType;
+        this.exceptionalCircumstances = exceptionalCircumstances;
+        this.remissionEcEvidenceDocuments = remissionEcEvidenceDocuments;
+    }
+
+    public void setRemissionDecision(String remissionDecision) {
+        this.remissionDecision = remissionDecision;
+    }
+
+    public void setFeeAmount(String feeAmount) {
+        this.feeAmount = feeAmount;
+    }
+
+    public void setAmountRemitted(String amountRemitted) {
+        this.amountRemitted = amountRemitted;
+    }
+
+    public void setAmountLeftToPay(String amountLeftToPay) {
+        this.amountLeftToPay = amountLeftToPay;
+    }
+
+    public void setRemissionDecisionReason(String remissionDecisionReason) {
+        this.remissionDecisionReason = remissionDecisionReason;
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/RemissionDetailsAppender.java
@@ -0,0 +1,106 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.RemissionDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+
+@Service
+public class RemissionDetailsAppender {
+
+    public List<IdValue<RemissionDetails>> appendAsylumSupportRemissionDetails(
+        List<IdValue<RemissionDetails>> existingRemissionDetails,
+        String feeRemissionType,
+        String asylumSupportReference,
+        Document asylumSupportDocument
+    ) {
+        final RemissionDetails newRemissionDetails = new RemissionDetails(feeRemissionType, asylumSupportReference, asylumSupportDocument);
+
+        return append(existingRemissionDetails, newRemissionDetails);
+    }
+
+    public List<IdValue<RemissionDetails>> appendLegalAidRemissionDetails(
+        List<IdValue<RemissionDetails>> existingRemissionDetails,
+        String feeRemissionType,
+        String legalAidAccountNumber
+    ) {
+        final RemissionDetails newRemissionDetails = new RemissionDetails(feeRemissionType, legalAidAccountNumber, "");
+
+        return append(existingRemissionDetails, newRemissionDetails);
+    }
+
+    public List<IdValue<RemissionDetails>> appendHelpWithFeeReferenceRemissionDetails(
+        List<IdValue<RemissionDetails>> existingRemissionDetails,
+        String feeRemissionType,
+        String helpWithFeesReferenceNumber
+    ) {
+        final RemissionDetails newRemissionDetails = new RemissionDetails(feeRemissionType, null, helpWithFeesReferenceNumber);
+
+        return append(existingRemissionDetails, newRemissionDetails);
+    }
+
+    public List<IdValue<RemissionDetails>> appendSection17RemissionDetails(
+        List<IdValue<RemissionDetails>> existingRemissionDetails,
+        String feeRemissionType,
+        Document section17Document
+    ) {
+        final RemissionDetails newRemissionDetails =
+            new RemissionDetails(feeRemissionType, section17Document, null, null);
+
+        return append(existingRemissionDetails, newRemissionDetails);
+    }
+
+    public List<IdValue<RemissionDetails>> appendSection20RemissionDetails(
+        List<IdValue<RemissionDetails>> existingRemissionDetails,
+        String feeRemissionType,
+        Document section20Document
+    ) {
+        final RemissionDetails newRemissionDetails =
+            new RemissionDetails(feeRemissionType, null, section20Document, null);
+
+        return append(existingRemissionDetails, newRemissionDetails);
+    }
+
+    public List<IdValue<RemissionDetails>> appendHomeOfficeWaiverRemissionDetails(
+        List<IdValue<RemissionDetails>> existingRemissionDetails,
+        String feeRemissionType,
+        Document homeOfficeWaiverDocument
+    ) {
+        final RemissionDetails newRemissionDetails =
+            new RemissionDetails(feeRemissionType, null, null, homeOfficeWaiverDocument);
+
+        return append(existingRemissionDetails, newRemissionDetails);
+    }
+
+    public List<IdValue<RemissionDetails>> appendExceptionalCircumstancesRemissionDetails(
+        List<IdValue<RemissionDetails>> existingRemissionDetails,
+        String feeRemissionType,
+        String exceptionalCircumstances,
+        List<IdValue<Document>> remissionEcEvidenceDocuments
+    ) {
+        final RemissionDetails newRemissionDetails =
+            new RemissionDetails(feeRemissionType, exceptionalCircumstances, remissionEcEvidenceDocuments);
+
+        return append(existingRemissionDetails, newRemissionDetails);
+    }
+
+    private List<IdValue<RemissionDetails>> append(
+        List<IdValue<RemissionDetails>> existingRemissionDetails,
+        RemissionDetails newRemissionDetails) {
+
+        final  List<IdValue<RemissionDetails>> allRemissionDetails = new ArrayList<>();
+
+        int index = existingRemissionDetails.size() + 1;
+
+        allRemissionDetails.add(new IdValue<>(String.valueOf(index--), newRemissionDetails));
+
+
+        for (IdValue<RemissionDetails> existingRemission : existingRemissionDetails) {
+            allRemissionDetails.add(new IdValue<>(String.valueOf(index--), existingRemission.getValue()));
+        }
+
+        return allRemissionDetails;
+    }
+}
