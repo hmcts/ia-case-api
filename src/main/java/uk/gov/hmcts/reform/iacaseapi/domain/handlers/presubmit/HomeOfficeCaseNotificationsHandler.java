7b393599dbda7a86b05fa6f9b1656bf74014d3f6
jkumar-moj <54027687+jkumar-moj@users.noreply.github.com>
2021-01-05 12:54:33 +0000

    RIA-3308: Changes to trigger HomeOffice notifications for request-evidence-bundle event (#592)
    Co-authored-by: Jagadeesh Kumar <jagadeesh.kumar@transformuk.com>

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HomeOfficeCaseNotificationsHandler.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
+
+import static java.util.Objects.requireNonNull;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.FeatureToggler;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.HomeOfficeApi;
+
+
+@Component
+public class HomeOfficeCaseNotificationsHandler implements PreSubmitCallbackHandler<AsylumCase> {
+
+    private final FeatureToggler featureToggler;
+    private final HomeOfficeApi<AsylumCase> homeOfficeApi;
+
+    private static final String HO_NOTIFICATION_FEATURE = "home-office-notification-feature";
+
+    public HomeOfficeCaseNotificationsHandler(
+        FeatureToggler featureToggler,
+        HomeOfficeApi<AsylumCase> homeOfficeApi) {
+        this.featureToggler = featureToggler;
+        this.homeOfficeApi = homeOfficeApi;
+    }
+
+    public boolean canHandle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+               && callback.getEvent() == Event.REQUEST_RESPONDENT_EVIDENCE
+               && featureToggler.getValue(HO_NOTIFICATION_FEATURE, false);
+    }
+
+
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCaseWithHomeOfficeData = homeOfficeApi.call(callback);
+
+        return new PreSubmitCallbackResponse<>(asylumCaseWithHomeOfficeData);
+    }
+}
