3054a7c2aed81f73ab1c7240cda4efc63b059ad1
Lee Philip <55591679+Lee365@users.noreply.github.com>
2021-01-07 14:53:31 +0000

    RIA-3316_no_hearing_date (#633)
    Co-authored-by: Lee Philip <Lee365>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HomeOfficeCaseNotificationsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HomeOfficeCaseNotificationsHandler.java
@@ -41,7 +41,8 @@ public class HomeOfficeCaseNotificationsHandler implements PreSubmitCallbackHand
                     Event.REQUEST_RESPONDENT_EVIDENCE,
                     Event.REQUEST_RESPONDENT_REVIEW,
                     Event.LIST_CASE,
-                    Event.EDIT_CASE_LISTING
+                    Event.EDIT_CASE_LISTING,
+                    Event.ADJOURN_HEARING_WITHOUT_DATE
                 ).contains(callback.getEvent())
                && featureToggler.getValue(HO_NOTIFICATION_FEATURE, false);
     }
