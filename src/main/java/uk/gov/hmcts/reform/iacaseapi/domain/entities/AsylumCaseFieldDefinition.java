200e8727d31c29ffa2f3b809cef7498245869694
Lee Philip <55591679+Lee365@users.noreply.github.com>
2021-01-14 10:57:38 +0000

    RIA-3305_hearing_bundle_ready (#638)
    Co-authored-by: Lee Philip <Lee365>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCaseFieldDefinition.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCaseFieldDefinition.java
@@ -1015,6 +1015,9 @@ public enum AsylumCaseFieldDefinition {
     HOME_OFFICE_ADJOURN_WITHOUT_DATE_INSTRUCT_STATUS(
         "homeOfficeAdjournWithoutDateInstructStatus", new TypeReference<String>() {}),
 
+    HOME_OFFICE_HEARING_BUNDLE_READY_INSTRUCT_STATUS(
+        "homeOfficeHearingBundleReadyInstructStatus", new TypeReference<String>() {}),
+
     HOME_OFFICE_APPEAL_DECIDED_INSTRUCT_STATUS(
         "homeOfficeAppealDecidedInstructStatus", new TypeReference<String>() {}),
 
