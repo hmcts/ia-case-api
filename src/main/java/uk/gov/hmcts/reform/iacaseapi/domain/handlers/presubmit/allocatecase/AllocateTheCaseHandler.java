05e11a0e61081cc09f94d0de895bdfb4d72f61cc
david.crespo <david.crespo@hmcts.net>
2021-01-14 16:50:32 +0100

    implement AC9: clear up caseworker list

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/allocatecase/AllocateTheCaseHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/allocatecase/AllocateTheCaseHandler.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.allocatecase;
 
-import static java.util.Objects.requireNonNull;
-
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
@@ -12,6 +10,9 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallb
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacaseapi.domain.service.FeatureToggler;
 import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.RoleAssignmentService;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CASEWORKER_NAME_LIST;
 
 @Component
 public class AllocateTheCaseHandler implements PreSubmitCallbackHandler<AsylumCase> {
@@ -42,6 +43,8 @@ public class AllocateTheCaseHandler implements PreSubmitCallbackHandler<AsylumCa
         CaseDetails<AsylumCase> caseDetails = callback.getCaseDetails();
 
         roleAssignmentService.assignRole(caseDetails);
+
+        caseDetails.getCaseData().clear(CASEWORKER_NAME_LIST);
 
         return new PreSubmitCallbackResponse<>(caseDetails.getCaseData());
     }
