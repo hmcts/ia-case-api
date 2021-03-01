f044ee1ae214ac7704c36f751aca72439afd0c6a
david.crespo <david.crespo@hmcts.net>
2021-03-01 18:18:59 +0000

    fix hystrix configuration to enable/disable per client basis

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/DisableHystrixFeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/DisableHystrixFeignConfiguration.java
@@ -2,10 +2,8 @@ package uk.gov.hmcts.reform.iacaseapi.infrastructure.config;
 
 import feign.Feign;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
 public class DisableHystrixFeignConfiguration {
 
     @Bean
