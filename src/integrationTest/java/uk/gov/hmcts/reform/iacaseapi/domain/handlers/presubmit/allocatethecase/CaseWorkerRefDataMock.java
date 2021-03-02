f58e32e9deaf846641a659d70407c814a093e2b5
david.crespo <david.crespo@hmcts.net>
2021-03-02 10:32:14 +0000

    refactor IT to reuse the wiremock configuration in place

--- src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/allocatethecase/CaseWorkerRefDataMock.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/allocatethecase/CaseWorkerRefDataMock.java
@@ -1,10 +1,14 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.allocatethecase;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.matching.RequestPatternBuilder.newRequestPattern;
 import static java.nio.charset.Charset.defaultCharset;
 import static org.springframework.util.StreamUtils.copyToString;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.http.RequestMethod;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import java.io.IOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,20 +22,28 @@ public final class CaseWorkerRefDataMock {
     }
 
     public static void setup200MockResponse(WireMockServer mockService) throws IOException {
-        mockService.stubFor(WireMock.post(WireMock.urlEqualTo(URL))
-            .willReturn(WireMock.aResponse()
+        mockService.addStubMapping(new StubMapping(
+            newRequestPattern(RequestMethod.POST, urlEqualTo(URL))
+                .build(),
+            aResponse()
                 .withStatus(HttpStatus.OK.value())
                 .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .withBody(
                     copyToString(
                         CaseWorkerRefDataMock.class.getClassLoader().getResourceAsStream("case-worker-ref-data-response.json"),
-                        defaultCharset()))));
+                        defaultCharset()))
+                .build()
+        ));
     }
 
     public static void setup404MockResponse(WireMockServer mockService) {
-        mockService.stubFor(WireMock.post(WireMock.urlEqualTo(URL))
-            .willReturn(WireMock.aResponse()
+        mockService.addStubMapping(new StubMapping(
+            newRequestPattern(RequestMethod.POST, urlEqualTo(URL))
+                .build(),
+            aResponse()
                 .withStatus(HttpStatus.NOT_FOUND.value())
-            ));
+                .build()
+        ));
     }
+
 }
