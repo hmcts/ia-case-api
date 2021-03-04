8a428d8dff70004f9f57fa3bb5621d74438e7fcc
Vamshidhar Chitti <indvamshi@gmail.com>
2021-03-04 15:26:18 +0000

    reverted test

--- src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/entities/AssignmentTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/entities/AssignmentTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.iacaseapi.Application;
+import uk.gov.hmcts.reform.iacaseapi.component.testutils.SpringBootIntegrationTest;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.roleassignment.ActorIdType;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.roleassignment.Assignment;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.roleassignment.Classification;
@@ -14,10 +19,12 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.roleassignment.RoleName;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.roleassignment.RoleType;
 
 
-public class AssignmentTest {
+public class AssignmentTest extends SpringBootIntegrationTest {
 
+    @Autowired
     private ObjectMapper objectMapper;
 
+    @Test
     void deserialize_as_expected_for_unknown_values() throws IOException {
         String jsonContent = "{\"id\":\"id\","
                 + "\"authorisations\":[],"
