d1f912f429aba9da7f537d77572a45a382c394a5
ruban72 <ruban72@hotmail.com>
2021-03-09 13:10:55 +0000

    TA-529: Rewrite Idam Consumer test for new provider

---
+++ src/contractTest/java/uk/gov/hmcts/reform/iacaseapi/consumer/IdamApiConsumerTest.java
@@ -0,0 +1,80 @@
+package uk.gov.hmcts.reform.iacaseapi.consumer;
+
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslJsonRootValue;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import au.com.dius.pact.core.model.annotations.PactFolder;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.IdamApi;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.model.idam.UserInfo;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@ExtendWith(PactConsumerTestExt.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@PactFolder("pacts")
+@PactTestFor(providerName = "ia_caseApi", port = "8892")
+@ContextConfiguration(classes = {IdamConsumerApplication.class})
+@TestPropertySource(locations={"classpath:application.properties"})
+public class IdamApiConsumerTest {
+
+    @Autowired
+    IdamApi idamApi;
+    private static final String AUTH_TOKEN = "Bearer someAuthorizationToken";
+
+    @Pact(provider = "idamApi_oidc", consumer = "ia_caseApi")
+    public RequestResponsePact generatePactFragment(PactDslWithProvider builder) throws JSONException, IOException {
+
+        return builder
+            .given("userinfo is requested")
+            .uponReceiving("A request for a UserInfo")
+            .path("/o/userinfo")
+            .method("GET")
+            .matchHeader("Authorization", AUTH_TOKEN)
+            .matchHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .willRespondWith()
+            .status(200)
+            .body(createUserDetailsResponse())
+            .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "generatePactFragment")
+    public void verifyIdamUserDetailsRolesPact() {
+        UserInfo userInfo = idamApi.userInfo(AUTH_TOKEN);
+        assertEquals("User is not Case Officer", "ia-caseofficer@fake.hmcts.net", userInfo.getEmail());
+    }
+
+
+    private PactDslJsonBody createUserDetailsResponse() {
+        PactDslJsonArray array = new PactDslJsonArray().stringValue("caseofficer-ia");
+
+        return new PactDslJsonBody()
+            .stringType("uid", "1111-2222-3333-4567")
+            .stringValue("sub", "ia-caseofficer@fake.hmcts.net")
+            .stringValue("givenName", "Case")
+            .stringValue("familyName", "Officer")
+            .minArrayLike("roles", 1, PactDslJsonRootValue.stringType("caseworker-ia-legalrep-solicitor"), 1)
+            .stringType("IDAM_ADMIN_USER");
+    }
+
+}
