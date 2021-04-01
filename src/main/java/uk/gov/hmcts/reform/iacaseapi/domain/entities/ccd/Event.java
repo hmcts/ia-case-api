49f51128913ce6817a01eed66e6835bf62831f7d
Moussa Kayad <mkayad@gmail.com>
2021-04-01 11:04:30 +0100

    RIA-4104 enable callback ccdSubmitted endpoint to ia-case-notificatio… (#818)
    * RIA-4104 enable callback ccdSubmitted endpoint to ia-case-notifications-api
    
    * Adjusting callback to the latest LLD
    
    Co-authored-by: makowalski <marcin.kowalski.it.consulting@gmail.com>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/Event.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/Event.java
@@ -100,6 +100,7 @@ public enum Event {
     ALLOCATE_THE_CASE("allocateTheCase"),
     RECORD_REMISSION_DECISION("recordRemissionDecision"),
     MANAGE_FEE_UPDATE("manageFeeUpdate"),
+    NOC_REQUEST("nocRequest"),
 
     @JsonEnumDefaultValue
     UNKNOWN("unknown");
