c9646b01b1694366db9efbf14ffbab853247545a
david.crespo <david.crespo@hmcts.net>
2021-04-09 11:59:55 +0100

    make enum more flexible when deserializing

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/roleassignment/Assignment.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/roleassignment/Assignment.java
@@ -29,7 +29,7 @@ public class Assignment {
     private final Classification classification;
     private final GrantType grantType;
     private final Boolean readOnly;
-    private final Map<Attributes, String> attributes;
+    private final Map<String, String> attributes;
 
     @JsonCreator
     public Assignment(@JsonProperty("id") String id,
@@ -43,7 +43,7 @@ public class Assignment {
                       @JsonProperty("classification") Classification classification,
                       @JsonProperty("grantType") GrantType grantType,
                       @JsonProperty("readOnly") Boolean readOnly,
-                      @JsonProperty("attributes") Map<Attributes, String> attributes) {
+                      @JsonProperty("attributes") Map<String, String> attributes) {
         this.id = id;
         this.created = created;
         this.authorisations = authorisations;
@@ -102,7 +102,7 @@ public class Assignment {
         return readOnly;
     }
 
-    public Map<Attributes, String> getAttributes() {
+    public Map<String, String> getAttributes() {
         return attributes;
     }
 }
