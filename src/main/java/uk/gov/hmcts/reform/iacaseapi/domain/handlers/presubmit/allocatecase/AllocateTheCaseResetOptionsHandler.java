4e3c929da6afd25058cc1a71759a6cd66e08a679
david.crespo <david.crespo@hmcts.net>
2021-04-14 14:17:28 +0100

    rwa-492 bug fix
    remove the clear the field line from the handlers to make the canHandle methods work accordingly
    add the clear the field line in a new about to start handler

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/allocatecase/AllocateTheCaseResetOptionsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/allocatecase/AllocateTheCaseResetOptionsHandler.java
@@ -1,46 +1,31 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.allocatecase;
 
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CASE_WORKER_NAME;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.ALLOCATE_THE_CASE_TO;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.UserDetails;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacaseapi.domain.service.FeatureToggler;
-import uk.gov.hmcts.reform.iacaseapi.domain.service.RoleAssignmentService;
 
 @Component
-public class AllocateTheCaseToMeHandler implements PreSubmitCallbackHandler<AsylumCase> {
+public class AllocateTheCaseResetOptionsHandler implements PreSubmitCallbackHandler<AsylumCase> {
 
-    private final RoleAssignmentService roleAssignmentService;
-    private final UserDetails userDetails;
     private final FeatureToggler featureToggler;
-    private final AllocateTheCaseService allocateTheCaseService;
 
-    public AllocateTheCaseToMeHandler(
-        RoleAssignmentService roleAssignmentService,
-        UserDetails userDetails,
-        FeatureToggler featureToggler,
-        AllocateTheCaseService allocateTheCaseService
-    ) {
-        this.roleAssignmentService = roleAssignmentService;
-        this.userDetails = userDetails;
+    public AllocateTheCaseResetOptionsHandler(FeatureToggler featureToggler) {
         this.featureToggler = featureToggler;
-        this.allocateTheCaseService = allocateTheCaseService;
     }
 
     public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
         requireNonNull(callbackStage, "callbackStage must not be null");
         requireNonNull(callback, "callback must not be null");
 
-        return !allocateTheCaseService.isAllocateToCaseWorkerOption(callback.getCaseDetails().getCaseData())
-            && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_START
             && callback.getEvent() == Event.ALLOCATE_THE_CASE
             && featureToggler.getValue("allocate-a-case-feature", false);
     }
@@ -51,12 +36,8 @@ public class AllocateTheCaseToMeHandler implements PreSubmitCallbackHandler<Asyl
             throw new IllegalStateException("Cannot handle callback");
         }
 
-        CaseDetails<AsylumCase> caseDetails = callback.getCaseDetails();
-        AsylumCase caseData = caseDetails.getCaseData();
-
-        roleAssignmentService.assignRole(caseDetails.getId(), userDetails.getId());
-
-        caseData.write(CASE_WORKER_NAME, String.format("%s %s", userDetails.getForename(), userDetails.getSurname()));
+        AsylumCase caseData = callback.getCaseDetails().getCaseData();
+        caseData.clear(ALLOCATE_THE_CASE_TO);
 
         return new PreSubmitCallbackResponse<>(caseData);
     }
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/allocatecase/AllocateTheCaseToCaseWorkerHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/allocatecase/AllocateTheCaseToCaseWorkerHandler.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.allocatecase;
 
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.ALLOCATE_THE_CASE_TO;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CASE_WORKER_LOCATION_LIST;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CASE_WORKER_NAME;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CASE_WORKER_NAME_LIST;
@@ -65,7 +64,6 @@ public class AllocateTheCaseToCaseWorkerHandler implements PreSubmitCallbackHand
 
         caseData.clear(CASE_WORKER_NAME_LIST);
         caseData.clear(CASE_WORKER_LOCATION_LIST);
-        caseData.clear(ALLOCATE_THE_CASE_TO);
 
         return new PreSubmitCallbackResponse<>(caseData);
     }
