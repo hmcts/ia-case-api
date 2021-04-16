41f96e99fc6f350bdbaa2ab6a223afcfe9431fc6
makowalski <marcin.kowalski.it.consulting@gmail.com>
2021-04-16 17:15:49 +0100

    fix fortify issues (#868)

--- wiremock/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Run a wiremock as a standalone dependency to represent Ref Data
-#
-# See ./mappings for stubbing configuration
-#
-java -jar wiremock-standalone-2.25.1.jar --port 8990 --verbose
