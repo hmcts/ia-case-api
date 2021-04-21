99adf62f6c7b270f424cee8a63f67242f8db47ba
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2021-04-21 12:26:16 +0100

    RWA-497: BaseLocation BugFix Newcastle (#866)

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/CaseManagementLocationService.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/CaseManagementLocationService.java
@@ -10,22 +10,18 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.Region;
 @Service
 public class CaseManagementLocationService {
 
-    private static final String LOCATION_WITH_NO_CODE = "Newcastle";
-
     public CaseManagementLocation getCaseManagementLocation(String staffLocationName) {
         Optional<BaseLocation> baseLocation = getBaseLocation(staffLocationName);
         return baseLocation.map(location ->
             new CaseManagementLocation(Region.NATIONAL, location))
-            .orElseGet(() -> new CaseManagementLocation(Region.NATIONAL, null));
+            .orElseGet(() -> new CaseManagementLocation(Region.NATIONAL, BaseLocation.TAYLOR_HOUSE));
     }
 
     private Optional<BaseLocation> getBaseLocation(String staffLocationName) {
-        if (!LOCATION_WITH_NO_CODE.equals(staffLocationName)) {
-            String fromStaffLocationNameToBaseLocationEnumName =
-                StringUtils.upperCase(staffLocationName).replace(" ", "_");
-            BaseLocation baseLocation = BaseLocation.valueOf(fromStaffLocationNameToBaseLocationEnumName);
-            return Optional.of(baseLocation);
-        }
-        return Optional.empty();
+        String fromStaffLocationNameToBaseLocationEnumName =
+            StringUtils.upperCase(staffLocationName).replace(" ", "_");
+        BaseLocation baseLocation = BaseLocation.valueOf(fromStaffLocationNameToBaseLocationEnumName);
+        return Optional.of(baseLocation);
+
     }
 }
