8c792dd8032346bb9454f86b953f5e1b0ccd56fd
Srihari-Nakkala <51116795+Srihari-Nakkala@users.noreply.github.com>
2021-06-30 15:58:27 +0100

    Update security script (#922)
    Co-authored-by: srihari <srihari.nakkalar@clarks.com>

--- security.sh
+++ security.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 #setting encoding for Python 2 / 3 compatibilities
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-
+export TEST_URL=http://ia-case-api-aat.service.core-compute-aat.internal
 echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
-cat zap.out
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o api-report.html -f html
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /Zap/api-report.html -f html
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
 
 cp report.json functional-output/
-cp api-report.html functional-output/
+cp /Zap/api-report.html functional-output/
 
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
