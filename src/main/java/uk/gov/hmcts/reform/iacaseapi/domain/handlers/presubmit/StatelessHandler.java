85643380eca836dabed46b38daad6c56651dbf51
Lee Philip <55591679+Lee365@users.noreply.github.com>
2021-09-22 12:46:45 +0100

    RIA-4711 (#964)

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/StatelessHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/StatelessHandler.java
@@ -25,7 +25,8 @@ public class StatelessHandler implements PreSubmitCallbackHandler<AsylumCase> {
 
         return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                 && (callback.getEvent() == Event.START_APPEAL
-                || callback.getEvent() == Event.EDIT_APPEAL);
+                || callback.getEvent() == Event.EDIT_APPEAL
+                || callback.getEvent() == Event.EDIT_APPEAL_AFTER_SUBMIT);
     }
 
     @Override
