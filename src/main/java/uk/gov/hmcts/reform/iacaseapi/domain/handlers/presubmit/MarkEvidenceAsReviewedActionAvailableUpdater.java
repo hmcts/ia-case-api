d88aaf0ee399ada1562af3ae775ebae08f8dfea6
Lee Philip <55591679+Lee365@users.noreply.github.com>
2021-11-18 10:37:31 +0000

    RIA-4747 Review additional evidence event (#987)

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/MarkEvidenceAsReviewedActionAvailableUpdater.java
@@ -0,0 +1,59 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.MARK_EVIDENCE_AS_REVIEWED_ACTION_AVAILABLE;
+
+import java.util.Arrays;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+
+@Component
+public class MarkEvidenceAsReviewedActionAvailableUpdater implements PreSubmitCallbackHandler<AsylumCase> {
+
+    public boolean canHandle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
+    }
+
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        CaseDetails<AsylumCase> caseDetails =
+            callback
+                .getCaseDetails();
+
+        AsylumCase asylumCase = caseDetails.getCaseData();
+
+        if (Arrays.asList(
+            State.CASE_UNDER_REVIEW,
+            State.RESPONDENT_REVIEW,
+            State.SUBMIT_HEARING_REQUIREMENTS,
+            State.LISTING,
+            State.PREPARE_FOR_HEARING,
+            State.FINAL_BUNDLING
+        ).contains(caseDetails.getState())) {
+            asylumCase.write(MARK_EVIDENCE_AS_REVIEWED_ACTION_AVAILABLE, YesOrNo.YES);
+        } else {
+            asylumCase.write(MARK_EVIDENCE_AS_REVIEWED_ACTION_AVAILABLE, YesOrNo.NO);
+        }
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
