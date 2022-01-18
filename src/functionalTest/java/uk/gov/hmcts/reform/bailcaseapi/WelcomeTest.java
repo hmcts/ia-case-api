2cd92cd29bf02cf72d0c63e3fdf46b350fc30ae7
Neha Aggarwal <neha.aggarwal@version1.com>
2022-01-18 15:48:35 +0000

    Added functional tests and unit tests for Welcome Controller

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/bailcaseapi/WelcomeTest.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.bailcaseapi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import io.restassured.*;
+import net.serenitybdd.rest.SerenityRest;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("functional")
+public class WelcomeTest {
+
+    @Value("${targetInstance}") private String targetInstance;
+
+    @Test
+    public void should_display_welcome_message_on_root_request_200_SC() {
+
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+
+        String response = SerenityRest.given()
+            .when()
+            .get("/")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .and()
+            .extract().body().asString();
+
+        assertThat(response).contains("Welcome to the Bail case API");
+    }
+}
