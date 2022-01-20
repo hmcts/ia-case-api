9810b96354285e470ba127e5c00b313185e98067
emma-v1 <95303387+emma-v1@users.noreply.github.com>
2022-01-20 20:30:45 +0000

    RIA-5317 Inital pipeline commit (#5)
    * RIA-5317 inital pipeline
    
    * RIA-5317 refactoring sprint boot template
    
    * RIA_5317 removing integration param from jacoco and updating default values from spring boot template
    
    * RIA-5317 reverting jar name to template for now

--- bin/init.sh
+++ bin/init.sh
@@ -18,25 +18,18 @@ pushd $(dirname "$0")/.. > /dev/null
 
 slug="$product_name-$component_name"
 
-declare -a files_with_port=(.env Dockerfile README.md src/main/resources/application.yaml charts/rpe-spring-boot-template/values.yaml)
-declare -a files_with_slug=(build.gradle docker-compose.yml Dockerfile README.md ./infrastructure/main.tf ./src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java charts/rpe-spring-boot-template/Chart.yaml)
-
-# Replace in CNP file
-for i in "Jenkinsfile_template"
-do
-  perl -i -pe "s/rpe/$product_name/g" ${i}
-  perl -i -pe "s/demo/$component_name/g" ${i}
-done
+declare -a files_with_port=(.env Dockerfile README.md src/main/resources/application.yaml charts/ia-bail-case-api/values.yaml)
+declare -a files_with_slug=(build.gradle docker-compose.yml Dockerfile README.md ./infrastructure/main.tf ./src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java charts/ia-bail-case-api/Chart.yaml)
 
 # Replace image repo
-for i in "charts/rpe-spring-boot-template/values.yaml"
+for i in "charts/ia-bail-case-api/values.yaml"
 do
   perl -i -pe "s/rpe/$product_name/g" ${i}
   perl -i -pe "s/spring-boot-template/$component_name/g" ${i}
 done
 
 #update maintainer name
-for i in "charts/rpe-spring-boot-template/Chart.yaml"
+for i in "charts/ia-bail-case-api/Chart.yaml"
 do
   perl -i -pe "s/rpe/$product_name/g" ${i}
 done
@@ -63,15 +56,9 @@ done
 find ./src -type f -print0 | xargs -0 perl -i -pe "s/reform.demo/reform.$package/g"
 perl -i -pe "s/reform.demo/reform.$package/g" build.gradle
 
-# Rename charts directory
-git mv charts/rpe-spring-boot-template charts/${slug}
-
 # Rename directory to provided package name
 git mv src/integrationTest/java/uk/gov/hmcts/reform/demo/ src/integrationTest/java/uk/gov/hmcts/reform/${package}
 git mv src/main/java/uk/gov/hmcts/reform/demo/ src/main/java/uk/gov/hmcts/reform/${package}
-
-# Rename CNP file
-git mv Jenkinsfile_template Jenkinsfile_CNP
 
 declare -a headers_to_delete=("Purpose" "What's inside" "Plugins" "Setup" "Hystrix")
 
