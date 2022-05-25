f627a3064be14573bb062ea07df03924cc10995c
indu2019 <indu.chaturvedi@hmcts.net>
2022-05-25 10:13:35 +0100

    RIA-5679-Update final commit

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/SecurityConfiguration.java
@@ -10,12 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
@@ -27,17 +27,18 @@ import uk.gov.hmcts.reform.iacaseapi.infrastructure.security.SpringAuthorizedRol
 @ConfigurationProperties(prefix = "security")
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration  {
 
     private final List<String> anonymousPaths = new ArrayList<>();
-    private final Map<String, List<Event>> roleEventAccess = new HashMap<>();
 
-    private final Converter<Jwt, Collection<GrantedAuthority>> idamAuthoritiesConverter;
+    private final Converter<Jwt, Collection<GrantedAuthority>> authoritiesConverter;
     private final ServiceAuthFilter serviceAuthFiler;
 
-    public SecurityConfiguration(Converter<Jwt, Collection<GrantedAuthority>> idamAuthoritiesConverter,
-                                 ServiceAuthFilter serviceAuthFiler) {
-        this.idamAuthoritiesConverter = idamAuthoritiesConverter;
+    public SecurityConfiguration(
+            Converter<Jwt, Collection<GrantedAuthority>> authoritiesConverter,
+            ServiceAuthFilter serviceAuthFiler
+    ) {
+        this.authoritiesConverter = authoritiesConverter;
         this.serviceAuthFiler = serviceAuthFiler;
     }
 
@@ -45,21 +46,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return anonymousPaths;
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().mvcMatchers(
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().mvcMatchers(
             anonymousPaths
                 .stream()
                 .toArray(String[]::new)
         );
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(idamAuthoritiesConverter);
+        //jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(idamAuthoritiesConverter);
 
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
 
         http
             .addFilterBefore(serviceAuthFiler, AbstractPreAuthenticatedProcessingFilter.class)
@@ -78,6 +80,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .and()
             .oauth2Client();
+
+        return http.build();
     }
 
     @Bean
@@ -89,13 +93,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public CcdEventAuthorizor getCcdEventAuthorizor(AuthorizedRolesProvider authorizedRolesProvider) {
 
         return new CcdEventAuthorizor(
-            ImmutableMap.copyOf(roleEventAccess),
+            ImmutableMap.copyOf(getRoleEventAccess()),
             authorizedRolesProvider
         );
     }
 
     public Map<String, List<Event>> getRoleEventAccess() {
-        return roleEventAccess;
+        return getRoleEventAccess();
     }
 
 }
