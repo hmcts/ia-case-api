6553e9ac16b43b33077901e21b7c5041456e018d
Mark Brooks <mark.brooks@hmcts.net>
2022-09-02 16:54:56 +0100

    RIA-6106 Rafactoring and unit tests

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/PinInPostDetails.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/PinInPostDetails.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.iacaseapi.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
@@ -12,12 +11,13 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 public class PinInPostDetails {
 
     private final String accessCode;
-    private final LocalDate expiryDate;
+
+    private final String expiryDate;
     private YesOrNo pinUsed;
 
     @JsonCreator
     public PinInPostDetails(@JsonProperty("accessCode") String accessCode,
-                            @JsonProperty("expiryDate") LocalDate expiryDate,
+                            @JsonProperty("expiryDate") String expiryDate,
                             @JsonProperty("pinUsed") YesOrNo pinUsed) {
         this.accessCode = accessCode;
         this.expiryDate = expiryDate;
