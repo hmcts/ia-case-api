a7bbed3fb7af626f7c9839b55f27a8629dfec765
Mark Brooks <mark.brooks@hmcts.net>
2022-09-26 11:32:37 +0100

    RIA-6046 Appellant creates account to access case

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PinInPostActivated.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+
+@Component
+public class PinInPostActivated implements PreSubmitCallbackHandler<AsylumCase> {
+    @Override
+    public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+                && callback.getEvent() == Event.PIP_ACTIVATION;
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<AsylumCase> handle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+        asylumCase.write(AsylumCaseFieldDefinition.JOURNEY_TYPE, JourneyType.AIP);
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PinInPostActivatedTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PinInPostActivatedTest.java
@@ -1,11 +1,5 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
-
-import java.time.LocalDate;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,16 +15,20 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
-public class PinInPostGeneratorTest {
+public class PinInPostActivatedTest {
 
-    private static final long ACCESS_CODE_EXPIRY_DAYS = 90;
-
-    private PinInPostGenerator pinInPostGenerator;
+    private PinInPostActivated pinInPostActivated;
 
     @Mock
     private Callback<AsylumCase> callback;
@@ -39,7 +37,7 @@ public class PinInPostGeneratorTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        pinInPostGenerator = new PinInPostGenerator(ACCESS_CODE_EXPIRY_DAYS);
+        pinInPostActivated = new PinInPostActivated();
     }
 
     @Test
@@ -48,23 +46,17 @@ public class PinInPostGeneratorTest {
 
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
-        when(callback.getEvent()).thenReturn(Event.REMOVE_LEGAL_REPRESENTATIVE);
+        when(callback.getEvent()).thenReturn(Event.PIP_ACTIVATION);
 
-        PreSubmitCallbackResponse<AsylumCase> response = pinInPostGenerator.handle(
+        PreSubmitCallbackResponse<AsylumCase> response = pinInPostActivated.handle(
                 PreSubmitCallbackStage.ABOUT_TO_SUBMIT,
                 callback
         );
 
         assertEquals(1, response.getData().size());
 
-        Optional<PinInPostDetails> details = response.getData().read(AsylumCaseFieldDefinition.APPELLANT_PIN_IN_POST, PinInPostDetails.class);
-
-        assertTrue(details.isPresent());
-        assertNotNull(details.get().getAccessCode());
-        assertNotNull(details.get().getExpiryDate());
-        assertNotNull(details.get().getPinUsed());
-        assertEquals(LocalDate.now().plusDays(ACCESS_CODE_EXPIRY_DAYS).toString(), details.get().getExpiryDate());
-        assertEquals(YesOrNo.NO, details.get().getPinUsed());
+        Optional<JourneyType> details = response.getData().read(AsylumCaseFieldDefinition.JOURNEY_TYPE, JourneyType.class);
+        assertEquals(JourneyType.AIP, details.get());
     }
 
     @Test
@@ -77,12 +69,10 @@ public class PinInPostGeneratorTest {
 
             for (PreSubmitCallbackStage callbackStage : PreSubmitCallbackStage.values()) {
 
-                boolean canHandle = pinInPostGenerator.canHandle(callbackStage, callback);
+                boolean canHandle = pinInPostActivated.canHandle(callbackStage, callback);
 
                 if (callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
-                        && (event == Event.REMOVE_REPRESENTATION
-                            || event == Event.REMOVE_LEGAL_REPRESENTATIVE)) {
-
+                    && event == Event.PIP_ACTIVATION) {
                     assertTrue(canHandle);
                 } else {
                     assertFalse(canHandle);
@@ -91,5 +81,4 @@ public class PinInPostGeneratorTest {
             reset(callback);
         }
     }
-
 }
