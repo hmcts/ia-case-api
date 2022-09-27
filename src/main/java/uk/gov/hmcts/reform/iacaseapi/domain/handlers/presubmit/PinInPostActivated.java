30eab62584c8b1207bb7ec38cc65c6cbec860420
Mark Brooks <mark.brooks@hmcts.net>
2022-09-27 16:09:47 +0100

    RIA-6049 Appellant views appeal details

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PinInPostActivated.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PinInPostActivated.java
@@ -1,17 +1,28 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
+import java.util.Arrays;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.UserDetailsProvider;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.*;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 
 @Component
 public class PinInPostActivated implements PreSubmitCallbackHandler<AsylumCase> {
+
+    private UserDetailsProvider userDetailsProvider;
+
+    public PinInPostActivated(UserDetailsProvider userDetailsProvider) {
+        this.userDetailsProvider = userDetailsProvider;
+    }
+
     @Override
     public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
         return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
@@ -25,7 +36,28 @@ public class PinInPostActivated implements PreSubmitCallbackHandler<AsylumCase> 
         }
 
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
-        asylumCase.write(AsylumCaseFieldDefinition.JOURNEY_TYPE, JourneyType.AIP);
+        updateJourneyType(asylumCase);
+        updateSubscription(asylumCase);
         return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+
+    private void updateJourneyType(AsylumCase asylumCase) {
+        asylumCase.write(AsylumCaseFieldDefinition.JOURNEY_TYPE, JourneyType.AIP);
+    }
+
+    private void updateSubscription(AsylumCase asylumCase) {
+        Optional<ContactPreference> contactPreference = asylumCase.read(AsylumCaseFieldDefinition.CONTACT_PREFERENCE);
+        Optional<String> mobileNumber = asylumCase.read(AsylumCaseFieldDefinition.MOBILE_NUMBER);
+        Optional<String> email = asylumCase.read(AsylumCaseFieldDefinition.EMAIL);
+
+        Subscriber subscriber = new Subscriber(
+            SubscriberType.APPELLANT,
+            email.orElse(null),
+            contactPreference.filter(p -> p.equals(ContactPreference.WANTS_EMAIL)).map(p -> YesOrNo.YES).orElse(YesOrNo.NO),
+            mobileNumber.orElse(null),
+            contactPreference.filter(p -> p.equals(ContactPreference.WANTS_SMS)).map(p -> YesOrNo.YES).orElse(YesOrNo.NO));
+
+        asylumCase.write(AsylumCaseFieldDefinition.SUBSCRIPTIONS, Arrays.asList(
+            new IdValue<>(userDetailsProvider.getUserDetails().getId(), subscriber)));
     }
 }
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PinInPostActivatedTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PinInPostActivatedTest.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,49 +14,108 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.UserDetailsProvider;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.*;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
 public class PinInPostActivatedTest {
 
+    private static final String USER_ID = "userId";
+
     private PinInPostActivated pinInPostActivated;
+
+    private AsylumCase asylumCase;
 
     @Mock
     private Callback<AsylumCase> callback;
     @Mock private CaseDetails<AsylumCase> caseDetails;
 
+    @Mock private UserDetailsProvider userDetailsProvider;
+
+    @Mock private UserDetails userDetails;
+
 
     @BeforeEach
     public void setUp() throws Exception {
-        pinInPostActivated = new PinInPostActivated();
-    }
-
-    @Test
-    public void appellantPinInPost_is_generated() {
-        AsylumCase asylumCase = new AsylumCase();
-
+        asylumCase = new AsylumCase();
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
         when(callback.getEvent()).thenReturn(Event.PIP_ACTIVATION);
+        when(userDetails.getId()).thenReturn(USER_ID);
+        when(userDetailsProvider.getUserDetails()).thenReturn(userDetails);
+        pinInPostActivated = new PinInPostActivated(userDetailsProvider);
+    }
+
+    @Test
+    public void journeyType_is_updated() {
+        PreSubmitCallbackResponse<AsylumCase> response = pinInPostActivated.handle(
+                PreSubmitCallbackStage.ABOUT_TO_SUBMIT,
+                callback
+        );
+
+        Optional<JourneyType> details = response.getData().read(AsylumCaseFieldDefinition.JOURNEY_TYPE, JourneyType.class);
+        assertEquals(JourneyType.AIP, details.get());
+    }
+
+    @Test
+    public void subscription_should_added_wantsEmail() {
+        asylumCase.write(AsylumCaseFieldDefinition.EMAIL,"appellant@examples.com");
+        asylumCase.write(AsylumCaseFieldDefinition.CONTACT_PREFERENCE, ContactPreference.WANTS_EMAIL);
 
         PreSubmitCallbackResponse<AsylumCase> response = pinInPostActivated.handle(
                 PreSubmitCallbackStage.ABOUT_TO_SUBMIT,
                 callback
         );
 
-        assertEquals(1, response.getData().size());
+        Optional<List<IdValue<Subscriber>>> subscriptions = response.getData().read(AsylumCaseFieldDefinition.SUBSCRIPTIONS);
 
-        Optional<JourneyType> details = response.getData().read(AsylumCaseFieldDefinition.JOURNEY_TYPE, JourneyType.class);
-        assertEquals(JourneyType.AIP, details.get());
+        assertTrue(subscriptions.isPresent());
+        assertEquals(1, subscriptions.get().size());
+        assertEquals(USER_ID, subscriptions.get().get(0).getId());
+
+        Subscriber expectedSubscriber = new Subscriber(
+                SubscriberType.APPELLANT,
+                "appellant@examples.com",
+                YesOrNo.YES,
+                null,
+                YesOrNo.NO);
+        assertThat(subscriptions.get().get(0).getValue()).usingRecursiveComparison().isEqualTo(expectedSubscriber);
+    }
+
+    @Test
+    public void subscription_should_added_wantsSms() {
+        asylumCase.write(AsylumCaseFieldDefinition.EMAIL, Optional.of("appellant@examples.com"));
+        asylumCase.write(AsylumCaseFieldDefinition.MOBILE_NUMBER, Optional.of("01234123123"));
+        asylumCase.write(AsylumCaseFieldDefinition.CONTACT_PREFERENCE, Optional.of(ContactPreference.WANTS_SMS));
+
+        PreSubmitCallbackResponse<AsylumCase> response = pinInPostActivated.handle(
+                PreSubmitCallbackStage.ABOUT_TO_SUBMIT,
+                callback
+        );
+
+        Optional<List<IdValue<Subscriber>>> subscriptions = response.getData().read(AsylumCaseFieldDefinition.SUBSCRIPTIONS);
+
+        assertTrue(subscriptions.isPresent());
+        assertEquals(1, subscriptions.get().size());
+        assertEquals(USER_ID, subscriptions.get().get(0).getId());
+
+        Subscriber expectedSubscriber = new Subscriber(
+                SubscriberType.APPELLANT,
+                "appellant@examples.com",
+                YesOrNo.NO,
+                "01234123123",
+                YesOrNo.YES);
+        assertThat(subscriptions.get().get(0).getValue()).usingRecursiveComparison().isEqualTo(expectedSubscriber);
     }
 
     @Test
