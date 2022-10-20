551abe60a7d74ed3930c7ccb22a93d6194682c4f
Marcellofabbri <marcello.fabbri@hmcts.net>
2022-10-20 19:20:28 +0100

    RIA-6232 Add manageCaseTTL trigger when events fail to start the clock a second time

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/RemoveAppealFromOnlineConfirmation.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/RemoveAppealFromOnlineConfirmation.java
@@ -4,13 +4,25 @@ import static java.util.Objects.*;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.TTL;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PostSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.ccddataservice.TimeToLiveDataService;
 
 @Component
 public class RemoveAppealFromOnlineConfirmation implements PostSubmitCallbackHandler<AsylumCase> {
+
+    private final TimeToLiveDataService timeToLiveDataService;
+
+    public RemoveAppealFromOnlineConfirmation(TimeToLiveDataService timeToLiveDataService) {
+        this.timeToLiveDataService = timeToLiveDataService;
+    }
 
     @Override
     public boolean canHandle(Callback<AsylumCase> callback) {
@@ -38,6 +50,25 @@ public class RemoveAppealFromOnlineConfirmation implements PostSubmitCallbackHan
             + "3.Email a link to the saved files with the appeal reference number to: BAUArnhemHouse@justice.gov.uk"
         );
 
+        // CCD doesn't set the "ttl.suspended" to NO (clock is active) unless it's null.
+        // When the clock has been stopped "ttl.suspended" gets populated with "YES" and isn't null anymore
+        // So it requires manual intervention to set "ttl.suspended = NO" when starting the clock again
+        if (wasSuccessful(callback) && !isClockActive(callback.getCaseDetails().getCaseData())) {
+            // stop the clock
+            timeToLiveDataService.updateTheClock(callback, false);
+        }
+
         return postSubmitResponse;
+    }
+
+    private boolean wasSuccessful(Callback<AsylumCase> callback) {
+        CaseDetails<AsylumCase> caseDetails = callback.getCaseDetails();
+        return caseDetails.getState().equals(State.ENDED);
+    }
+
+    private boolean isClockActive(AsylumCase asylumCase) {
+        return asylumCase.read(AsylumCaseFieldDefinition.TTL, TTL.class)
+            .map(ttl -> ttl.getSuspended().equals(YesOrNo.NO))
+            .orElse(false);
     }
 }
