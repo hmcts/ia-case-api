8d7887f6b3c55fa28045c00a7acbabee064b83c5
Marcellofabbri <marcello.fabbri@hmcts.net>
2022-10-21 12:32:04 +0100

    RIA-6232 Add CaseDataContentTest, StartEventDetailsTest, SubmitEventDetailsTest, TtlTest

---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/CaseDataContentTest.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class CaseDataContentTest {
+
+    private final String caseReference = "caseReference";
+    private final Map<String, Object> data = Map.of("data", "data");
+    private final Map<String, Object> event = Map.of("data", "data");
+    private final String eventToken = "eventToken";
+    private final boolean ignoreWarning = true;
+
+    private CaseDataContent caseDataContent = new CaseDataContent(
+        caseReference,
+        data,
+        event,
+        eventToken,
+        ignoreWarning);
+
+    @Test
+    void should_hold_onto_values() {
+        assertEquals(caseReference, caseDataContent.getCaseReference());
+        assertEquals(data, caseDataContent.getData());
+        assertEquals(event, caseDataContent.getEvent());
+        assertEquals(eventToken, caseDataContent.getEventToken());
+        assertEquals(ignoreWarning, caseDataContent.isIgnoreWarning());
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/field/TtlTest.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TtlTest {
+
+    private final String systemTTL = "2022-10-21";
+    private final String overrideTTL = "2023-10-21";
+    private final YesOrNo suspended = YesOrNo.YES;
+
+    private TTL ttl = TTL.builder()
+        .suspended(suspended)
+        .overrideTTL(overrideTTL)
+        .systemTTL(systemTTL)
+        .build();
+
+    @Test
+    void should_hold_onto_values() {
+
+        assertEquals(suspended, ttl.getSuspended());
+        assertEquals(overrideTTL, ttl.getOverrideTTL());
+        assertEquals(systemTTL, ttl.getSystemTTL());
+    }
+}
