48ebc9579569f2debbdb78ecb9665bd9407740fb
colmlynch60 <colm.lynch@hmcts.net>
2022-11-18 13:15:12 +0000

    RIA-6459 - Removed unused import

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/BailApplicationNumberHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/BailApplicationNumberHandler.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
 
 import java.util.Optional;
-import java.util.regex.Pattern;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
