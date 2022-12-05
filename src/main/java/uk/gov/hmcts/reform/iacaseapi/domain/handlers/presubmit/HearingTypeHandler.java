2cafdc020167e29d0ca63e2e28a0eafc86bc86ec
ThomasKKC <99262639+ThomasKKC@users.noreply.github.com>
2022-12-05 16:06:36 +0000

    RIA-6518 Updated hearing type handler to handle start appeal journey (#1182)
    * RIA-6518 Updated hearing type handler to handle start appeal journey
    
    * RIA-6518 fixed sonar qube scan

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HearingTypeHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HearingTypeHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPEAL_TYPE;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPEAL_TYPE_FOR_DISPLAY;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HEARING_TYPE_RESULT;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.IS_ACCELERATED_DETAINED_APPEAL;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo.NO;
@@ -9,6 +10,7 @@ import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo.YE
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AppealType;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AppealTypeForDisplay;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
@@ -48,10 +50,23 @@ public class HearingTypeHandler implements PreSubmitCallbackHandler<AsylumCase> 
         AppealType appealType = asylumCase.read(APPEAL_TYPE, AppealType.class)
             .orElse(null);
 
-        if (appealType != null) {
+        AppealTypeForDisplay appealTypeForDisplay = asylumCase.read(APPEAL_TYPE_FOR_DISPLAY, AppealTypeForDisplay.class)
+                .orElse(null);
+
+        if (callback.getEvent() == Event.EDIT_APPEAL && appealType != null) {
             if ((appealType == AppealType.DC || appealType == AppealType.RP)
                     || asylumCase.read(IS_ACCELERATED_DETAINED_APPEAL, YesOrNo.class)
                 .orElse(NO) == YES) {
+                asylumCase.write(HEARING_TYPE_RESULT, YES);
+            } else {
+                asylumCase.write(HEARING_TYPE_RESULT, YesOrNo.NO);
+            }
+        }
+
+        if (callback.getEvent() == Event.START_APPEAL && appealTypeForDisplay != null) {
+            if ((appealTypeForDisplay == AppealTypeForDisplay.DC || appealTypeForDisplay == AppealTypeForDisplay.RP)
+                    || asylumCase.read(IS_ACCELERATED_DETAINED_APPEAL, YesOrNo.class)
+                    .orElse(NO) == YES) {
                 asylumCase.write(HEARING_TYPE_RESULT, YES);
             } else {
                 asylumCase.write(HEARING_TYPE_RESULT, YesOrNo.NO);
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HearingTypeHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HearingTypeHandlerTest.java
@@ -2,9 +2,9 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.IS_ACCELERATED_DETAINED_APPEAL;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
 
@@ -12,15 +12,21 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AppealType;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AppealTypeForDisplay;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
@@ -45,6 +51,68 @@ class HearingTypeHandlerTest {
 
         when(asylumCase.read(IS_ACCELERATED_DETAINED_APPEAL, String.class)).thenReturn(Optional.of(isAcc));
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"DC", "RP"})
+    void should_write_to_hearing_type_result_yes_for_edit_appeal_event(String type) {
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(callback.getEvent()).thenReturn(Event.EDIT_APPEAL);
+        when(asylumCase.read(APPEAL_TYPE, AppealType.class)).thenReturn(Optional.of(AppealType.valueOf(type)));
+
+        hearingTypeHandler.handle(PreSubmitCallbackStage.MID_EVENT, callback);
+
+        verify(asylumCase, times(1))
+                .write(AsylumCaseFieldDefinition.HEARING_TYPE_RESULT, YesOrNo.YES);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"PA", "EA", "HU", "AG"})
+    void should_write_to_hearing_type_result_no_for_edit_appeal_event(String type) {
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(callback.getEvent()).thenReturn(Event.EDIT_APPEAL);
+        when(asylumCase.read(APPEAL_TYPE, AppealType.class)).thenReturn(Optional.of(AppealType.valueOf(type)));
+
+        hearingTypeHandler.handle(PreSubmitCallbackStage.MID_EVENT, callback);
+
+        verify(asylumCase, times(1))
+                .write(AsylumCaseFieldDefinition.HEARING_TYPE_RESULT, YesOrNo.NO);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"DC", "RP"})
+    void should_write_to_hearing_type_result_yes_for_start_appeal_event(String type) {
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(callback.getEvent()).thenReturn(Event.START_APPEAL);
+        when(asylumCase.read(APPEAL_TYPE_FOR_DISPLAY, AppealTypeForDisplay.class))
+                .thenReturn(Optional.of(AppealTypeForDisplay.valueOf(type)));
+
+        hearingTypeHandler.handle(PreSubmitCallbackStage.MID_EVENT, callback);
+
+        verify(asylumCase, times(1))
+                .write(AsylumCaseFieldDefinition.HEARING_TYPE_RESULT, YesOrNo.YES);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"PA", "EA", "HU"})
+    void should_write_to_hearing_type_result_no_for_start_appeal_event(String type) {
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(callback.getEvent()).thenReturn(Event.START_APPEAL);
+        when(asylumCase.read(APPEAL_TYPE_FOR_DISPLAY, AppealTypeForDisplay.class))
+                .thenReturn(Optional.of(AppealTypeForDisplay.valueOf(type)));
+
+        hearingTypeHandler.handle(PreSubmitCallbackStage.MID_EVENT, callback);
+
+        verify(asylumCase, times(1))
+                .write(AsylumCaseFieldDefinition.HEARING_TYPE_RESULT, YesOrNo.NO);
     }
 
     @Test
