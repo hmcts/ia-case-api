aa08fa92d18a11fc5a9c85bb90ff44ce85fc3056
Ryan-Nesbitt <95298094+Ryan-Nesbitt@users.noreply.github.com>
2022-12-08 13:16:35 +0000

    Ria-6482 admin edit appeal post submit (#1192)
    * Adding event to letter sent or received
    
    * Fixing test
    
    * Fix test syntax after handling conflicts

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/LetterSentOrReceivedHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/LetterSentOrReceivedHandlerTest.java
@@ -132,8 +132,7 @@ class LetterSentOrReceivedHandlerTest {
 
                 boolean canHandle = letterSentOrReceivedHandler.canHandle(callbackStage, callback);
 
-                if ((callbackStage == PreSubmitCallbackStage.MID_EVENT || callbackStage == ABOUT_TO_SUBMIT)
-                     && (event.equals(Event.START_APPEAL) || event.equals(Event.EDIT_APPEAL))
+                if ((callbackStage == PreSubmitCallbackStage.MID_EVENT || callbackStage == ABOUT_TO_SUBMIT) && (event.equals(Event.START_APPEAL)  || event.equals(Event.EDIT_APPEAL) || event.equals(Event.EDIT_APPEAL_AFTER_SUBMIT))
                      && !HandlerUtils.isAipJourney(callback.getCaseDetails().getCaseData())) {
                     assertThat(canHandle).isEqualTo(true);
                 } else {
