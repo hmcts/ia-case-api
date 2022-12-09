59c535d61482a3b4382cb104a90728c231f48dba
Dbarnes-v1 <99024336+Dbarnes-v1@users.noreply.github.com>
2022-12-09 09:23:42 +0000

    RIA-6530 - Updating confirmation page message for ADA submit hearing requirements event (#1193)

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/SubmitHearingRequirementsConfirmation.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/SubmitHearingRequirementsConfirmation.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit;
 
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.IS_ACCELERATED_DETAINED_APPEAL;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PostSubmitCallbackHandler;
 
 @Component
@@ -30,12 +32,27 @@ public class SubmitHearingRequirementsConfirmation implements PostSubmitCallback
         PostSubmitCallbackResponse postSubmitResponse =
             new PostSubmitCallbackResponse();
 
+        final AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+
+        boolean isAcceleratedDetainedAppeal = asylumCase.read(IS_ACCELERATED_DETAINED_APPEAL, YesOrNo.class)
+            .orElse(YesOrNo.NO)
+            .equals(YesOrNo.YES);
+
         postSubmitResponse.setConfirmationHeader("# You've submitted your hearing requirements");
-        postSubmitResponse.setConfirmationBody(
-            "#### What happens next\n\n"
-            + "The Tribunal will review your hearing requirements and any additional requests for adjustments.<br><br>"
-            + "We'll notify you when the hearing is listed. You'll then be able to review the hearing requirements."
-        );
+
+        if (isAcceleratedDetainedAppeal) {
+            postSubmitResponse.setConfirmationBody(
+                "#### What happens next\n\n"
+                + "The Tribunal will review your hearing requirements and any additional requests for adjustments.<br><br>"
+                + "You’ll be able to see any agreed adjustments in the hearing and appointment tab."
+            );
+        } else {
+            postSubmitResponse.setConfirmationBody(
+                "#### What happens next\n\n"
+                + "The Tribunal will review your hearing requirements and any additional requests for adjustments.<br><br>"
+                + "We'll notify you when the hearing is listed. You'll then be able to review the hearing requirements."
+            );
+        }
 
         return postSubmitResponse;
     }
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/SubmitHearingRequirementsConfirmationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/SubmitHearingRequirementsConfirmationTest.java
@@ -8,14 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -24,14 +28,21 @@ class SubmitHearingRequirementsConfirmationTest {
 
     @Mock
     private Callback<AsylumCase> callback;
+    @Mock
+    private CaseDetails<AsylumCase> caseDetails;
+    @Mock
+    private AsylumCase asylumCase;
 
     private SubmitHearingRequirementsConfirmation submitHearingRequirementsConfirmation =
         new SubmitHearingRequirementsConfirmation();
 
     @Test
-    void should_return_confirmation() {
+    void should_return_confirmation_appeal_journey() {
 
         when(callback.getEvent()).thenReturn(Event.DRAFT_HEARING_REQUIREMENTS);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.read(AsylumCaseFieldDefinition.IS_ACCELERATED_DETAINED_APPEAL, YesOrNo.class)).thenReturn(Optional.of(YesOrNo.NO));
 
         PostSubmitCallbackResponse callbackResponse =
             submitHearingRequirementsConfirmation.handle(callback);
@@ -53,6 +64,37 @@ class SubmitHearingRequirementsConfirmationTest {
             callbackResponse.getConfirmationBody().get())
             .contains(
                 "We'll notify you when the hearing is listed. You'll then be able to review the hearing requirements.");
+
+    }
+
+    @Test
+    void should_return_confirmation_ada() {
+
+        when(callback.getEvent()).thenReturn(Event.DRAFT_HEARING_REQUIREMENTS);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.read(AsylumCaseFieldDefinition.IS_ACCELERATED_DETAINED_APPEAL, YesOrNo.class)).thenReturn(Optional.of(YesOrNo.YES));
+
+        PostSubmitCallbackResponse callbackResponse =
+            submitHearingRequirementsConfirmation.handle(callback);
+
+        assertNotNull(callbackResponse);
+        assertTrue(callbackResponse.getConfirmationHeader().isPresent());
+        assertTrue(callbackResponse.getConfirmationBody().isPresent());
+
+        assertThat(
+            callbackResponse.getConfirmationHeader().get())
+            .contains("You've submitted your hearing requirements");
+
+        assertThat(
+            callbackResponse.getConfirmationBody().get())
+            .contains(
+                "The Tribunal will review your hearing requirements and any additional requests for adjustments.<br><br>");
+
+        assertThat(
+            callbackResponse.getConfirmationBody().get())
+            .contains(
+                "You’ll be able to see any agreed adjustments in the hearing and appointment tab.");
 
     }
 
