1f2362cb563944c650f9c9e5a1651b936ca61d54
Neha Aggarwal <neha.aggarwal@version1.com>
2022-12-16 09:01:01 +0000

    Fix checkstyle & unit test

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/FeesHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/FeesHandlerTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
