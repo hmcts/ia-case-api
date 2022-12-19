7e424e32f2be148246aa579b7e10864634ffb1e6
Dbarnes-v1 <daniel.barnes@hmcts.net>
2022-12-19 16:05:35 +0000

    RIA-6566 - Added adaHearingRequirementsUpdatable to case-api which will be set to "No" on completion of the sendDecisionAndReasons event. Required for ADA flow to disable access to this event post decided state.

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCaseFieldDefinition.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCaseFieldDefinition.java
@@ -812,6 +812,9 @@ public enum AsylumCaseFieldDefinition {
     ADA_HEARING_REQUIREMENTS_TO_REVIEW(
         "adaHearingRequirementsToReview", new TypeReference<YesOrNo>() {}),
 
+    ADA_HEARING_REQUIREMENTS_UPDATABLE(
+            "adaHearingRequirementsUpdatable", new TypeReference<YesOrNo>() {}),
+
     AUTOMATIC_DIRECTION_REQUESTING_HEARING_REQUIREMENTS(
         "automaticDirectionRequestingHearingRequirements", new TypeReference<String>(){}),
 
