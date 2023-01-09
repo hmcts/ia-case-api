e28f66eee91d8b655a45ac0c8dd5013a7d24d5c6
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2023-01-09 09:37:44 +0000

    RIA-6542 AIP new payment flow (#1211)
    * RIA-6542 AIP new payment flow
    
    * RIA-6542 AIP new payment flow
    
    * RIA-6542 Rearrange logic for AIP payment flow
    
    * RIA-6542 Add functional tests
    
    Co-authored-by: Marcellofabbri <marcello.fabbri@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/payment/FeesAndStatusCheckPreparer.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/payment/FeesAndStatusCheckPreparer.java
@@ -3,9 +3,6 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.payment;
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.RemissionDecision.REJECTED;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.RemissionType.EXCEPTIONAL_CIRCUMSTANCES_REMISSION;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.RemissionType.HELP_WITH_FEES;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.RemissionType.HO_WAIVER_REMISSION;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.PAYMENT_APPEAL;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State.*;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.PaymentStatus.PAID;
@@ -17,7 +14,6 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AppealType;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.RemissionDecision;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.RemissionType;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
@@ -82,17 +78,6 @@ public class FeesAndStatusCheckPreparer implements PreSubmitCallbackHandler<Asyl
 
         final PreSubmitCallbackResponse<AsylumCase> asylumCasePreSubmitCallbackResponse
             = new PreSubmitCallbackResponse<>(asylumCase);
-
-        Optional<RemissionType> remissionType = asylumCase.read(REMISSION_TYPE, RemissionType.class);
-        if (remissionType.isPresent()
-            && Arrays.asList(HO_WAIVER_REMISSION, HELP_WITH_FEES, EXCEPTIONAL_CIRCUMSTANCES_REMISSION)
-                .contains(remissionType.get())
-        ) {
-            asylumCasePreSubmitCallbackResponse
-                .addError(
-                    "The Pay and submit option is not available. Select Submit your appeal to submit the appeal.");
-            return asylumCasePreSubmitCallbackResponse;
-        }
 
         final PaymentStatus paymentStatus = asylumCase.read(PAYMENT_STATUS, PaymentStatus.class)
             .orElse(PaymentStatus.PAYMENT_PENDING);
