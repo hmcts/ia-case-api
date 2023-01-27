549867bc9cf7172a4430483084893880a1aed703
hussain-faruk <118450580+hussain-faruk@users.noreply.github.com>
2023-01-27 15:55:07 +0000

    Ria 6605 ada suitability document case api (#1246)
    * Saving work
    
    * Saving changes
    
    * Added allowed event for ADA suitability document generation
    
    * Rebased
    
    * Added ada suitability document tag. Added ada suitability event to GenerateDocumentHandler. Modified DocumentTagTest. Added ada suitability review to GenerateDocumentHandlerTest

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
@@ -39,6 +39,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(27, DocumentTag.values().length);
+        assertEquals(28, DocumentTag.values().length);
     }
 }
