ec74f8a757f569c20ff8eba3cef91df520de8e11
Jephtah Addison <Jephtah.Addison@HMCTS.NET>
2023-02-03 15:20:32 +0000

    build changes for feign

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/Application.java
@@ -1,22 +1,15 @@
 package uk.gov.hmcts.reform.iacaseapi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 @EnableRetry
-@EnableFeignClients(basePackages =
-    {
-    "uk.gov.hmcts.reform.auth",
-    "uk.gov.hmcts.reform.authorisation",
-    "uk.gov.hmcts.reform.iacaseapi"
-    })
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
     public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
+        run(Application.class, args);
     }
 }
