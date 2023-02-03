74edd3c538666419dc7894ec6a75c355ed85fa7a
Simon Buckley <106660286+sibuckley-v1@users.noreply.github.com>
2023-02-03 13:39:38 +0000

    Remove unused imports

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/StrategicCaseFlag.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/StrategicCaseFlag.java
@@ -5,13 +5,9 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.Value;
 
 @Value
