0ad83ef79eff6e411cf6e20760dc3c9435bb86d7
Jephtah Addison <Jephtah.Addison@HMCTS.NET>
2023-02-07 15:01:10 +0000

    fix for JSON parse error: Cannot deserialize value of type `java.util.ArrayList<uk.gov.hmcts.reform.ccd.document.am.model.Document>`

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/field/Document.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/field/Document.java
@@ -1,46 +1,17 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field;
 
-import static java.util.Objects.requireNonNull;
-
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@EqualsAndHashCode
-@ToString
-@Builder
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@Data
 public class Document {
 
-    private String documentUrl;
-    private String documentBinaryUrl;
-    private String documentFilename;
+    private final String documentUrl;
+    private final String documentBinaryUrl;
+    private final String documentFilename;
 
-    public Document(
-        String documentUrl,
-        String documentBinaryUrl,
-        String documentFilename
-    ) {
-        requireNonNull(documentUrl);
-        requireNonNull(documentBinaryUrl);
-        requireNonNull(documentFilename);
-
-        this.documentUrl = documentUrl;
-        this.documentBinaryUrl = documentBinaryUrl;
-        this.documentFilename = documentFilename;
-    }
-
-    public String getDocumentUrl() {
-        return documentUrl;
-    }
-
-    public String getDocumentBinaryUrl() {
-        return documentBinaryUrl;
-    }
-
-    public String getDocumentFilename() {
-        return documentFilename;
-    }
 }
