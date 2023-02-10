d51728d262db2ed464db3d2c1f6da609b49760ae
Jephtah Addison <Jephtah.Addison@HMCTS.NET>
2023-02-10 22:13:28 +0000

    fix for JSON parse error: Cannot deserialize value of type `java.util.ArrayList<uk.gov.hmcts.reform.ccd.document.am.model.Document>`

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/JacksonConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
 @Configuration
 public class JacksonConfiguration {
 
