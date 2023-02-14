1743f8f3b1e0295cfe92f5371f58fa6c9afdc1d1
Jephtah Addison <Jephtah.Addison@HMCTS.NET>
2023-02-14 15:06:14 +0000

    fix for JSON parse error: Cannot deserialize value of type `java.util.ArrayList<uk.gov.hmcts.reform.ccd.document.am.model.Document>`

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/SystemDocumentManagementUploader.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/SystemDocumentManagementUploader.java
@@ -1,92 +1,38 @@
 package uk.gov.hmcts.reform.iacaseapi.util;
 
-import static io.micrometer.core.instrument.binder.BaseUnits.FILES;
-import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.roleassignment.Classification.PUBLIC;
-
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.ByteStreams;
 import java.io.IOException;
-import java.util.Objects;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.io.ByteArrayResource;
+import java.util.Collections;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClient;
+import uk.gov.hmcts.reform.ccd.document.am.model.UploadResponse;
+import uk.gov.hmcts.reform.ccd.document.am.util.InMemoryMultipartFile;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
 
 
 @Service
-@ComponentScan("uk.gov.hmcts.reform.ccd.document.am.feign")
 public class SystemDocumentManagementUploader {
 
+    private final CaseDocumentClient caseDocumentClient;
     private final AuthorizationHeadersProvider authorizationHeadersProvider;
 
-    private static final String CLASSIFICATION = "classification";
-
-    private static final String CASE_TYPE_ID = "caseTypeId";
-
-    private static final String JURISDICTION_ID = "jurisdictionId";
-
-    private static final String DOCUMENTS = "documents";
-
-    private static final int FIRST = 0;
-
-    @Autowired
-    private RestTemplate restTemplate;
-
     public SystemDocumentManagementUploader(
+            CaseDocumentClient caseDocumentClient,
             AuthorizationHeadersProvider authorizationHeadersProvider
     ) {
+        this.caseDocumentClient = caseDocumentClient;
         this.authorizationHeadersProvider = authorizationHeadersProvider;
+
     }
 
     public Document upload(
             Resource resource,
             String contentType
     ) {
-
-        final ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        try {
-
-            final MultipartFile file = new MockMultipartFile(
-                    resource.getFilename(),
-                    resource.getFilename(),
-                    contentType,
-                    ByteStreams.toByteArray(resource.getInputStream())
-                 );
-
-            // the ip should be changed to hostname with a placeholder
-            final String t = restTemplate.postForObject("http://127.0.0.1:4455/cases/documents", httpEntity(file), String.class);
-
-            JsonNode jsonNode =
-                    Objects.requireNonNull(mapper.readTree(t))
-                            .get(DOCUMENTS)
-                            .get(FIRST);
-
-            return mapper.treeToValue(jsonNode, Document.class);
-
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
-
-    }
-
-    private HttpEntity<MultiValueMap<String, Object>> httpEntity(final MultipartFile file) {
-
         final String serviceAuthorizationToken =
                 authorizationHeadersProvider
                         .getLegalRepresentativeAuthorization()
@@ -97,37 +43,50 @@ public class SystemDocumentManagementUploader {
                         .getLegalRepresentativeAuthorization()
                         .getValue("Authorization");
 
-        MultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
-        HttpEntity<Resource> fileResource = new HttpEntity<>(buildByteArrayResource(file), buildPartHeaders(file));
-        parameters.add(FILES, fileResource);
-        parameters.add(CLASSIFICATION, PUBLIC.toString());
-        parameters.add(CASE_TYPE_ID, "Asylum");
-        parameters.add(JURISDICTION_ID, "IA");
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", accessToken);
-        headers.set("ServiceAuthorization", serviceAuthorizationToken);
-        return new HttpEntity<>(parameters, headers);
-
-    }
-
-    private static HttpHeaders buildPartHeaders(MultipartFile file) {
-        requireNonNull(file.getContentType());
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.valueOf(file.getContentType()));
-        return headers;
-    }
-
-    private static ByteArrayResource buildByteArrayResource(MultipartFile file) {
         try {
-            return new ByteArrayResource(file.getBytes()) {
-                    @Override
-                     public String getFilename() {
-                    return file.getOriginalFilename();
-                }
-            };
-        } catch (IOException ioException) {
-            throw new IllegalStateException(ioException);
+
+            MultipartFile file = new InMemoryMultipartFile(
+                    resource.getFilename(),
+                    resource.getFilename(),
+                    contentType,
+                    ByteStreams.toByteArray(resource.getInputStream())
+            );
+
+
+            UploadResponse  uploadResponse =
+                    caseDocumentClient.uploadDocuments(
+                            accessToken,
+                            serviceAuthorizationToken,
+                            "Asylum",
+                            "IA",
+                                    Collections.singletonList(file)
+                            );
+
+
+            uk.gov.hmcts.reform.ccd.document.am.model.Document uploadedDocument =
+                    uploadResponse
+                            .getDocuments()
+                            .get(0);
+
+            return new Document(
+                    uploadedDocument
+                            .links
+                            .self
+                            .href,
+                    uploadedDocument
+                            .links
+                            .binary
+                            .href,
+                    uploadedDocument
+                            .originalDocumentName
+            );
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
         }
     }
 }
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/JacksonConfiguration.java
@@ -5,14 +5,16 @@ import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_ENUMS_USING_TO_STRING;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-@Configuration
+
+@ComponentScan
 public class JacksonConfiguration {
     @Bean
     @Primary
@@ -21,7 +23,7 @@ public class JacksonConfiguration {
             .featuresToEnable(READ_ENUMS_USING_TO_STRING)
             .featuresToEnable(READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
             .featuresToEnable(WRITE_ENUMS_USING_TO_STRING)
-            .serializationInclusion(JsonInclude.Include.NON_ABSENT);
+            .serializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
     @Bean
@@ -29,6 +31,8 @@ public class JacksonConfiguration {
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         objectMapper.registerModule(new Jdk8Module());
+        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
     }
 
