5064038fe98d4fd232a23a44e3cbccab416f0db3
Jephtah Addison <Jephtah.Addison@HMCTS.NET>
2023-02-16 23:08:16 +0000

    fix for JSON parse error: Cannot deserialize value of type `java.util.ArrayList<uk.gov.hmcts.reform.ccd.document.am.model.Document>`

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/JacksonConfiguration.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
@@ -19,10 +17,7 @@ public class JacksonConfiguration {
     @Bean
     @Primary
     public ObjectMapper objectMapper() {
-        JacksonXmlModule xmlModule = new JacksonXmlModule();
-        xmlModule.setDefaultUseWrapper(false);
-        ObjectMapper objectMapper = new XmlMapper(xmlModule);
-        return objectMapper
+        return new ObjectMapper()
                 .registerModule(new Jdk8Module())
                 .registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES))
                 .registerModule(new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
