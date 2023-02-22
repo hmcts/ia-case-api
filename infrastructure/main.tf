3cf8e04a0207f9c77e77f9d06f1fbc37192c1a09
rajeshthuraiyur <rajesh.thuraiyur@gmail.com>
2023-02-22 13:05:15 +0000

    Updates to fix terraform warnings

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -32,7 +32,7 @@ module "ia_case_api_database_11" {
   database_name      = "${var.postgresql_database_name}"
   postgresql_user    = "${var.postgresql_user}"
   postgresql_version = "11"
-  common_tags        = "merge(var.common_tags, tomap({"lastUpdated" = "${timestamp()}"}))
+  common_tags        =  merge(var.common_tags, tomap({"lastUpdated" = "${timestamp()}"}))
   subscription       = "${var.subscription}"
 }
 
--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.40"
+      version = "~> 2.25"
     }
     azuread = {
       source  = "hashicorp/azuread"
