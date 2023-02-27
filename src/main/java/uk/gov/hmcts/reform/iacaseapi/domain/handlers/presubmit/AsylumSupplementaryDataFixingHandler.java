397502dbc8561cc335bad8c770d46c66d7f809bc
Dbarnes-v1 <daniel.barnes@hmcts.net>
2023-02-27 15:06:17 +0000

    RIA-6862 - Updating supplementary data handler to not be called for every callback

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AsylumSupplementaryDataFixingHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AsylumSupplementaryDataFixingHandler.java
@@ -5,12 +5,15 @@ import java.util.Map;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.CcdSupplementaryUpdater;
+
+import static java.util.Objects.requireNonNull;
 
 @Component
 public class AsylumSupplementaryDataFixingHandler implements PreSubmitCallbackHandler<AsylumCase> {
@@ -31,7 +34,11 @@ public class AsylumSupplementaryDataFixingHandler implements PreSubmitCallbackHa
             PreSubmitCallbackStage callbackStage,
             Callback<AsylumCase> callback
     ) {
-        return true;
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+                && callback.getEvent() != Event.START_APPEAL;
     }
 
     @Override
