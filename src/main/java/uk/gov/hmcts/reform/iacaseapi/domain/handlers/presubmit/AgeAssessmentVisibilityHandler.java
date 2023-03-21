a6a3458a7f4e16a96268657bb816c0cab68c0cb4
Marcellofabbri <marcello.fabbri@hmcts.net>
2023-03-21 11:57:06 +0000

    RIA-6877 RIA-6913 Consider specific NO values instead of !YES evaluated in HandlerUtils

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AgeAssessmentVisibilityHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AgeAssessmentVisibilityHandler.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPELLANT_IN_DETENTION;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.IS_ACCELERATED_DETAINED_APPEAL;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.IS_AGE_ASSESSMENT_VISIBLE;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.EDIT_APPEAL;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.START_APPEAL;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.MID_EVENT;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo.NO;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo.YES;
-import static uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isAcceleratedDetainedAppeal;
-import static uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isAppellantInDetention;
 
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +17,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 
 @Slf4j
@@ -44,7 +45,15 @@ public class AgeAssessmentVisibilityHandler implements PreSubmitCallbackHandler<
 
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
 
-        if (!isAppellantInDetention(asylumCase) || !isAcceleratedDetainedAppeal(asylumCase)) {
+        boolean isNonDetainedAppeal = asylumCase.read(APPELLANT_IN_DETENTION, YesOrNo.class)
+            .map(value -> value.equals(NO))
+            .orElse(false);
+
+        boolean isNonAcceleratedDetainedAppeal = asylumCase.read(IS_ACCELERATED_DETAINED_APPEAL, YesOrNo.class)
+            .map(value -> value.equals(NO))
+            .orElse(false);
+
+        if (isNonDetainedAppeal || isNonAcceleratedDetainedAppeal) {
             asylumCase.write(IS_AGE_ASSESSMENT_VISIBLE, YES);
         } else {
             asylumCase.write(IS_AGE_ASSESSMENT_VISIBLE, NO);
