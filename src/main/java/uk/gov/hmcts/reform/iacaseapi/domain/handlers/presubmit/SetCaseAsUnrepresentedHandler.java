f9201de03368238e869e266a76bb5c8a00f6bcaa
Ryan Nesbitt <rnesbitt03@qub.ac.uk>
2023-04-27 11:14:43 +0100

    RIA-7029 Handler to set isAdmin to Yes when stop representing a client

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SetCaseAsUnrepresentedHandler.java
@@ -0,0 +1,59 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.REMOVE_REPRESENTATION;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.DispatchPriority;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+
+@Component
+public class SetCaseAsUnrepresentedHandler implements PreSubmitCallbackHandler<AsylumCase> {
+
+    public boolean canHandle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+               && callback.getEvent() == REMOVE_REPRESENTATION;
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback) {
+
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCase =
+            callback
+                .getCaseDetails()
+                .getCaseData();
+
+        boolean isAdmin = HandlerUtils.isInternalCase(asylumCase);
+
+        if (!isAdmin) {
+            asylumCase.write(IS_ADMIN, YesOrNo.YES);
+            asylumCase.clear(LEGAL_REP_COMPANY);
+            asylumCase.clear(LEGAL_REP_COMPANY_ADDRESS);
+            asylumCase.clear(LEGAL_REP_NAME);
+            asylumCase.clear(LEGAL_REPRESENTATIVE_NAME);
+            asylumCase.clear(LEGAL_REP_REFERENCE_NUMBER);
+            asylumCase.clear(LEGAL_REPRESENTATIVE_EMAIL_ADDRESS);
+        }
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
