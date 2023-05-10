069b41427c0c66f180a0426aef13d4e481327415
Marcellofabbri <marcello.fabbri@hmcts.net>
2023-05-10 16:55:36 +0100

    RIA-6156 Amend integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/component/testutils/fixtures/CaseDetailsForTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/component/testutils/fixtures/CaseDetailsForTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.iacaseapi.component.testutils.fixtures;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
+import java.util.Map;
 import lombok.Data;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State;
@@ -16,13 +17,16 @@ public class CaseDetailsForTest {
     private AsylumCase caseData;
     @JsonProperty("created_date")
     private LocalDateTime createdDate;
+    @JsonProperty("supplementary_data")
+    private Map<String,String> supplementaryData;
 
-    CaseDetailsForTest(long id, String jurisdiction, State state, AsylumCase caseData, LocalDateTime createdDate) {
+    CaseDetailsForTest(long id, String jurisdiction, State state, AsylumCase caseData, LocalDateTime createdDate, Map<String,String> supplementaryData) {
         this.id = id;
         this.jurisdiction = jurisdiction;
         this.state = state;
         this.caseData = caseData;
         this.createdDate = createdDate;
+        this.supplementaryData = supplementaryData;
     }
 
     public static class CaseDetailsForTestBuilder implements Builder<CaseDetailsForTest> {
@@ -36,6 +40,7 @@ public class CaseDetailsForTest {
         private State state;
         private AsylumCase caseData;
         private LocalDateTime createdDate = LocalDateTime.now();
+        private Map<String,String> supplementaryData = Map.of("HMCTSServiceId","BFA1");
 
         CaseDetailsForTestBuilder() {
         }
@@ -65,12 +70,17 @@ public class CaseDetailsForTest {
             return this;
         }
 
+        public CaseDetailsForTestBuilder supplementaryData(Map<String,String> supplementaryData) {
+            this.supplementaryData = supplementaryData;
+            return this;
+        }
+
         public CaseDetailsForTest build() {
-            return new CaseDetailsForTest(id, jurisdiction, state, caseData, createdDate);
+            return new CaseDetailsForTest(id, jurisdiction, state, caseData, createdDate, supplementaryData);
         }
 
         public String toString() {
-            return "CaseDetailsForTest.CaseDetailsForTestBuilder(id=" + this.id + ", jurisdiction=" + this.jurisdiction + ", state=" + this.state + ", caseData=" + this.caseData + ", createdDate=" + this.createdDate + ")";
+            return "CaseDetailsForTest.CaseDetailsForTestBuilder(id=" + this.id + ", jurisdiction=" + this.jurisdiction + ", state=" + this.state + ", caseData=" + this.caseData + ", createdDate=" + this.createdDate + ", supplementaryData=" + this.supplementaryData + ")";
         }
     }
 }
