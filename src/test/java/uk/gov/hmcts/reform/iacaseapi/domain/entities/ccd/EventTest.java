5353cae15ef5f92582504e86a14c77094068f475
Neha Aggarwal <neha.aggarwal@version1.com>
2023-05-11 15:32:56 +0100

    RIA-7043: Fixed unit test after rebase

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -128,7 +128,6 @@ class EventTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(119, Event.values().length);
         assertEquals(121, Event.values().length);
     }
 }
