32103468e7c6f143212b39404f9d829948de1e71
DennisHoV1 <106669367+DennisHoV1@users.noreply.github.com>
2023-05-11 15:29:48 +0100

    RIA-7070 update radio button options for resident judge (#1373)
    * RIA-7070 update radio button options for resident judge
    
    * -update the import library in unit test
    -Temporary suppress the CVE problem (CVE-2023-20873 and CVE-2023-20862) with the expiry date
    
    * -set remade rule32 for legal representative journey only
    -update the unit test
    
    * -update if condition for checking non LR journey

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/FtpaDecisionMidEvent.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/FtpaDecisionMidEvent.java
@@ -14,6 +14,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 
 @Component
@@ -46,6 +47,21 @@ public class FtpaDecisionMidEvent implements PreSubmitCallbackHandler<AsylumCase
         final String ftpaApplicantType = asylumCase
             .read(FTPA_APPLICANT_TYPE, String.class)
             .orElseThrow(() -> new IllegalStateException("FtpaApplicantType is not present"));
+
+        Optional<String> ftpaRjDecisionOutcomeType = asylumCase.read(ftpaApplicantType.equals(APPELLANT.toString())
+                ? FTPA_APPELLANT_RJ_DECISION_OUTCOME_TYPE
+                : FTPA_RESPONDENT_RJ_DECISION_OUTCOME_TYPE, String.class);
+
+        if (!HandlerUtils.isRepJourney(asylumCase) && ftpaRjDecisionOutcomeType.isPresent()) {
+            String ftpaRjDecisionOutcomeTypeValue = ftpaRjDecisionOutcomeType.get();
+            if (ftpaRjDecisionOutcomeTypeValue.equals(FtpaResidentJudgeDecisionOutcomeType.REHEARD_RULE35.toString())
+                || ftpaRjDecisionOutcomeTypeValue.equals(FtpaResidentJudgeDecisionOutcomeType.REHEARD_RULE32.toString())
+                || ftpaRjDecisionOutcomeTypeValue.equals(FtpaResidentJudgeDecisionOutcomeType.REMADE_RULE32.toString())) {
+                PreSubmitCallbackResponse<AsylumCase> response = new PreSubmitCallbackResponse<>(asylumCase);
+                response.addError("For Legal Representative Journey Only");
+                return response;
+            }
+        }
 
         Optional<String> ftpaSubmitted = asylumCase.read(
             ftpaApplicantType.equals(APPELLANT.toString()) ? FTPA_APPELLANT_SUBMITTED : FTPA_RESPONDENT_SUBMITTED, String.class);
