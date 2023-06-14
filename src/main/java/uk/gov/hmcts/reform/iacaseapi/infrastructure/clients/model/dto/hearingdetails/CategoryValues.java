406d26ccd5b77fa357d6e18432f0e58d7f0fe356
Jamie <jamiemurphy@version1.com>
2023-06-14 15:54:16 +0100

    Fix for imports

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/dto/hearingdetails/CategoryValues.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/dto/hearingdetails/CategoryValues.java
@@ -2,12 +2,11 @@ package uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.model.dto.hearingde
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 
 @Data
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/dto/hearingdetails/CommonDataResponse.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/dto/hearingdetails/CommonDataResponse.java
@@ -1,9 +1,8 @@
 package uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.model.dto.hearingdetails;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
