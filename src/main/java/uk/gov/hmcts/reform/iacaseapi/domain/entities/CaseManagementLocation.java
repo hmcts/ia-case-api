f3fd50cd4c35aef9625a121b30aaede8521ab112
BurnsT <tim.burns@HMCTS.net>
2023-06-19 16:49:30 +0100

    RIA-7313 - another jackson attempt to fix

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/CaseManagementLocation.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/CaseManagementLocation.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.entities;
 
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 @Value
+@Builder
+@Jacksonized
 public class CaseManagementLocation {
     Region region;
     BaseLocation baseLocation;
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/HearingRecordingDocument.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/HearingRecordingDocument.java
@@ -1,10 +1,14 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.entities;
 
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.HasDocument;
 
 @Value
+@Builder
+@Jacksonized
 public class HearingRecordingDocument implements HasDocument {
     Document document;
     String description;
