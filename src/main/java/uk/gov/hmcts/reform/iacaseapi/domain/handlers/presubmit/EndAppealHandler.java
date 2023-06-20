0f2b4dc7b3ae71c064414f280686950de1039e78
kalachandrasekar1 <114995593+kalachandrasekar1@users.noreply.github.com>
2023-06-20 09:38:13 +0100

    changes for endAppealAutomatically (#1368)
    Co-authored-by: Filippo Possenti <filippo.possenti@version1.com>
    Co-authored-by: filippo-possenti-version1 <109352283+filippo-possenti-version1@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/EndAppealHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/EndAppealHandler.java
@@ -65,7 +65,7 @@ public class EndAppealHandler implements PreSubmitCallbackHandler<AsylumCase> {
         PaymentStatus paymentStatus = asylumCase.read(PAYMENT_STATUS, PaymentStatus.class)
             .orElse(PaymentStatus.PAYMENT_PENDING);
         if (callback.getEvent() == Event.END_APPEAL_AUTOMATICALLY && paymentStatus == PaymentStatus.PAID) {
-            throw new IllegalStateException("Cannot auto end appeal as the payment is already made!");
+            return new PreSubmitCallbackResponse<>(asylumCase);
         }
 
         asylumCase.write(END_APPEAL_DATE, dateProvider.now().toString());
