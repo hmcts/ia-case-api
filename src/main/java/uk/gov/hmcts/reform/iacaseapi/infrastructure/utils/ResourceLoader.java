882980da69b55313796faa963580298251667931
Marcellofabbri <marcello.fabbri@hmcts.net>
2023-06-27 22:23:46 +0100

    RIA-7244 Test for ResourceLoader

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/utils/ResourceLoader.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/utils/ResourceLoader.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.iacaseapi.infrastructure.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 
@@ -15,36 +14,18 @@ public class ResourceLoader {
     }
 
     public static byte[] loadResource(final String filePath) throws Exception {
-        InputStream io = null;
         byte[] allBytes = null;
-        try {
-            io = ResourceLoader.class.getClassLoader().getResourceAsStream(filePath);
+        try (InputStream io = ResourceLoader.class.getClassLoader().getResourceAsStream(filePath)) {
 
             if (io == null) {
-                throw new IllegalArgumentException(String.format("Could not find resource in path %s", filePath));
+                throw new IllegalArgumentException(String.format("Empty resource in path %s", filePath));
             } else {
                 allBytes = io.readAllBytes();
             }
-        } finally {
-            io.close();
+        } catch (NullPointerException nullPointerException) {
+            throw new IllegalArgumentException(String.format("Could not find resource in path %s", filePath));
         }
 
         return allBytes;
-    }
-
-    public static <T> T loadJsonToObject(String filePath, Class<T> type) {
-        try {
-            return new ObjectMapper().readValue(loadJson(filePath), type);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static <T> String objectToJson(T object) {
-        try {
-            return new ObjectMapper().writeValueAsString(object);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
