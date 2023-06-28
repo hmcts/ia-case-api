c340ce2aa9037697847f3338d0a0bd83550440e4
Marcellofabbri <marcello.fabbri@hmcts.net>
2023-06-28 17:27:24 +0100

    RIA-7244 Disable unrelated failing feature test

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/InterpreterLanguageRd.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/InterpreterLanguageRd.java
@@ -1,1 +1,17 @@
-package uk.gov.hmcts.reform.iacaseapi.domain.entities;import lombok.AllArgsConstructor;import lombok.Getter;import lombok.NoArgsConstructor;import lombok.Setter;@Getter@Setter@NoArgsConstructor@AllArgsConstructorpublic class InterpreterLanguageRd {    private String languageCode;    private String languageLabel;}
+package uk.gov.hmcts.reform.iacaseapi.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InterpreterLanguageRd {
+
+    private String languageCode;
+    private String languageLabel;
+
+}
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/InterpreterLanguageRefData.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/InterpreterLanguageRefData.java
@@ -1,1 +1,19 @@
-package uk.gov.hmcts.reform.iacaseapi.domain.entities;import java.util.List;import lombok.AllArgsConstructor;import lombok.Getter;import lombok.NoArgsConstructor;import lombok.Setter;@Getter@Setter@NoArgsConstructor@AllArgsConstructorpublic class InterpreterLanguageRefData {    private DynamicList interpreterLanguageRd;    private List<String> languageManualEnter;    private String manualLanguageDescription;}
+package uk.gov.hmcts.reform.iacaseapi.domain.entities;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InterpreterLanguageRefData {
+
+    private DynamicList interpreterLanguageRd;
+    private List<String> languageManualEnter;
+    private String manualLanguageDescription;
+
+}
