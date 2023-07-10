239b82c43f9fc758d48dc07c93495559ce9fc0cc
burnstHMCTS <134689347+burnstHMCTS@users.noreply.github.com>
2023-07-10 16:44:19 +0100

    RIA-7313 - All Java 17 Upgrades (#1444)
    * RIA-7313 - gradle upgrade + checkstyle & owasp suppressions
    
    This project has roughly 700~ checkstyle warnings, ignoring failures for the time being, can discuss later
    
    * RIA-7313 - re-enabled checkstyle fail on warnings + fixed a couple checkstyle errors
    
    * RIA-7313 - java 17 upgrade
    
    * RIA-7313 - app insights upgrade
    
    * RIA-7313 - removed temp config value
    
    * RIA-7313 - fixing jenkins issues with tests (jackson errors)
    
    * RIA-7313 - updated suppressions
    
    * RIA-7313 - resolved jackson issues, however, THIS SOLUTION NEEDS FURTHER REVIEW!!
    
    had to make the 2 classes changed mutable for the sake of jackson,
    additionally tried using @JsonCreator on constructors while the classes were still marked with @Value
    
    * RIA-7313 - revert jackson fixes
    
    * RIA-7313 - another jackson attempt to fix
    
    * RIA-7313 - another jackson attempt to fix
    
    * RIA-7313 - another jackson attempt to fix
    
    * RIA-7313 - debug
    
    * RIA-7313 - debug
    
    * RIA-7313 - bootstrap.yaml update for app insights
    
    * RIA-7313 - update for app insights
    
    * RIA-7313 - jackson upgrade
    
    * RIA-7313 - revert jackson upgrade + fix for jackson issue
    
    * RIA-7313 - sonar exclusion of domain class
    
    * RIA-7313 - sonar exclusion of domain class
    
    * RIA-7313 - jackson fix
    
    * RIA-7313 - jackson fix
    
    * RIA-7313 Temporarily suppressing vulnerabilities
    
    * RIA-7313 - adding aip-frontend to chart
    
    * RIA-7313 - matt carey's suggestion for aip fix
    
    * Revert "RIA-7313 - matt carey's suggestion for aip fix"
    
    This reverts commit 4feaf1dead1c4747b39e1ca103cc4067f1d3d20d.
    
    ---------
    
    Co-authored-by: filippo-possenti-version1 <109352283+filippo-possenti-version1@users.noreply.github.com>
    Co-authored-by: Filippo Possenti <filippo.possenti@version1.com>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/ccd/OrganisationPolicy.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/ccd/OrganisationPolicy.java
@@ -2,11 +2,15 @@ package uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder
+@Jacksonized
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrganisationPolicy {
 
