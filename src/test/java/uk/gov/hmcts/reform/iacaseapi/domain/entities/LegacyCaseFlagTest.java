9e900fe8807388241c3d238280f04b7300379b6b
rajeshthuraiyur <rajesh.thuraiyur@gmail.com>
2023-07-14 16:56:19 +0100

    Updates to resolve merge conflicts

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/LegacyCaseFlagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/LegacyCaseFlagTest.java
@@ -8,17 +8,27 @@ public class LegacyCaseFlagTest {
     private final CaseFlagType caseFlagType = CaseFlagType.ANONYMITY;
     private final String legacyCaseFlagAdditionalInformation = "Anonymity";
 
-    private LegacyCaseFlag legacyCaseFlag = new LegacyCaseFlag(
-        caseFlagType,
-        legacyCaseFlagAdditionalInformation
-    );
-
     @Test
     public void should_hold_onto_values() {
+        LegacyCaseFlag legacyCaseFlag = new LegacyCaseFlag(
+            caseFlagType,
+            legacyCaseFlagAdditionalInformation
+        );
         Assert.assertNotNull(legacyCaseFlag.getLegacyCaseFlagType());
         Assert.assertNotNull(legacyCaseFlag.getLegacyCaseFlagAdditionalInformation());
         Assert.assertEquals(caseFlagType, legacyCaseFlag.getLegacyCaseFlagType());
         Assert.assertEquals(legacyCaseFlagAdditionalInformation, legacyCaseFlag.getLegacyCaseFlagAdditionalInformation());
     }
-    
+
+    @Test
+    public void should_check_noargs() {
+        LegacyCaseFlag legacyCaseFlag1 = new LegacyCaseFlag();
+        legacyCaseFlag1.setLegacyCaseFlagType(caseFlagType);
+        legacyCaseFlag1.setLegacyCaseFlagAdditionalInformation(legacyCaseFlagAdditionalInformation);
+        Assert.assertNotNull(legacyCaseFlag1.getLegacyCaseFlagType());
+        Assert.assertNotNull(legacyCaseFlag1.getLegacyCaseFlagAdditionalInformation());
+        Assert.assertEquals(caseFlagType, legacyCaseFlag1.getLegacyCaseFlagType());
+        Assert.assertEquals(legacyCaseFlagAdditionalInformation, legacyCaseFlag1.getLegacyCaseFlagAdditionalInformation());
+    }
+
 }
