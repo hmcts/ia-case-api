772f5ffbf6f3492e9a64a827a909c0354b2e774e
rajeshthuraiyur <rajesh.thuraiyur@gmail.com>
2023-07-14 15:31:54 +0100

    Updates to resolve merge conflicts

---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/LegacyCaseFlagTest.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.entities;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LegacyCaseFlagTest {
+
+    private final CaseFlagType caseFlagType = CaseFlagType.ANONYMITY;
+    private final String legacyCaseFlagAdditionalInformation = "Anonymity";
+
+    private LegacyCaseFlag legacyCaseFlag = new LegacyCaseFlag(
+        caseFlagType,
+        legacyCaseFlagAdditionalInformation
+    );
+
+    @Test
+    public void should_hold_onto_values() {
+        Assert.assertEquals(caseFlagType, legacyCaseFlag.getLegacyCaseFlagType());
+        Assert.assertEquals(legacyCaseFlagAdditionalInformation, legacyCaseFlag.getLegacyCaseFlagAdditionalInformation());
+    }
+}
