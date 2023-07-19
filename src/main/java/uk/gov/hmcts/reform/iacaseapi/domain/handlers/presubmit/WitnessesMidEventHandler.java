e5a5eeefdeda9c16b9d5a14fb309ba36ddf4b6e9
Marcellofabbri <marcello.fabbri@hmcts.net>
2023-07-19 09:07:51 +0100

    RIA-7509 Add mapping for individual dynamic list of each kind of each witness

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/WitnessesMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/WitnessesMidEventHandler.java
@@ -5,9 +5,7 @@ import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefin
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.DRAFT_HEARING_REQUIREMENTS;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.UPDATE_HEARING_REQUIREMENTS;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -28,10 +26,8 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
 
     private static final String IS_WITNESSES_ATTENDING_PAGE_ID = "isWitnessesAttending";
     private static final String IS_ANY_WITNESS_INTERPRETER_REQUIRED_PAGE_ID = "isAnyWitnessInterpreterRequired";
-    private static final String WHICH_WITNESS_REQUIRES_INTERPRETER_PAGE_ID = "whichWitnessRequiresInterpreter";
+    //private static final String WHICH_WITNESS_REQUIRES_INTERPRETER_PAGE_ID = "whichWitnessRequiresInterpreter";
     private static final String WITNESSES_NUMBER_EXCEEDED_ERROR = "Maximum number of witnesses is 10";
-    private static final String NO_WITNESSES_SELECTED_ERROR = "Select at least one witness";
-    private static final String TOO_MANY_INTERPRETER_TYPES_ERROR = "Select only one interpreter type for witness ";
 
     private static final List<AsylumCaseFieldDefinition> WITNESS_N_FIELD = List.of(
         WITNESS_1,
@@ -44,7 +40,7 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
         WITNESS_8,
         WITNESS_9,
         WITNESS_10);
-    private static final List<AsylumCaseFieldDefinition> WITNESS_LIST_ELEMENT_N_FIELD = List.of(
+    protected static final List<AsylumCaseFieldDefinition> WITNESS_LIST_ELEMENT_N_FIELD = List.of(
         WITNESS_LIST_ELEMENT_1,
         WITNESS_LIST_ELEMENT_2,
         WITNESS_LIST_ELEMENT_3,
@@ -56,7 +52,7 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
         WITNESS_LIST_ELEMENT_9,
         WITNESS_LIST_ELEMENT_10
     );
-    private static final List<AsylumCaseFieldDefinition> WITNESS_N_INTERPRETER_CATEGORY_FIELD = List.of(
+    protected static final List<AsylumCaseFieldDefinition> WITNESS_N_INTERPRETER_CATEGORY_FIELD = List.of(
         WITNESS_1_INTERPRETER_LANGUAGE_CATEGORY,
         WITNESS_2_INTERPRETER_LANGUAGE_CATEGORY,
         WITNESS_3_INTERPRETER_LANGUAGE_CATEGORY,
@@ -79,10 +75,7 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
 
         return callbackStage == PreSubmitCallbackStage.MID_EVENT
                && Set.of(DRAFT_HEARING_REQUIREMENTS, UPDATE_HEARING_REQUIREMENTS).contains(callback.getEvent())
-               && Set.of(
-                   IS_WITNESSES_ATTENDING_PAGE_ID,
-            IS_ANY_WITNESS_INTERPRETER_REQUIRED_PAGE_ID,
-            WHICH_WITNESS_REQUIRES_INTERPRETER_PAGE_ID).contains(pageId);
+               && Set.of(IS_WITNESSES_ATTENDING_PAGE_ID, IS_ANY_WITNESS_INTERPRETER_REQUIRED_PAGE_ID).contains(pageId);
     }
 
     @Override
@@ -94,8 +87,6 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
         String pageId = callback.getPageId();
         PreSubmitCallbackResponse<AsylumCase> response = new PreSubmitCallbackResponse<>(asylumCase);
-
-        clearWitnessFieldsPreemptively(asylumCase);
 
         Optional<List<IdValue<WitnessDetails>>> optionalWitnesses = asylumCase.read(WITNESS_DETAILS);
 
@@ -111,6 +102,8 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
 
             case IS_ANY_WITNESS_INTERPRETER_REQUIRED_PAGE_ID:
 
+                clearWitnessFieldsPreemptively(asylumCase);
+
                 optionalWitnesses.ifPresent(witnesses -> {
                     int i = 0;
                     while (i < witnesses.size()) {
@@ -120,7 +113,7 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
                         asylumCase.write(WITNESS_N_FIELD.get(i), witnesses.get(i).getValue());
                         asylumCase.write(WITNESS_LIST_ELEMENT_N_FIELD.get(i),
                             new DynamicMultiSelectList(
-                                List.of(new Value("", "")),
+                                Collections.emptyList(),
                                 List.of(new Value(fullName, fullName))
                             ));
                         i++;
@@ -128,71 +121,7 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
                 });
                 break;
 
-
-            case WHICH_WITNESS_REQUIRES_INTERPRETER_PAGE_ID:
-
-                Set<Integer> selectedWitnessesIndexes = new HashSet<>();
-                List<DynamicMultiSelectList> selectedWitnesses = new ArrayList<>();
-
-                int i = 0;
-
-                while (i < WITNESS_LIST_ELEMENT_N_FIELD.size()) {
-                    DynamicMultiSelectList witnessElement = asylumCase
-                        .read(WITNESS_LIST_ELEMENT_N_FIELD.get(i), DynamicMultiSelectList.class).orElse(null);
-                    if (witnessElement != null && !witnessElement.getValues().isEmpty()) {
-                        selectedWitnesses.add(witnessElement);
-                        selectedWitnessesIndexes.add(i);
-                    }
-                    i++;
-                }
-
-                if (selectedWitnesses.isEmpty()) {
-                    response.addError(NO_WITNESSES_SELECTED_ERROR);
-                }
-
-                selectedWitnessesIndexes.forEach(indexOfSelectedWitness -> {
-                    Optional<List<String>> optionalWitnessInterpreters = asylumCase
-                        .read(WITNESS_N_INTERPRETER_CATEGORY_FIELD.get(indexOfSelectedWitness));
-                    if (!optionalWitnessInterpreters.isEmpty() && optionalWitnessInterpreters.get().size() == 2) {
-                        response.addError(TOO_MANY_INTERPRETER_TYPES_ERROR + indexOfSelectedWitness);
-                    }
-                });
-
         }
-
-        //optionalWitnesses.ifPresent(witnesses -> {
-        //    if (IS_WITNESSES_ATTENDING_PAGE_ID.equals(pageId)) {
-        //
-        //        if (witnesses.size() > WITNESS_N_FIELD.size()) {        // 10
-        //            response.addError(WITNESSES_NUMBER_EXCEEDED_ERROR);
-        //        }
-        //    } else {
-        //
-        //        int i = 0;
-        //        while (i < witnesses.size()) {
-        //
-        //            String fullName = buildWitnessFullName(witnesses.get(i).getValue());
-        //
-        //            asylumCase.write(WITNESS_N_FIELD.get(i), witnesses.get(i).getValue());
-        //            asylumCase.write(WITNESS_LIST_ELEMENT_N_FIELD.get(i),
-        //                new DynamicMultiSelectList(
-        //                    List.of(new Value("", "")),
-        //                    List.of(new Value(fullName, fullName))
-        //                ));
-        //            i++;
-        //        }
-        //    }
-
-
-
-            //while (i < WITNESS_N_FIELD.size()) { // 10
-            //    asylumCase.write(WITNESS_N_FIELD.get(i), null);
-            //    asylumCase.write(WITNESS_LIST_ELEMENT_N_FIELD.get(i), null);
-            //    asylumCase.write(WITNESS_N_INTERPRETER_CATEGORY_FIELD.get(i), null);
-            //    i++;
-            //}
-        //
-        //});
 
         return response;
     }
@@ -203,11 +132,7 @@ public class WitnessesMidEventHandler implements PreSubmitCallbackHandler<Asylum
 
     private void clearWitnessFieldsPreemptively(AsylumCase asylumCase) {
         WITNESS_N_FIELD.forEach(field -> asylumCase.write(field, new WitnessDetails("", "")));
-        //WITNESS_N_FIELD.forEach(asylumCase::clear);
-        WITNESS_LIST_ELEMENT_N_FIELD.forEach(field -> asylumCase.write(field, new DynamicMultiSelectList(
-            List.of(new Value("", "")),
-            List.of(new Value("", ""))
-        )));
+        WITNESS_LIST_ELEMENT_N_FIELD.forEach(field -> asylumCase.write(field, new DynamicMultiSelectList()));
         WITNESS_N_INTERPRETER_CATEGORY_FIELD.forEach(field -> asylumCase.write(field, Collections.emptyList()));
     }
 
