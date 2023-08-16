e072d5a2a7ca97290e632924f28ac4ec04a9f279
robin-ieong <robin.ieong@version1.com>
2023-08-16 17:21:26 +0100

    RIA-7699: Added confirmation page and tests

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/UpdateInterpreterBookingStatusConfirmation.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit;
+
+import static java.util.Objects.requireNonNull;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PostSubmitCallbackHandler;
+
+@Component
+public class UpdateInterpreterBookingStatusConfirmation implements PostSubmitCallbackHandler<AsylumCase> {
+
+    public boolean canHandle(
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callback, "callback must not be null");
+
+        return callback.getEvent() == Event.UPDATE_INTERPRETER_BOOKING_STATUS;
+    }
+
+    public PostSubmitCallbackResponse handle(
+        Callback<AsylumCase> callback
+    ) {
+        if (!canHandle(callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        PostSubmitCallbackResponse postSubmitResponse =
+            new PostSubmitCallbackResponse();
+
+        String hearingsTabUrl =
+            "/case/IA/Asylum/"
+                + callback.getCaseDetails().getId()
+                + "#hearing";
+
+        postSubmitResponse.setConfirmationHeader("# Booking statuses have been updated");
+        postSubmitResponse.setConfirmationBody(
+            "#### What happens next\n\n"
+            + "You now need to update the hearing in the "
+            + "[Hearings tab](" + hearingsTabUrl + ")"
+            + " to ensure the update is displayed in List Assist."
+            + "\n\nIf an interpreter status has been moved to booked, or has been cancelled,"
+            + " ensure that the interpreter details are up to date before updating the hearing."
+        );
+
+        return postSubmitResponse;
+    }
+}
