177863fdfad0ec6202a4be79afddf0699359522d
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-08-22 17:17:32 +0200

    RIA-7374_mark_appeal_as_ada: Add Event.Mark_appeal_as_ada for internal path in SendNotificationHandler

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendNotificationHandler.java
@@ -195,7 +195,8 @@ public class SendNotificationHandler implements PreSubmitCallbackHandler<AsylumC
                 Event.MARK_APPEAL_PAID,
                 Event.LIST_CASE,
                 Event.REQUEST_HEARING_REQUIREMENTS_FEATURE,
-                Event.REQUEST_RESPONSE_REVIEW
+                Event.REQUEST_RESPONSE_REVIEW,
+                Event.MARK_APPEAL_AS_ADA
         );
         if (!isSaveAndContinueEnabled) {
             //eventsToHandle.add(Event.BUILD_CASE);
