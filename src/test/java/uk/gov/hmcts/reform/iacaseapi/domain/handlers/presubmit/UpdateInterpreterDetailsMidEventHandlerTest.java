934782652e9bb7772ad51ee6d3212d9b57619afc
robin-ieong <robin.ieong@version1.com>
2023-08-24 10:04:40 +0100

    RIA-7635: Added validation for interpreters email addresses

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/UpdateInterpreterDetailsMidEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/UpdateInterpreterDetailsMidEventHandlerTest.java
@@ -1,32 +1,31 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.INTERPRETER_DETAILS;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.InterpreterDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
-@SuppressWarnings("unchecked")
-class UpdateInterpreterDetailsHandlerTest {
+class UpdateInterpreterDetailsMidEventHandlerTest {
 
     @Mock
     private Callback<AsylumCase> callback;
@@ -35,60 +34,56 @@ class UpdateInterpreterDetailsHandlerTest {
     @Mock
     private AsylumCase asylumCase;
 
-    private UpdateInterpreterDetailsHandler handler;
+    private UpdateInterpreterDetailsMidEventHandler handler;
+
 
     @BeforeEach
     void setUp() {
-        handler = new UpdateInterpreterDetailsHandler();
+        handler = new UpdateInterpreterDetailsMidEventHandler();
         when(callback.getEvent()).thenReturn(Event.UPDATE_INTERPRETER_DETAILS);
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
     }
 
     @Test
-    void testCanHandle() {
-        when(callback.getEvent()).thenReturn(Event.UPDATE_INTERPRETER_DETAILS);
-        boolean canHandle = handler.canHandle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
-        assertTrue(canHandle);
+    void canHandle_shouldReturnTrueForMidEventAndUpdateInterpreterDetailsEvent() {
+        boolean canHandle = handler.canHandle(PreSubmitCallbackStage.MID_EVENT, callback);
+        assertThat(canHandle).isTrue();
     }
 
     @Test
-    public void testHandleInterpreterDetails() {
-        InterpreterDetails interpreterWithId = InterpreterDetails.builder()
+    void handle_shouldValidateInterpretersEmailAddresses() {
+        InterpreterDetails interpreterWithValidEmail = InterpreterDetails.builder()
             .interpreterId("1")
             .interpreterBookingRef("ref1")
             .interpreterGivenNames("John")
             .interpreterFamilyName("Smith")
             .interpreterPhoneNumber("123")
-            .interpreterEmail("email")
+            .interpreterEmail("validEmail@test.com")
             .interpreterNote("note")
             .build();
 
-        InterpreterDetails interpreterWithoutId = InterpreterDetails.builder()
-            .interpreterId(null)
+        InterpreterDetails interpreterWithInvalidEmail = InterpreterDetails.builder()
+            .interpreterId("2")
             .interpreterBookingRef("ref2")
             .interpreterGivenNames("Jane")
             .interpreterFamilyName("Smith")
             .interpreterPhoneNumber("456")
-            .interpreterEmail("email2")
+            .interpreterEmail("invalid_email")
             .interpreterNote("note2")
             .build();
 
         List<IdValue<InterpreterDetails>> interpreterDetailsList = new ArrayList<>();
-        interpreterDetailsList.add(new IdValue<>("1", interpreterWithId));
-        interpreterDetailsList.add(new IdValue<>("2", interpreterWithoutId));
+        interpreterDetailsList.add(new IdValue<>("1", interpreterWithValidEmail));
+        interpreterDetailsList.add(new IdValue<>("2", interpreterWithInvalidEmail));
 
+        // Given that the case has two interpreters, one with a valid email address and one with an invalid email address
         when(asylumCase.read(eq(INTERPRETER_DETAILS))).thenReturn(Optional.of(interpreterDetailsList));
 
-        PreSubmitCallbackResponse<AsylumCase> response = handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
-        assertNotNull(response);
+        PreSubmitCallbackResponse<AsylumCase> response = handler.handle(PreSubmitCallbackStage.MID_EVENT, callback);
 
-        List<IdValue<InterpreterDetails>> updatedInterpreterDetails =
-            ((List<IdValue<InterpreterDetails>>) response.getData().read(INTERPRETER_DETAILS)
-                .orElse(Collections.emptyList()));
-
-        assertTrue(updatedInterpreterDetails.stream()
-            .noneMatch(idValue -> isEmpty(idValue.getValue().getInterpreterId())));
+        // Verify error message is generated for the second interpreter who has an invalid email address
+        assertThat(response.getErrors().iterator().next())
+            .isEqualTo("Interpreter 2 email address is invalid, please enter a valid email address.");
     }
-
 }
