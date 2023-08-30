49226aa36b024e92820fe7ea7820943e4ee0cf8f
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2023-08-30 11:16:28 +0100

    RIA-7816 retrofit bug fixes to case flags branch (#1596)
    * Updates to retrofit bug fixes
    
    * Updates to retrofit bug fixes

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/CaseFlagDetail.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/CaseFlagDetail.java
@@ -3,12 +3,19 @@ package uk.gov.hmcts.reform.iacaseapi.domain.entities;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Value;
+import lombok.Getter;
+import lombok.Setter;
 
-@Value
+@Getter
+@Setter
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CaseFlagDetail {
+
+    private CaseFlagDetail() {
+        // noop -- for deserializer
+    }
+    
     private String id;
     @JsonProperty("value")
     private CaseFlagValue caseFlagValue;
