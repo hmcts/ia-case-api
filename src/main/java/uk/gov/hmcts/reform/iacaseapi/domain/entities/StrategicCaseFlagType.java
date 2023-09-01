ebd29baac85137afa070569d546a1b08f72af0af
McCarthyC <cmccarthy95@icloud.com>
2023-09-01 11:32:14 +0100

    RIA-7412: updated flag name

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/StrategicCaseFlagType.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/StrategicCaseFlagType.java
@@ -8,7 +8,7 @@ public enum StrategicCaseFlagType {
     HEARING_LOOP("RA0043", "Hearing loop (hearing enhancement system)"),
     STEP_FREE_WHEELCHAIR_ACCESS("RA0019", "Step free / wheelchair access"),
     CASE_GIVEN_IN_PRIVATE("SM0004", "Evidence given in private"),
-    INTERPRETER_LANGUAGE_FLAG("PF0015", "Interpreter Language");
+    INTERPRETER_LANGUAGE_FLAG("PF0015", "Language Interpreter");
 
     @JsonValue
     private final String flagCode;
