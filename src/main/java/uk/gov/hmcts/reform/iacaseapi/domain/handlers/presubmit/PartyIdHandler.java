6d709163e871f4b1d88a586c0a89008d60bb0fbb
DennisHoV1 <106669367+DennisHoV1@users.noreply.github.com>
2023-09-11 13:48:18 +0100

    RIA-7752 party ID generation AIP journey (#1615)
    * RIA-7750 party ID generation AIP journey
    
    * temporarily suppress the CVE problem
    
    * resume the functional test when the event is draftHearingRequirements
    
    * Add the functional test of party ID generation for AIP journey when the event is start/edit appeal

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PartyIdHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/PartyIdHandler.java
@@ -20,6 +20,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacaseapi.domain.service.HearingPartyIdGenerator;
 
@@ -72,12 +73,14 @@ public class PartyIdHandler implements PreSubmitCallbackHandler<AsylumCase> {
 
     private void setLegalRepPartyId(AsylumCase asylumCase) {
 
-        if (asylumCase.read(LEGAL_REP_INDIVIDUAL_PARTY_ID, String.class).orElse("").isEmpty()) {
-            asylumCase.write(LEGAL_REP_INDIVIDUAL_PARTY_ID, HearingPartyIdGenerator.generate());
-        }
+        if (!HandlerUtils.isAipJourney(asylumCase)) {
+            if (asylumCase.read(LEGAL_REP_INDIVIDUAL_PARTY_ID, String.class).orElse("").isEmpty()) {
+                asylumCase.write(LEGAL_REP_INDIVIDUAL_PARTY_ID, HearingPartyIdGenerator.generate());
+            }
 
-        if (asylumCase.read(LEGAL_REP_ORGANISATION_PARTY_ID, String.class).orElse("").isEmpty()) {
-            asylumCase.write(LEGAL_REP_ORGANISATION_PARTY_ID, HearingPartyIdGenerator.generate());
+            if (asylumCase.read(LEGAL_REP_ORGANISATION_PARTY_ID, String.class).orElse("").isEmpty()) {
+                asylumCase.write(LEGAL_REP_ORGANISATION_PARTY_ID, HearingPartyIdGenerator.generate());
+            }
         }
     }
 
