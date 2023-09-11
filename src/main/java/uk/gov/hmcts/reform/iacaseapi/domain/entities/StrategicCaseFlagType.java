f9481497e96bcdc96332e77d2781b6a6bfa82aec
cmccarthyHMCTS <138468823+cmccarthyHMCTS@users.noreply.github.com>
2023-09-11 15:45:44 +0100

    RIA-7338 Sign language interpreter flag for appellant  (#1599)
    * RIA-7338: case flag for sign language appellant
    
    ---------
    
    Co-authored-by: McCarthyC <cmccarthy95@icloud.com>
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/StrategicCaseFlagType.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/StrategicCaseFlagType.java
@@ -8,6 +8,7 @@ public enum StrategicCaseFlagType {
     HEARING_LOOP("RA0043", "Hearing loop (hearing enhancement system)"),
     STEP_FREE_WHEELCHAIR_ACCESS("RA0019", "Step free / wheelchair access"),
     CASE_GIVEN_IN_PRIVATE("SM0004", "Evidence given in private"),
+    SIGN_LANGUAGE("RA0042", "Sign Language"),
     INTERPRETER_LANGUAGE_FLAG("PF0015", "Language Interpreter");
 
     @JsonValue
