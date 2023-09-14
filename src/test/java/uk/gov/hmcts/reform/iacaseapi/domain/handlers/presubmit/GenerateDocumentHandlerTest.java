04a483016e5867d2779107ab0509c53075745f83
Ryan Nesbitt <rnesbitt03@qub.ac.uk>
2023-09-14 17:02:54 +0100

    RIA-7427 Adding events for doc generation

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/GenerateDocumentHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/GenerateDocumentHandlerTest.java
@@ -314,7 +314,9 @@ class GenerateDocumentHandlerTest {
                         TRANSFER_OUT_OF_ADA,
                         RESIDENT_JUDGE_FTPA_DECISION,
                         APPLY_FOR_FTPA_APPELLANT,
-                        MAINTAIN_CASE_LINKS
+                        MAINTAIN_CASE_LINKS,
+                        UPLOAD_ADDENDUM_EVIDENCE_ADMIN_OFFICER,
+                        UPLOAD_ADDITIONAL_EVIDENCE
                     ).contains(event)) {
 
                     assertTrue(canHandle);
@@ -424,7 +426,9 @@ class GenerateDocumentHandlerTest {
                         TRANSFER_OUT_OF_ADA,
                         RESIDENT_JUDGE_FTPA_DECISION,
                         APPLY_FOR_FTPA_APPELLANT,
-                        MAINTAIN_CASE_LINKS
+                        MAINTAIN_CASE_LINKS,
+                        UPLOAD_ADDENDUM_EVIDENCE_ADMIN_OFFICER,
+                        UPLOAD_ADDITIONAL_EVIDENCE
                     );
 
                 if (callbackStage.equals(PreSubmitCallbackStage.ABOUT_TO_SUBMIT)
