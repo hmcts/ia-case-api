9e75e7c41d57e41c2ac8ce1527ef8647f7450aac
McCarthyC <cmccarthy95@icloud.com>
2023-09-27 09:37:22 +0100

    RIA-7657: added unit tests

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/WitnessCaseFlagsHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/WitnessCaseFlagsHandlerTest.java
@@ -3,27 +3,19 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-import uk.gov.hmcts.reform.iacaseapi.domain.DateProvider;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.*;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 
-import java.time.LocalDateTime;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.WITNESS_DETAILS;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.StrategicCaseFlagType.HEARING_LOOP;
+
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.StrategicCaseFlagType.INTERPRETER_LANGUAGE_FLAG;
 
 @ExtendWith(MockitoExtension.class)
@@ -54,14 +46,14 @@ public class WitnessCaseFlagsHandlerTest {
     void getWitnessCaseFlags_should_return_case_flags() {
         Optional<List<PartyFlagIdValue>> partyFlagIdValues = Optional.of(new ArrayList<>());
         partyFlagIdValues.get().add(partyFlagIdValue);
-        assertFalse(witnessCaseFlagsHandler.getWitnessCaseFlags(partyFlagIdValues ,witnessId).isEmpty());
+        assertFalse(witnessCaseFlagsHandler.getWitnessCaseFlags(partyFlagIdValues, witnessId).isEmpty());
     }
 
     @Test
     void getWitnessCaseFlags_should_return_no_case_flags() {
         Optional<List<PartyFlagIdValue>> partyFlagIdValues = Optional.of(new ArrayList<>());
         partyFlagIdValues.get().add(partyFlagIdValue);
-        assertTrue(witnessCaseFlagsHandler.getWitnessCaseFlags(partyFlagIdValues ,"fakeWitnessId").isEmpty());
+        assertTrue(witnessCaseFlagsHandler.getWitnessCaseFlags(partyFlagIdValues,"fakeWitnessId").isEmpty());
     }
 
 }
