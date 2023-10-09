ffd6e98d0d36fb9a63ef9498e197aa61aafae042
Javier Marin <fjavimarc@gmail.com>
2023-10-09 14:09:28 +0200

    RIA-7418 || RIA-7697: fixed send notification handler repeated event

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendNotificationHandler.java
@@ -213,8 +213,7 @@ public class SendNotificationHandler implements PreSubmitCallbackHandler<AsylumC
                 Event.CHANGE_DIRECTION_DUE_DATE,
                 Event.UPLOAD_ADDITIONAL_EVIDENCE_HOME_OFFICE,
                 Event.UPLOAD_ADDENDUM_EVIDENCE_HOME_OFFICE,
-                Event.UPLOAD_ADDENDUM_EVIDENCE,
-                Event.CHANGE_DIRECTION_DUE_DATE
+                Event.UPLOAD_ADDENDUM_EVIDENCE
         );
 
         if (!isSaveAndContinueEnabled) {
