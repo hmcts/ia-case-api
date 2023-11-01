6435ee07afadb8348ea003db5f0088c8465f7367
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2023-11-01 20:13:02 +0000

    Add validation for final decision and reasons document file type

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendDecisionAndReasonsMidEventHandler.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.FINAL_DECISION_AND_REASONS_DOCUMENT;
+
+@Component
+public class SendDecisionAndReasonsMidEventHandler implements PreSubmitCallbackHandler<AsylumCase> {
+
+    @Override
+    public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.MID_EVENT
+                && callback.getEvent() == Event.SEND_DECISION_AND_REASONS;
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<AsylumCase> handle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+
+        final Optional<Document> finalDecisionAndReasonsDoc = asylumCase.read(FINAL_DECISION_AND_REASONS_DOCUMENT, Document.class);
+
+        if (finalDecisionAndReasonsDoc.isPresent()
+                && !finalDecisionAndReasonsDoc.get().getDocumentFilename().endsWith(".pdf")) {
+
+            PreSubmitCallbackResponse<AsylumCase> asylumCasePreSubmitCallbackResponse =
+                    new PreSubmitCallbackResponse<>(asylumCase);
+            asylumCasePreSubmitCallbackResponse.addError("The Decision and reasons document must be a PDF file");
+            return asylumCasePreSubmitCallbackResponse;
+        }
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
