25d7ab031451c84f579a564bf3c75ca2655daae1
robin-ieong <101253854+robin-ieong@users.noreply.github.com>
2023-11-07 12:10:51 +0000

    RIA-7668 - Update field (#1781)
    * rename field
    
    * rename fields

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCaseFieldDefinitionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCaseFieldDefinitionTest.java
@@ -5,6 +5,8 @@ import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.ATTENDING_TCW;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CASE_LEVEL_FLAGS;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.MANUAL_CANCEL_HEARINGS_REQUIRED;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.MANUAL_UPDATE_HEARING_REQUIRED;
 
 import java.util.Set;
 import java.util.stream.Stream;
@@ -18,7 +20,9 @@ class AsylumCaseFieldDefinitionTest {
             // filter out below variable because of CCD defs constrains to edit existing fields
             .filter(val -> !Set.of(
                 ATTENDING_TCW,
-                CASE_LEVEL_FLAGS
+                CASE_LEVEL_FLAGS,
+                MANUAL_CANCEL_HEARINGS_REQUIRED,
+                MANUAL_UPDATE_HEARING_REQUIRED
             ).contains(val))
             .forEach(v -> assertThat(UPPER_UNDERSCORE.to(LOWER_CAMEL, v.name()))
                 .isEqualTo(v.value()));
