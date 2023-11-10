23de79a45d10ea32084afae33c278b640b33cd61
ikirsanov <105438011+ikirsanov@users.noreply.github.com>
2023-11-10 15:35:57 +0100

    RIA-7734_apply_for_costs_respondent_notification: Add new Event to notificationSendhandler (#1788)

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendNotificationHandler.java
@@ -148,7 +148,8 @@ public class SendNotificationHandler implements PreSubmitCallbackHandler<AsylumC
             Event.MAINTAIN_CASE_LINKS,
             Event.UPDATE_PAYMENT_STATUS,
             Event.MARK_AS_READY_FOR_UT_TRANSFER,
-            Event.UPDATE_DETENTION_LOCATION
+            Event.UPDATE_DETENTION_LOCATION,
+            Event.APPLY_FOR_COSTS
         );
         if (!isSaveAndContinueEnabled) {
             eventsToHandle.add(Event.BUILD_CASE);
