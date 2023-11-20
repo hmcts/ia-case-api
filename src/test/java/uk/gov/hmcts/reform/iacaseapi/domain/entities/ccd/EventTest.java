465c46a978fc4dca049f74610f6f9fd5ed2cad16
robin-ieong <robin.ieong@version1.com>
2023-11-20 14:50:55 +0000

    RIA-8081 - Fixed test

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -128,6 +128,6 @@ class EventTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(123, Event.values().length);
+        assertEquals(122, Event.values().length);
     }
 }
