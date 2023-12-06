36a5fec782d6f07a856bec22051ec3d146ebeafd
robin-ieong <robin.ieong@version1.com>
2023-12-06 15:29:48 +0000

    RIA-8034 - Create work allocation task when CMR is listed

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -125,10 +125,11 @@ class EventTest {
         assertEquals("handleHearingException", Event.HANDLE_HEARING_EXCEPTION.toString());
         assertEquals("recordAdjournmentDetails", Event.RECORD_ADJOURNMENT_DETAILS.toString());
         assertEquals("listAssistIntegration", Event.LIST_ASSIST_INTEGRATION.toString());
+        assertEquals("updateCmrNotification", Event.UPDATE_CMR_NOTIFICATION.toString());
     }
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(123, Event.values().length);
+        assertEquals(124, Event.values().length);
     }
 }
