1762af0ed0aede28f9641a4502b7e7928cf7a0ac
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-12-13 15:20:04 +0100

    RIA-7717_conside_making_costs: Add ConsiderMakingCosts handler, preparer, finalisation, Event, Populate TypesOfAppliedCosts

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ApplyForCosts.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ApplyForCosts.java
@@ -4,6 +4,8 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
@@ -11,6 +13,8 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
 @EqualsAndHashCode
 @ToString
+@Getter
+@Setter
 public class ApplyForCosts {
     //Applicant fields
     private String appliedCostsType;
@@ -19,7 +23,6 @@ public class ApplyForCosts {
     private List<IdValue<Document>> scheduleOfCostsDocuments;
     private YesOrNo applyForCostsHearingType;
     private String applyForCostsHearingTypeExplanation;
-    private String applyForCostsDecision;
     private String applyForCostsApplicantType;
     private String applyForCostsCreationDate;
     private String respondentToCostsOrder;
@@ -39,13 +42,46 @@ public class ApplyForCosts {
     private List<IdValue<Document>> respondentAdditionalEvidence;
 
     //Judge decision fields
+    private String applyForCostsDecision;
     private String costsDecisionType;
     private String costsOralHearingDate; // only if costsDecisionType is "with an oral hearing"
     private List<IdValue<Document>> uploadCostsOrder;
     private String dateOfDecision;
 
+    //Judge consideration fields
+    private String tribunalConsideringReason;
+    private List<IdValue<Document>> judgeEvidenceForCostsOrder;
+
     public ApplyForCosts() {
         // noop -- for deserializer
+    }
+
+    public ApplyForCosts(
+        String applyForCostsDecision,
+        String appliedCostsType,
+        String applyForCostsApplicantType,
+        String tribunalConsideringReason,
+        List<IdValue<Document>> judgeEvidenceForCostsOrder,
+        String applyForCostsCreationDate,
+        String respondentToCostsOrder,
+        String applyForCostsRespondentRole
+    ) {
+        requireNonNull(applyForCostsDecision);
+        requireNonNull(appliedCostsType);
+        requireNonNull(applyForCostsApplicantType);
+        requireNonNull(tribunalConsideringReason);
+        requireNonNull(applyForCostsCreationDate);
+        requireNonNull(respondentToCostsOrder);
+        requireNonNull(applyForCostsRespondentRole);
+
+        this.applyForCostsDecision = applyForCostsDecision;
+        this.appliedCostsType = appliedCostsType;
+        this.applyForCostsApplicantType = applyForCostsApplicantType;
+        this.tribunalConsideringReason = tribunalConsideringReason;
+        this.judgeEvidenceForCostsOrder = judgeEvidenceForCostsOrder;
+        this.applyForCostsCreationDate = applyForCostsCreationDate;
+        this.respondentToCostsOrder = respondentToCostsOrder;
+        this.applyForCostsRespondentRole = applyForCostsRespondentRole;
     }
 
     public ApplyForCosts(
@@ -94,160 +130,5 @@ public class ApplyForCosts {
         this.ootUploadEvidenceDocuments = ootUploadEvidenceDocuments;
         this.isApplyForCostsOot = isApplyForCostsOot;
         this.applyForCostsRespondentRole = applyForCostsRespondentRole;
-    }
-
-
-    public String getAppliedCostsType() {
-        requireNonNull(appliedCostsType);
-        return appliedCostsType;
-    }
-
-    public String getArgumentsAndEvidenceDetails() {
-        return argumentsAndEvidenceDetails;
-    }
-
-    public List<IdValue<Document>> getArgumentsAndEvidenceDocuments() {
-        requireNonNull(argumentsAndEvidenceDocuments);
-        return argumentsAndEvidenceDocuments;
-    }
-
-    public List<IdValue<Document>> getScheduleOfCostsDocuments() {
-        return scheduleOfCostsDocuments;
-    }
-
-    public YesOrNo getApplyForCostsHearingType() {
-        requireNonNull(applyForCostsHearingType);
-        return applyForCostsHearingType;
-    }
-
-    public String getApplyForCostsHearingTypeExplanation() {
-        if (getApplyForCostsHearingType().equals(YesOrNo.YES)) {
-            requireNonNull(applyForCostsHearingTypeExplanation);
-        }
-        return applyForCostsHearingTypeExplanation;
-    }
-
-
-    public String getApplyForCostsDecision() {
-        requireNonNull(applyForCostsDecision);
-        return applyForCostsDecision;
-    }
-
-    public void setApplyForCostsDecision(String applyForCostsDecision) {
-        this.applyForCostsDecision = applyForCostsDecision;
-    }
-
-    public String getApplyForCostsApplicantType() {
-        requireNonNull(applyForCostsApplicantType);
-        return applyForCostsApplicantType;
-    }
-
-    public String getApplyForCostsCreationDate() {
-        requireNonNull(applyForCostsCreationDate);
-        return applyForCostsCreationDate;
-    }
-
-    public String getRespondentToCostsOrder() {
-        requireNonNull(respondentToCostsOrder);
-        return respondentToCostsOrder;
-    }
-
-    public String getApplyForCostsOotExplanation() {
-        if (isApplyForCostsOot.equals(YesOrNo.YES)) {
-            requireNonNull(applyForCostsOotExplanation);
-        }
-        return applyForCostsOotExplanation;
-    }
-
-    public String getApplyForCostsRespondentRole() {
-        return applyForCostsRespondentRole;
-    }
-
-    public List<IdValue<Document>> getOotUploadEvidenceDocuments() {
-        return ootUploadEvidenceDocuments;
-    }
-
-    public YesOrNo getIsApplyForCostsOot() {
-        return isApplyForCostsOot;
-    }
-
-    public void setResponseToApplication(String responseToApplication) {
-        this.responseToApplication = responseToApplication;
-    }
-
-    public String getResponseToApplication() {
-        return responseToApplication;
-    }
-
-    public void setResponseHearingTypeExplanation(String responseHearingTypeExplanation) {
-        this.responseHearingTypeExplanation = responseHearingTypeExplanation;
-    }
-
-    public void setEvidence(List<IdValue<Document>> evidence) {
-        this.responseEvidence = evidence;
-    }
-
-    public String getResponseHearingTypeExplanation() {
-        return responseHearingTypeExplanation;
-    }
-
-    public List<IdValue<Document>> getResponseEvidence() {
-        return responseEvidence;
-    }
-
-    public YesOrNo getResponseHearingType() {
-        return responseHearingType;
-    }
-
-    public void setResponseHearingType(YesOrNo responseHearingType) {
-        this.responseHearingType = responseHearingType;
-    }
-
-    public List<IdValue<Document>> getApplicantAdditionalEvidence() {
-        return applicantAdditionalEvidence;
-    }
-
-    public void setApplicantAdditionalEvidence(List<IdValue<Document>> applicantAdditionalEvidence) {
-        this.applicantAdditionalEvidence = applicantAdditionalEvidence;
-    }
-
-    public List<IdValue<Document>> getRespondentAdditionalEvidence() {
-        return respondentAdditionalEvidence;
-    }
-
-    public void setRespondentAdditionalEvidence(List<IdValue<Document>> respondentAdditionalEvidence) {
-        this.respondentAdditionalEvidence = respondentAdditionalEvidence;
-    }
-
-    public String getCostsDecisionType() {
-        return costsDecisionType;
-    }
-
-    public void setCostsDecisionType(String costsDecisionType) {
-        this.costsDecisionType = costsDecisionType;
-    }
-
-    public String getCostsOralHearingDate() {
-        return costsOralHearingDate;
-    }
-
-    public void setCostsOralHearingDate(String costsOralHearingDate) {
-        this.costsOralHearingDate = costsOralHearingDate;
-    }
-
-    public List<IdValue<Document>> getUploadCostsOrder() {
-        return uploadCostsOrder;
-    }
-
-    public void setUploadCostsOrder(List<IdValue<Document>> uploadCostsOrder) {
-        this.uploadCostsOrder = uploadCostsOrder;
-    }
-
-    public String getDateOfDecision() {
-        return dateOfDecision;
-    }
-
-    public void setDateOfDecision(String dateOfDecision) {
-        this.dateOfDecision = dateOfDecision;
     }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/applyforcosts/ConsiderMakingCostsOrderHandler.java
@@ -0,0 +1,90 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.applyforcosts;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isLegalRepJourney;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ApplyForCosts;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.CostsDecision;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.DynamicList;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.ApplyForCostsAppender;
+
+@Component
+public class ConsiderMakingCostsOrderHandler implements PreSubmitCallbackHandler<AsylumCase> {
+
+    private final ApplyForCostsAppender applyForCostsAppender;
+
+    public ConsiderMakingCostsOrderHandler(ApplyForCostsAppender applyForCostsAppender) {
+        this.applyForCostsAppender = applyForCostsAppender;
+    }
+
+    @Override
+    public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+            && callback.getEvent() == Event.CONSIDER_MAKING_COSTS_ORDER
+            && isLegalRepJourney(callback.getCaseDetails().getCaseData());
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<AsylumCase> handle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        final AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+
+        String consideredCostsType = asylumCase.read(JUDGE_APPLIED_COSTS_TYPES, DynamicList.class)
+            .orElseThrow(() -> new IllegalStateException("judgeAppliedCostsTypes is not present"))
+            .getValue()
+            .getLabel();
+
+        String respondentToCostsOrder = asylumCase.read(RESPONDENT_TO_COSTS_ORDER, String.class)
+            .orElseThrow(() -> new IllegalStateException("respondentToCostsOrder is not present"));
+
+        String tribunalConsideringReason = asylumCase.read(TRIBUNAL_CONSIDERING_REASON, String.class)
+            .orElseThrow(() -> new IllegalStateException("tribunalConsideringReason is not present"));
+
+        Optional<List<IdValue<Document>>> judgeEvidenceForCostsOrder = asylumCase.read(JUDGE_EVIDENCE_FOR_COSTS_ORDER);
+
+        Optional<List<IdValue<ApplyForCosts>>> maybeExistingApplyForCosts = asylumCase.read(APPLIES_FOR_COSTS);
+
+        final List<IdValue<ApplyForCosts>> existingAppliesForCosts = maybeExistingApplyForCosts.orElse(Collections.emptyList());
+
+        List<IdValue<ApplyForCosts>> allApplyForCosts =
+            applyForCostsAppender.append(
+                existingAppliesForCosts,
+                CostsDecision.PENDING.toString(),
+                consideredCostsType,
+                tribunalConsideringReason,
+                judgeEvidenceForCostsOrder.orElse(Collections.emptyList()),
+                respondentToCostsOrder
+            );
+
+        asylumCase.write(APPLIES_FOR_COSTS, allApplyForCosts);
+        //Flag if the event has been completed
+        asylumCase.write(IS_APPLIED_FOR_COSTS, YesOrNo.YES);
+
+        asylumCase.clear(JUDGE_APPLIED_COSTS_TYPES);
+        asylumCase.clear(RESPONDENT_TO_COSTS_ORDER);
+        asylumCase.clear(TRIBUNAL_CONSIDERING_REASON);
+        asylumCase.clear(JUDGE_EVIDENCE_FOR_COSTS_ORDER);
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
