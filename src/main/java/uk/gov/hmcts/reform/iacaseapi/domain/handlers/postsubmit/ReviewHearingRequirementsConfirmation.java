6f0fe665d05f16f74b34ad7d96e68cdcfd2c53a2
DennisHoV1 <106669367+DennisHoV1@users.noreply.github.com>
2023-12-19 19:12:50 +0000

    RIA-7482 remind Legal Officer to add case flags after reviewing hearing requirements (#1872)
    * RIA-7482 remind Legal Officer to add case flags after reviewing hearing requirements
    
    * Bumping chart version/ fixing aliases
    
    ---------
    
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/ReviewHearingRequirementsConfirmation.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/ReviewHearingRequirementsConfirmation.java
@@ -37,10 +37,28 @@ public class ReviewHearingRequirementsConfirmation implements PostSubmitCallback
             new PostSubmitCallbackResponse();
 
         postSubmitResponse.setConfirmationHeader("# You've recorded the agreed hearing adjustments");
-        postSubmitResponse.setConfirmationBody(
-            "#### What happens next\n\n"
-            + "The listing team will now list the case. All parties will be notified when the Hearing Notice is available to view.<br><br>"
-        );
+        StringBuilder messageContentString = new StringBuilder("#### What happens next\n\n");
+
+        if (Arrays.asList(
+                Event.REVIEW_HEARING_REQUIREMENTS,
+                Event.UPDATE_HEARING_ADJUSTMENTS
+        ).contains(callback.getEvent())) {
+            String addCaseFlagUrl =
+                    "/case/IA/Asylum/"
+                            + callback.getCaseDetails().getId()
+                            + "/trigger/createFlag";
+
+            String manageCaseFlagUrl =
+                    "/case/IA/Asylum/"
+                            + callback.getCaseDetails().getId()
+                            + "/trigger/manageFlags";
+
+            messageContentString.append("You should ensure that the case flags reflect the hearing requests that have been approved. This may require adding new case flags or making active flags inactive.\n\n"
+                    + "[Add case flag](" + addCaseFlagUrl + ")<br>"
+                    + "[Manage case flags](" + manageCaseFlagUrl + ")<br><br>");
+        }
+        messageContentString.append("The listing team will now list the case. All parties will be notified when the Hearing Notice is available to view.<br><br>");
+        postSubmitResponse.setConfirmationBody(messageContentString.toString());
 
         return postSubmitResponse;
     }
