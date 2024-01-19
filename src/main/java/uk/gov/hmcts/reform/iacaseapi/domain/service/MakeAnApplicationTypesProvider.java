a638006fa1d2013036ce273626cd83300370ca99
Miguel.Martin1 <Miguel.Martin1@hmcts.net>
2024-01-19 13:53:42 +0100

    RIA-8496 Add change hearing type option to final bundling event for makeAnApplication

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/MakeAnApplicationTypesProvider.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/MakeAnApplicationTypesProvider.java
@@ -94,6 +94,7 @@ public class MakeAnApplicationTypesProvider {
                 values.add(new Value(WITHDRAW.name(), WITHDRAW.toString()));
                 values.add(new Value(LINK_OR_UNLINK.name(), LINK_OR_UNLINK.toString()));
                 values.add(new Value(JUDGE_REVIEW.name(), JUDGE_REVIEW.toString()));
+                values.add(new Value(CHANGE_HEARING_TYPE.name(), CHANGE_HEARING_TYPE.toString()));
                 values.add(new Value(OTHER.name(), OTHER.toString()));
                 break;
 
