c8f4ed49aba8a87c30193f61decec97c73c86789
JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
2024-01-25 15:15:52 +0000

    RIA-8101 DLRM judge rework - test update

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -122,6 +122,6 @@ class EventTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(117, Event.values().length);
+        assertEquals(116, Event.values().length);
     }
 }
