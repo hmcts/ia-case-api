0af34af4380fc7097548cd5763f1ee3fda9620af
colmlynch60 <113999496+colmlynch60@users.noreply.github.com>
2024-02-09 09:26:17 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -31,19 +31,6 @@ data "azurerm_key_vault" "ia_key_vault" {
   resource_group_name = "${local.key_vault_name}"
 }
 
-module "ia_case_api_database_11" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = "${var.product}-${var.component}-postgres-11-db"
-  location           = "${var.location}"
-  env                = "${var.env}"
-  database_name      = "${var.postgresql_database_name}"
-  postgresql_user    = "${var.postgresql_user}"
-  postgresql_version = "11"
-  common_tags        =  merge(var.common_tags, tomap({"lastUpdated" = "${timestamp()}"}))
-  subscription       = "${var.subscription}"
-  backup_retention_days = "${var.database_backup_retention_days}"
-}
-
 module "ia-case-api-db-v15" {
   providers = {
     azurerm.postgres_network = azurerm.cft_vnet
@@ -70,12 +57,6 @@ module "ia-case-api-db-v15" {
   pgsql_version   = "15"
   admin_user_object_id = var.jenkins_AAD_objectId
   force_user_permissions_trigger = "1"
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-PASS-11" {
-  name         = "${var.component}-POSTGRES-PASS-11"
-  value        = module.ia_case_api_database_11.postgresql_password
-  key_vault_id = data.azurerm_key_vault.ia_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS-15" {
