7886a32f29d3eb833156334634433ed657e94cea
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2024-02-28 09:26:54 +0000

    RIA-8714: Fix issue with hearings dynamic list on start 'update hearing request' event (#1985)

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/HearingsUpdateHearingRequestFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/HearingsUpdateHearingRequestFunctionalTest.java
@@ -1,11 +1,12 @@
 package uk.gov.hmcts.reform.iacaseapi;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.UPDATE_HEARING_REQUEST;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State.LISTING;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +19,13 @@ import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseData;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.HearingsUpdateHearingRequestMidEventHandler;
 
 /**
  * This functional test class covers all callback handlers in relation to Update Hearing including.
- * {@link uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.HearingsUpdateHearingRequest}
+ * {@link HearingsUpdateHearingRequestMidEventHandler}
+ * {@link uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.HearingsUpdateHearingRequestPreparer}
  * {@link uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.HearingsUpdateHearingRequestSubmit}
- * {@link uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit.HearingsUpdateHearingRequestConfirmation}
  */
 @Slf4j
 @ActiveProfiles("functional")
@@ -37,7 +39,7 @@ public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationT
 
     @ParameterizedTest
     @CsvSource({ "true", "false" })
-    void should_handle_update_hearing_request_mid_event_successfully(boolean isAipJourney) {
+    void should_handle_update_hearing_request_about_to_start_successfully(boolean isAipJourney) {
         Case result = createAndGetCase(isAipJourney);
 
         log.info("caseOfficerToken: " + caseOfficerToken);
@@ -53,7 +55,7 @@ public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationT
             null
         );
 
-        Callback callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
+        Callback<CaseData> callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
         given(caseApiSpecification)
             .when()
             .contentType("application/json")
@@ -64,6 +66,68 @@ public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationT
             .then()
             .statusCode(HttpStatus.SC_OK)
             .log().all(true);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "true", "false" })
+    void should_fail_to_handle_update_hearing_request_about_to_start_due_to_invalid_authentication(boolean isAipJourney) {
+        Case result = createAndGetCase(isAipJourney);
+
+        CaseDetails<CaseData> caseDetails = new CaseDetails<>(
+            result.getCaseId(),
+            "IA",
+            LISTING,
+            result.getCaseData(),
+            LocalDateTime.now(),
+            "securityClassification",
+            null
+        );
+
+        Callback<CaseData> callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
+
+        Response response = given(caseApiSpecification)
+            .when()
+            .contentType("application/json")
+            .header(new Header(AUTHORIZATION, "invalidToken"))
+            .header(new Header(SERVICE_AUTHORIZATION, s2sToken))
+            .body(callback)
+            .post("/asylum/ccdAboutToStart")
+            .then()
+            .log().all(true)
+            .extract().response();
+
+        assertEquals(401, response.getStatusCode());
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "true", "false" })
+    void should_handle_update_hearing_request_mid_event_successfully(boolean isAipJourney) {
+        Case result = createAndGetCase(isAipJourney);
+
+        CaseDetails<CaseData> caseDetails = new CaseDetails<>(
+            result.getCaseId(),
+            "IA",
+            LISTING,
+            result.getCaseData(),
+            LocalDateTime.now(),
+            "securityClassification",
+            null
+        );
+
+        Callback<CaseData> callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
+
+        Response response = given(caseApiSpecification)
+            .when()
+            .contentType("application/json")
+            .header(new Header(AUTHORIZATION, caseOfficerToken))
+            .header(new Header(SERVICE_AUTHORIZATION, s2sToken))
+            .body(callback)
+            .post("/asylum/ccdMidEvent")
+            .then()
+            .log().all(true)
+            .extract().response();
+
+        assertEquals(200, response.getStatusCode());
     }
 
     @ParameterizedTest
@@ -81,7 +145,7 @@ public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationT
             null
         );
 
-        Callback callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
+        Callback<CaseData> callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
 
         Response response = given(caseApiSpecification)
             .when()
@@ -89,7 +153,7 @@ public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationT
             .header(new Header(AUTHORIZATION, "invalidToken"))
             .header(new Header(SERVICE_AUTHORIZATION, s2sToken))
             .body(callback)
-            .post("/asylum/ccdAboutToStart")
+            .post("/asylum/ccdMidEvent")
             .then()
             .log().all(true)
             .extract().response();
@@ -112,7 +176,7 @@ public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationT
             null
         );
 
-        Callback callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
+        Callback<CaseData> callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
 
         Response response = given(caseApiSpecification)
             .when()
@@ -120,7 +184,7 @@ public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationT
             .header(new Header(AUTHORIZATION, caseOfficerToken))
             .header(new Header(SERVICE_AUTHORIZATION, s2sToken))
             .body(callback)
-            .post("/asylum/ccdMidEvent")
+            .post("/asylum/ccdAboutToSubmit")
             .then()
             .log().all(true)
             .extract().response();
@@ -143,69 +207,7 @@ public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationT
             null
         );
 
-        Callback callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
-
-        Response response = given(caseApiSpecification)
-            .when()
-            .contentType("application/json")
-            .header(new Header(AUTHORIZATION, "invalidToken"))
-            .header(new Header(SERVICE_AUTHORIZATION, s2sToken))
-            .body(callback)
-            .post("/asylum/ccdMidEvent")
-            .then()
-            .log().all(true)
-            .extract().response();
-
-        assertEquals(401, response.getStatusCode());
-    }
-
-    @ParameterizedTest
-    @CsvSource({ "true", "false" })
-    void should_handle_update_hearing_request_confirmation_successfully(boolean isAipJourney) {
-        Case result = createAndGetCase(isAipJourney);
-
-        CaseDetails<CaseData> caseDetails = new CaseDetails<>(
-            result.getCaseId(),
-            "IA",
-            LISTING,
-            result.getCaseData(),
-            LocalDateTime.now(),
-            "securityClassification",
-            null
-        );
-
-        Callback callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
-
-        Response response = given(caseApiSpecification)
-            .when()
-            .contentType("application/json")
-            .header(new Header(AUTHORIZATION, caseOfficerToken))
-            .header(new Header(SERVICE_AUTHORIZATION, s2sToken))
-            .body(callback)
-            .post("/asylum/ccdAboutToSubmit")
-            .then()
-            .log().all(true)
-            .extract().response();
-
-        assertEquals(200, response.getStatusCode());
-    }
-
-    @ParameterizedTest
-    @CsvSource({ "true", "false" })
-    void should_fail_to_handle_update_hearing_request_confirmation_due_to_invalid_authentication(boolean isAipJourney) {
-        Case result = createAndGetCase(isAipJourney);
-
-        CaseDetails<CaseData> caseDetails = new CaseDetails<>(
-            result.getCaseId(),
-            "IA",
-            LISTING,
-            result.getCaseData(),
-            LocalDateTime.now(),
-            "securityClassification",
-            null
-        );
-
-        Callback callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
+        Callback<CaseData> callback = new Callback<>(caseDetails, Optional.of(caseDetails), UPDATE_HEARING_REQUEST);
 
         Response response = given(caseApiSpecification)
             .when()
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HearingsUpdateHearingRequestMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HearingsUpdateHearingRequestMidEventHandler.java
@@ -1,23 +1,18 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CHANGE_HEARINGS;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CHANGE_HEARING_DATE_RANGE_EARLIEST;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CHANGE_HEARING_DATE_RANGE_LATEST;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CHANGE_HEARING_DATE_TYPE;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CHANGE_HEARING_DATE_YES_NO;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.MANUAL_UPDATE_HEARING_REQUIRED;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.UPDATE_HEARING_REQUEST;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.MID_EVENT;
 
-import java.util.Objects;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.DynamicList;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
@@ -26,22 +21,15 @@ import uk.gov.hmcts.reform.iacaseapi.domain.service.IaHearingsApiService;
 
 @Component
 @Slf4j
-public class HearingsUpdateHearingRequest implements PreSubmitCallbackHandler<AsylumCase> {
+@RequiredArgsConstructor
+public class HearingsUpdateHearingRequestMidEventHandler implements PreSubmitCallbackHandler<AsylumCase> {
 
-    public static final String NO_HEARINGS_ERROR_MESSAGE =
-        "You've made an invalid request. You must request a substantive hearing before you can update a hearing.";
     private static final String UPDATE_HEARING_DATE_PAGE_ID = "updateHearingDate";
     private static final String UPDATE_HEARING_LIST_PAGE_ID = "updateHearingList";
     private static final String COMPLIANT_DATE_RANGE_NEEDED = "Earliest hearing date or Latest hearing date required";
     private static final String CHOOSE_A_DATE_RANGE = "ChooseADateRange";
 
-    private IaHearingsApiService iaHearingsApiService;
-
-    public HearingsUpdateHearingRequest(
-            IaHearingsApiService iaHearingsApiService
-    ) {
-        this.iaHearingsApiService = iaHearingsApiService;
-    }
+    private final IaHearingsApiService iaHearingsApiService;
 
     @Override
     public boolean canHandle(
@@ -51,9 +39,7 @@ public class HearingsUpdateHearingRequest implements PreSubmitCallbackHandler<As
         requireNonNull(callbackStage, "callbackStage must not be null");
         requireNonNull(callback, "callback must not be null");
 
-        return (callbackStage == ABOUT_TO_START
-                || callbackStage == MID_EVENT)
-                && Objects.equals(Event.UPDATE_HEARING_REQUEST, callback.getEvent());
+        return callbackStage == MID_EVENT && callback.getEvent() == UPDATE_HEARING_REQUEST;
     }
 
     public PreSubmitCallbackResponse<AsylumCase> handle(
@@ -66,54 +52,22 @@ public class HearingsUpdateHearingRequest implements PreSubmitCallbackHandler<As
         requireNonNull(callback, "callback must not be null");
 
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+        String errorMessage = null;
 
-        String pageId = callback.getPageId();
-
-        if (StringUtils.equals(pageId, UPDATE_HEARING_LIST_PAGE_ID)) {
-            if (StringUtils.equals(pageId, UPDATE_HEARING_LIST_PAGE_ID)
-                && asylumCase.read(CHANGE_HEARINGS).isEmpty()) {
-
-                asylumCase = getHearings(callback);
-
-                if (hasNoHearings(asylumCase)) {
-                    PreSubmitCallbackResponse<AsylumCase> response = new PreSubmitCallbackResponse<>(asylumCase);
-                    response.addError(NO_HEARINGS_ERROR_MESSAGE);
-                    return response;
-                }
-            } else {
-                asylumCase = getHearingDetails(callback);
-            }
+        switch (callback.getPageId()) {
+            case UPDATE_HEARING_LIST_PAGE_ID -> asylumCase = iaHearingsApiService.midEvent(callback);
+            case UPDATE_HEARING_DATE_PAGE_ID -> errorMessage = isNeededDateRangeNonCompliant(asylumCase)
+                ? COMPLIANT_DATE_RANGE_NEEDED
+                : null;
+            default -> { }
         }
 
-        if (StringUtils.equals(pageId, UPDATE_HEARING_DATE_PAGE_ID)
-            && isNeededDateRangeNonCompliant(asylumCase)) {
-
-            PreSubmitCallbackResponse<AsylumCase> response = new PreSubmitCallbackResponse<>(asylumCase);
-            response.addError(COMPLIANT_DATE_RANGE_NEEDED);
-            return response;
+        PreSubmitCallbackResponse<AsylumCase> response = new PreSubmitCallbackResponse<>(asylumCase);
+        if (errorMessage != null) {
+            response.addError(errorMessage);
         }
 
-        asylumCase.clear(MANUAL_UPDATE_HEARING_REQUIRED);
-
-        return new PreSubmitCallbackResponse<>(asylumCase);
-    }
-
-    private boolean hasNoHearings(AsylumCase asylumCase) {
-        Optional<DynamicList> hearings = asylumCase.read(CHANGE_HEARINGS, DynamicList.class);
-
-        if (hearings.isEmpty()) {
-            return true;
-        } else {
-            return hearings.get().getListItems().isEmpty();
-        }
-    }
-
-    private AsylumCase getHearings(Callback<AsylumCase> callback) {
-        return iaHearingsApiService.aboutToStart(callback);
-    }
-
-    private AsylumCase getHearingDetails(Callback<AsylumCase> callback) {
-        return iaHearingsApiService.midEvent(callback);
+        return response;
     }
 
     private boolean isNeededDateRangeNonCompliant(AsylumCase asylumCase) {
