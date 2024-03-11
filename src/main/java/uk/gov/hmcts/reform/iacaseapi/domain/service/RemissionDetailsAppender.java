22adc8aa2b9f4ad849e610381fe43149db51b441
JakeCohenSol <jake.cohen@solirius.com>
2024-03-11 17:47:13 +0000

    DIAC-298 adding logging statements for remissions details appender

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/RemissionDetailsAppender.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/RemissionDetailsAppender.java
@@ -107,10 +107,13 @@ public class RemissionDetailsAppender {
     }
 
     public void setRemissions(List<IdValue<RemissionDetails>> remissions) {
+        log.info("SETTING REMISSION: " + remissions);
+        log.info("TO: " + this.remissions);
         this.remissions = remissions;
     }
 
     public List<IdValue<RemissionDetails>> getRemissions() {
+        log.info("GETTING REMISSIONS: " + remissions);
         return remissions;
     }
 }
