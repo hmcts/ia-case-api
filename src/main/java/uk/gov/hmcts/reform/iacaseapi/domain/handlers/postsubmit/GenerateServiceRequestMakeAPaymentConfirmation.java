b770dd728274c421a44348eb2191de29c4cd16de
nilay <nilay.patel@hmcts.net>
2024-03-20 14:58:02 +0000

    DIAC-210 debug for admin officer authorisation

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/GenerateServiceRequestMakeAPaymentConfirmation.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/GenerateServiceRequestMakeAPaymentConfirmation.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit;
 
 import static java.util.Objects.requireNonNull;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
@@ -9,6 +10,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PostSubmitCallbackHandler;
 
+@Slf4j
 @Component
 public class GenerateServiceRequestMakeAPaymentConfirmation implements PostSubmitCallbackHandler<AsylumCase> {
 
@@ -16,20 +18,21 @@ public class GenerateServiceRequestMakeAPaymentConfirmation implements PostSubmi
         Callback<AsylumCase> callback
     ) {
         requireNonNull(callback, "callback must not be null");
-
+        log.info("Test 12");
         return callback.getEvent() == Event.GENERATE_SERVICE_REQUEST;
     }
 
     public PostSubmitCallbackResponse handle(
         Callback<AsylumCase> callback
     ) {
+        log.info("Test 13");
         if (!canHandle(callback)) {
             throw new IllegalStateException("Cannot handle callback");
         }
-
+        log.info("Test 14");
         PostSubmitCallbackResponse postSubmitResponse =
             new PostSubmitCallbackResponse();
-
+        log.info("Test 15");
         postSubmitResponse.setConfirmationHeader("# You have created a service request");
         postSubmitResponse.setConfirmationBody(
                 "### What happens next\n\n"
@@ -37,7 +40,7 @@ public class GenerateServiceRequestMakeAPaymentConfirmation implements PostSubmi
                         + "[Service requests](cases/case-details/"
                         + callback.getCaseDetails().getId() + "#Service%20Request)\n\n"
         );
-
+        log.info("Test 16");
         return postSubmitResponse;
     }
 
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ServiceRequestHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ServiceRequestHandler.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.JOURNEY_TYPE;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
@@ -14,6 +15,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacaseapi.domain.service.FeePayment;
 
+@Slf4j
 @Component
 public class ServiceRequestHandler implements PreSubmitCallbackHandler<AsylumCase> {
 
@@ -33,7 +35,7 @@ public class ServiceRequestHandler implements PreSubmitCallbackHandler<AsylumCas
     ) {
         requireNonNull(callbackStage, "callbackStage must not be null");
         requireNonNull(callback, "callback must not be null");
-
+        log.info("Test 3");
         return (callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT)
                &&  callback.getEvent() == Event.GENERATE_SERVICE_REQUEST
                && isFeePaymentEnabled;
@@ -43,18 +45,19 @@ public class ServiceRequestHandler implements PreSubmitCallbackHandler<AsylumCas
         PreSubmitCallbackStage callbackStage,
         Callback<AsylumCase> callback
     ) {
+        log.info("Test 4");
         if (!canHandle(callbackStage, callback)) {
             throw new IllegalStateException("Cannot handle callback");
         }
-
+        log.info("Test 5");
 
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
-
+        log.info("Test 6");
         if (!isAipJourney(asylumCase)) {
             asylumCase = feePayment.aboutToSubmit(callback);
 
         }
-
+        log.info("Test 7");
         return new PreSubmitCallbackResponse<>(asylumCase);
     }
 
