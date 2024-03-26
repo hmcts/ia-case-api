f1d45fb8c0aa2841d68d8a71e2774cbc09a084c7
JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
2024-03-26 11:44:08 +0000

    RIA-8290 - Reheard appeal - event refactor

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/MarkAppealAsRemittedUploadDecisionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/MarkAppealAsRemittedUploadDecisionHandler.java
@@ -2,14 +2,8 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPEAL_REMITTED_DATE;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CASE_NOTES;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.COURT_REFERENCE_NUMBER;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.JUDGES_NAMES_TO_EXCLUDE;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.REHEARING_REASON;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.REMITTED_ADDITIONAL_INSTRUCTIONS;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.SOURCE_OF_REMITTAL;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.UPLOAD_REMITTAL_DECISION_DOC;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo.YES;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +18,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallb
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacaseapi.domain.service.Appender;
 
@@ -63,16 +58,21 @@ public class MarkAppealAsRemittedUploadDecisionHandler implements PreSubmitCallb
                 .getCaseDetails()
                 .getCaseData();
 
-        Document mayBeDecisionDocument = asylumCase.read(UPLOAD_REMITTAL_DECISION_DOC, Document.class)
-            .orElseThrow(() -> new IllegalStateException("uploadRemittalDecisionDoc is not present"));
+        if (asylumCase.read(IS_REHEARD_APPEAL_ENABLED, YesOrNo.class).map(flag -> flag.equals(YES)).orElse(false)) {
+            asylumCase.write(CASE_FLAG_SET_ASIDE_REHEARD_EXISTS, YES);
 
-        Document decisionDocumnent = new Document(mayBeDecisionDocument.getDocumentUrl(),
-            mayBeDecisionDocument.getDocumentBinaryUrl(), getRemittalDecisionFilename(asylumCase));
+            Document mayBeDecisionDocument = asylumCase.read(UPLOAD_REMITTAL_DECISION_DOC, Document.class)
+                .orElseThrow(() -> new IllegalStateException("uploadRemittalDecisionDoc is not present"));
 
-        asylumCase.write(UPLOAD_REMITTAL_DECISION_DOC, decisionDocumnent);
-        asylumCase.write(APPEAL_REMITTED_DATE, dateProvider.now().toString());
-        asylumCase.write(REHEARING_REASON, "Remitted");
-        addRemittedCaseNote(asylumCase);
+            Document decisionDocumnent = new Document(mayBeDecisionDocument.getDocumentUrl(),
+                mayBeDecisionDocument.getDocumentBinaryUrl(), getRemittalDecisionFilename(asylumCase));
+
+            asylumCase.write(UPLOAD_REMITTAL_DECISION_DOC, decisionDocumnent);
+            asylumCase.write(APPEAL_REMITTED_DATE, dateProvider.now().toString());
+            asylumCase.write(REHEARING_REASON, "Remitted");
+            addRemittedCaseNote(asylumCase);
+
+        }
         return new PreSubmitCallbackResponse<>(asylumCase);
     }
 
@@ -83,36 +83,36 @@ public class MarkAppealAsRemittedUploadDecisionHandler implements PreSubmitCallb
             .orElseThrow(() -> new IllegalStateException("Court reference number not present"));
 
         return courtReferenceNumber
-            + "-Decision-to-remit.pdf";
+               + "-Decision-to-remit.pdf";
     }
 
     private void addRemittedCaseNote(AsylumCase asylumCase) {
         final SourceOfRemittal sourceOfRemittal = asylumCase.read(SOURCE_OF_REMITTAL, SourceOfRemittal.class)
-                .orElseThrow(() -> new IllegalStateException("sourceOfRemittal is not present"));
+            .orElseThrow(() -> new IllegalStateException("sourceOfRemittal is not present"));
         final String courtRef = asylumCase.read(COURT_REFERENCE_NUMBER, String.class)
-                .orElseThrow(() -> new IllegalStateException("Court reference number not present"));
+            .orElseThrow(() -> new IllegalStateException("Court reference number not present"));
         final String judgesExcluded = asylumCase.read(JUDGES_NAMES_TO_EXCLUDE, String.class)
-                .orElse("");
+            .orElse("");
         final String additionalInst = asylumCase.read(REMITTED_ADDITIONAL_INSTRUCTIONS, String.class)
-                .orElse("");
+            .orElse("");
 
         String description = String.format("Reason for rehearing: Remitted" + System.lineSeparator() +
-                "Remitted from: %s" + System.lineSeparator() +
-                "%s reference: %s" + System.lineSeparator() +
-                "Excluded judges: %s" + System.lineSeparator() +
-                "Listing instructions: %s" + System.lineSeparator(),
-                sourceOfRemittal, sourceOfRemittal, courtRef, judgesExcluded, additionalInst);
+                                           "Remitted from: %s" + System.lineSeparator() +
+                                           "%s reference: %s" + System.lineSeparator() +
+                                           "Excluded judges: %s" + System.lineSeparator() +
+                                           "Listing instructions: %s" + System.lineSeparator(),
+            sourceOfRemittal, sourceOfRemittal, courtRef, judgesExcluded, additionalInst);
 
         final CaseNote newCaseNote = new CaseNote(
-                "Appeal marked as remitted",
-                description,
-                "Admin",
-                dateProvider.now().toString()
+            "Appeal marked as remitted",
+            description,
+            "Admin",
+            dateProvider.now().toString()
         );
 
         Optional<List<IdValue<CaseNote>>> maybeExistingCaseNotes = asylumCase.read(CASE_NOTES);
         List<IdValue<CaseNote>> allCaseNotes =
-                caseNoteAppender.append(newCaseNote, maybeExistingCaseNotes.orElse(emptyList()));
+            caseNoteAppender.append(newCaseNote, maybeExistingCaseNotes.orElse(emptyList()));
         asylumCase.write(CASE_NOTES, allCaseNotes);
 
     }
