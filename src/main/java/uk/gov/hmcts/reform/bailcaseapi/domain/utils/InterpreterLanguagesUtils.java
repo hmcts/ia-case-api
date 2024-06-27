1e0dadecbea9f2955da328192a8f0bd60318d96b
DennisHoV1 <106669367+DennisHoV1@users.noreply.github.com>
2024-06-27 10:27:37 +0100

    RIA-9104 bails interpreter language stored incorrectly (#509)
    * RIA-9104 bails interpreter language stored incorrectly
    
    * remove the duplicated value for applicant and fcs interpreter
    
    * import the class one by one instead of importing all class

--- src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/utils/InterpreterLanguagesUtils.java
+++ src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/utils/InterpreterLanguagesUtils.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.bailcaseapi.domain.utils;
 
 import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.*;
 
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCase;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.DynamicList;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.InterpreterLanguageRefData;
@@ -12,37 +13,13 @@ import uk.gov.hmcts.reform.bailcaseapi.infrastructure.clients.model.dto.hearingd
 import uk.gov.hmcts.reform.bailcaseapi.infrastructure.service.RefDataUserService;
 
 import java.util.List;
+import java.util.Optional;
 
 public class InterpreterLanguagesUtils {
     public static final String IS_CHILD_REQUIRED = "Y";
 
     private InterpreterLanguagesUtils() {
         // Utils classes should not have public or default constructors
-    }
-
-    public static InterpreterLanguageRefData generateDynamicList(RefDataUserService refDataUserService, String languageCategory) {
-        List<CategoryValues> languages;
-        DynamicList dynamicListOfLanguages;
-
-        try {
-            CommonDataResponse commonDataResponse = refDataUserService.retrieveCategoryValues(
-                languageCategory,
-                IS_CHILD_REQUIRED
-            );
-
-            languages = refDataUserService.filterCategoryValuesByCategoryId(commonDataResponse, languageCategory);
-
-            dynamicListOfLanguages = new DynamicList(new Value("", ""),
-                                                     refDataUserService.mapCategoryValuesToDynamicListValues(languages));
-
-        } catch (Exception e) {
-            throw new BailCaseServiceResponseException(String.format("Could not read response by RefData service for %s(s)", languageCategory), e);
-        }
-
-        return new InterpreterLanguageRefData(
-            dynamicListOfLanguages,
-            "",
-            "");
     }
 
     public static final List<BailCaseFieldDefinition> FCS_N_INTERPRETER_LANGUAGE_CATEGORY_FIELD = List.of(
@@ -94,5 +71,55 @@ public class InterpreterLanguagesUtils {
         FCS_INTERPRETER_SIGN_LANGUAGE_BOOKING_STATUS_4
     );
 
+    public static InterpreterLanguageRefData generateDynamicList(RefDataUserService refDataUserService, String languageCategory) {
+        List<CategoryValues> languages;
+        DynamicList dynamicListOfLanguages;
 
+        try {
+            CommonDataResponse commonDataResponse = refDataUserService.retrieveCategoryValues(
+                languageCategory,
+                IS_CHILD_REQUIRED
+            );
+
+            languages = refDataUserService.filterCategoryValuesByCategoryId(commonDataResponse, languageCategory);
+
+            dynamicListOfLanguages = new DynamicList(new Value("", ""),
+                                                     refDataUserService.mapCategoryValuesToDynamicListValues(languages));
+
+        } catch (Exception e) {
+            throw new BailCaseServiceResponseException(String.format("Could not read response by RefData service for %s(s)", languageCategory), e);
+        }
+
+        return new InterpreterLanguageRefData(
+            dynamicListOfLanguages,
+            "",
+            "");
+    }
+
+    public static void sanitizeInterpreterLanguageRefDataComplexType(BailCase bailCase, BailCaseFieldDefinition bailCaseFieldDefinition) {
+        InterpreterLanguageRefData sanitizedComplexType;
+        Optional<InterpreterLanguageRefData> language =
+            bailCase.read(bailCaseFieldDefinition, InterpreterLanguageRefData.class);
+
+        if (language.isPresent()) {
+            InterpreterLanguageRefData spokenComplexType = language.get();
+
+            sanitizedComplexType = clearComplexTypeField(spokenComplexType);
+            bailCase.write(bailCaseFieldDefinition, sanitizedComplexType);
+        }
+    }
+
+    private static InterpreterLanguageRefData clearComplexTypeField(InterpreterLanguageRefData languageComplexType) {
+        if (languageComplexType.getLanguageManualEntry().contains("No")
+            && languageComplexType.getLanguageManualEntryDescription() != null) {
+
+            languageComplexType.setLanguageManualEntryDescription(null);
+
+        } else if (languageComplexType.getLanguageManualEntry().contains("Yes")
+            && languageComplexType.getLanguageRefData() != null) {
+
+            languageComplexType.setLanguageRefData(null);
+        }
+        return languageComplexType;
+    }
 }
--- src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/utils/InterpreterLanguagesUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/utils/InterpreterLanguagesUtilsTest.java
@@ -4,17 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCase;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.DynamicList;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.InterpreterLanguageRefData;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.Value;
 import uk.gov.hmcts.reform.bailcaseapi.infrastructure.clients.model.dto.hearingdetails.CategoryValues;
 import uk.gov.hmcts.reform.bailcaseapi.infrastructure.clients.model.dto.hearingdetails.CommonDataResponse;
 import uk.gov.hmcts.reform.bailcaseapi.infrastructure.service.RefDataUserService;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.APPLICANT_INTERPRETER_SPOKEN_LANGUAGE;
+import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.FCS1_INTERPRETER_SIGN_LANGUAGE;
 
 @ExtendWith(MockitoExtension.class)
 public class InterpreterLanguagesUtilsTest {
@@ -28,6 +35,8 @@ public class InterpreterLanguagesUtilsTest {
     RefDataUserService refDataUserService;
     @Mock
     Value value;
+    @Mock
+    private BailCase bailCase;
 
     @Test
     void should_generate_dynamic_list() {
@@ -46,5 +55,31 @@ public class InterpreterLanguagesUtilsTest {
         assertEquals(List.of(value), result.getLanguageRefData().getListItems());
         assertTrue(result.getLanguageManualEntry().isEmpty());
         assertEquals("", result.getLanguageManualEntryDescription());
+    }
+
+    @Test
+    void should_clear_duplicated_interpreter_value_when_applicant_or_fcs_require_interpreter_service() {
+        InterpreterLanguageRefData spokenInterpreterLanguage = new InterpreterLanguageRefData(
+            new DynamicList(new Value("scl", "Shina"), Collections.emptyList()),
+            "Yes",
+            "spoken manual");
+        InterpreterLanguageRefData signInterpreterLanguage = new InterpreterLanguageRefData(
+            new DynamicList(new Value("jpn", "Japanese"), Collections.emptyList()),
+            "No",
+            "sign manual");
+
+        when(bailCase.read(APPLICANT_INTERPRETER_SPOKEN_LANGUAGE, InterpreterLanguageRefData.class))
+            .thenReturn(Optional.of(spokenInterpreterLanguage));
+        when(bailCase.read(FCS1_INTERPRETER_SIGN_LANGUAGE, InterpreterLanguageRefData.class))
+            .thenReturn(Optional.of(signInterpreterLanguage));
+
+        InterpreterLanguagesUtils.sanitizeInterpreterLanguageRefDataComplexType(bailCase, APPLICANT_INTERPRETER_SPOKEN_LANGUAGE);
+        InterpreterLanguagesUtils.sanitizeInterpreterLanguageRefDataComplexType(bailCase, FCS1_INTERPRETER_SIGN_LANGUAGE);
+
+        assertNull(bailCase.read(APPLICANT_INTERPRETER_SPOKEN_LANGUAGE,InterpreterLanguageRefData.class)
+                       .get().getLanguageRefData());
+        assertNull(bailCase.read(FCS1_INTERPRETER_SIGN_LANGUAGE,InterpreterLanguageRefData.class)
+                       .get().getLanguageManualEntryDescription());
+
     }
 }
