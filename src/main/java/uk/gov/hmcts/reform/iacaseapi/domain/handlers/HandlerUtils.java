5dd976ed838dd527df536c7ed1db3adfd2fdd834
robin-ieong <robin.ieong@version1.com>
2024-07-03 16:11:08 +0100

    Fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/HandlerUtils.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/HandlerUtils.java
@@ -27,7 +27,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import com.fasterxml.jackson.databind.JsonNode;
