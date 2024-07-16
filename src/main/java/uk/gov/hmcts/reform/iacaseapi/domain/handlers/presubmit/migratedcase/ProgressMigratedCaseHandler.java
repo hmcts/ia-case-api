ff55a698f2cd9be333d39d6af2ccb1791a4374d7
Ivan Kirsanov <ivan.kirsanov@version1.com>
2024-07-16 12:22:07 +0200

    RIA-8969: Quick fix for saving state

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/migratedcase/ProgressMigratedCaseHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/migratedcase/ProgressMigratedCaseHandler.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.migratedcase;
 
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.ARIA_DESIRED_STATE;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.ARIA_DESIRED_STATE_SELECTED_VALUE;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.DESIRED_STATE_CORRECT;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.PROGRESS_MIGRATED_CASE;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_START;
 
@@ -13,12 +11,11 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
-import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackStateHandler;
 
 
 @Component
-public class ProgressMigratedCaseHandler implements PreSubmitCallbackHandler<AsylumCase> {
+public class ProgressMigratedCaseHandler implements PreSubmitCallbackStateHandler<AsylumCase> {
 
     public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
         requireNonNull(callbackStage, "callbackStage must not be null");
@@ -27,23 +24,18 @@ public class ProgressMigratedCaseHandler implements PreSubmitCallbackHandler<Asy
         return callbackStage == ABOUT_TO_START && callback.getEvent() == PROGRESS_MIGRATED_CASE;
     }
 
-    public PreSubmitCallbackResponse<AsylumCase> handle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage, Callback<AsylumCase>
+        callback, PreSubmitCallbackResponse<AsylumCase> callbackResponse) {
         if (!canHandle(callbackStage, callback)) {
             throw new IllegalStateException("Cannot handle callback");
         }
 
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
 
-        YesOrNo isTheDesiredStateCorrect = asylumCase.read(DESIRED_STATE_CORRECT, YesOrNo.class)
-            .orElseThrow(() -> new IllegalStateException("desiredStateCorrect is not present"));
+        State newDesiredState = asylumCase.read(ARIA_DESIRED_STATE, State.class)
+            .orElseThrow(() -> new IllegalStateException("ariaDesiredState is not present"));
 
-        if (isTheDesiredStateCorrect.equals(YesOrNo.NO)) {
-            State newDesiredState = asylumCase.read(ARIA_DESIRED_STATE, State.class)
-                .orElseThrow(() -> new IllegalStateException("ariaDesiredState is not present"));
-
-            asylumCase.write(ARIA_DESIRED_STATE_SELECTED_VALUE, newDesiredState.getDescription());
-        }
-
-        return new PreSubmitCallbackResponse<>(asylumCase);
+        return new PreSubmitCallbackResponse<>(asylumCase, newDesiredState);
     }
 }
