e1e30bbc0cfb7e4caa3d37097cf1f9ecea3364a8
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2024-07-22 14:01:25 +0100

    Update RoleAssignmentApiConsumerTest.java

--- src/contractTest/java/uk/gov/hmcts/reform/iacaseapi/consumer/roleassignment/RoleAssignmentApiConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/iacaseapi/consumer/roleassignment/RoleAssignmentApiConsumerTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
