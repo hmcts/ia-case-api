d07edf453697e477286ed27df7c37e6fd54d117f
nilay <nilay.patel@hmcts.net>
2024-08-13 15:03:11 +0100

    DIAC-546 scheduling timed event service to trigger SAVE_NOTIFICATIONS_TO_DATA event in 15s after a notification sends

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/AsylumCasePostNotificationApiSender.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/AsylumCasePostNotificationApiSender.java
@@ -4,10 +4,17 @@ import static java.util.Objects.requireNonNull;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.iacaseapi.domain.DateProvider;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.service.PostNotificationSender;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.Scheduler;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.model.TimedEvent;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Service
 public class AsylumCasePostNotificationApiSender implements PostNotificationSender<AsylumCase> {
@@ -15,32 +22,44 @@ public class AsylumCasePostNotificationApiSender implements PostNotificationSend
     private final AsylumCaseCallbackApiDelegator asylumCaseCallbackApiDelegator;
     private final String notificationsApiEndpoint;
     private final String ccdSubmittedPath;
+    private final boolean timedEventServiceEnabled;
+    private final DateProvider dateProvider;
+    private final Scheduler scheduler;
 
     public AsylumCasePostNotificationApiSender(
         AsylumCaseCallbackApiDelegator asylumCaseCallbackApiDelegator,
         @Value("${notificationsApi.endpoint}") String notificationsApiEndpoint,
-        @Value("${notificationsApi.ccdSubmittedPath}") String ccdSubmittedPath
+        @Value("${notificationsApi.ccdSubmittedPath}") String ccdSubmittedPath,
+        @Value("${featureFlag.timedEventServiceEnabled}") boolean timedEventServiceEnabled,
+        DateProvider dateProvider,
+        Scheduler scheduler
     ) {
         this.asylumCaseCallbackApiDelegator = asylumCaseCallbackApiDelegator;
         this.notificationsApiEndpoint = notificationsApiEndpoint;
         this.ccdSubmittedPath = ccdSubmittedPath;
+        this.timedEventServiceEnabled = timedEventServiceEnabled;
+        this.dateProvider = dateProvider;
+        this.scheduler = scheduler;
     }
 
     @Override
     public PostSubmitCallbackResponse send(Callback<AsylumCase> callback) {
         requireNonNull(callback, "callback must not be null");
-        //        ZonedDateTime scheduledDate = ZonedDateTime.of(dateProvider
-        //            .nowWithTime(), ZoneId.systemDefault()).plusMinutes(1L);
-        //        scheduler.schedule(
-        //            new TimedEvent(
-        //                "",
-        //                Event.SAVE_NOTIFICATION_TO_DATA,
-        //                scheduledDate,
-        //                "IA",
-        //                "Asylum",
-        //                callback.getCaseDetails().getId()
-        //            )
-        //        );
+        if (timedEventServiceEnabled ) {
+            ZonedDateTime scheduledTime = ZonedDateTime.of(dateProvider.nowWithTime(), ZoneId.systemDefault())
+                .plusSeconds(15);
+            scheduler.schedule(
+                new TimedEvent(
+                    "",
+                    Event.SAVE_NOTIFICATIONS_TO_DATA,
+                    scheduledTime,
+                    "IA",
+                    "Asylum",
+                    callback.getCaseDetails().getId()
+                )
+            );
+        }
+
         return asylumCaseCallbackApiDelegator.delegatePostSubmit(
             callback,
             notificationsApiEndpoint + ccdSubmittedPath
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/AsylumCasePostNotificationApiSenderTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/AsylumCasePostNotificationApiSenderTest.java
@@ -4,14 +4,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.iacaseapi.domain.DateProvider;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.Scheduler;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.model.TimedEvent;
+
+import java.time.*;
 
 @ExtendWith(MockitoExtension.class)
 @SuppressWarnings("unchecked")
@@ -24,30 +30,33 @@ class AsylumCasePostNotificationApiSenderTest {
     private AsylumCaseCallbackApiDelegator asylumCaseCallbackApiDelegator;
     @Mock
     private Callback<AsylumCase> callback;
+    @Mock
+    private CaseDetails<AsylumCase> caseDetails;
+    @Mock
+    private DateProvider dateProvider;
+    @Mock
+    private Scheduler scheduler;
 
     private AsylumCasePostNotificationApiSender asylumCasePostNotificationApiSender;
 
-    @BeforeEach
-    public void setUp() {
-
+    @Test
+    void should_delegate_callback_to_downstream_api() {
         asylumCasePostNotificationApiSender =
             new AsylumCasePostNotificationApiSender(
                 asylumCaseCallbackApiDelegator,
                 ENDPOINT,
-                CCD_SUBMITTED_PATH
+                CCD_SUBMITTED_PATH,
+                false,
+                dateProvider,
+                scheduler
             );
-    }
-
-    @Test
-    void should_delegate_callback_to_downstream_api() {
-
         final PostSubmitCallbackResponse notifiedAsylumCase = mock(PostSubmitCallbackResponse.class);
 
         when(asylumCaseCallbackApiDelegator.delegatePostSubmit(callback, ENDPOINT + CCD_SUBMITTED_PATH))
             .thenReturn(notifiedAsylumCase);
 
         final PostSubmitCallbackResponse postSubmitCallbackResponse = asylumCasePostNotificationApiSender.send(callback);
-
+        verify(scheduler, never()).schedule(any(TimedEvent.class));
         verify(asylumCaseCallbackApiDelegator, times(1))
             .delegatePostSubmit(callback, ENDPOINT + CCD_SUBMITTED_PATH);
 
@@ -56,14 +65,22 @@ class AsylumCasePostNotificationApiSenderTest {
 
     @Test
     void should_delegate_about_to_start_callback_to_downstream_api() {
-
+        asylumCasePostNotificationApiSender =
+            new AsylumCasePostNotificationApiSender(
+                asylumCaseCallbackApiDelegator,
+                ENDPOINT,
+                CCD_SUBMITTED_PATH,
+                false,
+                dateProvider,
+                scheduler
+            );
         final PostSubmitCallbackResponse notifiedAsylumCase = mock(PostSubmitCallbackResponse.class);
 
         when(asylumCaseCallbackApiDelegator.delegatePostSubmit(callback, ENDPOINT + CCD_SUBMITTED_PATH))
             .thenReturn(notifiedAsylumCase);
 
         final PostSubmitCallbackResponse actualAsylumCase = asylumCasePostNotificationApiSender.send(callback);
-
+        verify(scheduler, never()).schedule(any(TimedEvent.class));
         verify(asylumCaseCallbackApiDelegator, times(1))
             .delegatePostSubmit(callback, ENDPOINT + CCD_SUBMITTED_PATH);
 
@@ -72,9 +89,55 @@ class AsylumCasePostNotificationApiSenderTest {
 
     @Test
     void should_not_allow_null_arguments() {
-
+        asylumCasePostNotificationApiSender =
+            new AsylumCasePostNotificationApiSender(
+                asylumCaseCallbackApiDelegator,
+                ENDPOINT,
+                CCD_SUBMITTED_PATH,
+                false,
+                dateProvider,
+                scheduler
+            );
         assertThatThrownBy(() -> asylumCasePostNotificationApiSender.send(null))
             .hasMessage("callback must not be null")
             .isExactlyInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void schedules_save_notification_to_data_event_if_tes_enabled() {
+        asylumCasePostNotificationApiSender =
+            new AsylumCasePostNotificationApiSender(
+                asylumCaseCallbackApiDelegator,
+                ENDPOINT,
+                CCD_SUBMITTED_PATH,
+                true,
+                dateProvider,
+                scheduler
+            );
+        final PostSubmitCallbackResponse notifiedAsylumCase = mock(PostSubmitCallbackResponse.class);
+
+        when(asylumCaseCallbackApiDelegator.delegatePostSubmit(callback, ENDPOINT + CCD_SUBMITTED_PATH))
+            .thenReturn(notifiedAsylumCase);
+        LocalDateTime localDateTime =
+            LocalDateTime.of(2004, Month.FEBRUARY, 5, 10, 57, 33);
+        when(dateProvider.nowWithTime()).thenReturn(localDateTime);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getId()).thenReturn(0L);
+        final PostSubmitCallbackResponse actualAsylumCase = asylumCasePostNotificationApiSender.send(callback);
+        ZonedDateTime expectedTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault())
+            .plusSeconds(15);
+        TimedEvent expectedTimedEvent = new TimedEvent(
+            "",
+            Event.SAVE_NOTIFICATIONS_TO_DATA,
+            expectedTime,
+            "IA",
+            "Asylum",
+            0L
+        );
+        verify(scheduler).schedule(refEq(expectedTimedEvent));
+        verify(asylumCaseCallbackApiDelegator, times(1))
+            .delegatePostSubmit(callback, ENDPOINT + CCD_SUBMITTED_PATH);
+
+        assertEquals(notifiedAsylumCase, actualAsylumCase);
     }
 }
