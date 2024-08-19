870df28bd55f3d6e686da4c2c76635ba0583537a
DGordonV1 <david.gordon@hmcts.net>
2024-08-19 12:21:03 +0100

    RIA-9261 - Enable ILA FTs in Pipeline

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/CcdCaseCreationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/CcdCaseCreationTest.java
@@ -17,7 +17,6 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,7 +38,6 @@ import uk.gov.hmcts.reform.iacaseapi.util.MapValueExpander;
 @Slf4j
 @SpringBootTest()
 @ActiveProfiles("functional")
-@Disabled
 public class CcdCaseCreationTest {
 
     @Value("classpath:templates/start-appeal-aip.json")
--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/HearingsUpdateHearingRequestFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/HearingsUpdateHearingRequestFunctionalTest.java
@@ -12,7 +12,6 @@ import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.test.context.ActiveProfiles;
@@ -29,7 +28,6 @@ import uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.HearingsUpdateHea
  */
 @Slf4j
 @ActiveProfiles("functional")
-@Disabled
 public class HearingsUpdateHearingRequestFunctionalTest extends CcdCaseCreationTest {
 
     @BeforeEach
