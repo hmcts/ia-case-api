f89a070658b920a7497c49ef60ca2ef44f0361b5
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2024-09-13 20:54:06 +0100

    add about to start callback handler

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ResetRemoveRepresentationPreparer.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ResetRemoveRepresentationPreparer.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
@@ -11,11 +12,14 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallb
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.ChangeOrganisationRequest;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
 
+import java.util.Optional;
+
 import static java.util.Objects.requireNonNull;
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
 @Component
 @Slf4j
-public class ResetRemoveRepresentationHandler implements PreSubmitCallbackHandler<AsylumCase> {
+public class ResetRemoveRepresentationPreparer implements PreSubmitCallbackHandler<AsylumCase> {
 
     public boolean canHandle(
         PreSubmitCallbackStage callbackStage,
@@ -24,7 +28,7 @@ public class ResetRemoveRepresentationHandler implements PreSubmitCallbackHandle
         requireNonNull(callbackStage, "callbackStage must not be null");
         requireNonNull(callback, "callback must not be null");
 
-        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_START
                && callback.getEvent() == Event.RESET_REMOVE_REPRESENTATION;
     }
 
@@ -39,12 +43,31 @@ public class ResetRemoveRepresentationHandler implements PreSubmitCallbackHandle
         }
 
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
-        asylumCase.write(AsylumCaseFieldDefinition.CHANGE_ORGANISATION_REQUEST_FIELD, null);
-        log.info("Reset Organisation request field data is reset for {} and value {} ", callback.getCaseDetails().getId(),
-                asylumCase.read(AsylumCaseFieldDefinition.CHANGE_ORGANISATION_REQUEST_FIELD, ChangeOrganisationRequest.class).orElse(null));
+        PreSubmitCallbackResponse<AsylumCase> response = new PreSubmitCallbackResponse<>(asylumCase);
+
+        if (isValidChangeOrganisationRequest(asylumCase)) {
+
+            response.addError("No changes required to reset Change representation event");
+            return response;
+        }
 
         return new PreSubmitCallbackResponse<>(asylumCase);
     }
 
+    private boolean isValidChangeOrganisationRequest(AsylumCase asylumCase) {
+
+        Optional<ChangeOrganisationRequest> changeOrganisationRequestOptional = asylumCase.read(
+                AsylumCaseFieldDefinition.CHANGE_ORGANISATION_REQUEST_FIELD, ChangeOrganisationRequest.class);
+
+        if (changeOrganisationRequestOptional.isEmpty()) {
+            return true;
+        }
+
+        ChangeOrganisationRequest request = changeOrganisationRequestOptional.get();
+
+        return (request.getCaseRoleId() == null || isEmpty(request.getCaseRoleId().getListItems()))
+                && StringUtils.isEmpty(request.getRequestTimestamp())
+                && StringUtils.isEmpty(request.getApprovalStatus());
+    }
 }
 
