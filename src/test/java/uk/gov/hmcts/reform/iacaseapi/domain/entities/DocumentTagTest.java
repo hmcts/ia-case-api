2f0fc96ec43c463c2f2ac00d92585bfef54e89ec
Ivan Kirsanov <ivan.kirsanov@version1.com>
2024-09-27 14:23:03 +0200

    DocumentTagTest Junit fix

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
@@ -55,6 +55,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(42, DocumentTag.values().length);
+        assertEquals(44, DocumentTag.values().length);
     }
 }
