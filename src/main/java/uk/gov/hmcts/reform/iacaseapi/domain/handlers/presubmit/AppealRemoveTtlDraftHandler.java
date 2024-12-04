f6273aeeb4432872e489e4d97fed1fe80d198353
Andrey Pereverzin <andrey.pereverzin1@hmcts.net>
2024-12-04 18:05:13 +0000

    Introduced TTL related handlers

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AppealRemoveTtlDraftHandler.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+
+import static java.util.Objects.requireNonNull;
+
+@Slf4j
+@Service
+public class AppealRemoveTtlDraftHandler implements PreSubmitCallbackHandler<AsylumCase> {
+
+    @Override
+    public boolean canHandle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+               && callback.getEvent() == Event.SUBMIT_APPEAL;
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback) {
+
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+
+        log.info("Removing TTL when submitting appeal, caseId [{}]", callback.getCaseDetails().getId());
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
