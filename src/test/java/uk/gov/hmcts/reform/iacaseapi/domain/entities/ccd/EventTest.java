00029f527e29663a6d60ed03021bc0896928c8ce
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2024-12-05 14:41:52 +0000

    fix test

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -160,6 +160,6 @@ class EventTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(150, Event.values().length);
+        assertEquals(151, Event.values().length);
     }
 }
