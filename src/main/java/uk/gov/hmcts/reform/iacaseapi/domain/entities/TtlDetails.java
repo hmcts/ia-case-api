7f1ea827713be386e289e94622bb5d643e54ff21
Andrey Pereverzin <andrey.pereverzin1@hmcts.net>
2024-12-10 13:59:24 +0000

    Introduced ttl attribute

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/TtlDetails.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/TtlDetails.java
@@ -2,6 +2,10 @@ package uk.gov.hmcts.reform.iacaseapi.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
@@ -11,15 +15,22 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class TtlDetails {
-
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private final LocalDate systemSetTtl;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private final LocalDate manualTtlOverride;
+
     private YesOrNo doNotDelete;
 
     @JsonCreator
-    public TtlDetails(@JsonProperty("system_set_ttl") LocalDate systemSetTtl,
-                      @JsonProperty("manual_ttl_override") LocalDate manualTtlOverride,
-                      @JsonProperty("do_not_delete") YesOrNo doNotDelete) {
+    public TtlDetails(
+        @JsonProperty("system_set_ttl") LocalDate systemSetTtl,
+        @JsonProperty("manual_ttl_override") LocalDate manualTtlOverride,
+        @JsonProperty("do_not_delete") YesOrNo doNotDelete
+    ) {
         this.systemSetTtl = systemSetTtl;
         this.manualTtlOverride = manualTtlOverride;
         this.doNotDelete = doNotDelete;
