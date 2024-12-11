d3b4872bb4d77224b87c17696e6d95d57bc27098
JakeCohenSol <jake.cohen@solirius.com>
2024-12-11 13:25:33 +0000

    Removing hardcoded password and secret

--- bin/utils/idam-user-token-aat.sh
+++ bin/utils/idam-user-token-aat.sh
@@ -8,12 +8,10 @@
 ## Returns a valid IDAM user token for the given username and password.
 
 USERNAME=servicesatcdmiac@gmail.com
-PASSWORD=IacConfig29
 REDIRECT_URI="https://ia-case-api-aat.service.core-compute-aat.internal/oauth2/callback"
 CLIENT_ID="ccd_admin"
-CLIENT_SECRET="HoxnBugNkz9jE9QK0pvMVGjgEhBtN9Zv"
 SCOPE="openid%20profile%20roles"
 
 curl --silent --show-error \
     -H "Content-Type: application/x-www-form-urlencoded" \
-    -XPOST "${IDAM_API_BASE_URL}/o/token?grant_type=password&redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&username=${USERNAME}&password=${PASSWORD}&scope=${SCOPE}" -d "" | jq -r .access_token
+    -XPOST "${IDAM_API_BASE_URL}/o/token?grant_type=password&redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&client_secret=${CCD_CLIENT_SECRET}&username=${USERNAME}&password=${CCD_UPLOAD_PASSWORD}&scope=${SCOPE}" -d "" | jq -r .access_token
