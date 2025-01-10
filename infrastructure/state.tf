9bb7f030ba60ea70dc61e676460b3dd594d07022
alivenichoppa <97441560+alivenichoppa@users.noreply.github.com>
2025-01-10 11:48:48 +0000

    DIAC-000 Upgrade terraform and azuread versions (#543)

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.47.0"
+      version = "~> 4.14.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "1.6.0"
+      version = "3.0.2"
     }
   }
 }
