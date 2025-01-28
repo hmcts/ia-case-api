314daf10e44897db96c8871f7dcf7ffc634e2b98
Andrey Pereverzin <andrey.pereverzin1@hmcts.net>
2025-01-28 20:04:15 +0000

    Avoid duplicates

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HearingIdListProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HearingIdListProcessorTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
