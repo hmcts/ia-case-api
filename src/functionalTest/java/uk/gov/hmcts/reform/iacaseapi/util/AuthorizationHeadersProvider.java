944409b24e1b06ff7045deb580dc47322ccb1a42
kleog <kleogeorgiou@hotmail.com>
2025-02-10 17:39:50 +0000

    remove duplicate code

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/AuthorizationHeadersProvider.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/AuthorizationHeadersProvider.java
@@ -326,27 +326,4 @@ public class AuthorizationHeadersProvider {
             new Header("Authorization", accessToken)
         );
     }
-
-    public Headers getSystemUserAuthorization() {
-
-        MultiValueMap<String, String> tokenRequestForm = new LinkedMultiValueMap<>();
-        tokenRequestForm.add("grant_type", "password");
-        tokenRequestForm.add("redirect_uri", idamRedirectUrl);
-        tokenRequestForm.add("client_id", idamClientId);
-        tokenRequestForm.add("client_secret", idamClientSecret);
-        tokenRequestForm.add("username", System.getenv("IA_SYSTEM_USERNAME"));
-        tokenRequestForm.add("password", System.getenv("IA_SYSTEM_PASSWORD"));
-        tokenRequestForm.add("scope", userScope);
-
-        String serviceToken = tokens.computeIfAbsent("ServiceAuth", user -> serviceAuthTokenGenerator.generate());
-        String accessToken = tokens.computeIfAbsent(
-                "SystemUser",
-                user -> "Bearer " + idamApi.token(tokenRequestForm).getAccessToken()
-        );
-
-        return new Headers(
-                new Header("ServiceAuthorization", serviceToken),
-                new Header("Authorization", accessToken)
-        );
-    }
 }
