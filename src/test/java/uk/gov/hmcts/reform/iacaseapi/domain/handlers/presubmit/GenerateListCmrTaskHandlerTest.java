048a00ba8a6fedb4ee267a28cfd4fc943f2bfad6
Sabah Irfan <sabah.irfan@gmail.com>
2025-05-30 14:50:39 +0100

    Optimise tests

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/GenerateListCmrTaskHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/GenerateListCmrTaskHandlerTest.java
@@ -13,9 +13,14 @@ import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.GENERATE_L
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo.YES;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
@@ -61,21 +66,52 @@ class GenerateListCmrTaskHandlerTest {
     }
 
     @Test
-    void it_can_handle_callback() {
-        for (Event event : Event.values()) {
-            when(callback.getEvent()).thenReturn(event);
+    void should_handle_valid_callback_combination() {
+        when(callback.getEvent()).thenReturn(GENERATE_LIST_CMR_TASK);
 
-            for (PreSubmitCallbackStage callbackStage : PreSubmitCallbackStage.values()) {
-                boolean canHandle = generateListCmrTaskHandler.canHandle(callbackStage, callback);
+        boolean canHandle = generateListCmrTaskHandler.canHandle(ABOUT_TO_SUBMIT, callback);
 
-                if (callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
-                        && callback.getEvent() == GENERATE_LIST_CMR_TASK) {
-                    assertTrue(canHandle, "Can handle event " + event);
-                } else {
-                    assertFalse(canHandle, "Cannot handle event " + event);
-                }
-            }
-        }
+        assertTrue(canHandle);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = PreSubmitCallbackStage.class, names = {"ABOUT_TO_START", "MID_EVENT"})
+    void should_not_handle_generate_list_cmr_task_with_invalid_callback_stages(PreSubmitCallbackStage callbackStage) {
+        when(callback.getEvent()).thenReturn(GENERATE_LIST_CMR_TASK);
+
+        boolean canHandle = generateListCmrTaskHandler.canHandle(callbackStage, callback);
+
+        assertFalse(canHandle);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Event.class, names = {"GENERATE_LIST_CMR_TASK"}, mode = EnumSource.Mode.EXCLUDE)
+    void should_not_handle_other_events_with_about_to_submit_stage(Event event) {
+        when(callback.getEvent()).thenReturn(event);
+
+        boolean canHandle = generateListCmrTaskHandler.canHandle(ABOUT_TO_SUBMIT, callback);
+
+        assertFalse(canHandle);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidEventAndStageCombo")
+    void should_not_handle_invalid_event_and_stage_combinations(Event event, PreSubmitCallbackStage stage) {
+        when(callback.getEvent()).thenReturn(event);
+
+        boolean canHandle = generateListCmrTaskHandler.canHandle(stage, callback);
+
+        assertFalse(canHandle);
+    }
+
+    private static Stream<Arguments> invalidEventAndStageCombo() {
+        return Stream.of(
+                Arguments.of(Event.START_APPEAL, PreSubmitCallbackStage.ABOUT_TO_START),
+                Arguments.of(Event.SUBMIT_APPEAL, PreSubmitCallbackStage.MID_EVENT),
+                Arguments.of(Event.LIST_CASE, PreSubmitCallbackStage.ABOUT_TO_START),
+                Arguments.of(Event.SEND_DIRECTION, PreSubmitCallbackStage.MID_EVENT),
+                Arguments.of(Event.REQUEST_RESPONDENT_EVIDENCE, PreSubmitCallbackStage.ABOUT_TO_START)
+        );
     }
 
     @Test
@@ -97,22 +133,28 @@ class GenerateListCmrTaskHandlerTest {
                 .isExactlyInstanceOf(NullPointerException.class);
     }
 
-    @Test
-    void handler_throws_error_if_cannot_actually_handle() {
+    @ParameterizedTest
+    @EnumSource(value = PreSubmitCallbackStage.class, names = {"ABOUT_TO_SUBMIT"}, mode = EnumSource.Mode.EXCLUDE)
+    void should_throw_error_when_handling_with_wrong_callback_stage(PreSubmitCallbackStage callbackStage) {
         when(callback.getEvent()).thenReturn(GENERATE_LIST_CMR_TASK);
 
-        assertThatThrownBy(() -> generateListCmrTaskHandler.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback))
+        assertThatThrownBy(() -> generateListCmrTaskHandler.handle(callbackStage, callback))
                 .hasMessage("Cannot handle callback")
                 .isExactlyInstanceOf(IllegalStateException.class);
+    }
 
-        when(callback.getEvent()).thenReturn(Event.START_APPEAL);
+    @ParameterizedTest
+    @EnumSource(value = Event.class, names = {"GENERATE_LIST_CMR_TASK"}, mode = EnumSource.Mode.EXCLUDE)
+    void should_throw_error_when_handling_with_wrong_event(Event event) {
+        when(callback.getEvent()).thenReturn(event);
+
         assertThatThrownBy(() -> generateListCmrTaskHandler.handle(ABOUT_TO_SUBMIT, callback))
                 .hasMessage("Cannot handle callback")
                 .isExactlyInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    void handling_should_throw_if_cannot_actually_handle() {
+    void should_throw_error_when_cannot_handle_callback() {
         assertThatThrownBy(() -> generateListCmrTaskHandler.handle(ABOUT_TO_SUBMIT, callback))
                 .hasMessage("Cannot handle callback")
                 .isExactlyInstanceOf(IllegalStateException.class);
