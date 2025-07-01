dd7a5274413ee8c07551b8683002cd22a44282f8
Sabah Irfan <sabah.irfan@gmail.com>
2025-07-01 10:36:56 +0100

    Fix failing tests after rebase from master

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -164,6 +164,6 @@ class EventTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(154, Event.values().length);
+        assertEquals(155, Event.values().length);
     }
 }
