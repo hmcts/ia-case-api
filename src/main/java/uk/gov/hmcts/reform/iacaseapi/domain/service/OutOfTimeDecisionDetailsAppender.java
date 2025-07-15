366b98585299ade57fe137bb5e912fd6b7fcb6c0
david-gimelle <david.gimelle@gmail.com>
2025-07-15 11:48:46 +0100

    replace synchronisation by request scope

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/OutOfTimeDecisionDetailsAppender.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/OutOfTimeDecisionDetailsAppender.java
@@ -2,24 +2,30 @@ package uk.gov.hmcts.reform.iacaseapi.domain.service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.OutOfTimeDecisionDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 
 @Service
+@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class OutOfTimeDecisionDetailsAppender {
 
     private List<IdValue<OutOfTimeDecisionDetails>> allOutOfTimeDecisionDetails;
 
-    public synchronized List<IdValue<OutOfTimeDecisionDetails>> getAllOutOfTimeDecisionDetails() {
+    public List<IdValue<OutOfTimeDecisionDetails>> getAllOutOfTimeDecisionDetails() {
         return allOutOfTimeDecisionDetails;
     }
 
-    public synchronized void setAllOutOfTimeDecisionDetails(List<IdValue<OutOfTimeDecisionDetails>> allOutOfTimeDecisionDetails) {
-        this.allOutOfTimeDecisionDetails = allOutOfTimeDecisionDetails;
+    public void clearAllOutOfTimeDecisionDetails() {
+        this.allOutOfTimeDecisionDetails = null;
     }
 
-    public synchronized List<IdValue<OutOfTimeDecisionDetails>> append(
+    public List<IdValue<OutOfTimeDecisionDetails>> append(
         List<IdValue<OutOfTimeDecisionDetails>> existingOutOfTimeDecisionDetails,
         OutOfTimeDecisionDetails outOfTimeDecisionDetails
     ) {
