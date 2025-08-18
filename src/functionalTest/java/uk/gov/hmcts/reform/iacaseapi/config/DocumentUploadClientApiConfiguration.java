0798bdb42a2af2e6ac559f0fa75879c1c4451605
nilay <nilay.patel@solirius.com>
2025-08-18 17:31:26 +0100

    fix for functionals

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/config/DocumentUploadClientApiConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/config/DocumentUploadClientApiConfiguration.java
@@ -1,28 +1,28 @@
-package uk.gov.hmcts.reform.iacaseapi.testutils;
+package uk.gov.hmcts.reform.iacaseapi.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.document.DocumentUploadClientApi;
 
+/**
+ * Superseded. Will need to be removed as soon as the "use-ccd-document-am" feature flag is permanently on
+ */
 @Configuration
+@Deprecated
 public class DocumentUploadClientApiConfiguration {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
+    @Primary
     public DocumentUploadClientApi documentUploadClientApi(
-        @Value("${ccdGatewayUrl}") final String ccdGatewayUrl,
-        RestTemplate restTemplate
+        @Value("${ccdGatewayUrl}") final String ccdGatewayUrl
     ) {
         return new DocumentUploadClientApi(
             ccdGatewayUrl,
-            restTemplate,
+            new RestTemplate(),
             new ObjectMapper()
         );
     }
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/idam/UserInfo.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/idam/UserInfo.java
@@ -4,19 +4,21 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class UserInfo {
-
+public class UserInfo implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     @JsonProperty("sub")
     private String email;
     private String uid;
