1d316e558587634e57266453c6982aa79bce4e01
nilay <nilay.patel@solirius.com>
2025-08-19 17:49:50 +0100

    functional fix?

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/LaunchDarklyTestFeatureToggler.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/LaunchDarklyTestFeatureToggler.java
@@ -3,15 +3,19 @@ package uk.gov.hmcts.reform.iacaseapi.infrastructure.clients;
 import com.launchdarkly.sdk.LDUser;
 import com.launchdarkly.sdk.LDValue;
 import com.launchdarkly.sdk.server.interfaces.LDClientInterface;
+import java.util.Collections;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacaseapi.domain.UserDetailsProvider;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.UserDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.service.FeatureToggler;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.security.idam.IdamUserDetails;
 
 @Service
 @Primary
+@Slf4j
 @Profile("functional")
 public class LaunchDarklyTestFeatureToggler implements FeatureToggler {
 
@@ -25,8 +29,14 @@ public class LaunchDarklyTestFeatureToggler implements FeatureToggler {
     }
 
     public boolean getValue(String key, Boolean defaultValue) {
-
-        UserDetails userDetails = userDetailsProvider.getUserDetails();
+        UserDetails userDetails;
+        try {
+            userDetails = userDetailsProvider.getUserDetails();
+        } catch (Exception e) {
+            log.error("Error retrieving user details for LaunchDarkly feature toggling", e);
+            userDetails = new IdamUserDetails("token", "id", Collections.emptyList(),
+                "emailAddress", "forename", "surname");
+        }
 
         return ldClient.boolVariation(
             key,
