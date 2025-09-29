a09cef198034500751f2c6a8507fa5d48d85800a
tiagreaney <tiagreaney@gmail.com>
2025-09-29 12:33:17 +0100

    update RespondentReviewAppealResponseAddedUpdater

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RespondentReviewAppealResponseAddedUpdater.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RespondentReviewAppealResponseAddedUpdater.java
@@ -55,21 +55,18 @@ public class RespondentReviewAppealResponseAddedUpdater implements PreSubmitCall
                     asylumCase.read(APPEAL_RESPONSE_AVAILABLE, YesOrNo.class).orElse(NO)
             );
 
-            String endAppealOutcome =
-                    asylumCase
-                            .read(END_APPEAL_OUTCOME, String.class)
-                            .orElseThrow(() -> new IllegalStateException("endAppealOutcome is not present"));
-            if (WITHDRAWN.toString().equalsIgnoreCase(endAppealOutcome)) {
-                asylumCase.write(END_APPEAL_OUTCOME_REASON,
-                        "The Respondent has withdrawn the decision under appeal and invited the Tribunal to treat the appeal as withdrawn under Rule 17(2).\n\n"
-                                + "Upon considering the documents in this appeal, the Tribunal is satisfied that there is no good reason not to treat the appeal as withdrawn.\n\n"
-                                + "The appeal is at an end.\n\n"
-                                + "Legal Officer\n\n"
-                                + "PLEASE NOTE: This decision is made by a Legal Officer in exercise of a specified power granted by the Senior President of Tribunals under rules 3(1) and (2) of the Tribunals Procedure "
-                                + "(First-tier Tribunal) (Immigration and Asylum Chamber) rules 2014.  Any Party may, within 14 days of the date of this decision, apply in writing to the Tribunal for the decision to be considered afresh by a judge under rule 3(4)."
-                );
-            }
-
+            asylumCase
+                    .read(END_APPEAL_OUTCOME, String.class)
+                    .filter(outcome -> WITHDRAWN.toString().equalsIgnoreCase(outcome))
+                    .ifPresent(outcome -> asylumCase.write(
+                            END_APPEAL_OUTCOME_REASON,
+                            "The Respondent has withdrawn the decision under appeal and invited the Tribunal to treat the appeal as withdrawn under Rule 17(2).\n\n"
+                                    + "Upon considering the documents in this appeal, the Tribunal is satisfied that there is no good reason not to treat the appeal as withdrawn.\n\n"
+                                    + "The appeal is at an end.\n\n"
+                                    + "Legal Officer\n\n"
+                                    + "PLEASE NOTE: This decision is made by a Legal Officer in exercise of a specified power granted by the Senior President of Tribunals under rules 3(1) and (2) of the Tribunals Procedure "
+                                    + "(First-tier Tribunal) (Immigration and Asylum Chamber) rules 2014.  Any Party may, within 14 days of the date of this decision, apply in writing to the Tribunal for the decision to be considered afresh by a judge under rule 3(4)."
+                    ));
 
         } else {
             asylumCase.clear(RESPONDENT_REVIEW_APPEAL_RESPONSE_ADDED);
