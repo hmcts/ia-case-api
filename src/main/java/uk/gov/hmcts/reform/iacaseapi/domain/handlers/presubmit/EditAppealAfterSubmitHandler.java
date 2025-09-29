baad2f2963115d3ea5a1a1ee2fd8b8ae5b2a60da
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2025-09-29 17:11:10 +0100

    DIAC-1425 updates to stop clearing LR details as part of remove representation (#2752)
    * updates to stop clearing LR details as part of remove representation
    
    * updates to fix failing FTs
    
    ---------
    
    Co-authored-by: Rajesh Thuraiyur Moorthy <rajeshthuraiyurmoorthy@Mac.powerhub>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/EditAppealAfterSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/EditAppealAfterSubmitHandler.java
@@ -25,6 +25,7 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ApplicationType;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ContactPreference;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.OutOfCountryDecisionType;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
@@ -86,7 +87,19 @@ public class EditAppealAfterSubmitHandler implements PreSubmitCallbackHandler<As
                 .getCaseDetails()
                 .getCaseData();
 
-        asylumCase.write(HAS_ADDED_LEGAL_REP_DETAILS, YesOrNo.YES);
+
+        final String legalRepReferenceNumber = asylumCase.read(LEGAL_REP_REFERENCE_NUMBER, String.class)
+                .orElse("");
+        Optional<CaseDetails<AsylumCase>> caseDetailsBefore = callback.getCaseDetailsBefore();
+        if (caseDetailsBefore.isPresent()) {
+            final String prevLegalRepReferenceNumber = caseDetailsBefore
+                    .get().getCaseData().read(LEGAL_REP_REFERENCE_NUMBER, String.class).orElse("");
+            if (!legalRepReferenceNumber.equals(prevLegalRepReferenceNumber)) {
+                asylumCase.write(HAS_ADDED_LEGAL_REP_DETAILS, YesOrNo.YES);
+            }
+        }
+
+
 
         Optional<OutOfCountryDecisionType> outOfCountryDecisionTypeOptional = asylumCase.read(OUT_OF_COUNTRY_DECISION_TYPE, OutOfCountryDecisionType.class);
         YesOrNo appellantInUk = asylumCase.read(APPELLANT_IN_UK, YesOrNo.class).orElse(NO);
