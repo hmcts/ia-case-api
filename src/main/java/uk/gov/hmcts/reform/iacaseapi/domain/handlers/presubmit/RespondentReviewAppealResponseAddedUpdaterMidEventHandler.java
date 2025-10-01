982a4612836545d5ac5c60f6620889b8a4c1e0fb
tiagreaney <tiagreaney@gmail.com>
2025-10-01 12:26:29 +0100

    add logs

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RespondentReviewAppealResponseAddedUpdaterMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RespondentReviewAppealResponseAddedUpdaterMidEventHandler.java
@@ -5,6 +5,7 @@ import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefin
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.END_APPEAL_OUTCOME_REASON;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.FtpaResidentJudgeDecisionOutcomeType.WITHDRAWN;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
