32a9993b07c67f8c2090ff6f5363f421f486da4c
Velican <duzgunce.vd@gmail.com>
2025-10-10 13:15:58 +0100

    add handler confirmation

---
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/AddFasterCaseStatusConfirmation.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PostSubmitCallbackHandler;
+
+import static java.util.Objects.requireNonNull;
+
+@Component
+public class AddFasterCaseStatusConfirmation implements PostSubmitCallbackHandler<AsylumCase> {
+
+    public boolean canHandle(
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callback, "callback must not be null");
+
+        return callback.getEvent() == Event.ADD_FASTER_CASE_STATUS;
+    }
+
+    public PostSubmitCallbackResponse handle(
+        Callback<AsylumCase> callback
+    ) {
+        if (!canHandle(callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        PostSubmitCallbackResponse postSubmitResponse =
+            new PostSubmitCallbackResponse();
+
+        postSubmitResponse.setConfirmationHeader("# You have added a faster case status");
+        postSubmitResponse.setConfirmationBody(
+            "#### What happens next\n\n"
+            + "You can review this status in the faster case status tab."
+        );
+
+        return postSubmitResponse;
+    }
+}
