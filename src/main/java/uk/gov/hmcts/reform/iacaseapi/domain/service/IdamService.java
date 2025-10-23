8aca02d84377ff57060c71f4ea3653b624e8bc2d
david-gimelle <david.gimelle@gmail.com>
2025-10-23 08:28:15 +0100

    clean up logs

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/IdamService.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/IdamService.java
@@ -63,7 +63,6 @@ public class IdamService {
 
         for (Map.Entry<String, String> entry : idamAuthDetails.entrySet()) {
             log.info("idamAuthDetails - {}: {}", entry.getKey(), entry.getValue());
-            System.out.println("idamAuthDetails - " + entry.getKey() + ": " + entry.getValue());
         }
 
         logIdamEnvironmentVariables();
