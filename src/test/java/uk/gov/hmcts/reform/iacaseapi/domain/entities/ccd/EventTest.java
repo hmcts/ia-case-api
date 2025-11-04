50c3dd85c7b8191d3f329c498d78eb07fc4a9ba4
Velican <duzgunce.vd@gmail.com>
2025-11-04 12:38:54 +0000

    split event into 2 separate events for add and removal of status.

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -165,6 +165,6 @@ class EventTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(157, Event.values().length);
+        assertEquals(158, Event.values().length);
     }
 }
