3870d72bf754646f4a30134a792094239537d767
Andrey Pereverzin <andreypereverzin@Mac.broadband>
2025-11-10 11:41:07 +0000

    DIAC-1021 documents-api PR-1015

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/RestTemplateConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/RestTemplateConfiguration.java
@@ -3,8 +3,11 @@ package uk.gov.hmcts.reform.iacaseapi.infrastructure.config;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
@@ -29,11 +32,22 @@ public class RestTemplateConfiguration {
     public RestTemplate restTemplate(
         ObjectMapper objectMapper
     ) {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
-        restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter(objectMapper));
+        HttpClient httpClient = HttpClients.custom()
+                .disableContentCompression()  // Optional: prevent double compression issues
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        factory.setConnectTimeout(30_000);
+        factory.setReadTimeout(30_000);
+
+        RestTemplate restTemplate = new RestTemplate(factory);
 
         return restTemplate;
+        // RestTemplate restTemplate = new RestTemplate();
+        // restTemplate.getMessageConverters().removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
+        // restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter(objectMapper));
+
+        // return restTemplate;
     }
 
     @Bean
