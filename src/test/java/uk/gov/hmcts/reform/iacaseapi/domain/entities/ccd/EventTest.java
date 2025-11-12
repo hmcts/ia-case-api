3710a8ab259f382b288622948620176af3ec1771
david-gimelle <david.gimelle@gmail.com>
2025-11-12 11:53:23 +0000

    fix tests

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -166,6 +166,6 @@ class EventTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(158, Event.values().length);
+        assertEquals(159, Event.values().length);
     }
 }
