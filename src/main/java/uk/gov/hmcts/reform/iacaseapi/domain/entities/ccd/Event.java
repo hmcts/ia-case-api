68fb9c2cab23e8436b75e702511a69cde9fae412
Sabah u Din Irfan <sabah.irfan@gmail.com>
2025-11-27 17:39:01 +0000

    DIAC-1538 Turn on notification/WA tasks event (Rehydrated appeal) (#2867)
    * DIAC-1538 Turn on notification/WA tasks event (Rehydrated appeal)
    
    * Add tests
    
    * DIAC-1538 Turn on notification/WA tasks event (Rehydrated appeal)
    
    * action code reveiw
    
    * Add functional tests
    
    ---------
    
    Co-authored-by: rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/Event.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/Event.java
@@ -164,6 +164,7 @@ public enum Event {
     UPDATE_NEXT_HEARING_INFO("UpdateNextHearingInfo"),
     APPELLANT_IN_PERSON_MANUAL("appellantInPersonManual"),
     GENERATE_LIST_CMR_TASK("generateListCmrTask"),
+    TURN_ON_NOTIFICATIONS_WA_TASKS("turnOnNotificationsWATasks"),
 
     @JsonEnumDefaultValue
     UNKNOWN("unknown");
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/EventTest.java
@@ -162,12 +162,13 @@ class EventTest {
         assertEquals("removeCaseManagerBulk", Event.REMOVE_CASE_MANAGER_BULK.toString());
         assertEquals("generateListCmrTask", Event.GENERATE_LIST_CMR_TASK.toString());
         assertEquals("appellantInPersonManual", Event.APPELLANT_IN_PERSON_MANUAL.toString());
+        assertEquals("turnOnNotificationsWATasks", Event.TURN_ON_NOTIFICATIONS_WA_TASKS.toString());
         assertEquals("addStatutoryTimeframe24Weeks", Event.ADD_STATUTORY_TIMEFRAME_24_WEEKS.toString());
         assertEquals("removeStatutoryTimeframe24Weeks", Event.REMOVE_STATUTORY_TIMEFRAME_24_WEEKS.toString());
     }
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(159, Event.values().length);
+        assertEquals(160, Event.values().length);
     }
 }
