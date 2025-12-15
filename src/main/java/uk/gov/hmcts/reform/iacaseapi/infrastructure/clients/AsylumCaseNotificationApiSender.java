cab3d2202e3671f539797fc0edd8766e81ead507
Andrey Pereverzin <andreypereverzin@Mac.broadband>
2025-12-15 16:36:10 +0000

    DIAC-1284 Added saveNotificationsToDataDate field

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/AsylumCaseNotificationApiSender.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/AsylumCaseNotificationApiSender.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.iacaseapi.infrastructure.clients;
 
+import static java.time.LocalDate.parse;
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.SAVE_NOTIFICATIONS_TO_DATA_DATE;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,9 +17,11 @@ import uk.gov.hmcts.reform.iacaseapi.domain.service.Scheduler;
 import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.model.TimedEvent;
 
 import java.security.SecureRandom;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -69,7 +73,13 @@ public class AsylumCaseNotificationApiSender implements NotificationSender<Asylu
         log.info("save-notifications-feature LD flag value: {}", featureTogglerValue);
 
         if (featureTogglerValue && saveNotificationToDataEnabled) {
-            scheduleSaveNotificationToData(callback);
+            AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+            Optional<String> saveNotificationToDataDateOpt = asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class);
+            if (saveNotificationToDataDateOpt.isEmpty()
+                    || parse(saveNotificationToDataDateOpt.get()).isBefore(LocalDate.now())) {
+                scheduleSaveNotificationToData(callback);
+                asylumCase.write(SAVE_NOTIFICATIONS_TO_DATA_DATE, LocalDate.now());
+            }
         } else {
             log.info("Skipping saveNotificationsToDate event schedule");
         }
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/AsylumCaseNotificationApiSenderTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/AsylumCaseNotificationApiSenderTest.java
@@ -24,11 +24,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.SAVE_NOTIFICATIONS_TO_DATA_DATE;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -44,6 +46,8 @@ class AsylumCaseNotificationApiSenderTest {
     private Callback<AsylumCase> callback;
     @Mock
     private CaseDetails<AsylumCase> caseDetails;
+    @Mock
+    private AsylumCase asylumCase;
     @Mock
     private DateProvider dateProvider;
     @Mock
@@ -75,16 +79,21 @@ class AsylumCaseNotificationApiSenderTest {
                 featureToggler
             );
         when(featureToggler.getValue("save-notifications-feature", false)).thenReturn(true);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
     }
     
     @Test
-    void should_delegate_callback_to_downstream_api() {
+    void should_delegate_callback_to_downstream_api_when_previous_scheduled_time_empty() {
         final AsylumCase notifiedAsylumCase = mock(AsylumCase.class);
 
         when(asylumCaseCallbackApiDelegator.delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH))
             .thenReturn(notifiedAsylumCase);
 
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.empty());
+
         final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
+
         verify(scheduler, never()).schedule(any(TimedEvent.class));
         verify(asylumCaseCallbackApiDelegator, times(1))
             .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
@@ -93,16 +102,56 @@ class AsylumCaseNotificationApiSenderTest {
     }
 
     @Test
-    void should_delegate_about_to_start_callback_to_downstream_api() {
+    void should_delegate_callback_to_downstream_api_when_previous_scheduled_time_today() {
         final AsylumCase notifiedAsylumCase = mock(AsylumCase.class);
 
         when(asylumCaseCallbackApiDelegator.delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH))
             .thenReturn(notifiedAsylumCase);
 
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.of(LocalDate.now().toString()));
+
+        final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
+
+        verify(scheduler, never()).schedule(any(TimedEvent.class));
+        verify(asylumCaseCallbackApiDelegator, times(1))
+            .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
+
+        assertEquals(notifiedAsylumCase, callbackResponse);
+    }
+
+    @Test
+    void should_delegate_callback_to_downstream_api_when_previous_scheduled_time_yesterday() {
+        final AsylumCase notifiedAsylumCase = mock(AsylumCase.class);
+
+        when(asylumCaseCallbackApiDelegator.delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH))
+            .thenReturn(notifiedAsylumCase);
+
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.of(LocalDate.now().minusDays(1).toString()));
+
+        final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
+
+        verify(scheduler, never()).schedule(any(TimedEvent.class));
+        verify(asylumCaseCallbackApiDelegator, times(1))
+            .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
+
+        assertEquals(notifiedAsylumCase, callbackResponse);
+    }
+
+    @Test
+    void should_delegate_about_to_start_callback_to_downstream_api_when_previous_scheduled_time_empty() {
+        final AsylumCase notifiedAsylumCase = mock(AsylumCase.class);
+
+        when(asylumCaseCallbackApiDelegator.delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH))
+            .thenReturn(notifiedAsylumCase);
+
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.empty());
+
         final AsylumCase actualAsylumCase = asylumCaseNotificationApiSender.send(callback);
         verify(scheduler, never()).schedule(any(TimedEvent.class));
         verify(asylumCaseCallbackApiDelegator, times(1))
             .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
+
+        verify(asylumCase).write(SAVE_NOTIFICATIONS_TO_DATA_DATE, LocalDate.now());
 
         assertEquals(notifiedAsylumCase, actualAsylumCase);
     }
@@ -115,7 +164,7 @@ class AsylumCaseNotificationApiSenderTest {
     }
 
     @Test
-    void schedules_save_notification_to_data_event_if_tes_enabled() {
+    void schedules_save_notification_to_data_event_if_tes_enabled_and_previous_scheduled_time_empty() {
         asylumCaseNotificationApiSender =
             new AsylumCaseNotificationApiSender(
                 asylumCaseCallbackApiDelegator,
@@ -138,6 +187,8 @@ class AsylumCaseNotificationApiSenderTest {
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getId()).thenReturn(1L);
 
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.empty());
+
         final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
 
         verify(scheduler).schedule(timedEventCaptor.capture());
@@ -148,11 +199,90 @@ class AsylumCaseNotificationApiSenderTest {
         verify(asylumCaseCallbackApiDelegator, times(1))
             .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
 
+        verify(asylumCase).write(SAVE_NOTIFICATIONS_TO_DATA_DATE, LocalDate.now());
+
         assertEquals(notifiedAsylumCase, callbackResponse);
     }
 
     @Test
-    void should_schedule_to_next_day_when_tes_enabled_and_current_time_is_past_the_schedule_hour() {
+    void schedules_not_save_notification_to_data_event_if_tes_enabled_and_previous_scheduled_time_today() {
+        asylumCaseNotificationApiSender =
+            new AsylumCaseNotificationApiSender(
+                asylumCaseCallbackApiDelegator,
+                ENDPOINT,
+                CCD_SUBMITTED_PATH,
+                true,
+                true,
+                SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR,
+                SAVE_NOTIFICATIONS_DATA_SCHEDULE_MAX_MINUTES,
+                dateProvider,
+                scheduler,
+                featureToggler
+            );
+        final AsylumCase notifiedAsylumCase = mock(AsylumCase.class);
+
+        when(asylumCaseCallbackApiDelegator.delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH))
+            .thenReturn(notifiedAsylumCase);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        when(dateProvider.nowWithTime()).thenReturn(localDateTime);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getId()).thenReturn(1L);
+
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.of(LocalDate.now().toString()));
+
+        final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
+
+        verifyNoInteractions(scheduler);
+
+        verify(asylumCaseCallbackApiDelegator, times(1))
+            .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
+
+        assertEquals(notifiedAsylumCase, callbackResponse);
+    }
+
+    @Test
+    void schedules_save_notification_to_data_event_if_tes_enabled_and_previous_scheduled_time_yesterday() {
+        asylumCaseNotificationApiSender =
+            new AsylumCaseNotificationApiSender(
+                asylumCaseCallbackApiDelegator,
+                ENDPOINT,
+                CCD_SUBMITTED_PATH,
+                true,
+                true,
+                SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR,
+                SAVE_NOTIFICATIONS_DATA_SCHEDULE_MAX_MINUTES,
+                dateProvider,
+                scheduler,
+                featureToggler
+            );
+        final AsylumCase notifiedAsylumCase = mock(AsylumCase.class);
+
+        when(asylumCaseCallbackApiDelegator.delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH))
+            .thenReturn(notifiedAsylumCase);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        when(dateProvider.nowWithTime()).thenReturn(localDateTime);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getId()).thenReturn(1L);
+
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.of(LocalDate.now().minusDays(1).toString()));
+
+        final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
+
+        verify(scheduler).schedule(timedEventCaptor.capture());
+        TimedEvent scheduledTimedEventValue = timedEventCaptor.getValue();
+        verifyTimedEventSchedule(scheduledTimedEventValue);
+        assertEquals(scheduledTimedEventValue.getScheduledDateTime().toLocalDate(), LocalDate.now());
+
+        verify(asylumCase).write(SAVE_NOTIFICATIONS_TO_DATA_DATE, LocalDate.now());
+
+        verify(asylumCaseCallbackApiDelegator, times(1))
+            .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
+
+        assertEquals(notifiedAsylumCase, callbackResponse);
+    }
+
+    @Test
+    void should_schedule_to_next_day_when_tes_enabled_and_previous_scheduled_time_empty_and_current_time_is_past_the_schedule_hour() {
         asylumCaseNotificationApiSender =
                 new AsylumCaseNotificationApiSender(
                         asylumCaseCallbackApiDelegator,
@@ -175,6 +305,8 @@ class AsylumCaseNotificationApiSenderTest {
         LocalDateTime afterScheduleHour = LocalDateTime.now().withHour(SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR).withMinute(1);
         when(dateProvider.nowWithTime()).thenReturn(afterScheduleHour);
 
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.empty());
+
         final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
 
         verify(scheduler).schedule(timedEventCaptor.capture());
@@ -185,7 +317,81 @@ class AsylumCaseNotificationApiSenderTest {
         verify(asylumCaseCallbackApiDelegator, times(1))
                 .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
 
+        verify(asylumCase).write(SAVE_NOTIFICATIONS_TO_DATA_DATE, LocalDate.now());
+
         assertEquals(notifiedAsylumCase, callbackResponse);
+    }
+
+    @Test
+    void should_schedule_to_next_day_when_tes_enabled_and_current_time_is_past_the_schedule_hour_previous_scheduled_time_yesterday() {
+        asylumCaseNotificationApiSender =
+                new AsylumCaseNotificationApiSender(
+                        asylumCaseCallbackApiDelegator,
+                        ENDPOINT,
+                        CCD_SUBMITTED_PATH,
+                        true,
+                        true,
+                        SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR,
+                        SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR,
+                        dateProvider,
+                        scheduler,
+                        featureToggler
+                );
+        final AsylumCase notifiedAsylumCase = mock(AsylumCase.class);
+
+        when(asylumCaseCallbackApiDelegator.delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH))
+                .thenReturn(notifiedAsylumCase);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getId()).thenReturn(1L);
+        LocalDateTime afterScheduleHour = LocalDateTime.now().withHour(SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR).withMinute(1);
+        when(dateProvider.nowWithTime()).thenReturn(afterScheduleHour);
+
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.of(LocalDate.now().minusDays(1).toString()));
+
+        final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
+
+        verify(scheduler).schedule(timedEventCaptor.capture());
+        TimedEvent timedEventCaptorValue = timedEventCaptor.getValue();
+        verifyTimedEventSchedule(timedEventCaptorValue);
+        assertTrue(timedEventCaptorValue.getScheduledDateTime().isAfter(ZonedDateTime.now().plusDays(1)));
+
+        verify(asylumCaseCallbackApiDelegator, times(1))
+                .delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH);
+
+        verify(asylumCase).write(SAVE_NOTIFICATIONS_TO_DATA_DATE, LocalDate.now());
+
+        assertEquals(notifiedAsylumCase, callbackResponse);
+    }
+
+    @Test
+    void should_not_schedule_when_tes_enabled_and_current_time_is_past_the_schedule_hour_previous_scheduled_time_today() {
+        asylumCaseNotificationApiSender =
+                new AsylumCaseNotificationApiSender(
+                        asylumCaseCallbackApiDelegator,
+                        ENDPOINT,
+                        CCD_SUBMITTED_PATH,
+                        true,
+                        true,
+                        SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR,
+                        SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR,
+                        dateProvider,
+                        scheduler,
+                        featureToggler
+                );
+        final AsylumCase notifiedAsylumCase = mock(AsylumCase.class);
+
+        when(asylumCaseCallbackApiDelegator.delegate(callback, ENDPOINT + CCD_SUBMITTED_PATH))
+                .thenReturn(notifiedAsylumCase);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getId()).thenReturn(1L);
+        LocalDateTime afterScheduleHour = LocalDateTime.now().withHour(SAVE_NOTIFICATIONS_DATA_SCHEDULE_HOUR).withMinute(1);
+        when(dateProvider.nowWithTime()).thenReturn(afterScheduleHour);
+
+        when(asylumCase.read(SAVE_NOTIFICATIONS_TO_DATA_DATE, String.class)).thenReturn(Optional.of(LocalDate.now().toString()));
+
+        final AsylumCase callbackResponse = asylumCaseNotificationApiSender.send(callback);
+
+        verifyNoInteractions(scheduler);
     }
 
     private static void verifyTimedEventSchedule(TimedEvent timedEventCaptorValue) {
