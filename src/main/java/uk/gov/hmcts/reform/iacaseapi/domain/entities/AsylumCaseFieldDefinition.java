79d201bab7b7c9d4b0a456eef79b23085e854d72
David Gimelle <david.gimelle2@hmcts.net>
2025-12-16 12:12:39 +0000

    DIAC-1852 Update 24 Weeks Domain (#2883)
    * DIAC-1514 faster case status handler
    
    * update test
    
    * add unit test
    
    * add handler confirmation
    
    * add event to application.yaml
    
    * fix naming to submit faster case status
    
    * fix naming to confirmed ones
    
    * update to store time with date
    
    * fix unit test
    
    * split event into 2 separate events for add and removal of status.
    
    * update role config for new event names
    
    * sonar exclusion
    
    * Revert "sonar exclusion"
    
    This reverts commit 39b68f42c0b775a12b64da8ef2027aea0df458a0.
    
    * update/add tests
    
    * update role access
    
    * Add info to case notes when event is triggered.
    
    * add integration test for events
    
    * clean up
    
    * fix checkstyle issue
    
    * clean up confirmation + test
    
    * fix checkstyleTest
    
    * updating data structure and functionality to update it
    
    * update integration tests to use the new data structure
    
    * set condition to check if the status is already set to the same value.
    
    * review fix
    
    * update unit tests
    
    * add 24 weeks for system
    
    * update home office lib
    
    * skip duplicate of 24 weeks record after ho call
    
    * increase test coverage
    
    * clear only when required
    
    * fix duplicate
    
    * Revert 24weeks home office changes
    
    * make update 24 weeks indempotent
    
    * update case only if status has changed
    
    * update test
    
    * clean code
    
    * checkstyle
    
    * add homeOfficeCaseType
    
    * add home office case type
    
    * fix integration tests
    
    * Change message
    
    * fix ho ctype
    
    * fix integration tests
    
    * add currentHomeOfficeCaseTypeAutoGenerated
    
    * update clear 24 weeks
    
    * Revert "update clear 24 weeks"
    
    This reverts commit 5afca7568d4dd7c82fc4bd7143f6fa5532f9753f.
    
    * empty field when removed event
    
    * restore home office api dependency to latest
    
    * add test on currentHomeOfficeCaseTypeAutoGenerated
    
    * do not clear home office case type
    
    * Revert "restore home office api dependency to latest"
    
    This reverts commit 835372852ab8d200681c559bfe418c535144d6ae.
    
    * fix preview build
    
    * fix
    
    * add removeStatutoryTimeframe24Weeks to ia system
    
    * use latest for home office api
    
    ---------
    
    Co-authored-by: Velican <duzgunce.vd@gmail.com>
    Co-authored-by: Velican <31857663+velimd@users.noreply.github.com>
    Co-authored-by: david-gimelle <david.gimelle@gmail.com>
    Co-authored-by: Nilay Patel <nilay.patel@solirius.com>

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCaseFieldDefinition.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/AsylumCaseFieldDefinition.java
@@ -2874,6 +2874,9 @@ public enum AsylumCaseFieldDefinition {
 
     STATUTORY_TIMEFRAME_24_WEEKS_REASON(
         "statutoryTimeframe24WeeksReason", new TypeReference<String>(){}),
+
+    STATUTORY_TIMEFRAME_24_WEEKS_HOME_OFFICE_CASE_TYPE(
+        "statutoryTimeframe24WeeksHomeOfficeCaseType", new TypeReference<String>(){}),
     ;
 
     private final String value;
