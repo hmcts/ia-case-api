e6ac540dcbd556332144a8debc857b29cbcb5bee
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2026-01-12 16:03:09 +0000

    Update WithRoleAssignmentStub.java

--- src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/component/testutils/WithRoleAssignmentStub.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/component/testutils/WithRoleAssignmentStub.java
@@ -9,10 +9,10 @@ import com.github.tomakehurst.wiremock.http.RequestMethod;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 
 public interface WithRoleAssignmentStub {
-    default void addRoleAssignmentQueryStub(WireMockServer server) {
+    default void addRoleAssignmentActorStub(WireMockServer server) {
         server.addStubMapping(
             new StubMapping(
-                newRequestPattern(RequestMethod.POST, urlMatching("/amRoleAssignment/am/role-assignments/query"))
+                newRequestPattern(RequestMethod.GET, urlMatching("/amRoleAssignment/am/role-assignments/actors/.{1,3}"))
                     .build(),
                 aResponse()
                     .withStatus(200)
