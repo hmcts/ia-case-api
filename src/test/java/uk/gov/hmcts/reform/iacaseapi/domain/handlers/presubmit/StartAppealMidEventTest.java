26f1114f18152e5dc023f056e6f227f426f0b59d
tiagreaney <tiagreaney@gmail.com>
2026-01-15 14:19:09 +0000

    update logs

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/StartAppealMidEventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/StartAppealMidEventTest.java
@@ -365,7 +365,7 @@ class StartAppealMidEventTest {
         when(asylumCase.read(MOBILE_NUMBER_RETYPE, String.class))
                 .thenReturn(Optional.of("07898999991"));
 
-        log.info("test " + callback.getPageId());
+        log.info("tia " + callback.getPageId());
 
         PreSubmitCallbackResponse<AsylumCase> callbackResponse =
                 startAppealMidEvent.handle(PreSubmitCallbackStage.MID_EVENT, callback);
