dc422b889dd78edec7d9cdf7bae30bc0799fdc93
SIVA CHIMPIRI <siva.chimpiri@hmcts.net>
2026-02-02 15:23:51 +0000

    review comments addressed.

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/statutorytimeframe24weeks/AddStatutoryTimeframe24WeeksHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/statutorytimeframe24weeks/AddStatutoryTimeframe24WeeksHandler.java
@@ -43,7 +43,6 @@ public class AddStatutoryTimeframe24WeeksHandler implements PreSubmitCallbackHan
                 .getCaseData();
 
         YesOrNo status = YesOrNo.YES;
-
         AsylumCase updatedAsylum = updateStatutoryTimeframe24WeeksService.updateAsylumCase(asylumCase, status);
 
         return new PreSubmitCallbackResponse<>(updatedAsylum);
