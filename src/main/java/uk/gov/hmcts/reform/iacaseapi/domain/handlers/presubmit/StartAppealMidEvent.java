7fe099962c353e4591c60a3a3be1ed337c7b438b
tiagreaney <tiagreaney@gmail.com>
2026-02-04 14:16:26 +0000

    fix check for appellant number and lr number

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/StartAppealMidEvent.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/StartAppealMidEvent.java
@@ -151,13 +151,23 @@ public class StartAppealMidEvent implements PreSubmitCallbackHandler<AsylumCase>
         }
 
         if (callback.getPageId().equals(LEGAL_REPRESENTATIVE_DETAILS)) {
-            Optional<String> appellantsMobileNumber = asylumCase.read(MOBILE_NUMBER, String.class);
-            Optional<String> legalRepMobileNumber = asylumCase.read(LEGAL_REP_MOBILE_PHONE_NUMBER, String.class);
 
-            if (appellantsMobileNumber == legalRepMobileNumber) {
-                response.addError("Contact number is already in use for the appellant.  Please amend the appellant's mobile phone number before proceeding.");
+            Optional<String> appellantsMobileNumber =
+                    asylumCase.read(MOBILE_NUMBER, String.class);
+
+            Optional<String> legalRepMobileNumber =
+                    asylumCase.read(LEGAL_REP_MOBILE_PHONE_NUMBER, String.class);
+
+            if (appellantsMobileNumber.isPresent()
+                    && legalRepMobileNumber.isPresent()
+                    && appellantsMobileNumber.get().equals(legalRepMobileNumber.get())) {
+
+                response.addError(
+                        "Contact number is already in use for the appellant. Please amend the appellant's mobile phone number before proceeding."
+                );
             }
         }
+
 
         if (callback.getPageId().equals(SUITABILITY_ATTENDANCE_PAGE_ID) && (callback.getEvent() == Event.EDIT_APPEAL || callback.getEvent() == Event.EDIT_APPEAL_AFTER_SUBMIT || callback.getEvent() == Event.START_APPEAL)) {
             boolean suitabilityHearingType = asylumCase.read(SUITABILITY_HEARING_TYPE_YES_OR_NO, YesOrNo.class).orElse(YesOrNo.NO).equals(YesOrNo.YES);
