583cfdee5f37cb39623a648c41b060c6a2353980
tiagreaney <tiagreaney@gmail.com>
2026-02-10 14:15:08 +0000

    clear EMAIL_RETYPE and MOBILE_NUMBER_RETYPE once checked

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RemoveDetainedStatusHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RemoveDetainedStatusHandler.java
@@ -77,6 +77,8 @@ public class RemoveDetainedStatusHandler implements PreSubmitCallbackHandler<Asy
             if (emailMismatch || mobileMismatch) {
                 response.addError("The details given do not match");
             }
+            asylumCase.clear(EMAIL_RETYPE);
+            asylumCase.clear(MOBILE_NUMBER_RETYPE);
         }
 
         return response;
