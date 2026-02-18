6d428be0f9c0ea3145e9085790d1fe9127478dbc
erclaudio <claudio.andrei@icloud.com>
2026-02-18 09:52:09 +0000

    remove tribunal received date

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/migratedcase/AriaCreateCaseHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/migratedcase/AriaCreateCaseHandler.java
@@ -66,7 +66,7 @@ public class AriaCreateCaseHandler implements PreSubmitCallbackHandler<AsylumCas
             return asylumCasePreSubmitCallbackResponse;
         }
 
-        String tribunalReceivedDate = asylumCase.read(TRIBUNAL_RECEIVED_DATE, String.class).orElse(dateProvider.now().toString());
+        String appealSubmissionDate = asylumCase.read(APPEAL_SUBMISSION_DATE, String.class).orElse(dateProvider.now().toString());
 
         AppealType appealType =
             asylumCase
@@ -82,7 +82,7 @@ public class AriaCreateCaseHandler implements PreSubmitCallbackHandler<AsylumCas
         }
 
         asylumCase.write(APPEAL_REFERENCE_NUMBER, appealReferenceNumber);
-        asylumCase.write(APPEAL_SUBMISSION_DATE, tribunalReceivedDate );
+        asylumCase.write(APPEAL_SUBMISSION_DATE, appealSubmissionDate );
         asylumCase.write(IS_ARIA_MIGRATED, YesOrNo.YES);
         //isAriaMigratedFilter is used separately for case list filtering on ExUI
         asylumCase.write(IS_ARIA_MIGRATED_FILTER, YesOrNo.YES);
