9216b914c65f8bdaf9f9978b2bab751c1299f1cf
tiagreaney <tiagreaney@gmail.com>
2026-02-24 16:45:10 +0000

    change tests

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RaiseQueryCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RaiseQueryCallbackHandlerTest.java
@@ -24,6 +24,8 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.controllers.model.querymanagement.CaseMessage;
 import uk.gov.hmcts.reform.iacaseapi.infrastructure.controllers.model.querymanagement.CaseQueriesCollection;
 import uk.gov.hmcts.reform.iacaseapi.infrastructure.controllers.model.querymanagement.LatestQuery;
 
@@ -35,6 +37,7 @@ class RaiseQueryCallbackHandlerTest {
     @Mock private CaseDetails<AsylumCase> caseDetails;
     @Mock private AsylumCase asylumCase;
     @Mock private UserDetails userDetails;
+    @Captor private ArgumentCaptor<LatestQuery> latestQueryArgumentCaptor;
 
     private RaiseQueryCallbackHandler handler;
 
@@ -43,17 +46,16 @@ class RaiseQueryCallbackHandlerTest {
         handler = new RaiseQueryCallbackHandler(userDetails);
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
     }
 
     @Test
     void should_handle_correct_event_and_stage() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
         assertTrue(handler.canHandle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback));
     }
 
     @Test
     void should_not_handle_wrong_stage() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
         assertFalse(handler.canHandle(PreSubmitCallbackStage.ABOUT_TO_START, callback));
     }
 
@@ -64,129 +66,87 @@ class RaiseQueryCallbackHandlerTest {
     }
 
     @Test
-    void should_write_latest_query_id_when_queries_exist() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
-
-        try (MockedStatic<uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils> utils =
-                     mockStatic(uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.class)) {
-
-            utils.when(() -> uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isLegalRepJourney(asylumCase))
-                    .thenReturn(true);
-
-            List<IdValue<CaseQueriesCollection>> queries = asList(
-                    new IdValue<>("1", mock(CaseQueriesCollection.class)),
-                    new IdValue<>("3", mock(CaseQueriesCollection.class)),
-                    new IdValue<>("2", mock(CaseQueriesCollection.class))
-            );
-
-            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES))
-                    .thenReturn(Optional.of(queries));
-
-            handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
-
-            ArgumentCaptor<LatestQuery> captor = ArgumentCaptor.forClass(LatestQuery.class);
-            verify(asylumCase).write(eq(QM_LATEST_QUERY), captor.capture());
-
-            LatestQuery written = captor.getValue();
-            assertEquals("3", written.getQueryId());
-            assertEquals(YesOrNo.NO, written.getIsHearingRelated());
-        }
-    }
-
-    @Test
     void should_default_to_one_when_no_queries_exist() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
+        try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
+            utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(true);
 
-        try (MockedStatic<uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils> utils =
-                     mockStatic(uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.class)) {
-
-            utils.when(() -> uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isLegalRepJourney(asylumCase))
-                    .thenReturn(true);
-
-            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES))
+            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES, CaseQueriesCollection.class))
                     .thenReturn(Optional.empty());
 
             handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
 
-            ArgumentCaptor<LatestQuery> captor = ArgumentCaptor.forClass(LatestQuery.class);
-            verify(asylumCase).write(eq(QM_LATEST_QUERY), captor.capture());
-
-            assertEquals("1", captor.getValue().getQueryId());
+            verify(asylumCase).write(eq(QM_LATEST_QUERY), latestQueryArgumentCaptor.capture());
+            assertEquals("1", latestQueryArgumentCaptor.getValue().getQueryId());
         }
     }
 
     @Test
-    void should_do_nothing_when_target_collection_is_null() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
+    void should_throw_exception_when_target_collection_is_null() {
+        try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
+            utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(false);
+            utils.when(() -> HandlerUtils.isAipJourney(asylumCase)).thenReturn(false);
+            utils.when(() -> HandlerUtils.isInternalCase(asylumCase)).thenReturn(false);
 
-        try (MockedStatic<uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils> utils =
-                     mockStatic(uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.class)) {
+            Exception ex = assertThrows(IllegalStateException.class, () ->
+                    handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback)
+            );
+            assertEquals("Unable to determine query collection for this asylum case", ex.getMessage());
+        }
+    }
 
-            utils.when(() -> uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isLegalRepJourney(asylumCase))
-                    .thenReturn(false);
-            utils.when(() -> uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isAipJourney(asylumCase))
-                    .thenReturn(false);
-            utils.when(() -> uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isInternalCase(asylumCase))
-                    .thenReturn(false);
+    @Test
+    void should_set_is_hearing_related_to_no() {
+        try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
+            utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(true);
+
+            CaseMessage message = new CaseMessage();
+            message.setId("7");
+
+            List<IdValue<CaseMessage>> caseMessages = List.of(new IdValue<>("7", message));
+
+            CaseQueriesCollection collection = CaseQueriesCollection.builder()
+                    .caseMessages(caseMessages)
+                    .build();
+
+            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES, CaseQueriesCollection.class))
+                    .thenReturn(Optional.of(collection));
 
             handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
 
-            verify(asylumCase, never()).write(any(), any());
+            verify(asylumCase).write(eq(QM_LATEST_QUERY), latestQueryArgumentCaptor.capture());
+            assertEquals(YesOrNo.NO, latestQueryArgumentCaptor.getValue().getIsHearingRelated());
         }
     }
 
     @Test
-    void should_handle_single_query_correctly() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
+    void should_select_lexicographically_largest_query_id() {
+        try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
+            utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(true);
 
-        try (MockedStatic<uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils> utils =
-                     mockStatic(uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.class)) {
+            CaseMessage msgAbc = new CaseMessage();
+            msgAbc.setId("abc");
+            CaseMessage msgXyz = new CaseMessage();
+            msgXyz.setId("xyz");
+            CaseMessage msgDef = new CaseMessage();
+            msgDef.setId("def");
 
-            utils.when(() -> uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isLegalRepJourney(asylumCase))
-                    .thenReturn(true);
-
-            List<IdValue<CaseQueriesCollection>> queries = List.of(
-                    new IdValue<>("7", mock(CaseQueriesCollection.class))
+            List<IdValue<CaseMessage>> caseMessages = asList(
+                    new IdValue<>("1", msgAbc),
+                    new IdValue<>("2", msgXyz),
+                    new IdValue<>("3", msgDef)
             );
 
-            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES))
-                    .thenReturn(Optional.of(queries));
+            CaseQueriesCollection collection = CaseQueriesCollection.builder()
+                    .caseMessages(caseMessages)
+                    .build();
+
+            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES, CaseQueriesCollection.class))
+                    .thenReturn(Optional.of(collection));
 
             handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
 
-            ArgumentCaptor<LatestQuery> captor = ArgumentCaptor.forClass(LatestQuery.class);
-            verify(asylumCase).write(eq(QM_LATEST_QUERY), captor.capture());
-
-            assertEquals("7", captor.getValue().getQueryId());
-            assertEquals(YesOrNo.NO, captor.getValue().getIsHearingRelated());
-        }
-    }
-
-    @Test
-    void should_handle_non_numeric_ids_lexicographically() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
-
-        try (MockedStatic<uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils> utils =
-                     mockStatic(uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.class)) {
-
-            utils.when(() -> uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isLegalRepJourney(asylumCase))
-                    .thenReturn(true);
-
-            List<IdValue<CaseQueriesCollection>> queries = asList(
-                    new IdValue<>("abc", mock(CaseQueriesCollection.class)),
-                    new IdValue<>("xyz", mock(CaseQueriesCollection.class)),
-                    new IdValue<>("def", mock(CaseQueriesCollection.class))
-            );
-
-            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES))
-                    .thenReturn(Optional.of(queries));
-
-            handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
-
-            ArgumentCaptor<LatestQuery> captor = ArgumentCaptor.forClass(LatestQuery.class);
-            verify(asylumCase).write(eq(QM_LATEST_QUERY), captor.capture());
-
-            assertEquals("xyz", captor.getValue().getQueryId());
+            verify(asylumCase).write(eq(QM_LATEST_QUERY), latestQueryArgumentCaptor.capture());
+            assertEquals("xyz", latestQueryArgumentCaptor.getValue().getQueryId());
         }
     }
 }
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RaiseQueryCallbackPreparerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RaiseQueryCallbackPreparerTest.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
-import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -14,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
@@ -23,7 +20,6 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils;
 import uk.gov.hmcts.reform.iacaseapi.infrastructure.controllers.model.querymanagement.CaseQueriesCollection;
 
@@ -31,12 +27,11 @@ import uk.gov.hmcts.reform.iacaseapi.infrastructure.controllers.model.querymanag
 @ExtendWith(MockitoExtension.class)
 class RaiseQueryCallbackPreparerTest {
 
-    @Mock
-    private Callback<AsylumCase> callback;
-    @Mock
-    private CaseDetails<AsylumCase> caseDetails;
+    @Mock private Callback<AsylumCase> callback;
+    @Mock private CaseDetails<AsylumCase> caseDetails;
     @Mock private AsylumCase asylumCase;
     @Mock private UserDetails userDetails;
+    @Captor private ArgumentCaptor<CaseQueriesCollection> collectionCaptor;
 
     private RaiseQueryCallbackPreparer handler;
 
@@ -45,93 +40,103 @@ class RaiseQueryCallbackPreparerTest {
         handler = new RaiseQueryCallbackPreparer(userDetails);
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
     }
 
     @Test
     void canHandle_should_return_true_for_correct_stage_and_event() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
         assertTrue(handler.canHandle(PreSubmitCallbackStage.ABOUT_TO_START, callback));
     }
 
     @Test
     void canHandle_should_return_false_for_wrong_stage() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
         assertFalse(handler.canHandle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback));
     }
 
     @Test
     void canHandle_should_return_false_for_wrong_event() {
-        when(callback.getEvent()).thenReturn(Event.LIST_CASE); // any other event
+        when(callback.getEvent()).thenReturn(Event.LIST_CASE);
         assertFalse(handler.canHandle(PreSubmitCallbackStage.ABOUT_TO_START, callback));
     }
 
     @Test
-    void should_initialize_legal_rep_queries_if_empty() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
-
-        try (MockedStatic<uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils> utils =
-                     mockStatic(uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.class)) {
-
+    void should_initialize_legal_rep_queries_with_empty_collection_if_absent() {
+        try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
             utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(true);
-            utils.when(() -> HandlerUtils.isAipJourney(asylumCase)).thenReturn(false);
-            utils.when(() -> HandlerUtils.isInternalCase(asylumCase)).thenReturn(false);
 
-            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES)).thenReturn(Optional.empty());
+            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES, CaseQueriesCollection.class))
+                    .thenReturn(Optional.empty());
 
             handler.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback);
 
-            verify(asylumCase).write(QM_LEGAL_REPRESENTATIVE_QUERIES, emptyList());
+            verify(asylumCase).write(eq(QM_LEGAL_REPRESENTATIVE_QUERIES), collectionCaptor.capture());
+            assertEquals(emptyList(), collectionCaptor.getValue().getCaseMessages());
         }
     }
 
     @Test
-    void should_initialize_aip_queries_if_empty() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
-
+    void should_initialize_aip_queries_with_empty_collection_if_absent() {
         try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
             utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(false);
             utils.when(() -> HandlerUtils.isAipJourney(asylumCase)).thenReturn(true);
-            utils.when(() -> HandlerUtils.isInternalCase(asylumCase)).thenReturn(false);
 
-            when(asylumCase.read(QM_AIP_QUERIES)).thenReturn(Optional.empty());
+            when(asylumCase.read(QM_AIP_QUERIES, CaseQueriesCollection.class))
+                    .thenReturn(Optional.empty());
 
             handler.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback);
 
-            verify(asylumCase).write(QM_AIP_QUERIES, emptyList());
+            verify(asylumCase).write(eq(QM_AIP_QUERIES), collectionCaptor.capture());
+            assertEquals(emptyList(), collectionCaptor.getValue().getCaseMessages());
         }
     }
 
     @Test
-    void should_initialize_admin_queries_if_empty() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
-
+    void should_initialize_admin_queries_with_empty_collection_if_absent() {
         try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
             utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(false);
             utils.when(() -> HandlerUtils.isAipJourney(asylumCase)).thenReturn(false);
             utils.when(() -> HandlerUtils.isInternalCase(asylumCase)).thenReturn(true);
 
-            when(asylumCase.read(QM_ADMIN_QUERIES)).thenReturn(Optional.empty());
+            when(asylumCase.read(QM_ADMIN_QUERIES, CaseQueriesCollection.class))
+                    .thenReturn(Optional.empty());
 
             handler.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback);
 
-            verify(asylumCase).write(QM_ADMIN_QUERIES, emptyList());
+            verify(asylumCase).write(eq(QM_ADMIN_QUERIES), collectionCaptor.capture());
+            assertEquals(emptyList(), collectionCaptor.getValue().getCaseMessages());
         }
     }
 
     @Test
-    void should_not_overwrite_existing_legal_rep_collection() {
-        when(callback.getEvent()).thenReturn(Event.QUERY_MANAGEMENT_RAISE_QUERY);
-
+    void should_preserve_existing_collection_when_already_present() {
         try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
             utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(true);
 
-            List<IdValue<CaseQueriesCollection>> existing = asList(new IdValue<>("1", new CaseQueriesCollection()));
-            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES)).thenReturn(Optional.of(existing));
+            CaseQueriesCollection existing = CaseQueriesCollection.builder()
+                    .caseMessages(emptyList())
+                    .build();
+
+            when(asylumCase.read(QM_LEGAL_REPRESENTATIVE_QUERIES, CaseQueriesCollection.class))
+                    .thenReturn(Optional.of(existing));
 
             handler.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback);
 
-            verify(asylumCase, never()).write(eq(QM_LEGAL_REPRESENTATIVE_QUERIES), any());
+            verify(asylumCase).write(eq(QM_LEGAL_REPRESENTATIVE_QUERIES), collectionCaptor.capture());
+            assertSame(existing, collectionCaptor.getValue());
         }
     }
 
+    @Test
+    void should_throw_when_journey_type_cannot_be_determined() {
+        try (MockedStatic<HandlerUtils> utils = mockStatic(HandlerUtils.class)) {
+            utils.when(() -> HandlerUtils.isLegalRepJourney(asylumCase)).thenReturn(false);
+            utils.when(() -> HandlerUtils.isAipJourney(asylumCase)).thenReturn(false);
+            utils.when(() -> HandlerUtils.isInternalCase(asylumCase)).thenReturn(false);
+
+            Exception ex = assertThrows(IllegalStateException.class, () ->
+                    handler.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback)
+            );
+            assertEquals("Unable to determine query collection for this asylum case", ex.getMessage());
+        }
+    }
 }
