c09adf766f32f34915375263cc806eda8e6d6aaf
tiagreaney <tiagreaney@gmail.com>
2026-02-27 08:04:32 +0000

    checkstyleMain fix

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ListingPaPayLaterDirectionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/ListingPaPayLaterDirectionHandler.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.DateProvider;
