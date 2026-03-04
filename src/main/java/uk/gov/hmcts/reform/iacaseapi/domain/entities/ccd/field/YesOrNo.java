0b7866b747d813f77fb8716e18f0151155a6baa5
tiagreaney <tiagreaney@gmail.com>
2026-03-04 12:15:52 +0000

    update yesOrNo

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/field/YesOrNo.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/ccd/field/YesOrNo.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum YesOrNo {
@@ -12,6 +13,18 @@ public enum YesOrNo {
 
     YesOrNo(String id) {
         this.id = id;
+    }
+
+    @JsonCreator
+    public static YesOrNo fromString(String value) {
+        if (value == null) {
+            return null;
+        }
+        switch (value.toLowerCase()) {
+            case "yes": return YES;
+            case "no": return NO;
+            default: return null; // or throw IllegalArgumentException
+        }
     }
 
     @Override
