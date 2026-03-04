46009d5777bd76fa3a4185411990ade4cc0a4b0a
kleog <kleogeorgiou@hotmail.com>
2026-03-04 12:34:38 +0000

    DIAC-2293 - initial commit to add test to check current caffeine config works so when migrating to redis, should still work

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/config/SystemUserCachingTest.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.iacaseapi.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.IdamService;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.RoleAssignmentService;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.IdamApi;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.clients.model.idam.Token;
+import uk.gov.hmcts.reform.iacaseapi.infrastructure.config.CacheConfiguration;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import({ CacheConfiguration.class, IdamService.class })
+@ExtendWith(SpringExtension.class)
+@EnableCaching
+@ImportAutoConfiguration(classes = {
+//        CacheAutoConfiguration.class,
+//        RedisAutoConfiguration.class
+})
+public class SystemUserCachingTest {
+
+    private static final String BEARER_AUTH = "Bearer ";
+    private static final String TOKEN = "SOME_TOKEN";
+
+    @Autowired
+    private IdamService idamService;
+
+    @MockBean
+    private IdamApi idamApi;
+
+    @MockBean
+    private RoleAssignmentService ras;
+
+    @BeforeEach
+    void setUp() {
+        given(idamApi.token(any()))
+                .willReturn(new Token(TOKEN, BEARER_AUTH));
+    }
+
+    @Test
+    void givenRedisCaching_whenFindItemById_thenItemReturnedFromCache() {
+        String itemCacheMiss = idamService.getServiceUserToken();
+        String itemCacheHit = idamService.getServiceUserToken();
+
+        assertThat(itemCacheMiss).isEqualTo(BEARER_AUTH + TOKEN);
+        assertThat(itemCacheHit).isEqualTo(BEARER_AUTH + TOKEN);
+
+        verify(idamApi, times(1)).token(any());
+    }
+}
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/CacheConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/CacheConfiguration.java
@@ -1,10 +1,16 @@
 package uk.gov.hmcts.reform.iacaseapi.infrastructure.config;
 
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+//import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.cache.RedisCacheConfiguration;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.RedisSerializationContext;
+//
+//import java.time.Duration;
 
 @EnableCaching
 @Configuration
@@ -14,4 +20,30 @@ public class CacheConfiguration {
     public CacheManagerCustomizer<CaffeineCacheManager> cacheManagerCustomizer() {
         return cacheManager -> cacheManager.setAllowNullValues(false);
     }
+
+    // need this for tests
+    @Bean
+    public CaffeineCacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setAllowNullValues(false);
+        // cacheManager.setCaffeineSpec(CaffeineSpec.parse("expireAfterWrite=1h"));
+        return cacheManager;
+    }
+
+//    @Bean
+//    public RedisCacheConfiguration cacheConfiguration() {
+//        return RedisCacheConfiguration.defaultCacheConfig()
+//                .entryTtl(Duration.ofMinutes(55))
+//                .disableCachingNullValues()
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+//    }
+//
+//    @Bean
+//    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+//        return (builder) -> builder
+//                .withCacheConfiguration("itemCache",
+//                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
+//                .withCacheConfiguration("customerCache",
+//                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
+//    }
 }
