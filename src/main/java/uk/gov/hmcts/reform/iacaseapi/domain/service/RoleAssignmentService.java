c6681f0bf09a9da1a7af69513d4fbdc344dd1ca2
nilay <nilay.patel@solirius.com>
2026-03-04 12:56:48 +0000

    altering revoke filter slightly and updating DL label with NLR or Citizen user

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/RoleAssignmentService.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/RoleAssignmentService.java
@@ -104,8 +104,8 @@ public class RoleAssignmentService {
     public RoleAssignmentResource getUsersAssignedToCase(long caseId) {
         QueryRequest queryRequest = QueryRequest.builder()
             .roleType(List.of(RoleType.CASE))
-            .roleCategory(List.of(RoleCategory.PROFESSIONAL, RoleCategory.CITIZEN))
-            .roleName(List.of(RoleName.CREATOR, RoleName.LEGAL_REPRESENTATIVE))
+            .roleCategory(List.of(RoleCategory.CITIZEN))
+            .roleName(List.of(RoleName.CREATOR))
             .attributes(Map.of(
                 Attributes.JURISDICTION, List.of(Jurisdiction.IA.name()),
                 Attributes.CASE_TYPE, List.of("Asylum"),
