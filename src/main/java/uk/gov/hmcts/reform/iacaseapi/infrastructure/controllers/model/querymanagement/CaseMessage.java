51a0fcc07eaadb3464f26d0a5c38cbd1f9c2ceaf
tiagreaney <tiagreaney@gmail.com>
2026-03-06 16:00:06 +0000

    update CaseMessage JsonProperty hearingDate

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/model/querymanagement/CaseMessage.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/model/querymanagement/CaseMessage.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
