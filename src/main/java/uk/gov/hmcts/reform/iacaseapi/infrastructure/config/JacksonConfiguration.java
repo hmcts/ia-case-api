7d7f61c223ab8ac7845a3b9b7523c833e6ed3c3a
tiagreaney <tiagreaney@gmail.com>
2026-03-11 12:55:35 +0000

    update CaseMessage types JacksonConfiguration

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/JacksonConfiguration.java
@@ -15,6 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -22,6 +23,9 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfiguration {
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Bean
     @Primary
