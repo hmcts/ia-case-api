da161e08e2fa6adae8d1ac794c948d6586add564
tiagreaney <tiagreaney@gmail.com>
2026-03-12 14:17:05 +0000

    update AsylumCase

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/model/querymanagement/CaseMessage.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/model/querymanagement/CaseMessage.java
@@ -35,7 +35,7 @@ public class CaseMessage {
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate hearingDate;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
     private OffsetDateTime createdOn;
     private String createdBy;
     private String parentId;
