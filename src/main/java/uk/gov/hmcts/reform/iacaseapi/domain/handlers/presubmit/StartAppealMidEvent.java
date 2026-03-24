dfcd1bfcec9c444b13549f6dbe1c660758c6e34a
Sahil Goonraz <sahil.goonraz@solirius.com>
2026-03-24 12:30:38 +0000

    Remove email and mobile number retype clearing

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/StartAppealMidEvent.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/StartAppealMidEvent.java
@@ -161,10 +161,11 @@ public class StartAppealMidEvent implements PreSubmitCallbackHandler<AsylumCase>
 
             if (emailMismatch || mobileMismatch) {
                 response.addError("The details given do not match");
-            } else {
-                asylumCase.clear(EMAIL_RETYPE);
-                asylumCase.clear(MOBILE_NUMBER_RETYPE);
             }
+//            else {
+//                asylumCase.clear(EMAIL_RETYPE);
+//                asylumCase.clear(MOBILE_NUMBER_RETYPE);
+//            }
         }
 
         if (callback.getPageId().equals(LEGAL_REPRESENTATIVE_DETAILS)) {
