edb391eced6a7ebb488a5cc2e338d6cee84142c0
nilay <nilay.patel@solirius.com>
2026-03-25 16:55:40 +0000

    adding extra event to call sponsor set

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HasNonLegalRepHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/HasNonLegalRepHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.EDIT_APPEAL_AFTER_SUBMIT;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.NLR_DETAILS_UPDATED;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.SEND_PIP_TO_NON_LEGAL_REP;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.SUBMIT_APPEAL;
 import static uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.isAipJourney;
@@ -10,6 +11,7 @@ import static uk.gov.hmcts.reform.iacaseapi.domain.handlers.HandlerUtils.setSpon
 import java.util.List;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
@@ -23,10 +25,11 @@ public class HasNonLegalRepHandler implements PreSubmitCallbackHandler<AsylumCas
     ) {
         requireNonNull(callbackStage, "callbackStage must not be null");
         requireNonNull(callback, "callback must not be null");
-
+        List<Event> allowedEvents = List.of(SUBMIT_APPEAL,
+            EDIT_APPEAL_AFTER_SUBMIT, SEND_PIP_TO_NON_LEGAL_REP, NLR_DETAILS_UPDATED);
         return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
             && isAipJourney(callback.getCaseDetails().getCaseData())
-            && List.of(SUBMIT_APPEAL, EDIT_APPEAL_AFTER_SUBMIT, SEND_PIP_TO_NON_LEGAL_REP).contains(callback.getEvent());
+            && allowedEvents.contains(callback.getEvent());
     }
 
     public PreSubmitCallbackResponse<AsylumCase> handle(
