bed6fd777aad0572e5f2cd21c450750a84b29ff9
tiagreaney <tiagreaney@gmail.com>
2026-03-26 15:44:24 +0000

    update decorators

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/model/querymanagement/CaseQueriesCollection.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/controllers/model/querymanagement/CaseQueriesCollection.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.iacaseapi.infrastructure.controllers.model.querymanagement;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@Builder
 @AllArgsConstructor
 public class CaseQueriesCollection {
 
