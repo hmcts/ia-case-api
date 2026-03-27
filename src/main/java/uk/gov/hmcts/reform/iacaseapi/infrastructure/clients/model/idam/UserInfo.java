dcd4ea14b8c0e374e459e3b490a7567febfede0d
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2026-03-27 17:46:16 +0000

    remove setter

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/idam/UserInfo.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/model/idam/UserInfo.java
@@ -15,7 +15,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserInfo implements Serializable {
