c0340d942ed75f3082615c14f929cf064212cd35
tiagreaney <tiagreaney@gmail.com>
2026-05-06 11:59:16 +0100

    DIAC-2192 Spike test

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/removedocs/RemoveDocsAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/removedocs/RemoveDocsAboutToSubmitHandler.java
@@ -1,27 +1,40 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.removedocs;
 
+import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentTag.IAUT_2_FORM;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentTag.UPPER_TRIBUNAL_TRANSFER_ORDER_DOCUMENT;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.*;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.editdocs.EditDocsCaseNoteService;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.editdocs.EditDocsService;
 
 @Component
 public class RemoveDocsAboutToSubmitHandler implements PreSubmitCallbackHandler<AsylumCase> {
 
     @Autowired
-    private RemoveDocsService removeDocService;
+    private EditDocsCaseNoteService editDocsCaseNoteService;
+    @Autowired
+    private EditDocsService editDocService;
 
     public DispatchPriority getDispatchPriority() {
         return DispatchPriority.EARLIEST;
@@ -31,14 +44,31 @@ public class RemoveDocsAboutToSubmitHandler implements PreSubmitCallbackHandler<
     public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
         requireNonNull(callbackStage, "callbackStage must not be null");
         requireNonNull(callback, "callback must not be null");
-        return callback.getEvent() == Event.REMOVE_DOCUMENTS && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
+        return callback.getEvent() == Event.EDIT_DOCUMENTS && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
     }
 
     @Override
     public PreSubmitCallbackResponse<AsylumCase> handle(PreSubmitCallbackStage callbackStage,
                                                         Callback<AsylumCase> callback) {
+        CaseDetails<AsylumCase> caseDetailsBefore = callback.getCaseDetailsBefore().orElse(null);
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+        AsylumCase asylumCaseBefore = caseDetailsBefore == null ? null : caseDetailsBefore.getCaseData();
+        long caseId = callback.getCaseDetails().getId();
+        restoreDocumentTagForDocs(asylumCase, asylumCaseBefore);
+        editDocsCaseNoteService.writeAuditCaseNoteForGivenCaseId(caseId, asylumCase, asylumCaseBefore);
+        editDocService.cleanUpOverviewTabDocs(asylumCase, asylumCaseBefore);
+        updateEjpDocumentFields(asylumCase);
         return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+
+    private void restoreDocumentTagForDocs(AsylumCase asylumCase, AsylumCase asylumCaseBefore) {
+        getFieldDefinitions().forEach(field -> {
+            Optional<List<IdValue<DocumentWithMetadata>>> idValuesOptional = asylumCase.read(field);
+            idValuesOptional.ifPresent(idValues -> {
+                List<IdValue<DocumentWithMetadata>> idValuesBefore = getIdValuesBefore(asylumCaseBefore, field);
+                asylumCase.write(field, getIdValuesWithBeforeDocumentTags(idValues, idValuesBefore));
+            });
+        });
     }
 
     private List<AsylumCaseFieldDefinition> getFieldDefinitions() {
@@ -58,6 +88,29 @@ public class RemoveDocsAboutToSubmitHandler implements PreSubmitCallbackHandler<
                 ALL_FTPA_RESPONDENT_DECISION_DOCS,
                 FTPA_RESPONDENT_DOCUMENTS
         );
+    }
+
+    private List<IdValue<DocumentWithMetadata>> getIdValuesBefore(AsylumCase asylumCaseBefore,
+                                                                  AsylumCaseFieldDefinition fieldDefinition) {
+        if (asylumCaseBefore != null) {
+            Optional<List<IdValue<DocumentWithMetadata>>> idValuesBeforeOptional = asylumCaseBefore
+                    .read(fieldDefinition);
+            if (idValuesBeforeOptional.isPresent()) {
+                return idValuesBeforeOptional.get();
+            }
+        }
+        return Collections.emptyList();
+    }
+
+    private List<IdValue<DocumentWithMetadata>> getIdValuesWithBeforeDocumentTags(
+            List<IdValue<DocumentWithMetadata>> idValues, List<IdValue<DocumentWithMetadata>> idValuesBefore) {
+        List<IdValue<DocumentWithMetadata>> idValuesWithBeforeTag = new ArrayList<>();
+        idValues.forEach(idValue -> {
+            DocumentWithMetadata docWithBeforeTags = buildDocWithBeforeTags(idValuesBefore, idValue);
+            IdValue<DocumentWithMetadata> idValueWithBeforeTag = new IdValue<>(idValue.getId(), docWithBeforeTags);
+            idValuesWithBeforeTag.add(idValueWithBeforeTag);
+        });
+        return idValuesWithBeforeTag;
     }
 
     private DocumentWithMetadata buildDocWithBeforeTags(List<IdValue<DocumentWithMetadata>> idValuesBefore,
@@ -83,5 +136,44 @@ public class RemoveDocsAboutToSubmitHandler implements PreSubmitCallbackHandler<
         return new IdValue<>(idValueId, docMeta);
     }
 
+    private void updateEjpDocumentFields(AsylumCase asylumCase) {
+        Optional<List<IdValue<DocumentWithMetadata>>> maybeExistingTribunalDocuments =
+                asylumCase.read(TRIBUNAL_DOCUMENTS);
 
+        List<IdValue<DocumentWithMetadata>> existingTribunalDocuments =
+                maybeExistingTribunalDocuments.orElse(emptyList());
+
+        List<Document> updatedUtTransferOrderValues =
+                fetchDocumentsByTag(existingTribunalDocuments, UPPER_TRIBUNAL_TRANSFER_ORDER_DOCUMENT);
+        writeEjpDocuments(asylumCase, UT_TRANSFER_DOC, updatedUtTransferOrderValues);
+
+        List<Document> updatedIaut2Values =
+                fetchDocumentsByTag(existingTribunalDocuments, IAUT_2_FORM);
+        writeEjpDocuments(asylumCase, UPLOAD_EJP_APPEAL_FORM_DOCS, updatedIaut2Values);
+
+    }
+
+    private void writeEjpDocuments(AsylumCase asylumCase,
+                                   AsylumCaseFieldDefinition ejpDocumentField,
+                                   List<Document> updatedValues) {
+        if (updatedValues.isEmpty()) {
+            asylumCase.clear(ejpDocumentField);
+        } else {
+            List<IdValue<Document>> allDocs = new ArrayList<>();
+            int index = updatedValues.size();
+
+            for (Document value : updatedValues) {
+                allDocs.add(new IdValue<>(String.valueOf(index--), value));
+            }
+            asylumCase.write(ejpDocumentField, allDocs);
+        }
+    }
+
+    private List<Document> fetchDocumentsByTag(List<IdValue<DocumentWithMetadata>> existingTribunalDocuments,
+                                               DocumentTag documentTag) {
+        return existingTribunalDocuments.stream()
+                .filter(d -> d.getValue().getTag().equals(documentTag))
+                .map(v -> v.getValue().getDocument())
+                .collect(Collectors.toList());
+    }
 }
