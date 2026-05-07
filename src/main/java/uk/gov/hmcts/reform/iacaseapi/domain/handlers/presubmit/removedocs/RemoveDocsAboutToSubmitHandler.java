6eb0a18ca2ac28d668e16a6762c6b73ffde181dc
tiagreaney <tiagreaney@gmail.com>
2026-05-07 14:52:18 +0100

    update pr images

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/removedocs/RemoveDocsAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/removedocs/RemoveDocsAboutToSubmitHandler.java
@@ -31,149 +31,18 @@ import uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.editdocs.EditDocs
 @Component
 public class RemoveDocsAboutToSubmitHandler implements PreSubmitCallbackHandler<AsylumCase> {
 
-    @Autowired
-    private EditDocsCaseNoteService editDocsCaseNoteService;
-    @Autowired
-    private EditDocsService editDocService;
-
-    public DispatchPriority getDispatchPriority() {
-        return DispatchPriority.EARLIEST;
-    }
-
     @Override
     public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
         requireNonNull(callbackStage, "callbackStage must not be null");
         requireNonNull(callback, "callback must not be null");
-        return callback.getEvent() == Event.EDIT_DOCUMENTS && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
+        return callback.getEvent() == Event.REMOVE_DOCUMENTS && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
     }
 
     @Override
     public PreSubmitCallbackResponse<AsylumCase> handle(PreSubmitCallbackStage callbackStage,
                                                         Callback<AsylumCase> callback) {
-        CaseDetails<AsylumCase> caseDetailsBefore = callback.getCaseDetailsBefore().orElse(null);
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
-        AsylumCase asylumCaseBefore = caseDetailsBefore == null ? null : caseDetailsBefore.getCaseData();
-        long caseId = callback.getCaseDetails().getId();
-        restoreDocumentTagForDocs(asylumCase, asylumCaseBefore);
-        editDocsCaseNoteService.writeAuditCaseNoteForGivenCaseId(caseId, asylumCase, asylumCaseBefore);
-        editDocService.cleanUpOverviewTabDocs(asylumCase, asylumCaseBefore);
-        updateEjpDocumentFields(asylumCase);
+
         return new PreSubmitCallbackResponse<>(asylumCase);
-    }
-
-    private void restoreDocumentTagForDocs(AsylumCase asylumCase, AsylumCase asylumCaseBefore) {
-        getFieldDefinitions().forEach(field -> {
-            Optional<List<IdValue<DocumentWithMetadata>>> idValuesOptional = asylumCase.read(field);
-            idValuesOptional.ifPresent(idValues -> {
-                List<IdValue<DocumentWithMetadata>> idValuesBefore = getIdValuesBefore(asylumCaseBefore, field);
-                asylumCase.write(field, getIdValuesWithBeforeDocumentTags(idValues, idValuesBefore));
-            });
-        });
-    }
-
-    private List<AsylumCaseFieldDefinition> getFieldDefinitions() {
-        return Arrays.asList(
-                ADDITIONAL_EVIDENCE_DOCUMENTS,
-                TRIBUNAL_DOCUMENTS,
-                REHEARD_HEARING_DOCUMENTS,
-                HEARING_DOCUMENTS,
-                LEGAL_REPRESENTATIVE_DOCUMENTS,
-                ADDENDUM_EVIDENCE_DOCUMENTS,
-                RESPONDENT_DOCUMENTS,
-                DRAFT_DECISION_AND_REASONS_DOCUMENTS,
-                FINAL_DECISION_AND_REASONS_DOCUMENTS,
-                UPLOAD_SENSITIVE_DOCS,
-                ALL_FTPA_APPELLANT_DECISION_DOCS,
-                FTPA_APPELLANT_DOCUMENTS,
-                ALL_FTPA_RESPONDENT_DECISION_DOCS,
-                FTPA_RESPONDENT_DOCUMENTS
-        );
-    }
-
-    private List<IdValue<DocumentWithMetadata>> getIdValuesBefore(AsylumCase asylumCaseBefore,
-                                                                  AsylumCaseFieldDefinition fieldDefinition) {
-        if (asylumCaseBefore != null) {
-            Optional<List<IdValue<DocumentWithMetadata>>> idValuesBeforeOptional = asylumCaseBefore
-                    .read(fieldDefinition);
-            if (idValuesBeforeOptional.isPresent()) {
-                return idValuesBeforeOptional.get();
-            }
-        }
-        return Collections.emptyList();
-    }
-
-    private List<IdValue<DocumentWithMetadata>> getIdValuesWithBeforeDocumentTags(
-            List<IdValue<DocumentWithMetadata>> idValues, List<IdValue<DocumentWithMetadata>> idValuesBefore) {
-        List<IdValue<DocumentWithMetadata>> idValuesWithBeforeTag = new ArrayList<>();
-        idValues.forEach(idValue -> {
-            DocumentWithMetadata docWithBeforeTags = buildDocWithBeforeTags(idValuesBefore, idValue);
-            IdValue<DocumentWithMetadata> idValueWithBeforeTag = new IdValue<>(idValue.getId(), docWithBeforeTags);
-            idValuesWithBeforeTag.add(idValueWithBeforeTag);
-        });
-        return idValuesWithBeforeTag;
-    }
-
-    private DocumentWithMetadata buildDocWithBeforeTags(List<IdValue<DocumentWithMetadata>> idValuesBefore,
-                                                        IdValue<DocumentWithMetadata> idValue) {
-        DocumentWithMetadata value = idValue.getValue();
-        DocumentTag beforeTagForGivenIdValue = getBeforeTagForGivenIdValue(idValue.getId(), idValuesBefore);
-        return new DocumentWithMetadata(value.getDocument(),
-                value.getDescription() == null ? "" : value.getDescription(), value.getDateUploaded(),
-                beforeTagForGivenIdValue, value.getSuppliedBy());
-    }
-
-    private DocumentTag getBeforeTagForGivenIdValue(String idValueId,
-                                                    List<IdValue<DocumentWithMetadata>> fieldBeforeList) {
-        return fieldBeforeList.stream()
-                .filter(beforeDoc -> beforeDoc.getId().equals(idValueId))
-                .findFirst()
-                .orElse(getIdValueWithDefaultTag(idValueId)).getValue().getTag();
-    }
-
-    private IdValue<DocumentWithMetadata> getIdValueWithDefaultTag(String idValueId) {
-        DocumentWithMetadata docMeta = new DocumentWithMetadata(null, null, null,
-                DocumentTag.NONE);
-        return new IdValue<>(idValueId, docMeta);
-    }
-
-    private void updateEjpDocumentFields(AsylumCase asylumCase) {
-        Optional<List<IdValue<DocumentWithMetadata>>> maybeExistingTribunalDocuments =
-                asylumCase.read(TRIBUNAL_DOCUMENTS);
-
-        List<IdValue<DocumentWithMetadata>> existingTribunalDocuments =
-                maybeExistingTribunalDocuments.orElse(emptyList());
-
-        List<Document> updatedUtTransferOrderValues =
-                fetchDocumentsByTag(existingTribunalDocuments, UPPER_TRIBUNAL_TRANSFER_ORDER_DOCUMENT);
-        writeEjpDocuments(asylumCase, UT_TRANSFER_DOC, updatedUtTransferOrderValues);
-
-        List<Document> updatedIaut2Values =
-                fetchDocumentsByTag(existingTribunalDocuments, IAUT_2_FORM);
-        writeEjpDocuments(asylumCase, UPLOAD_EJP_APPEAL_FORM_DOCS, updatedIaut2Values);
-
-    }
-
-    private void writeEjpDocuments(AsylumCase asylumCase,
-                                   AsylumCaseFieldDefinition ejpDocumentField,
-                                   List<Document> updatedValues) {
-        if (updatedValues.isEmpty()) {
-            asylumCase.clear(ejpDocumentField);
-        } else {
-            List<IdValue<Document>> allDocs = new ArrayList<>();
-            int index = updatedValues.size();
-
-            for (Document value : updatedValues) {
-                allDocs.add(new IdValue<>(String.valueOf(index--), value));
-            }
-            asylumCase.write(ejpDocumentField, allDocs);
-        }
-    }
-
-    private List<Document> fetchDocumentsByTag(List<IdValue<DocumentWithMetadata>> existingTribunalDocuments,
-                                               DocumentTag documentTag) {
-        return existingTribunalDocuments.stream()
-                .filter(d -> d.getValue().getTag().equals(documentTag))
-                .map(v -> v.getValue().getDocument())
-                .collect(Collectors.toList());
     }
 }
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/removedocs/RemoveDocsAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/removedocs/RemoveDocsAboutToSubmitHandlerTest.java
@@ -1,95 +1,36 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit.removedocs;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.powermock.api.mockito.PowerMockito.when;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentTag.*;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentTag.ADDITIONAL_EVIDENCE;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentTag;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.DocumentWithMetadata;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.Document;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
 class RemoveDocsAboutToSubmitHandlerTest {
 
-    public static final String ID_VALUE = "0a165fa5-086b-49d6-8b7e-f00ed34d941a";
-
-    private final Document newUtTransferOrderDocumentOne = new Document(
-            "someurl_ut_transfer_order_one",
-            "someurl_ut_transfer_order_binaryurl_one",
-            "someurl_ut_transfer_order_filename_one.pdf");
-
-    private final Document newEjpAppealFormDocumentOne = new Document(
-            "someurl_ejp_appeal_form_one",
-            "someurl_ejp_appeal_form_binaryurl_one",
-            "someurl_ejp_appeal_form_filename_one.pdf");
-
-    private final DocumentWithMetadata someTribunalMetaOne = new DocumentWithMetadata(
-            newUtTransferOrderDocumentOne,
-            "some description",
-            "21/07/2021",
-            DocumentTag.UPPER_TRIBUNAL_TRANSFER_ORDER_DOCUMENT,
-            "some supplier"
-    );
-
-    private final DocumentWithMetadata someTribunalMetaTwo = new DocumentWithMetadata(
-            newEjpAppealFormDocumentOne,
-            "some description",
-            "21/07/2021",
-            DocumentTag.IAUT_2_FORM,
-            "some supplier"
-    );
-
     @Mock
     private Callback<AsylumCase> callback;
-    @Mock
-    private CaseDetails<AsylumCase> caseDetailsBefore;
-    @Mock
-    private CaseDetails<AsylumCase> caseDetails;
-    @Mock
-    private AsylumCase asylumCaseEjp;
-    @Mock
-    private RemoveDocsService removeDocService;
     @InjectMocks
     private RemoveDocsAboutToSubmitHandler removeDocsAboutToSubmitHandler;
 
     @ParameterizedTest
     @CsvSource({
-            "REMOVE_DOCS, ABOUT_TO_SUBMIT, true",
+            "REMOVE_DOCUMENTS, ABOUT_TO_SUBMIT, true",
             "START_APPEAL, ABOUT_TO_SUBMIT, false",
-            "REMOVE_DOCS, ABOUT_TO_START, false"
+            "REMOVE_DOCUMENTS, ABOUT_TO_START, false"
     })
     void canHandleHappyPathScenarios(Event event, PreSubmitCallbackStage callbackStage, boolean expectedResult) {
         given(callback.getEvent()).willReturn(event);
