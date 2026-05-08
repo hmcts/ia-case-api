b434206820773fea5836572ff0d8048110df6552
david-gimelle <david.gimelle@gmail.com>
2026-05-08 15:07:35 +0100

    hack call to home office api

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/HomeOfficeReferenceService.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/service/HomeOfficeReferenceService.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.service;
 
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPELLANT_DATE_OF_BIRTH;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPELLANT_FAMILY_NAME;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.APPELLANT_GIVEN_NAMES;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HOME_OFFICE_APPELLANTS;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HOME_OFFICE_APPELLANT_API_RESPONSE_STATUS;
 
@@ -32,44 +35,56 @@ public class HomeOfficeReferenceService {
         Optional<List<IdValue<HomeOfficeAppellant>>> homeOfficeAppellants = asylumCase.read(HOME_OFFICE_APPELLANTS);
         // If we have a list of appellants already, don't call the API again.
         if (homeOfficeAppellants.isPresent()) {
-            log.info("Returning previously retrieved Home Office reference data for case with Home Office reference {}.", hoReference);
+            log.info("[HOME OFFICE CACHE HIT] Returning {} appellant(s) already stored in case data for HO reference {}.",
+                homeOfficeAppellants.get().size(), hoReference);
             return homeOfficeAppellants;
         }
 
-        // Home Office API has not been called yet (or was unavailable the last time we tried) - call it now
-        log.info("Getting Home Office biographic data for case with reference ID {} ...", hoReference);
-        // Raise an event in home-office-integration-api
-        
-        AsylumCase asylumCaseWithHomeOfficeData = homeOfficeApi.midEvent(callback);
-        // Check return status and store it in the case record
-        HomeOfficeApiResponseStatusType responseStatus = 
-                                        asylumCaseWithHomeOfficeData.read(HOME_OFFICE_APPELLANT_API_RESPONSE_STATUS, HomeOfficeApiResponseStatusType.class)
-                                        .orElse(HomeOfficeApiResponseStatusType.UNKNOWN);
-        asylumCase.write(HOME_OFFICE_APPELLANT_API_RESPONSE_STATUS, responseStatus);
-        if (responseStatus.equals(HomeOfficeApiResponseStatusType.OK)) {
-            log.info("Home Office biographic data retrieved for case with reference ID {}.", hoReference);
-            // Update the case record object with the Home Office reference data
-            homeOfficeAppellants = asylumCaseWithHomeOfficeData.read(HOME_OFFICE_APPELLANTS);
-        
-            asylumCase.write(HOME_OFFICE_APPELLANTS, homeOfficeAppellants);
-        } else {
-            // The API did not return any data; log the diagnostic message appropriately
-            String logMessage = "Biographic information from Home Office asylum (etc.) application with Home Office reference "
-                              + hoReference + " could not be retrieved.\n\n" + responseStatus.getHoIntegrationErrorText(hoReference)
-                              + "\n\nSee the corresponding logs in ia-home-office-integration-api for more details.";                    
-            int statusCode = responseStatus.getStatusCode();
-            if (statusCode == 404) {
-                // User error (appeal not found)
-                log.info(logMessage);
-            } else if (statusCode <= 0 || statusCode >= 500) {
-                // Server error
-                log.warn(logMessage);
-            } else {
-                // Client error
-                log.error(logMessage);
-            }
-        }
+        // TEMP: return dummy appellant to skip Home Office API call during local investigation
+        log.info("[HOME OFFICE DUMMY DATA] Returning dummy appellant for HO reference {}.", hoReference);
+        HomeOfficeAppellant dummy = new HomeOfficeAppellant();
+        dummy.setFamilyName(asylumCase.read(APPELLANT_FAMILY_NAME, String.class).orElse("Smith"));
+        dummy.setGivenNames(asylumCase.read(APPELLANT_GIVEN_NAMES, String.class).orElse("John"));
+        dummy.setDateOfBirth(asylumCase.read(APPELLANT_DATE_OF_BIRTH, String.class).orElse("1990-01-01"));
+        homeOfficeAppellants = Optional.of(List.of(new IdValue<>("1", dummy)));
+        asylumCase.write(HOME_OFFICE_APPELLANTS, homeOfficeAppellants);
+        asylumCase.write(HOME_OFFICE_APPELLANT_API_RESPONSE_STATUS, HomeOfficeApiResponseStatusType.OK);
         return homeOfficeAppellants;
+
+        // Home Office API has not been called yet (or was unavailable the last time we tried) - call it now
+        // log.info("[HOME OFFICE API CALL] No cached data found for HO reference {} — calling ia-home-office-integration-api now.", hoReference);
+        // Raise an event in home-office-integration-api
+
+        // AsylumCase asylumCaseWithHomeOfficeData = homeOfficeApi.midEvent(callback);
+        // Check return status and store it in the case record
+        // HomeOfficeApiResponseStatusType responseStatus =
+        //                                 asylumCaseWithHomeOfficeData.read(HOME_OFFICE_APPELLANT_API_RESPONSE_STATUS, HomeOfficeApiResponseStatusType.class)
+        //                                 .orElse(HomeOfficeApiResponseStatusType.UNKNOWN);
+        // asylumCase.write(HOME_OFFICE_APPELLANT_API_RESPONSE_STATUS, responseStatus);
+        // if (responseStatus.equals(HomeOfficeApiResponseStatusType.OK)) {
+        //     homeOfficeAppellants = asylumCaseWithHomeOfficeData.read(HOME_OFFICE_APPELLANTS);
+        //     log.info("[HOME OFFICE API CALL] Successfully retrieved {} appellant(s) from ia-home-office-integration-api for HO reference {}.",
+        //         homeOfficeAppellants.map(List::size).orElse(0), hoReference);
+        //     // Update the case record object with the Home Office reference data
+        //     asylumCase.write(HOME_OFFICE_APPELLANTS, homeOfficeAppellants);
+        // } else {
+        //     // The API did not return any data; log the diagnostic message appropriately
+        //     String logMessage = "Biographic information from Home Office asylum (etc.) application with Home Office reference "
+        //                       + hoReference + " could not be retrieved.\n\n" + responseStatus.getHoIntegrationErrorText(hoReference)
+        //                       + "\n\nSee the corresponding logs in ia-home-office-integration-api for more details.";
+        //     int statusCode = responseStatus.getStatusCode();
+        //     if (statusCode == 404) {
+        //         log.info(logMessage);
+        //     } else if (statusCode <= 0 || statusCode >= 500) {
+        //         log.warn(logMessage);
+        //     } else {
+        //         log.error(logMessage);
+        //     }
+        // }
+        // return homeOfficeAppellants;
+
+
+        //Probably this need to be amended to persist in the asylmy case from ccd api not home office api
     }
 
 }
