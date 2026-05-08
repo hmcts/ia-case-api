65324ccc51fc7ce04fbb94990095707467171758
Ed Graham <ed.graham@hmcts.net>
2026-05-08 18:32:43 +0100

    Ooops

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/HomeOfficeStatutoryTimeframeTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/HomeOfficeStatutoryTimeframeTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.entities;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
