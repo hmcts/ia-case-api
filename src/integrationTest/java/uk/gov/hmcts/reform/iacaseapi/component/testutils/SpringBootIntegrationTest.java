54fd728dcfc06453e31a3219346c96cfdb4d5482
kleog <kleogeorgiou@hotmail.com>
2026-05-13 17:24:36 +0100

    DIAC-2517 - fix checkstyles

--- src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/component/testutils/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/component/testutils/SpringBootIntegrationTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
--- src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/config/OpenApiPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacaseapi/config/OpenApiPublisher.java
@@ -2,24 +2,13 @@ package uk.gov.hmcts.reform.iacaseapi.config;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockFilterConfig;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.iacaseapi.component.testutils.SpringBootIntegrationTest;
 
 /**
