07a088eed707d38acbc4ea095b9c3d328cfd41d3
nilay <nilay.patel@solirius.com>
2026-06-15 14:55:59 +0100

    creating case note to state event submitted, nlr name and any docs uploaded or generated from it

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AipNlrEventSubmissionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AipNlrEventSubmissionHandler.java
@@ -1,22 +1,42 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.CaseNote;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.NonLegalRepDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit.editdocs.EditDocsAuditLogService;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.Appender;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.CASE_NOTES;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HAS_NLR_SUBMITTED;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.NLR_DETAILS;
 import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo.NO;
 
 @Component
 public class AipNlrEventSubmissionHandler implements PreSubmitCallbackHandler<AsylumCase> {
+
+    @Autowired
+    private Appender<CaseNote> appender;
+    @Autowired
+    private EditDocsAuditLogService editDocsAuditLogService;
+
+    @Override
     public DispatchPriority getDispatchPriority() {
-        return DispatchPriority.LATEST;
+        return DispatchPriority.LAST;
     }
 
     public boolean canHandle(
@@ -39,6 +59,30 @@ public class AipNlrEventSubmissionHandler implements PreSubmitCallbackHandler<As
         }
         AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
         asylumCase.clear(HAS_NLR_SUBMITTED);
+
+        String nlrFullName = asylumCase.read(NLR_DETAILS, NonLegalRepDetails.class)
+                .map(NonLegalRepDetails::getFullName)
+                .orElseThrow(() -> new IllegalStateException("Non-legal representative details are not present"));
+        Optional<List<IdValue<CaseNote>>> maybeExistingCaseNotes = asylumCase.read(CASE_NOTES);
+        List<IdValue<CaseNote>> allCaseNotes = appender.append(
+            buildNewCaseNote(callback, nlrFullName), maybeExistingCaseNotes.orElse(Collections.emptyList()));
+        asylumCase.write(CASE_NOTES, allCaseNotes);
+
         return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+
+    private CaseNote buildNewCaseNote(Callback<AsylumCase> callback, String nlrFullName) {
+        List<String> docList = editDocsAuditLogService.getUploadedOrGeneratedDocumentNames(callback);
+        String caseNoteDescription = "Non-legal representative submitted the event on behalf of the appellant.";
+        if (!docList.isEmpty()) {
+            caseNoteDescription += "\nThe following documents were uploaded or generated as a result of this event: "
+                + String.join(",\n", docList);
+        }
+        return new CaseNote(
+            "NLR event submission - " + callback.getEvent().toString(),
+            caseNoteDescription,
+            nlrFullName,
+            LocalDate.now().toString()
+        );
     }
 }
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AipNlrEventSubmissionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AipNlrEventSubmissionHandlerTest.java
@@ -5,46 +5,81 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.CaseNote;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.NonLegalRepDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.iacaseapi.domain.handlers.postsubmit.editdocs.EditDocsAuditLogService;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.Appender;
 
+import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.quality.Strictness.LENIENT;
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = LENIENT)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class AipNlrEventSubmissionHandlerTest {
 
     @Mock
     private Callback<AsylumCase> callback;
-    @Mock private CaseDetails<AsylumCase> caseDetails;
-    @Mock private AsylumCase asylumCase;
+    @Mock
+    private CaseDetails<AsylumCase> caseDetails;
+    @Mock
+    private AsylumCase asylumCase;
+    @Mock
+    private Appender<CaseNote> appender;
+    @Mock
+    private EditDocsAuditLogService editDocsAuditLogService;
+    @Mock
+    private List<IdValue<CaseNote>> mockList;
 
+    @Captor
+    private ArgumentCaptor<CaseNote> caseNoteCaptor;
+
+    @InjectMocks
     private AipNlrEventSubmissionHandler handler;
 
+    private final String forenameAndSurname = "John Doe";
     @BeforeEach
     void setUp() {
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
-        handler = new AipNlrEventSubmissionHandler();
+        String forename = "John";
+        String surname = "Doe";
+        when(asylumCase.read(AsylumCaseFieldDefinition.NLR_DETAILS, NonLegalRepDetails.class))
+            .thenReturn(Optional.of(NonLegalRepDetails.builder().givenNames(forename).familyName(surname).build()));
+    }
+
+    @Test
+    void getDispatchPriority_should_return_last() {
+        assertEquals(DispatchPriority.LAST, handler.getDispatchPriority());
     }
 
     @Test
@@ -89,16 +124,83 @@ class AipNlrEventSubmissionHandlerTest {
     }
 
     @Test
+    void handle_should_throw_if_no_nlr_name() {
+        when(callback.getEvent()).thenReturn(Event.SUBMIT_APPEAL);
+        when(asylumCase.read(AsylumCaseFieldDefinition.HAS_NLR_SUBMITTED, YesOrNo.class))
+            .thenReturn(Optional.of(YesOrNo.YES));
+        when(asylumCase.read(AsylumCaseFieldDefinition.NLR_DETAILS, NonLegalRepDetails.class))
+            .thenReturn(Optional.empty());
+        when(editDocsAuditLogService.getUploadedOrGeneratedDocumentNames(callback)).thenReturn(emptyList());
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+            () -> handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback));
+        assertEquals("Non-legal representative details are not present", exception.getMessage());
+    }
+
+    @Test
     void handle_should_clear_hasNlrSubmitted() {
         when(callback.getEvent()).thenReturn(Event.SUBMIT_APPEAL);
         when(asylumCase.read(AsylumCaseFieldDefinition.HAS_NLR_SUBMITTED, YesOrNo.class))
             .thenReturn(Optional.of(YesOrNo.YES));
-
+        when(editDocsAuditLogService.getUploadedOrGeneratedDocumentNames(callback)).thenReturn(emptyList());
         PreSubmitCallbackResponse<AsylumCase> response =
             handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
 
         assertNotNull(response);
         assertTrue(response.getErrors().isEmpty());
         verify(asylumCase, times(1)).clear(AsylumCaseFieldDefinition.HAS_NLR_SUBMITTED);
+    }
+
+    @ParameterizedTest
+    @EnumSource(Event.class)
+    void handle_should_create_case_notes_if_empty_doc_list_and_none_existing(Event event) {
+        when(callback.getEvent()).thenReturn(event);
+        when(asylumCase.read(AsylumCaseFieldDefinition.HAS_NLR_SUBMITTED, YesOrNo.class))
+            .thenReturn(Optional.of(YesOrNo.YES));
+        when(editDocsAuditLogService.getUploadedOrGeneratedDocumentNames(callback)).thenReturn(emptyList());
+        when(appender.append(any(CaseNote.class), eq(emptyList()))).thenReturn(mockList);
+        PreSubmitCallbackResponse<AsylumCase> response =
+            handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertNotNull(response);
+        assertTrue(response.getErrors().isEmpty());
+        verify(asylumCase, times(1)).clear(AsylumCaseFieldDefinition.HAS_NLR_SUBMITTED);
+        verify(appender, times(1)).append(caseNoteCaptor.capture(), eq(emptyList()));
+        CaseNote capturedCaseNote = caseNoteCaptor.getValue();
+        assertEquals("NLR event submission - " + event.toString(), capturedCaseNote.getCaseNoteSubject());
+        assertEquals("Non-legal representative submitted the event on behalf of the appellant.", capturedCaseNote.getCaseNoteDescription());
+        assertEquals(forenameAndSurname, capturedCaseNote.getUser());
+        verify(asylumCase, times(1)).write(AsylumCaseFieldDefinition.CASE_NOTES, mockList);
+    }
+
+    @Test
+    void handle_should_append_if_non_empty_doc_list_and_existing() {
+        when(callback.getEvent()).thenReturn(Event.SUBMIT_APPEAL);
+        when(asylumCase.read(AsylumCaseFieldDefinition.HAS_NLR_SUBMITTED, YesOrNo.class))
+            .thenReturn(Optional.of(YesOrNo.YES));
+        when(editDocsAuditLogService.getUploadedOrGeneratedDocumentNames(callback)).thenReturn(List.of("doc1.pdf", "doc2.docx"));
+        CaseNote caseNote = mock(CaseNote.class);
+        IdValue<CaseNote> idValue = new IdValue<>("123", caseNote);
+        when(asylumCase.read(AsylumCaseFieldDefinition.CASE_NOTES)).thenReturn(Optional.of(List.of(idValue)));
+        when(appender.append(any(CaseNote.class), eq(List.of(idValue)))).thenReturn(mockList);
+        PreSubmitCallbackResponse<AsylumCase> response =
+            handler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertNotNull(response);
+        assertTrue(response.getErrors().isEmpty());
+        verify(asylumCase, times(1)).clear(AsylumCaseFieldDefinition.HAS_NLR_SUBMITTED);
+        verify(appender, times(1)).append(caseNoteCaptor.capture(), eq(List.of(idValue)));
+        verify(asylumCase, times(1)).write(AsylumCaseFieldDefinition.CASE_NOTES, mockList);
+        CaseNote capturedCaseNote = caseNoteCaptor.getValue();
+        assertEquals("NLR event submission - submitAppeal" , capturedCaseNote.getCaseNoteSubject());
+        assertTrue(capturedCaseNote.getCaseNoteDescription()
+            .contains("Non-legal representative submitted the event on behalf of the appellant."));
+        assertTrue(capturedCaseNote.getCaseNoteDescription()
+            .contains("The following documents were uploaded or generated as a result of this event:"));
+        assertTrue(capturedCaseNote.getCaseNoteDescription()
+            .contains("doc1.pdf"));
+        assertTrue(capturedCaseNote.getCaseNoteDescription()
+            .contains("doc2.docx"));
+        assertEquals(forenameAndSurname, capturedCaseNote.getUser());
+        verify(asylumCase, times(1)).write(AsylumCaseFieldDefinition.CASE_NOTES, mockList);
     }
 }
