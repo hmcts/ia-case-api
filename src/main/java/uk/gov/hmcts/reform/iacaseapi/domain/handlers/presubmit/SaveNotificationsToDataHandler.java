297a4bca9d8ed29eacf6d874c97f5af8643d9920
nilay <nilay.patel@solirius.com>
2026-07-23 17:01:44 +0100

    DIAC-123 fixing non standard quote marks for PDF generation

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SaveNotificationsToDataHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SaveNotificationsToDataHandler.java
@@ -137,6 +137,8 @@ public class SaveNotificationsToDataHandler implements PreSubmitCallbackHandler<
             .replace("\r\n", "<br>")
             .replace("’", "'")
             .replace("‘", "'")
+            .replace("“", "\"")
+            .replace("”", "\"")
             .replace(" ", " ")
             .replace("&nbsp;", " ")
             + "</div>";
