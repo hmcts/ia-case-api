666dc681ec7b96a03f2589cd652094864db9b846
david-gimelle <david.gimelle@gmail.com>
2026-07-28 10:08:22 +0100

    [DIAC-2702] update next text 24 weeks

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/CaseSubmittedConfirmationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/postsubmit/CaseSubmittedConfirmationTest.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.STF_24W_PREVIOUS_STATUS_WAS_YES_AUTO_GENERATED;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import lombok.Value;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,25 +21,35 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PostSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
-@MockitoSettings(strictness = Strictness.STRICT_STUBS)
+@MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
 class CaseSubmittedConfirmationTest {
 
     @Mock
     private Callback<AsylumCase> callback;
+    @Mock
+    private CaseDetails<AsylumCase> caseDetails;
+    @Mock
+    private AsylumCase asylumCase;
 
     private CaseSubmittedConfirmation caseSubmittedConfirmation =
         new CaseSubmittedConfirmation();
 
     @Test
-    void should_return_confirmation() {
+    void should_return_bau_confirmation_when_not_24_week_case() {
         ReflectionTestUtils.setField(caseSubmittedConfirmation, "isSaveAndContinueEnabled", true);
 
         when(callback.getEvent()).thenReturn(Event.SUBMIT_CASE);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.read(STF_24W_PREVIOUS_STATUS_WAS_YES_AUTO_GENERATED, YesOrNo.class))
+            .thenReturn(Optional.of(YesOrNo.NO));
 
         PostSubmitCallbackResponse callbackResponse =
             caseSubmittedConfirmation.handle(callback);
@@ -47,8 +59,49 @@ class CaseSubmittedConfirmationTest {
         assertTrue(callbackResponse.getConfirmationBody().isPresent());
 
         assertTrue(callbackResponse.getConfirmationHeader().get().contains("You have submitted your case"));
-
         assertTrue(callbackResponse.getConfirmationBody().get().contains("The case officer will now review your appeal"));
+        assertTrue(callbackResponse.getConfirmationBody().get().contains("they will send it to the respondent for them to review"));
+    }
+
+    @Test
+    void should_return_24_week_confirmation_when_24_week_flag_is_yes() {
+        ReflectionTestUtils.setField(caseSubmittedConfirmation, "isSaveAndContinueEnabled", true);
+
+        when(callback.getEvent()).thenReturn(Event.SUBMIT_CASE);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.read(STF_24W_PREVIOUS_STATUS_WAS_YES_AUTO_GENERATED, YesOrNo.class))
+            .thenReturn(Optional.of(YesOrNo.YES));
+
+        PostSubmitCallbackResponse callbackResponse =
+            caseSubmittedConfirmation.handle(callback);
+
+        assertNotNull(callbackResponse);
+        assertTrue(callbackResponse.getConfirmationHeader().isPresent());
+        assertTrue(callbackResponse.getConfirmationBody().isPresent());
+
+        assertTrue(callbackResponse.getConfirmationHeader().get().contains("You have submitted your case"));
+        assertTrue(callbackResponse.getConfirmationBody().get().contains("The case officer will now review the appeal"));
+        assertTrue(callbackResponse.getConfirmationBody().get().contains("you will be asked to provide any hearing requirements"));
+    }
+
+    @Test
+    void should_return_bau_confirmation_when_24_week_flag_is_absent() {
+        ReflectionTestUtils.setField(caseSubmittedConfirmation, "isSaveAndContinueEnabled", true);
+
+        when(callback.getEvent()).thenReturn(Event.SUBMIT_CASE);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.read(STF_24W_PREVIOUS_STATUS_WAS_YES_AUTO_GENERATED, YesOrNo.class))
+            .thenReturn(Optional.empty());
+
+        PostSubmitCallbackResponse callbackResponse =
+            caseSubmittedConfirmation.handle(callback);
+
+        assertNotNull(callbackResponse);
+        assertTrue(callbackResponse.getConfirmationBody().isPresent());
+        assertTrue(callbackResponse.getConfirmationBody().get().contains("The case officer will now review your appeal"));
+        assertTrue(callbackResponse.getConfirmationBody().get().contains("they will send it to the respondent for them to review"));
     }
 
     @Test
