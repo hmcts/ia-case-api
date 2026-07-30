28f0fc7434ac3ec4286b370dc50e1715b515b772
tiagreaney <tiagreaney@gmail.com>
2026-07-30 11:09:12 +0100

    test update

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
@@ -58,6 +58,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(46, DocumentTag.values().length);
+        assertEquals(47, DocumentTag.values().length);
     }
 }
