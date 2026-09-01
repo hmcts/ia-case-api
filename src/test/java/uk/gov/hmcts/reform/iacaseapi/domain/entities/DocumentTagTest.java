d05ffbb80194a767b47db586de87cba0502a3f90
tiagreaney <tiagreaney@gmail.com>
2026-09-01 11:05:13 +0100

    fix test

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/entities/DocumentTagTest.java
@@ -69,6 +69,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(56, DocumentTag.values().length);
+        assertEquals(57, DocumentTag.values().length);
     }
 }
