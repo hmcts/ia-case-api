bc6aded37081b114947a256b737d7d47823f6c5e
kleog <kleogeorgiou@hotmail.com>
2026-09-09 16:41:28 +0100

    DIAC-2899 - previous commit but for functional tests only

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
-import uk.gov.hmcts.reform.iacaseapi.infrastructure.config.FeignConfiguration;
-import uk.gov.hmcts.reform.iacaseapi.infrastructure.config.LegacyFeignConfiguration;
 
 @SpringBootApplication
 @EnableRetry
@@ -15,10 +13,6 @@ import uk.gov.hmcts.reform.iacaseapi.infrastructure.config.LegacyFeignConfigurat
     "uk.gov.hmcts.reform.authorisation",
     "uk.gov.hmcts.reform.iacaseapi",
     "uk.gov.hmcts.reform.ccd"
-    },
-    defaultConfiguration = {
-        FeignConfiguration.class,
-        LegacyFeignConfiguration.class
     })
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
