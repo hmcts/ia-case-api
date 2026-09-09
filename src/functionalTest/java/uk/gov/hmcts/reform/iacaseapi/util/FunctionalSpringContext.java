8e82f5d16b2e95afb3f6f888a9cfc41731664aec
kleog <kleogeorgiou@hotmail.com>
2026-09-09 17:19:02 +0100

    DIAC-2899 - try and resolve feign by adding jsonmapper

--- src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/FunctionalSpringContext.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacaseapi/util/FunctionalSpringContext.java
@@ -6,19 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import uk.gov.hmcts.reform.iacaseapi.Application;
-import uk.gov.hmcts.reform.iacaseapi.infrastructure.config.FeignConfiguration;
-import uk.gov.hmcts.reform.iacaseapi.infrastructure.config.LegacyFeignConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {
     "uk.gov.hmcts.reform.authorisation",
     "uk.gov.hmcts.reform.iacaseapi.infrastructure.clients",
-    "uk.gov.hmcts.reform.ccd"
-    },
-    defaultConfiguration = {
-        FeignConfiguration.class,
-        LegacyFeignConfiguration.class
-    })
+})
 @ComponentScan(basePackages = {"uk.gov.hmcts.reform.iacaseapi"})
 @SuppressWarnings("HideUtilityClassConstructor")
 public class FunctionalSpringContext {
--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/config/FeignConfiguration.java
@@ -1,22 +1,40 @@
 package uk.gov.hmcts.reform.iacaseapi.infrastructure.config;
 
-import org.springframework.boot.http.converter.autoconfigure.ClientHttpMessageConvertersCustomizer;
-import org.springframework.cloud.openfeign.support.*;
-import tools.jackson.databind.DeserializationFeature;
-import tools.jackson.databind.cfg.EnumFeature;
-import tools.jackson.databind.json.JsonMapper;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.http.converter.autoconfigure.ClientHttpMessageConvertersCustomizer;
+import org.springframework.cloud.openfeign.support.FeignHttpMessageConverters;
+import org.springframework.cloud.openfeign.support.HttpMessageConverterCustomizer;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
+import tools.jackson.databind.DeserializationFeature;
+import tools.jackson.databind.cfg.EnumFeature;
+import tools.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class FeignConfiguration {
+
+    @Bean
+    @Primary
+    public JsonMapper jsonMapper() {
+        return JsonMapper.builder()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(EnumFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true)
+            .build();
+    }
+
+    @Bean
+    public HttpMessageConverter<?> feignJacksonHttpMessageConverter(JsonMapper jsonMapper) {
+        return new JacksonJsonHttpMessageConverter(jsonMapper);
+    }
 
     @Bean
     public FeignHttpMessageConverters feignHttpMessageConverters(
@@ -35,21 +53,8 @@ public class FeignConfiguration {
     }
 
     @Bean
+    @Primary
     public Decoder decoder(ObjectProvider<FeignHttpMessageConverters> feignHttpMessageConverters) {
         return new ResponseEntityDecoder(new SpringDecoder(feignHttpMessageConverters));
-    }
-
-    @Bean
-    public HttpMessageConverter<?> feignJacksonHttpMessageConverter(JsonMapper jsonMapper) {
-        return new JacksonJsonHttpMessageConverter(jsonMapper);
-    }
-
-    @Bean
-    @Primary
-    public JsonMapper objectMapper(JsonMapper.Builder builder) {
-        return builder
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(EnumFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true)
-            .build();
     }
 }
