4a9bb3496e3c9de7ed15217aaaeb9a68015d07cb
kleog <kleogeorgiou@hotmail.com>
2026-09-21 11:25:39 +0100

    Checkstyles

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/TimedEventServiceApi.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/infrastructure/clients/TimedEventServiceApi.java
@@ -3,20 +3,10 @@ package uk.gov.hmcts.reform.iacaseapi.infrastructure.clients;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static uk.gov.hmcts.reform.iacaseapi.infrastructure.config.ServiceTokenGeneratorConfiguration.SERVICE_AUTHORIZATION;
 
-import java.util.Collections;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Feign;
-import feign.codec.Decoder;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.cloud.openfeign.support.FeignHttpMessageConverters;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
-import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.boot.http.converter.autoconfigure.HttpMessageConverters;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,8 +37,6 @@ public interface TimedEventServiceApi {
     );
 
     class Configuration {
-
-
 
         @Bean
         @Scope("prototype")
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RevokeCaseAccessHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RevokeCaseAccessHandlerTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
