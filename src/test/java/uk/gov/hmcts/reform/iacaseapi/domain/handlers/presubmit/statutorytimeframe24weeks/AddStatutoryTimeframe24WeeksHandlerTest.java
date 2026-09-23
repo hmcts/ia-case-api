e79e5bd1b978f9f80c9017afd0b9411e4ae07865
nilay <nilay.patel@solirius.com>
2026-09-23 12:39:08 +0100

    updating code to work with api calls

--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/statutorytimeframe24weeks/AddStatutoryTimeframe24WeeksHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/statutorytimeframe24weeks/AddStatutoryTimeframe24WeeksHandlerTest.java
@@ -11,7 +11,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
-import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
@@ -19,14 +18,13 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallb
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.YesOrNo;
 
-import java.time.LocalDate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
@@ -61,7 +59,6 @@ class AddStatutoryTimeframe24WeeksHandlerTest {
 
         assertThat(callbackResponse.getData()).isEqualTo(asylumCase);
         verify(updateStatutoryTimeframe24WeeksService).updateAsylumCase(asylumCase, YesOrNo.YES);
-        verify(asylumCase).writeIfEmpty(AsylumCaseFieldDefinition.STF_24W_SET_DATE, LocalDate.now().toString());
     }
 
     @Test
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/statutorytimeframe24weeks/UpdateStatutoryTimeframe24WeeksServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/statutorytimeframe24weeks/UpdateStatutoryTimeframe24WeeksServiceTest.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
@@ -133,6 +134,8 @@ class UpdateStatutoryTimeframe24WeeksServiceTest {
         verify(statutoryTimeframe24WeeksHistoryAppender, times(1)).append(
             newStatutoryTimeframe24WeeksHistoryCaptor.capture(),
             existingStatutoryTimeframe24WeeksHistoryCaptor.capture());
+        verify(asylumCase).writeIfEmpty(STF_24W_SET_DATE, dateProvider.now());
+        verify(asylumCase, never()).writeIfEmpty(STF_24W_REMOVE_DATE, dateProvider.now());
 
         StatutoryTimeframe24WeeksHistory capturedStatutoryTimeframe24Weeks = newStatutoryTimeframe24WeeksHistoryCaptor.getValue();
 
@@ -274,6 +277,9 @@ class UpdateStatutoryTimeframe24WeeksServiceTest {
             newStatutoryTimeframe24WeeksHistoryCaptor.capture(),
             existingStatutoryTimeframe24WeeksHistoryCaptor.capture());
 
+        verify(asylumCase, never()).writeIfEmpty(STF_24W_SET_DATE, dateProvider.now());
+        verify(asylumCase).writeIfEmpty(STF_24W_REMOVE_DATE, dateProvider.now());
+
         StatutoryTimeframe24WeeksHistory capturedStatutoryTimeframe24Weeks = newStatutoryTimeframe24WeeksHistoryCaptor.getValue();
 
         assertThat(capturedStatutoryTimeframe24Weeks.getStatus()).isEqualTo(newStatus);
@@ -354,4 +360,22 @@ class UpdateStatutoryTimeframe24WeeksServiceTest {
         verify(asylumCase, times(1)).write(eq(CASE_NOTES), anyList());
     }
 
+    @Test
+    void should_not_set_remove_date_if_status_changed_to_no_system_user() {
+        YesOrNo currentStatus = YesOrNo.YES;
+        when(asylumCase.read(STF_24W_CURRENT_STATUS_AUTO_GENERATED, YesOrNo.class))
+            .thenReturn(Optional.of(currentStatus));
+        when(userDetails.getRoles()).thenReturn(singletonList(UserRole.SYSTEM.getId()));
+
+        when(asylumCase.read(REMOVAL_OF_24W_DECISION_REASON, String.class)).thenReturn(Optional.of(newStatutoryTimeframe24WeeksReason));
+
+        updateStatutoryTimeframe24WeeksService.updateAsylumCase(asylumCase, YesOrNo.NO);
+
+        verify(statutoryTimeframe24WeeksHistoryAppender, times(1)).append(
+            newStatutoryTimeframe24WeeksHistoryCaptor.capture(),
+            existingStatutoryTimeframe24WeeksHistoryCaptor.capture());
+
+        verify(asylumCase, never()).writeIfEmpty(STF_24W_SET_DATE, dateProvider.now());
+        verify(asylumCase, never()).writeIfEmpty(STF_24W_REMOVE_DATE, dateProvider.now());
+    }
 }
