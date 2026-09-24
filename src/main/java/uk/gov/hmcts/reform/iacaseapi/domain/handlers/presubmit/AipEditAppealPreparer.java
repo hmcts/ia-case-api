97e1efb5975425f518ac5af1968b82a4ff8b8d99
nilay <nilay.patel@solirius.com>
2026-09-24 10:55:44 +0100

    removing unneeded handler

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AipEditAppealPreparer.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AipEditAppealPreparer.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HOME_OFFICE_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HOME_OFFICE_REFERENCE_NUMBER_BEFORE_EDIT;
+
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
@@ -7,12 +11,6 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.handlers.PreSubmitCallbackHandler;
-
-import java.util.Set;
-
-import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HOME_OFFICE_REFERENCE_NUMBER;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HOME_OFFICE_REFERENCE_NUMBER_BEFORE_EDIT;
 
 
 @Component
@@ -24,34 +22,33 @@ public class AipEditAppealPreparer implements PreSubmitCallbackHandler<AsylumCas
     }
 
     public boolean canHandle(
-        PreSubmitCallbackStage callbackStage,
-        Callback<AsylumCase> callback
+            PreSubmitCallbackStage callbackStage,
+            Callback<AsylumCase> callback
     ) {
         requireNonNull(callbackStage, "callbackStage must not be null");
         requireNonNull(callback, "callback must not be null");
 
         return callbackStage == PreSubmitCallbackStage.ABOUT_TO_START
-            && Set.of(Event.EDIT_APPEAL_AFTER_SUBMIT, Event.EDIT_APPELLANT_PERSONAL_DATA)
-            .contains(callback.getEvent());
+                && (callback.getEvent() == Event.EDIT_APPEAL_AFTER_SUBMIT);
     }
 
     public PreSubmitCallbackResponse<AsylumCase> handle(
-        PreSubmitCallbackStage callbackStage,
-        Callback<AsylumCase> callback
+            PreSubmitCallbackStage callbackStage,
+            Callback<AsylumCase> callback
     ) {
         if (!canHandle(callbackStage, callback)) {
             throw new IllegalStateException("Cannot handle callback");
         }
 
         AsylumCase asylumCase =
-            callback
-                .getCaseDetails()
-                .getCaseData();
+                callback
+                        .getCaseDetails()
+                        .getCaseData();
 
         PreSubmitCallbackResponse<AsylumCase> response = new PreSubmitCallbackResponse<>(asylumCase);
 
         String homeOfficeReferenceNumber =
-            asylumCase.read(HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse("");
+                asylumCase.read(HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse("");
         asylumCase.write(HOME_OFFICE_REFERENCE_NUMBER_BEFORE_EDIT, homeOfficeReferenceNumber);
 
         return response;
--- src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AipEditAppealPreparerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/AipEditAppealPreparerTest.java
@@ -1,10 +1,20 @@
 package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HOME_OFFICE_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.HOME_OFFICE_REFERENCE_NUMBER_BEFORE_EDIT;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.JOURNEY_TYPE;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.EDIT_APPEAL_AFTER_SUBMIT;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_START;
+
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,17 +28,6 @@ import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallb
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.JourneyType;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.EDIT_APPEAL_AFTER_SUBMIT;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event.SUBMIT_APPEAL;
-import static uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_START;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -40,6 +39,8 @@ class AipEditAppealPreparerTest {
     private CaseDetails<AsylumCase> caseDetails;
     @Mock
     private AsylumCase asylumCase;
+
+    private String homeOfficeReferenceNumber = "someHomeOfficeReference";
 
     private AipEditAppealPreparer aipEditAppealPreparer;
 
@@ -71,41 +72,38 @@ class AipEditAppealPreparerTest {
         when(callback.getEvent()).thenReturn(EDIT_APPEAL_AFTER_SUBMIT);
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
-        String homeOfficeReferenceNumber = "someHomeOfficeReference";
         when(asylumCase.read(HOME_OFFICE_REFERENCE_NUMBER, String.class))
-            .thenReturn(Optional.of(homeOfficeReferenceNumber));
+                .thenReturn(Optional.of(homeOfficeReferenceNumber));
 
         PreSubmitCallbackResponse<AsylumCase> response =
-            aipEditAppealPreparer.handle(ABOUT_TO_START, callback);
+                aipEditAppealPreparer.handle(ABOUT_TO_START, callback);
 
         assertThat(response).isNotNull();
         assertThat(response.getData()).isNotEmpty();
         assertThat(response.getData()).isEqualTo(asylumCase);
 
         Mockito.verify(asylumCase, Mockito.times(1))
-            .write(HOME_OFFICE_REFERENCE_NUMBER_BEFORE_EDIT, homeOfficeReferenceNumber);
+                .write(HOME_OFFICE_REFERENCE_NUMBER_BEFORE_EDIT, homeOfficeReferenceNumber);
 
     }
 
-    @ParameterizedTest
-    @EnumSource(value = Event.class, names = {"EDIT_APPEAL_AFTER_SUBMIT", "EDIT_APPELLANT_PERSONAL_DATA"})
-    void it_can_handle_callback(Event event) {
-        when(callback.getEvent()).thenReturn(event);
-        assertTrue(aipEditAppealPreparer.canHandle(ABOUT_TO_START, callback));
-    }
+    @Test
+    void it_can_handle_callback() {
 
-    @ParameterizedTest
-    @EnumSource(value = Event.class, names = {"EDIT_APPEAL_AFTER_SUBMIT", "EDIT_APPELLANT_PERSONAL_DATA"}, mode = EnumSource.Mode.EXCLUDE)
-    void it_cannot_handle_callback_bad_event(Event event) {
-        when(callback.getEvent()).thenReturn(event);
-        assertFalse(aipEditAppealPreparer.canHandle(ABOUT_TO_START, callback));
-    }
+        for (Event event : Event.values()) {
 
-    @ParameterizedTest
-    @EnumSource(value = PreSubmitCallbackStage.class, names = {"ABOUT_TO_START"}, mode = EnumSource.Mode.EXCLUDE)
-    void it_cannot_handle_callback_bad_stage(PreSubmitCallbackStage stage) {
-        when(callback.getEvent()).thenReturn(EDIT_APPEAL_AFTER_SUBMIT);
-        assertFalse(aipEditAppealPreparer.canHandle(stage, callback));
+            when(callback.getEvent()).thenReturn(event);
+
+            for (PreSubmitCallbackStage callbackStage : PreSubmitCallbackStage.values()) {
+                boolean canHandle = aipEditAppealPreparer.canHandle(callbackStage, callback);
+                if (callbackStage == ABOUT_TO_START
+                    && callback.getEvent() == EDIT_APPEAL_AFTER_SUBMIT) {
+                    assertTrue(canHandle);
+                } else {
+                    assertFalse(canHandle);
+                }
+            }
+        }
     }
 
     @Test
@@ -131,10 +129,15 @@ class AipEditAppealPreparerTest {
 
     @Test
     void handler_throws_error_if_cannot_actually_handle() {
-        when(callback.getEvent()).thenReturn(SUBMIT_APPEAL);
+
         assertThatThrownBy(() -> aipEditAppealPreparer.handle(ABOUT_TO_START, callback))
             .hasMessage("Cannot handle callback")
             .isExactlyInstanceOf(IllegalStateException.class);
+
+        when(callback.getEvent()).thenReturn(EDIT_APPEAL_AFTER_SUBMIT);
+        assertThatThrownBy(() -> aipEditAppealPreparer.handle(ABOUT_TO_START, callback))
+            .isExactlyInstanceOf(NullPointerException.class);
+
     }
 
 }
