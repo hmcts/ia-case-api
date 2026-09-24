03e37b31ed0bdd66e8c931dc23aceda5859089be
Velican <31857663+velimd@users.noreply.github.com>
2026-09-24 11:27:12 +0100

    Update SendNotificationHandler.java

--- src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/SendNotificationHandler.java
@@ -224,6 +224,7 @@ public class SendNotificationHandler implements PreSubmitCallbackHandler<AsylumC
             Event.RECORD_REMISSION_DECISION,
             Event.MARK_APPEAL_PAID,
             Event.LIST_CASE,
+            Event.REVIEW_HEARING_REQUIREMENTS,
             Event.REQUEST_HEARING_REQUIREMENTS_FEATURE,
             Event.REQUEST_RESPONSE_REVIEW,
             Event.MARK_APPEAL_AS_ADA,
